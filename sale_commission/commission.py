6bb20a1f2ee2559da2b36b133733488cbed4d6a1
pmo <>
2007-04-17 10:36:08 +0000

    change at 17-4-07 by 18:05 pm

--- sale_commission/commission.py
+++ sale_commission/commission.py
@@ -21,22 +21,23 @@ class report_commission_month(osv.osv):
     def init(self, cr):
         print "In init of commision month ..";
         cr.execute(""" create or replace view report_commission_month as (select min(A.id) as id,A.name as name,S.name as sono,I.number as invno
-              ,sum(L.quantity * L.price_unit) as inv_total,substring(I.date_invoice for 10) as in_date,
-              (1-R.price_discount) as comrate,((sum(L.quantity * L.price_unit)*(1-R.price_discount)))
-              as commission,I.state,AM.date_created as pdate from
-account_invoice_line L,
-account_move_line AM,
+              ,(L.quantity * L.price_unit) as inv_total,substring(I.date_invoice for 10) as in_date,
+              ((1-R.price_discount)*100) as comrate,((sum(L.quantity * L.price_unit)*(1-R.price_discount)))
+              as commission,I.state from
+
+
 sale_agent A,
 res_partner P,
 account_invoice I,
 product_pricelist_item R,
+account_invoice_line L,
 sale_order_line SL,
 product_pricelist_version PV,
 sale_order S,
-product_pricelist PP
-where  R.price_version_id=PV.id AND A.comprice_id = PV.pricelist_id AND I.origin=S.name
-AND SL.order_id=s.id AND R.price_discount > 0 AND
-S.partner_id = P.id  AND A.id = P.agent_id AND I.partner_id=P.id AND I.id=L.invoice_id  group by R.price_discount,I.state,I.id,A.name,P.name,I.date_invoice,P.id,P.agent_id,
-     S.name,I.number,A.id,AM.date_created  ) """)
+product_pricelist PP where
+R.price_version_id=PV.id AND A.comprice_id = PV.pricelist_id AND I.origin=S.name AND SL.order_id=s.id AND R.price_discount > 0 AND S.partner_id = P.id AND P.agent_id=A.id AND I.partner_id=P.id AND I.id=L.invoice_id
+group by L.quantity , L.price_unit,R.price_discount,I.state,I.id,A.name,P.name,I.state,I.date_invoice,
+S.name,I.number,A.id
+) """)
 report_commission_month()
 
