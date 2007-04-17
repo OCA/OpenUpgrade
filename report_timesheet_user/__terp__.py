019d2356ac892f380fa55d87ffe0c2dd50e7f6a7
Fabien Pinckaers <fp@tinyerp.com>
2007-04-17 15:14:26 +0000

    New report for timsheets in multi-company

---
+++ report_timesheet_user/__terp__.py
@@ -0,0 +1,22 @@
+#####################################################################
+#Created By 	: Eiffel Consultancy Pvt. Ltd.
+#Created Date 	: 14/02/2007
+#####################################################################
+
+
+{
+	'name' : 'Report for timesheet',
+	'version' :'1.0',
+	'author' : 'Tiny',
+	'depends' : ['hr_timesheet','multi_company'],
+	'description': 'New report for timesheet',
+	'init_xml' : [],
+	'update_xml': [
+		'report_timesheet_user_view.xml',
+	],
+	'active': True,
+	'installable': True
+}
+
+
+
