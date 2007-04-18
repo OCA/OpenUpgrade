1066d467b65ba3704fa197f4cc1802b02dd827db
pmo <>
2007-04-18 05:19:33 +0000

    change at 18-4-07 by 12:068 pm

--- sale_commission/commission.py
+++ sale_commission/commission.py
@@ -8,7 +8,9 @@ class report_commission_month(osv.osv):
         'name': fields.char('Salesagent Name',size=64, readonly=True),
         'sono':fields.integer('Sales Order No', readonly=True),
         'invno':fields.integer('Invice Number', readonly=True),
-        'inv_total':fields.integer('Invioce Total', readonly=True),
+        'product_quantity':fields.integer('product quantity', readonly=True),
+        'productname':fields.char('product Name',size=256, readonly=True),
+        'inv_total':fields.float('Invioce Total', readonly=True),
         'in_date': fields.date('Date of Invoice', readonly=True),
         'comrate': fields.float('CommmissionRate(%)', readonly=True),
         'commission': fields.float('Commission of sales agent', readonly=True),
@@ -21,10 +23,9 @@ class report_commission_month(osv.osv):
     def init(self, cr):
         print "In init of commision month ..";
         cr.execute(""" create or replace view report_commission_month as (select min(A.id) as id,A.name as name,S.name as sono,I.number as invno
-              ,(L.quantity * L.price_unit) as inv_total,substring(I.date_invoice for 10) as in_date,
-              ((1-R.price_discount)*100) as comrate,((sum(L.quantity * L.price_unit)*(1-R.price_discount)))
+              ,L.quantity as product_quantity,L.name as productname,(L.quantity * L.price_unit) as inv_total,substring(I.date_invoice for 10) as in_date,
+              ((1-R.price_discount)*100) as comrate,((L.quantity * L.price_unit)*(1-R.price_discount))
               as commission,I.state from
-
 
 sale_agent A,
 res_partner P,
@@ -36,8 +37,7 @@ product_pricelist_version PV,
 sale_order S,
 product_pricelist PP where
 R.price_version_id=PV.id AND A.comprice_id = PV.pricelist_id AND I.origin=S.name AND SL.order_id=s.id AND R.price_discount > 0 AND S.partner_id = P.id AND P.agent_id=A.id AND I.partner_id=P.id AND I.id=L.invoice_id
-group by L.quantity , L.price_unit,R.price_discount,I.state,I.id,A.name,P.name,I.state,I.date_invoice,
-S.name,I.number,A.id
-) """)
+group by L.quantity, L.price_unit,R.price_discount,I.state,I.id,A.name,P.name,I.state,I.date_invoice,L.name,
+S.name,I.number,A.id) """)
 report_commission_month()
 
