1b697a909c8af99df7deb0ac9276766e38b150d2
pmo <>
2007-04-20 12:21:54 +0000

    update at 20-4-07 at 7.:51

--- sale_commission/commission.py
+++ sale_commission/commission.py
@@ -22,7 +22,30 @@ class report_commission_month(osv.osv):
 
     def init(self, cr):
         print "In init of commision month ..";
-        cr.execute(""" create or replace view report_commission_month as (select min(A.id) as id,A.name as name,S.name as sono,I.number as invno
+        cr.execute(""" create or replace view report_commission_month as (select * from (select sg.id as id,sg.name as name,so.name as sono,ai.number as invno,
+    al.quantity as product_quantity,al.name as productname,(al.quantity * al.price_unit) as inv_total,substring(ai.date_invoice for 10) as in_date,
+    ((1-pi.price_discount)*100) as comrate,((al.quantity *al.price_unit)*(1-pi.price_discount))
+              as commission,ai.state,'' as pdate
+from
+account_invoice ai,
+sale_order so,
+account_invoice_line al,
+res_partner p,
+sale_agent sg,
+product_pricelist_item pi,
+product_pricelist_version pv
+
+where ai.origin=so.name
+and ai.state in ('draft','open','proforma','cancel')
+and al.invoice_id=ai.id and p.id=ai.partner_id
+and sg.id=p.agent_id and pi.price_version_id=pv.id
+
+and sg.comprice_id = pv.pricelist_id and pi.price_discount > 0) as a
+
+UNION
+
+(
+select min(A.id) as id,A.name as name,S.name as sono,I.number as invno
               ,L.quantity as product_quantity,L.name as productname,(L.quantity * L.price_unit) as inv_total,substring(I.date_invoice for 10) as in_date,
               ((1-R.price_discount)*100) as comrate,((L.quantity * L.price_unit)*(1-R.price_discount))
               as commission,I.state,AMR.name as pdate from
@@ -33,12 +56,16 @@ res_partner P,
 account_invoice I,
 product_pricelist_item R,
 account_invoice_line L,
-sale_order_line SL,
-product_pricelist_version PV,
 sale_order S,
-product_pricelist PP where
-R.price_version_id=PV.id AND A.comprice_id = PV.pricelist_id AND I.origin=S.name AND SL.order_id=s.id AND R.price_discount > 0 AND S.partner_id = P.id AND P.agent_id=A.id AND I.partner_id=P.id AND I.id=L.invoice_id
+product_pricelist_version PV
+
+ where
+I.state='paid' and
+R.price_version_id=PV.id AND A.comprice_id = PV.pricelist_id AND I.origin=S.name AND
+R.price_discount > 0 AND S.partner_id = P.id AND P.agent_id=A.id AND I.partner_id=P.id AND I.id=L.invoice_id
 group by L.quantity, L.price_unit,R.price_discount,I.state,I.id,A.name,P.name,I.state,I.date_invoice,L.name,
-S.name,I.number,A.id,AMR.name) """)
+S.name,I.number,A.id,AMR.name
+)
+) """)
 report_commission_month()
 
