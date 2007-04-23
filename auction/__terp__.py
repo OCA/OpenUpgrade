953c1a67858307aa08fe7a4711bd52232106cb15
Fabien Pinckaers <fp@tinyerp.com>
2007-04-23 11:37:15 +0000

    Auction house module

---
+++ auction/__terp__.py
@@ -0,0 +1,7 @@
+{
+	"name" : "Tiny TERP Accounting",
+	"version" : "1.0",
+	"depends" : ["base","account"],
+	"update_xml" : [ "auction_view.xml", "auction_report.xml", "auction_wizard.xml"],
+	"init_xml" : [ "auction_data.xml", "auction_demo.xml"],
+}
