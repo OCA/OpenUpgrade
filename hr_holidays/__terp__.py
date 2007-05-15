7dad1a0c88f32fdce1fcb28dc17237198488b56f
Fabien Pinckaers <fp@tinyerp.com>
2007-05-15 18:20:38 +0000

    Modifs

---
+++ hr_holidays/__terp__.py
@@ -0,0 +1,14 @@
+{
+	"name" : "Human Resources: Holidays management",
+	"version" : "0.1",
+	"author" : "Axelor",
+	"category" : "Generic Modules/Human Resources",
+	"website" : "http://www.axelor.com/",
+	"description": "Human Ressources: Holidays tracking and workflow",
+	"depends" : ["hr"],
+	"init_xml" : [],
+	"demo_xml" : [],
+	"update_xml" : ["hr_workflow.xml","hr_view.xml"],
+	"active": False,
+	"installable": True
+}
