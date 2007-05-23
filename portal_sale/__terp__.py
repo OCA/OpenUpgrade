2e1a69587e881c9b96a96314f082737bd18f6391
pmo <>
2007-05-23 13:05:11 +0000

    create portal_sale

---
+++ portal_sale/__terp__.py
@@ -0,0 +1,15 @@
+{
+    "name" : "Portal Sale Management",
+    "version" : "0.1",
+    "author" : "Tiny",
+    "website" : "http://tinyerp.com/",
+    "depends" : ["base","portal","sale"],
+    "category" : "Generic Modules/Others",
+    "description": """
+     """,
+    "init_xml" : [],
+    "update_xml" : ["portal_sale_view.xml"],
+    "demo_xml" : [],
+    "active": False,
+    "installable": True
+}
