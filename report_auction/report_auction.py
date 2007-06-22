1cb6b997daa04c52693eb387e1db0ad59fe01147
pmo <>
2007-06-22 04:21:37 +0000

    with latest 10 objects

--- report_auction/report_auction.py
+++ report_auction/report_auction.py
@@ -2,7 +2,7 @@ from osv import fields,osv
 
 class report_per_seller_customer(osv.osv):
         _name = "report.per.seller.customer"
-        _description = "Next production order"
+        _description = "Customer per seller"
         _auto = False
         _columns = {
                     'name': fields.char('Depositer Inventory', size=64, required=True),
@@ -21,7 +21,7 @@ report_per_seller_customer()
 
 class report_latest_doposit(osv.osv):
         _name = "report.latest.doposit"
-        _description = "Next production order"
+        _description = "Latest 10 Deposits"
         _auto = False
         _columns = {
             'name': fields.char('Depositer Inventory', size=64, required=True),
@@ -47,7 +47,44 @@ class report_latest_doposit(osv.osv):
                        l.info as info
 
                    from auction_deposit l
-                   limit 4
+                   order by l.create_date desc
+                   limit 3
 
                   )""")
 report_latest_doposit()
+
+class report_latest_objects(osv.osv):
+        _name = "report.latest.objects"
+        _description = "Latest 10 Objects"
+        _auto = False
+        _columns = {
+                'partner_id': fields.many2one('res.partner', 'Seller', required=True, change_default=True),
+                'auction_id': fields.many2one('auction.dates', 'Auction Date'),
+                'bord_vnd_id': fields.many2one('auction.deposit', 'Depositer Inventory', required=True),
+                'obj_desc': fields.text('Object Description'),
+                'obj_num': fields.integer('Catalog Number'),
+                'obj_ret': fields.float('Price retired'),
+                'obj_comm': fields.boolean('Commission'),
+                'obj_price': fields.float('Adjudication price'),
+
+        }
+        def init(self, cr):
+            cr.execute("""
+                create or replace view report_latest_objects as (
+                    SELECT
+                       l.id as id,
+                       m.partner_id as partner_id,
+                       l.auction_id as auction_id,
+                       l.obj_desc as obj_desc,
+                       l.obj_num as obj_num,
+                       l.obj_ret as obj_ret,
+                       l.obj_comm as obj_comm,
+                       l.obj_price as obj_price
+
+                   from auction_lots l,auction_deposit m
+                   where l.bord_vnd_id = m.id
+                   order by l.create_date desc
+                   limit 3
+
+                  )""")
+report_latest_objects()
