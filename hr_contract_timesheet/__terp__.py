c716e2a27ce5cfe62f962ce24243168e9f3db486
Fabien Pinckaers <fp@tinyerp.com>
2007-07-05 07:14:40 +0000

    Module to compute timesheet costs base on employee's contracts.

---
+++ hr_contract_timesheet/__terp__.py
@@ -0,0 +1,19 @@
+{
+	"name" : "Human Resources Timesheets on contracts",
+	"version" : "0.1",
+	"author" : "Tiny",
+	"category" : "Generic Modules/Human Resources",
+	"website" : "http://tinyerp.com/module_hr.html",
+	"depends" : ["hr_contract","hr_timesheet"],
+	"module": "",
+	"description": """
+		Compute the cost of an employee for his timesheets according
+		to his contract definitions. If no contract are defined, it
+		uses the product costs linked to the employee.
+	""",
+	"init_xml" : [],
+	"demo_xml" : [],
+	"update_xml" : [],
+	"active": False,
+	"installable": True
+}
