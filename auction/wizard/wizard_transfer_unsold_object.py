1c9daf606a7e8f56dbf53570d7a36429ec7973dd
kap <>
2007-07-10 10:04:16 +0000

    with transfer unsold objects wizard

---
+++ auction/wizard/wizard_transfer_unsold_object.py
@@ -0,0 +1,75 @@
+#############################################################################
+#
+# Copyright (c) 2004 TINY SPRL. (http://tiny.be) All Rights Reserved.
+#                    Fabien Pinckaers <fp@tiny.Be>
+#
+# WARNING: This program as such is intended to be used by professional
+# programmers who take the whole responsability of assessing all potential
+# consequences resulting from its eventual inadequacies and bugs
+# End users who are looking for a ready-to-use solution with commercial
+# garantees and support are strongly adviced to contract a Free Software
+# Service Company
+#
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+##############################################################################
+
+import time
+import wizard
+import netsvc
+import pooler
+from osv.orm import browse_record
+import sql_db
+
+transfer_unsold_object_form = """<?xml version="1.0"?>
+<form string="Draft To Posted">
+    <group col="1" colspan="2">
+    <separator colspan="4" string="Transfer unsold Object: Current auction date to another " />
+    </group>
+    <newline/>
+    <field name="auction_id_from"/>
+    <newline/>
+    <field name="auction_id_to"/>
+</form>
+"""
+
+transfer_unsold_object_fields = {
+         'auction_id_from': {'string':'From Auction Date', 'type':'many2one', 'required':True, 'relation':'auction.dates'},
+         'auction_id_to': {'string':'To Auction Date', 'type':'many2one', 'required':True, 'relation':'auction.dates'},
+}
+def _transfer_unsold_object(self, cr, uid, data, context):
+        obj_pool = pooler.get_pool(cr.dbname).get('auction.lots')
+        ids= pooler.get_pool(cr.dbname).get('auction.lots').search(cr,uid,[('auction_id','=',data['form']['auction_id_from']),('state','=','unsold')])
+
+        for o in obj_pool.browse(cr, uid, ids, context):
+                o.write(cr, uid, ids, {'auction_id':data['form']['auction_id_to']})
+        return {}
+
+class transfer_object(wizard.interface):
+    states = {
+        'init' : {
+            'actions' : [],
+            'result' : {'type' : 'form',
+                    'arch' : transfer_unsold_object_form,
+                    'fields' :transfer_unsold_object_fields,
+                    'state' : [('end', 'Cancel'),('transfer', 'Transfer') ]}
+        },
+        'transfer' : {
+            'actions' : [_transfer_unsold_object],
+            'result' : {'type' : 'state',
+                        'state' : 'end'}
+        },
+    }
+transfer_object('auction.lots.transfer.unsold.object')
