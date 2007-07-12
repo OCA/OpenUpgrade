08976d141cf50c6ea9f1d411025719a8ca08a38a
pmo <>
2007-07-12 12:08:49 +0000

    add a new report

--- auction/report/auction_objects.py
+++ auction/report/auction_objects.py
@@ -35,15 +35,24 @@ class auction_objects(report_sxw.rml_parse):
 		super(auction_objects, self).__init__(cr, uid, name, context)
 		self.localcontext.update({
 			'time': time,
-			'get_data' : self.get_data
+			#'lines': self.lines
+			#'get_data' : self.get_data
 		})
-	def get_data(self, auction_id):
-		res = self.pool.get('auction.bid.lines').read(self.cr,self.uid,[lot_id])
-		print res;
-		print "=================================================="
-		return True
+
+#	def lines(self, auction_id):
+#		 print " in the lines fuction"
+#		 print "value of  auction_id",auction_id.id,auction_id.name........................
+#
+#		 cr.execute('select ad.name from auction_dates ad, a1uction_lots al where ad.id=al.%d group by ad.name',(auction_id))
+#		 print "value of query",cr.fetchone()
+#		 return self.cr.fetchone()[0]
+#	def get_data(self, auction_id):
+#		res = self.pool.get('auction.bid.lines').read(self.cr,self.uid,[lot_id])
+#		print res;
+#		print "=================================================="
+#		return True
 
 
 
-report_sxw.report_sxw('report.auction_objects', 'auction.lots', 'addons/auction/report/auction_objects.rml', parser=auction_objects)
+report_sxw.report_sxw('report.auction.objects', 'auction.lots', 'addons/auction/report/auction_objects.rml', parser=auction_objects)
 
