d810d76cabab62b614b4e86c4b71396bee97c0ea
Fabien Pinckaers <fp@tinyerp.com>
2007-07-16 17:17:43 +0000

    BI Profile module

---
+++ profile_bi/__terp__.py
@@ -0,0 +1,11 @@
+{
+    "name":"Business Intelligence profile",
+    "version":"1.0",
+    "author":"Tiny",
+    "category":"Profile",
+    "depends":["olap"],
+    "demo_xml":[],
+    "update_xml":[],
+    "active":False,
+    "installable":True,
+}
