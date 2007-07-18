afb80569e146abc0f4dd400cf32c4dee7a9bb18a
Harshad Modi <hmo@tinyerp.com>
2007-07-18 03:39:59 +0000

    new

---
+++ payment/__terp__.py
@@ -0,0 +1,16 @@
+{
+    "name" : "Payment Management",
+    "version" : "1.0",
+    "author" : "Tiny",
+    "category" : "Generic Modules/Payment",
+    "depends" : ["sale"],
+    "init_xml" : [],
+    "demo_xml" : [],
+    "description": """
+     This is the module which Provide an more efficient way to manage invoice payment,
+     Provide a basic mechanism to easily plug various automated payment.
+     """,
+    "update_xml" : ["payment_view.xml","payment_wizard.xml"],
+    "active": False,
+    "installable": True
+}
