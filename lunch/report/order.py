46ec950f2e942d216649952ed02b43066bc64078
ced <>
2007-07-30 08:28:52 +0000

    Revert "Prix/total/print order/desc order"
    This reverts commit 9020dc8ed2947463d6fc6923cba904151fa80029.

--- lunch/report/order.py
+++ lunch/report/order.py
@@ -1,4 +1,3 @@
-
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
@@ -32,50 +31,43 @@ from report import report_sxw
 from osv import osv
 
 class order(report_sxw.rml_parse):
- def order_list(self, cr,uid,ids):
+	def order_list(self, cr,uid,ids):
+		moveline_obj = pooler.get_pool(self.cr.dbname).get('lunch.order')		 
+		cr.execute("select lo.user_id,lo.product,lo.descript,lp.price from lunch_order lo join lunch_product lp on lo.product=lp.name join lunch_category lc on lp.category_id=lc.name order by lc.category desc")
+		res=cr.fetchall()
 
-##        obj_cat=self.pool.get('lunch.category')
-##        res=obj_cat.search(cr,uid,[])
-##        res=obj_cat.browse(cr,uid,obj_cat,['cat_sequence'])
-##
-        moveline_obj = pooler.get_pool(self.cr.dbname).get('lunch.order')         
-#        obj=self.browse(cr,uid,ids)
-        cr.execute("select lo.user_id,lo.product,lo.descript,lp.price from lunch_order lo join lunch_product lp on lo.product=lp.name join lunch_category lc on lp.category_id=lc.name order by lc.category desc")
-        res=cr.fetchall()
+		for r in res:
+			if not res:
+				self.ret_list.append(0.0)
+			else:
+				ret_dict={
+						'user_id':res[0]['user_id'],
+						'product':res[0]['product'],
+						'descript':res[0]['descript'],
+						'price':res[0]['price']
+						}
 
-        for r in res:
-            if not res:
-                self.ret_list.append(0.0)
-            else:
-                ret_dict={
-                'user_id':res[0]['user_id'],
-                'product':res[0]['product'],
-                'descript':res[0]['descript'],
-                'price':res[0]['price']
-                        }
-
-            self.ret_list.append(ret_dict)
-	return self.ret_list
-		
+			self.ret_list.append(ret_dict)
+		return self.ret_list
 
 	def __init__(self, cr, uid, name, context):
 		super(order, self).__init__(cr, uid, name, context)
 		user=self.pool.get('res.users').browse(cr,uid,uid)
-	#	partner = user.company_id.partner_id
-	#	address=partner.address and partner.address[0] or ""
-	#	street = address and address.street or ""
-	#	city=address and address.city
-	#	tel=address and address.mobile
+		partner = user.company_id.partner_id
+		address=partner.address and partner.address[0] or ""
+		street = address and address.street or ""
+		city=address and address.city
+		tel=address and address.mobile
 
 		self.localcontext.update({
 		'time': time,
-     #   'street': street,
-      #  'tel':tel,
-       # 'city':city,
-        #'oder_list': self.order_list,
-	    })
+				'street': street,
+				'tel':tel,
+				'city':city,
+				'oder_list': self.order_list,
+				
+		})
 	def _add_header(self, node):
 		return True
 
-	
 report_sxw.report_sxw('report.lunch.order','lunch.order','addons/lunch/report/order.rml',parser=order)
