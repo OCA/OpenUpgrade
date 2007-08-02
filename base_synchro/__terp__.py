fd59b0c91575e9930dfea9406b06437e7711d1c8
Fabien Pinckaers <fp@tinyerp.com>
2007-08-02 10:14:12 +0000

    Better category in base synchro module

--- base_synchro/__terp__.py
+++ base_synchro/__terp__.py
@@ -2,7 +2,7 @@
 	"name":"Base Synchro",
 	"version":"0.1",
 	"author":"Tiny",
-	"category":"Base Synchro",
+	"category":"Generic Modules/Base",
 	"depends":["base"],
 	"demo_xml":[],
 	"update_xml":["base_synchro_wizard.xml","base_synchro_view.xml"],
