35085f17d9d6a4ed24c640013c88f74545e33938
pmo <>
2007-08-08 07:43:55 +0000

    update the auction_total report

--- auction/report/auction_total_rml.py
+++ auction/report/auction_total_rml.py
@@ -43,12 +43,21 @@ class auction_total_rml(report_sxw.rml_parse):
             'sum_buyer':self.sum_buyer,
             'sum_seller':self.sum_seller,
             'sum_buyer_paid':self.sum_buyer_paid,
+            'sum_adj':self.sum_adj,
+            'count_comm':self.count_comm,
+            'sum_minadj':self.sum_minadj,
+            'sum_maxadj':self.sum_maxadj,
+            'sum_credit':self.sum_credit,
+            'sum_debit': self.sum_debit,
+            'chek_paid': self.chek_paid
         })
 
     def sum_taxes(self, auction_id):
         return len(self.pool.get('auction.lots').search(self.cr,self.uid,([('auction_id','=',auction_id)])))
+
     def sold_item(self, auction_id):
         return len(self.pool.get('auction.lots').search(self.cr,self.uid,([('state','in',['unsold','draft'])])))
+
     def sum_buyer(self, auction_id):
         print "################buyer sum function",auction_id
         self.cr.execute('select count(*) from auction_lots where auction_id=%d AND ach_uid is not null'%(auction_id))
@@ -62,9 +71,71 @@ class auction_total_rml(report_sxw.rml_parse):
         print "print thte value",str(res[0]);
         return str(res[0])
 
+    def sum_adj(self, auction_id):
+        print "################sum adjustication function",auction_id
+        self.cr.execute('select sum(obj_price) from auction_lots where auction_id=%d '%(auction_id))
+        res = self.cr.fetchone()
+        print "print thte value",str(res[0]);
+        return str(res[0])
+
+#    def chek_paid(self, auction_id):
+#        print "################chek paid function",auction_id
+#        self.cr.execute("select count(*) from auction_lots where auction_id=%d and paid_ach= 'True'"%(auction_id))
+#        res = self.cr.fetchone()
+#        print "print thte value",str(res[0]);
+#        return str(res[0])
+#
+#
+#
+#    def sum_credit(self, auction_id):
+#        print "################sum credit function",auction_id
+#        self.cr.execute("select sum(buyer_price) from auction_lots where auction_id=%d and paid_ach= 'True'"%(auction_id))
+#        res = self.cr.fetchone()
+#        print "print thte value",str(res[0]);
+#        return str(res[0])
+#
+#    def sum_debit(self, auction_id):
+#        print "################sum debit function",auction_id
+#        self.cr.execute("select sum(seller_price) from auction_lots where auction_id=%d and paid_ach= 'True'"%(auction_id))
+#        res = self.cr.fetchone()
+#        print "print thte value",str(res[0]);
+#        return str(res[0])
+
+
+
+
+
+    def sum_minadj(self, auction_id):
+        print "################sum minadjustication function",auction_id
+        self.cr.execute('select sum(lot_est1) from auction_lots where auction_id=%d '%(auction_id))
+        res = self.cr.fetchone()
+        print "print thte value",str(res[0]);
+        return str(res[0])
+
+    def sum_maxadj(self, auction_id):
+        print "################sum  maxadjustication function",auction_id
+        self.cr.execute('select sum(lot_est2) from auction_lots where auction_id=%d '%(auction_id))
+        res = self.cr.fetchone()
+        print "print thte value",str(res[0]);
+        return str(res[0])
+
+
+
+
+
+
+
     def sum_buyer_paid(self, auction_id):
         print "###############in byerpaid function",auction_id
         self.cr.execute("select count(*) from auction_lots where auction_id=%d AND state = 'paid'"%(auction_id))
+        res = self.cr.fetchone()
+        print "^^^^^^",res;
+        print "%%%%%%%%rts",str(res[0]);
+        return str(res[0])
+
+    def count_comm(self, auction_id):
+        print "###############in count commission",auction_id
+        self.cr.execute("select count(*) from auction_lots where auction_id=%d AND obj_comm is not null"%(auction_id))
         res = self.cr.fetchone()
         print "^^^^^^",res;
         print "%%%%%%%%rts",str(res[0]);
