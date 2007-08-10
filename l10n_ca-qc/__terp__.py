51bbac8f1434983ef76d96eb06848e0d3a815621
ced <>
2007-08-10 06:48:31 +0000

    Account chart from terp repo

---
+++ l10n_ca-qc/__terp__.py
@@ -0,0 +1,12 @@
+{
+	"name" : "Canada - Quebec",
+	"version" : "1.0",
+	"author" : "Tiny",
+	"category" : "Localisation/Account charts",
+	"depends" : ["base", "account", "account_chart"],
+	"init_xml" : [],
+	"demo_xml" : ["account_demo.xml"],
+	"update_xml" : ["account_pc_canada.xml"],
+	"active": False,
+	"installable": True
+}
