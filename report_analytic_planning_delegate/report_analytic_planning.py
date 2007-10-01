bd47a6437551aa3d07b1a13ac2357fe8b051e5a2
Fabien Pinckaers <fp@tinyerp.com>
2007-10-01 20:44:45 +0000

    Improvement of delegation and reporting

--- report_analytic_planning_delegate/report_analytic_planning.py
+++ report_analytic_planning_delegate/report_analytic_planning.py
@@ -29,7 +29,22 @@
 
 from osv import fields,osv
 
-def _get_delegated_ids(self, cr, uid, dt_from, dt_to):
+def _get_delegated_ids(self, cr, uid, context):
+	val = self.pool.get('report_account_analytic.planning').default_get(cr, uid, ['date_from','date_to'])
+	cr.execute('''select
+			l.id 
+		from
+			report_account_analytic_planning_line l
+		left join
+			report_account_analytic_planning p on (l.planning_id=p.id)
+		where
+			l.delegate_id=%d and 
+			l.user_id is NULL and
+			p.date_from<=%s and
+			p.date_to>=%s''', (uid, val['date_from'], val['date_to'])
+	)
+	res = map(lambda x: x[0], cr.fetchall())
+	return self.pool.get('report_account_analytic.planning.line').read(cr, uid, res, context=context)
 
 class delegated_field(fields.one2many):
 	def set(self, cr, obj, id, field, values, user=None, context=None):
@@ -54,10 +69,15 @@ class delegated_field(fields.one2many):
 class report_account_analytic_planning(osv.osv):
 	_inherit = "report_account_analytic.planning"
 	_columns = {
-		'delegated_ids': delegated_field('report_account_analytic.planning.line', 'planning_id', readonly=True)
+		'delegate_ids': delegated_field('report_account_analytic.planning.line', 'planning_id', readonly=True)
 	}
 	_defaults = {
-		'delegate_ids': lambda self,cr, uid, ctx:
+		'delegate_ids': lambda self,cr, uid, ctx: _get_delegated_ids(
+			self,
+			cr,
+			uid,
+			ctx
+		)
 	}
 report_account_analytic_planning()
 
