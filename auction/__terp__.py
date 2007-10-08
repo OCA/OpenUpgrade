df2b30741ab2e3c6f4c3f00c7461766b1623e35b
bch <>
2007-10-08 07:51:57 +0000

    Re-added auction_demo.xml in __terp__.py

--- auction/__terp__.py
+++ auction/__terp__.py
@@ -4,7 +4,7 @@
 	"depends" : ["base","account","l10n_be","hr"],
 	"update_xml" : ["auction_view.xml", "auction_report.xml", "auction_wizard.xml"],
 	"demo_xml" : [
-		#"auction_demo.xml"
+		"auction_demo.xml"
 	],
 	"init_xml" : ["auction_sequence.xml"],
 	"active": False,
