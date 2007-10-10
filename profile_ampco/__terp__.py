3be1c9f5d84aff87fef9dabcd92157192449fc2d
Fabien Pinckaers <fp@tinyerp.com>
2007-10-10 01:32:24 +0000

    Added profile Ampco

---
+++ profile_ampco/__terp__.py
@@ -0,0 +1,11 @@
+{
+    "name":"AMPCO profile",
+    "version":"1.0",
+    "author":"Tiny",
+    "category":"Profile",
+    "depends":["product_size", "profile_manufacturing"],
+    "demo_xml":[],
+    "update_xml":[],
+    "active":False,
+    "installable":True,
+}
