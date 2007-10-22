cad8175809d52a3bd8fffe497a47e6ddf3bb4894
Fabien Pinckaers <fp@tinyerp.com>
2007-10-22 15:43:19 +0000

    Portal: Analytic Package

---
+++ portal_analytic_package/__init__.py
@@ -0,0 +1,1 @@
+import portal_analytic_package
---
+++ portal_analytic_package/__terp__.py
@@ -0,0 +1,14 @@
+{
+	"name" : "Portal Management - Analytic Entries",
+	"version" : "0.1",
+	"author" : "Tiny",
+	"website" : "http://tinyerp.com/",
+	"depends" : ["portal", "account_analytic_package", "account_analytic_analysis","hr_timesheet_invoice" ],
+	"category" : "Generic Modules/Others",
+	"description": "Potal Management - Service company specific data for analytic accounting.",
+	"init_xml" : [],
+	"update_xml" : ["portal_analytic_view.xml","portal_analytic_data.xml" ],
+	"demo_xml" : [],
+	"active": False,
+	"installable": True
+}
