c229e9c640f54d123d3d0b91580af38b2f1376a7
Fabien Pinckaers <fp@tinyerp.com>
2007-10-28 12:44:32 +0000

    Portal: 	Bugfixes Flagey: 	New module

---
+++ flagey/__terp__.py
@@ -0,0 +1,10 @@
+{
+	"name" : "Auction module - Flagey Customs",
+	"version" : "1.0",
+	"depends" : ["auction"],
+	"update_xml" : ["flagey_report.xml"],
+	"demo_xml" : [ ],
+	"init_xml" : [],
+	"active": False,
+	"installable": True
+}
