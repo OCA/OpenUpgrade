27943750a8ada413aee0ea42c198a254f8b0864b
Mantavya Gajjar <mga@tinyerp.com>
2007-10-30 09:30:45 +0000

    payroll module * initial code * documents
    according to indian scenario this module is design

---
+++ payroll/payroll-4.0.3/__terp__.py
@@ -0,0 +1,14 @@
+{
+	"name" : "Payroll",
+	"version" : "0.3",
+	"author" : "Tiny",
+	"category" : "Generic Modules/Payroll",
+	"website" : "http://tinyerp.com/module_hr.html",
+	"description": "Module for payroll, India",
+	"depends" : ["base"],
+	"init_xml" : [],
+	"demo_xml" : ["setup/payroll_demo.xml"],
+	"update_xml" : ["payroll_view.xml","setup/setup_view.xml","employee/setup_view.xml","tax/setup_view.xml", "hr_wizard.xml"],
+	"active": False,
+	"installable": True
+}
