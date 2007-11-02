550105c30783925b7b72581a15d5c5160d333340
Fabien Pinckaers <fp@tinyerp.com>
2007-11-02 16:41:25 +0000

    NEW MODULE: base_module_record 	Module Recorder, it allows you to create module without any development 	it records all operation of creations and modifications on records 	during the recording session and produce a .ZIP module. 	Should be added: 		Workflow Support 		Adding Assertions

---
+++ base_module_record/__terp__.py
@@ -0,0 +1,14 @@
+{
+	"name" : "Module recorder",
+	"version" : "1.0",
+	"author" : "Tiny",
+	"website" : "http://tinyerp.com",
+	"category" : "Generic Modules/Base",
+	"description": """
+	""",
+	"depends" : ["base"],
+	"init_xml" : [ ],
+	"demo_xml" : [ ],
+	"update_xml" : [ "base_module_record_wizard.xml" ],
+	"installable": True
+}
