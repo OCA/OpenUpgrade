4dbb549ff2a3d9ad62ffb775063f6e9623a427b9
fmr <>
2007-11-05 07:40:25 +0000

    initial commit pour le module profile_association et les modules dépendants

---
+++ membership/__terp__.py
@@ -0,0 +1,15 @@
+{
+	"name" : "Membership",
+	"version" : "0.1",
+	"author" : "Tiny",
+	"category" : "Generic Modules/Association",
+	"depends" : [
+		"base"
+		],
+	"demo_xml" : [],
+	"update_xml" : [
+		"membership_view.xml",
+		],
+	"active" : False,
+	"installable" : True,
+}
---
+++ profile_association/__terp__.py
@@ -0,0 +1,12 @@
+{
+	"name":"Association profile",
+	"version":"0.1",
+	"author":"Tiny",
+	"category":"Profile",
+	"depends":["base"
+	], 
+	"demo_xml":[],
+	"update_xml":["association_menu.xml"],
+	"active":False,
+	"installable":True,
+}
