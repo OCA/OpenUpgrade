3d09da97b40d4153179d2cf0f649b572cb61a8a9
fmr <>
2007-11-07 14:56:00 +0000

    events module skel

---
+++ events/__terp__.py
@@ -0,0 +1,15 @@
+{
+	"name" : "Events",
+	"version" : "0.1",
+	"author" : "Tiny",
+	"category" : "Generic Modules/Association",
+	"depends" : [
+		"base"
+		],
+	"demo_xml" : [],
+	"update_xml" : [
+		"events_view.xml",
+		],
+	"active" : False,
+	"installable" : True,
+}
