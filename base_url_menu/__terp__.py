e0a1570a9faa9b8043c498e6bcef0ed6f3d60953
qdp <>
2007-11-08 08:37:36 +0000

    module base_url_menu added

---
+++ base_url_menu/__terp__.py
@@ -0,0 +1,14 @@
+
+{
+	"name" : "Url Menus",
+	"version" : "1.0",
+	"author" : "Tiny",
+	"category" : "Generic Modules/Others",
+	"website": "http://www.tinyerp.com",
+	"description": "Module for creating url menus",
+	"depends" : ["base"],
+	"init_xml" : [],
+	"update_xml" : ["base_url_menu_wizard.xml"],
+	"active": False,
+	"installable": True
+}
