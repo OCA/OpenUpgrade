ee35dc0a89b9a65acc0a0cee7ef67e52415ea597
Fabien Pinckaers <fp@tinyerp.com>
2007-11-19 06:17:13 +0000

    No Auto-Picking

---
+++ stock_no_autopicking/__terp__.py
@@ -0,0 +1,15 @@
+{
+	"name":"Stock No Auto-Picking",
+	"version":"1.0",
+	"author":"Tiny",
+	"category":"Generic Modules/Production",
+	"description": """
+	This module allows an intermediate picking process between all production
+	orders.
+	""",
+	"depends":["mrp"],
+	"demo_xml":[],
+	"update_xml":[],
+	"active":False,
+	"installable":True,
+}
