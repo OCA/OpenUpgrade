7d5c8e9f00bf2d73617656232ba8115f58a30619
qdp <>
2007-11-23 09:18:51 +0000

    module segmentation first version, ready for the client

---
+++ segmentation/__init__.py
@@ -0,0 +1,2 @@
+import segmentation
+import wizard
---
+++ segmentation/__terp__.py
@@ -0,0 +1,17 @@
+{
+	"name" : "segmentation management",
+	"version" : "1.1",
+	"depends" : ["base"],
+	"author" : "Tiny",
+	"description": """
+	This module allow users to create profile and compute automatically which partners do fit the profile criteria
+	""",
+	"website" : "http://tinyerp.com/",
+	"category" : "Generic Modules/Project & Services",
+	"init_xml" : [],
+	"demo_xml" : ["segmentation_demo.xml"],
+	"update_xml" : ["segmentation_view.xml",
+			],
+	"active": False,
+	"installable": True
+}
