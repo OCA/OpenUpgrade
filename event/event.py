f5dfa7364860f92f03bf41e04bac2fa2ee03be34
Fabien Pinckaers <fp@tinyerp.com>
2007-11-27 06:06:20 +0000

    Event

--- event/event.py
+++ event/event.py
@@ -56,7 +56,7 @@ class event(osv.osv):
 		res={}
 		for event in self.browse(cr, uid, ids, context):
 			cr.execute('SELECT sum(nb_register) FROM crm_case WHERE section_id=%d and state in (\'open\',\'done\')',  (event.section_id.id,))
-			res2 = cr.fetchone()·
+			res2 = cr.fetchone()
 			res[event.id] = res2 and res2[0] or 0
 		return res
 
@@ -64,7 +64,7 @@ class event(osv.osv):
 		res={}
 		for event in self.browse(cr, uid, ids, context):
 			cr.execute('SELECT sum(nb_register) FROM crm_case WHERE section_id=%d and state in (\'draft\')',  (event.section_id.id,))
-			res2 = cr.fetchone()·
+			res2 = cr.fetchone()
 			res[event.id] = res2 and res2[0] or 0
 		return res
 
