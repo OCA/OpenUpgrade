2302e9089114e794fd91b3b859e515cf243208ae
Husen Daudi <hda@tinyerp.com>
2007-12-03 12:46:31 +0000

    Module to make Account->Invoic->customer Invoice-> Invoice Line->Analytic account required.

---
+++ account_invoice_analytic_required/__terp__.py
@@ -0,0 +1,15 @@
+{
+	"name" : "Account invoice analytic required",
+	"version" : "1.0",
+	"depends" : ["account", "base"],
+	"author" : "Tiny",
+	"description": """Modules to make analytic account field required in sale.invoice.line""",
+	"website" : "http://tinyerp.com/module_account.html",
+	"category" : "Generic Modules/Accounting",
+	"init_xml" : [
+	],
+	"update_xml" : [
+	],
+	"active": False,
+	"installable": True
+}
---
+++ account_invoice_analytic_required/account_invoice_analytic_required.py
@@ -0,0 +1,11 @@
+from osv import fields, osv
+
+class account_invoice_line(osv.osv):
+    _name = "account.invoice.line"
+    _inherit="account.invoice.line"
+    _description = "Invoice line"
+    _columns = {
+        'account_analytic_id':  fields.many2one('account.analytic.account', 'Analytic Account', required=True),
+    }
+
+account_invoice_line()
