cf2634181b14382eef09bd36b07c93a9ab78dd85
ced <>
2007-12-10 09:42:30 +0000

    New module that put payment line in the invoice report

---
+++ account_invoice_payment_term/__terp__.py
@@ -0,0 +1,9 @@
+{
+	'name': 'Invoice with payment term lines',
+	'version': '0.1',
+	'depends': ['account'],
+	'author': 'Tiny',
+	'description': 'Add payment term lines on invoice',
+	'category': 'Generic Modules/Accounting',
+	'update_xml': ['report.xml'],
+}
