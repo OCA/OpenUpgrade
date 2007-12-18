37731153b98745d099f8d39b61cf3f5082edb031
Fabien Pinckaers <fp@tinyerp.com>
2007-12-18 12:42:10 +0000

    Moved to trunk

---
+++ account_reporting/__terp__.py
@@ -0,0 +1,20 @@
+{
+	"name" : "Reporting of Balancesheet for accounting",
+	"version" : "1.0",
+	"depends" : ["account"],
+	"author" : "Tiny",
+	"description": """Financial and accounting reporting""",
+	"category" : "Generic Modules/Accounting",
+	"init_xml" : [ ],
+	"demo_xml" : [ ],
+	"update_xml" : [
+				    "account_view.xml",
+				    "account_report.xml",
+				    "account_data.xml",
+				    ],
+#	"translations" : {
+#		"fr": "i18n/french_fr.csv"
+#	},
+	"active": False,
+	"installable": True
+}
