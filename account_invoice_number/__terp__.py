a53b91d74bf75018b48f50856700f247f80e583f
Husen Daudi <hda@tinyerp.com>
2008-01-07 10:21:46 +0000

    Module to change the defination of Invoice number field.

---
+++ account_invoice_number/__terp__.py
@@ -0,0 +1,19 @@
+{
+    "name" : "account_invoice_number",
+    "version" : "1.0",
+    "depends" : ["account", "sale", "purchase"],
+    "author" : "Tiny",
+    "description": """Module to change defination of the name field of invoice
+    using this module you can change the invoice numbet when it is in draft state.
+    """,
+    "website" : "http://tinyerp.com/module_account.html",
+    "category" : "Generic Modules/Accounting",
+    "init_xml" : [
+    ],
+    "demo_xml" : [
+    ],
+    "update_xml" : [
+    ],
+    "active": False,
+    "installable": True
+}
