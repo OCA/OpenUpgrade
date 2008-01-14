ae7799dedbc5fc48cc2e43455de66f9d89c5aadb
qdp <>
2008-01-14 11:25:53 +0000

    bugfix on the quantity field to use. The previous one didn't work and it was always the line.product_uom_qty which was chosen.

--- purchase_delivery/delivery.py
+++ purchase_delivery/delivery.py
@@ -30,54 +30,54 @@ import netsvc
 from osv import fields,osv,orm
 
 class delivery_grid(osv.osv):
-    _name = "delivery.grid"
-    _inherit = "delivery.grid"
+	_name = "delivery.grid"
+	_inherit = "delivery.grid"
 
-    def get_price(self, cr, uid, id, order, dt, context):
+	def get_price(self, cr, uid, id, order, dt, context):
+		"""
+		This is not very clean because the function has to check the type of order.
+		It could be improve by changing the signature of the method and by passing a dict in place of a browse record.
+		"""
+		total = 0
+		weight = 0
+		volume = 0
 
-        total = 0
-        weight = 0
-        volume = 0
+		for line in order.order_line:
+			if not line.product_id:
+				continue
+			qty = 0.0
+			
+			if order._table_name == "sale.order":
+				qty = line.product_uom_qty
+			else:
+				qty = line.product_qty
 
-        for line in order.order_line:
-            if not line.product_id:
-                continue
-            qty = 0.0
-            if hasattr(line,"product_uom_qty"):
-                qty = line.product_uom_qty
-            else:
-                qty = line.product_qty
-            total += line.price_subtotal or 0.0
-            weight += (line.product_id.weight or 0.0) * qty
-            volume += (line.product_id.volume or 0.0) * qty
+			total += line.price_subtotal or 0.0
+			weight += (line.product_id.weight or 0.0) * qty
+			volume += (line.product_id.volume or 0.0) * qty
 
+		return self.get_price_from_picking(cr, uid, id, total,weight, volume, context)
 
-        return self.get_price_from_picking(cr, uid, id, total,weight, volume, context)
+	def get_price_from_picking(self, cr, uid, id, total, weight, volume, context={}):
+		grid = self.browse(cr, uid, id, context)
 
+		price = 0.0
+		ok = False
 
-    def get_price_from_picking(self, cr, uid, id, total, weight, volume, context={}):
-        grid = self.browse(cr, uid, id, context)
-
-        price = 0.0
-        ok = False
-
-        for line in grid.line_ids:
-            price_dict = {'price': total, 'volume':volume, 'weight': weight, 'wv':volume*weight}
-            test = eval(line.type+line.operator+str(line.max_value), price_dict)
-            if test:
-                if context.has_key('object') and context['object']=='purchase':
-                    price = line.standard_price
-                else:
-                    price = line.list_price
-                if line.price_type=='variable':
-                        price *= price_dict[line.variable_factor]
-                ok = True
-                break
-        if not ok:
-            raise except_osv('No price avaible !', 'No line matched this order in the choosed delivery grids !')
-        return price
-
+		for line in grid.line_ids:
+			price_dict = {'price': total, 'volume':volume, 'weight': weight, 'wv':volume*weight}
+			test = eval(line.type+line.operator+str(line.max_value), price_dict)
+			if test:
+				if context.has_key('object') and context['object']=='purchase':
+					price = line.standard_price
+				else:
+					price = line.list_price
+				if line.price_type=='variable':
+					price *= price_dict[line.variable_factor]
+				ok = True
+				break
+		if not ok:
+			raise except_osv('No price avaible !', 'No line matched this order in the choosed delivery grids !')
+		return price
 
 delivery_grid()
-
-
