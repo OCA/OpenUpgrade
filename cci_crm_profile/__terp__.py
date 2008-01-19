632835397da78fccf7f14625175d3e8c28e99308
Mustufa Rangwala <mra@tinyerp.com>
2008-01-19 04:27:02 +0000

    cci project't terp file,depend on crm_profiling module

---
+++ cci_crm_profile/__terp__.py
@@ -0,0 +1,16 @@
+{
+    "name" : "CCI CRM Profile",
+    "version" : "1.0",
+    "author" : "Tiny",
+    "website" : "http://tinyerp.com",
+    "category" : "Generic Modules/CCI CRM Profile",
+    "description": """
+        specific module for cci project which will use crm_profile module.
+    """,
+    "depends" : ["base","crm_profiling"],
+    "init_xml" : [],
+    "demo_xml" : [],
+    "update_xml" : [],
+    "active": False,
+    "installable": True
+}
