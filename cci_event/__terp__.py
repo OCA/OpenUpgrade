a7befc4ae15d1858345400167bd760b4fec7b1be
Mustufa Rangwala <mra@tinyerp.com>
2008-01-21 11:51:38 +0000

    add cci_event module,depends on event

---
+++ cci_event/__terp__.py
@@ -0,0 +1,16 @@
+{
+    "name" : "CCI EVENT",
+    "version" : "1.0",
+    "author" : "Tiny",
+    "website" : "http://tinyerp.com",
+    "category" : "Generic Modules/CCI Event",
+    "description": """
+        specific module for cci project which will use event module from extra addons.
+    """,
+    "depends" : ["base","event"],
+    "init_xml" : [],
+    "demo_xml" : [],
+    "update_xml" : ["cci_event_view.xml"],
+    "active": False,
+    "installable": True
+}
