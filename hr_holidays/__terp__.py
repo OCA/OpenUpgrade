238d0d61bfa0c8eae19c67d51667d8e49a201605
qdp <>
2008-01-28 14:00:25 +0000

    improvement of the module description

--- hr_holidays/__terp__.py
+++ hr_holidays/__terp__.py
@@ -3,8 +3,23 @@
 	"version" : "0.1",
 	"author" : "Tiny & Axelor",
 	"category" : "Generic Modules/Human Resources",
-	"website" : "http://www.axelor.com/",
-	"description": "Human Ressources: Holidays tracking and workflow",
+	"website" : "http://tinyerp.com/",
+	"description": """Human Ressources: Holidays tracking and workflow
+
+	Note that: 
+	- You can set up your color preferencies in 
+				HR \ Configuration \ Holidays Status
+	- There are two ways to print the employee's holidays:
+		* The first will allow to choose employees by department and is used by clicking the menu item located in 
+				HR \ Holidays Request \ Print Summary of Holidays
+		* The second will allow you to choose the holidays report for specific employees. Go on the list 
+				HR \ Employees \ Employees
+			then select the ones you want to choose, click on the print icon and select the option
+				'Print Summary of Employee's Holidays'
+	- The wizard allows you to choose if you want to print either the Confirmed & Validated holidays or only the Validated ones. These states must be set up by a user from the group 'HR' and with the role 'holidays'. You can define these features in the security tab from the user data in 
+				Administration \ Users \ Users
+			for example, you maybe will do it for the user 'admin'.
+""",
 	"depends" : ["hr"],
 	"init_xml" : [],
 	"demo_xml" : ["hr_bel_holidays_2008.xml",],
