e1b025cc06e68942435087b2d0a5c6dda5420705
Mustufa Rangwala <mra@tinyerp.com>
2008-02-06 10:29:27 +0000

    profile for cci project

---
+++ profile_cci/__terp__.py
@@ -0,0 +1,19 @@
+{
+    "name":"cci profile",
+    "version":"1.0",
+    "author":"Tiny",
+    "category":"Profile",
+    "depends":[
+        "base_contact",
+        "cci_partner",
+        "cci_base_contact",
+        "event",
+        "cci_event",
+        "cci_crm_profile",
+        "account_payment"
+    ],
+    "demo_xml":[],
+    "update_xml":[],
+    "active":False,
+    "installable":True,
+}
