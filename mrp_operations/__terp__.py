79658023b18412cdb36b2b75b66fc374b82fd99d
Husen Daudi <hda@tinyerp.com>
2008-02-07 06:20:06 +0000

    * Module to add state, date_start, date_stop in production order operation lines   (in the "Workcenters" tab) and workflow gor state draft,confirm,done and cancel

---
+++ mrp_operations/__terp__.py
@@ -0,0 +1,30 @@
+{
+	"name" : "Workcenter Production start end workflow",
+	"version" : "1.0",
+	"author" : "Tiny",
+	"website" : "http://tinyerp.com/module_mrp.html",
+	"category" : "Generic Modules/Production",
+	"depends" : ["stock", "hr", "purchase", "product", "mrp"],
+	"description": """
+	 This module adds state, date_start,date_stop in production order operation lines
+     (in the "Workcenters" tab)
+     State: draft, confirm, done, cancel
+     When finnishing/confirming,cancelling production orders set all state lines to the according state
+     Create menus:
+         Production Management > All Operations
+         Production Management > All Operations > Operations To Do (state="confirm")
+     Which is a view on "Workcenters" lines in production order,
+	 editable tree
+
+	Add buttons in the form view of production order under workcenter tab:
+	* start (set state to confirm), set date_start
+	* done (set state to done), set date_stop
+	* set to draft (set state to draft)
+	* cancel set state to cancel
+	""",
+	"init_xml" : [],
+	"demo_xml" : [],
+	"update_xml" : ["mrp_operations_workflow.xml","mrp_operations_view.xml",],
+	"active": False,
+	"installable": True
+}
---
+++ mrp_operations/mrp_operations.py
@@ -0,0 +1,77 @@
+##############################################################################
+#
+# Copyright (c) 2005-2006 TINY SPRL. (http://tiny.be) All Rights Reserved.
+#
+# $Id: mrp.py 1292 2005-09-08 03:26:33Z pinky $
+#
+# WARNING: This program as such is intended to be used by professional
+# programmers who take the whole responsability of assessing all potential
+# consequences resulting from its eventual inadequacies and bugs
+# End users who are looking for a ready-to-use solution with commercial
+# garantees and support are strongly adviced to contract a Free Software
+# Service Company
+#
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+##############################################################################
+
+from osv import fields
+from osv import osv
+import ir
+
+import netsvc
+import time
+from mx import DateTime
+
+#----------------------------------------------------------
+# Workcenters
+#----------------------------------------------------------
+# capacity_hour : capacity per hour. default: 1.0.
+#          Eg: If 5 concurrent operations at one time: capacity = 5 (because 5 employees)
+# unit_per_cycle : how many units are produced for one cycle
+#
+# TODO: Work Center may be recursive ?
+#
+
+class mrp_production_workcenter_line(osv.osv):
+	_inherit = 'mrp.production.workcenter.line'
+	_name = 'mrp.production.workcenter.line'
+	_description = 'Production workcenters used'
+	_columns = {
+		'state': fields.selection([('draft','Draft'),('confirm', 'Confirm'),('cancel','Canceled'),('done','Done')],'State', readonly=True),
+		'date_start': fields.datetime('Start Date'),
+		'date_finnished': fields.datetime('End Date'),
+	}
+	_defaults = {
+		'state': lambda *a: 'draft',
+	}
+
+	def action_draft(self, cr, uid, ids):
+		self.write(cr, uid, ids, {'state':'draft','date_start':None})
+		return True
+
+	def action_confirm(self, cr, uid, ids):
+		self.write(cr, uid, ids, {'state':'confirm','date_start':DateTime.now().strftime('%Y-%m-%d %H:%M:%S')})
+		return True
+
+	def action_done(self, cr, uid, ids):
+		self.write(cr, uid, ids, {'state':'done','date_finnished':DateTime.now().strftime('%Y-%m-%d %H:%M:%S')})
+		return True
+
+	def action_cancel(self, cr, uid, ids):
+		self.write(cr, uid, ids, {'state':'cancel','date_start':None})
+		return True
+
+mrp_production_workcenter_line()
