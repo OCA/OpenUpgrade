06808c497438c4ebb044756f93886aca97268357
Fabien Pinckaers <fp@tinyerp.com>
2008-02-08 09:25:24 +0000

    Modifs

--- profile_cci/__terp__.py
+++ profile_cci/__terp__.py
@@ -1,17 +1,11 @@
 {
-    "name":"cci profile",
+    "name":"Profile Chamber of Commerce",
     "version":"1.0",
     "author":"Tiny",
     "category":"Profile",
     "depends":[
-        "base_contact",
-        "cci_partner",
-        "cci_base_contact",
-        "event",
-        "cci_event",
-        "cci_crm_profile",
-        "account_payment",
-        "membership"
+        "base_contact", "cci_partner", "cci_base_contact", "event", "cci_event",
+        "cci_crm_profile", "account_payment", "membership", "base_module_record", "l10n_be"
     ],
     "demo_xml":[],
     "update_xml":[],
