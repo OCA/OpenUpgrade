edc64aec1cea27eee3edfe57c62863180f4cf498
Fabien Pinckaers <fp@tinyerp.com>
2008-02-08 23:52:05 +0000

    Morroco Chart of Account

---
+++ l10n_ma/__terp__.py
@@ -0,0 +1,17 @@
+#
+# Plan comptable général pour le Maroc,
+# Mise en forme et paramétrage par http://sisatlas.com
+# 
+{
+	"name" : "Maroc Plan comptable général pour les sociétés",
+	"version" : "1.0",
+	"author" : "SISatlas",
+	"category" : "Localisation/Account charts",
+	"website": "http://comptabilite.erp-libre.info",
+	"depends" : ["base", "account"],
+	"init_xml" : [],
+	"update_xml" : ["types_de_comptes.xml","currency.xml","plan-maroc.xml","taxes.xml"],
+	"demo_xml" : [],
+	"active": False,
+	"installable": True
+}
