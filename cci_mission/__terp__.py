d8838c30a4706316e7e900fa48b9f556a083ba2b
Mustufa Rangwala <mra@tinyerp.com>
2008-02-21 04:02:25 +0000

    cci_mission

---
+++ cci_mission/__terp__.py
@@ -0,0 +1,16 @@
+{
+    "name" : "CCI mission",
+    "version" : "1.0",
+    "author" : "Tiny",
+    "website" : "http://tinyerp.com",
+    "category" : "Generic Modules/CCI mission",
+    "description": """
+        specific module for cci project.
+    """,
+    "depends" : ["base"],
+    "init_xml" : [],
+    "demo_xml" : [],
+    "update_xml" : ["cci_mission_view.xml"],
+    "active": False,
+    "installable": True
+}
