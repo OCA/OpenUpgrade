db3bf5bdc19792169abd35b35914c20c38bee49e
Fabien Pinckaers <fp@tinyerp.com>
2008-02-26 09:27:42 +0000

    Depends

--- profile_cci/__terp__.py
+++ profile_cci/__terp__.py
@@ -5,7 +5,8 @@
     "category":"Profile",
     "depends":[
         "base_contact", "cci_partner", "cci_base_contact", "event", "cci_event",
-        "cci_crm_profile", "account_payment", "membership", "base_module_record", "l10n_be"
+        "cci_crm_profile", "account_payment", "membership", "base_module_record", "l10n_be",
+		"base_report_creator"
     ],
     "demo_xml":[],
     "update_xml":[],
