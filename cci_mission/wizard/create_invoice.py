ba8f224427f352f11d4cbd4e2a47798a85120953
Mustufa Rangwala <mra@tinyerp.com>
2008-02-27 13:22:16 +0000

    add create invoice function on wizard (certificate object)

--- cci_mission/wizard/create_invoice.py
+++ cci_mission/wizard/create_invoice.py
@@ -40,10 +40,86 @@ form = """<?xml version="1.0"?>
 
 fields = {}
 
+def _createInvoices(self, cr, uid, data, context):
+    print "make invoice....."
+    invoices = {}
+    invoice_ids = []
+    create_ids = []
+    tax_ids = []
+
+    pool_obj = pooler.get_pool(cr.dbname)
+    obj_certificate = pool_obj.get('cci_missions.certificate')
+    data_certificate = obj_certificate.browse(cr,uid,data['ids'])
+
+    obj_lines=pool_obj.get('account.invoice.line')
+
+    list = []
+    value = []
+    address_contact = False
+    address_invoice = False
+    for certificate in data_certificate:
+        list.append(certificate.type_id.original_product_id.id)
+        list.append(certificate.type_id.copy_product_id.id)
+        print certificate.order_partner_id.address,"addressss....."
+
+        for add in certificate.order_partner_id.address:
+            if add.type == 'contact' or add.type == 'default':
+                address_contact = add.id
+            if add.type == 'invoice' or add.type == 'default':
+                address_invoice = add.id
+
+        if not address_contact:
+            raise wizard.except_wizard('Warning !', 'Please Enter Partner Address (contact/default) on Billed Customer : %s'%(certificate.order_partner_id.name))
+        if not address_invoice:
+            raise wizard.except_wizard('Warning !', 'Please Enter Partner Address (invoice/default) on Billed Customer : %s'%(certificate.order_partner_id.name))
+
+        for prod_id in list:
+            val = obj_lines.product_id_change(cr, uid, [], prod_id,uom =False, partner_id=certificate.order_partner_id.id)
+            val['value'].update({'product_id' : prod_id })
+            data_product=pool_obj.get('product.product').browse(cr,uid,prod_id)
+            for tax in data_product.taxes_id:
+                tax_ids.append(tax.id)
+            val['value'].update({'taxes_id' : tax_ids })
+            tax_ids = []
+            value.append(val)
+        for val in value:
+            inv_id =pool_obj.get('account.invoice.line').create(cr, uid, {
+                    'name': certificate.name,
+                    'account_id':val['value']['account_id'],
+                    'price_unit': val['value']['price_unit'],
+                    'quantity': 1,
+                    'discount': False,
+                    'uos_id': val['value']['uos_id'],
+                    'product_id':val['value']['product_id'],
+                    'invoice_line_tax_id': [(6,0,val['value']['taxes_id'])],
+                    'note':certificate.text_on_invoice,
+            })
+            create_ids.append(inv_id)
+        inv = {
+                'name': certificate.name,
+                'origin': certificate.name,
+                'type': 'out_invoice',
+                'reference': False,
+                'account_id': certificate.order_partner_id.property_account_receivable.id,
+                'partner_id': certificate.order_partner_id.id,
+                'address_invoice_id':address_invoice,
+                'address_contact_id':address_contact,
+                'invoice_line': [(6,0,create_ids)],
+                'currency_id' :certificate.order_partner_id.property_product_pricelist.currency_id.id,# 1,
+                'comment': certificate.text_on_invoice,
+                'payment_term':certificate.order_partner_id.property_payment_term.id,
+            }
+
+        inv_obj = pool_obj.get('account.invoice')
+        inv_id = inv_obj.create(cr, uid, inv)
+        return {}
+
+
+    return {}
 class create_invoice(wizard.interface):
     states = {
         'init' : {
-            'actions' : [],
+            'actions' : [_createInvoices],
             'result' : {'type' : 'form' ,   'arch' : form,
                     'fields' : fields,
                     'state' : [('end','Ok')]}
