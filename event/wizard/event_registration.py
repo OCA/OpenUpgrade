2dfa86372483bfff20357b913db35e3cd6936566
Mustufa Rangwala <mra@tinyerp.com>
2008-03-01 07:37:56 +0000

    corect the if condition (in _event_task function)

--- event/wizard/event_registration.py
+++ event/wizard/event_registration.py
@@ -33,7 +33,7 @@ def _event_tasks(self, cr, uid, data, context):
 	event_id = data['id']
 	cr.execute('''SELECT project_id FROM event_event WHERE id = %d '''% (event_id, ))
 	res = cr.fetchone()
-	if not res:
+	if not res[0]:
 		raise wizard.except_wizard('Error !', 'No project defined for this event.\nYou can create one with the retro-planning button !')
 	value = {
 		'domain': [('project_id', '=', res[0])],
