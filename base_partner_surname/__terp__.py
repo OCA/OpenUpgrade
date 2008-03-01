2a92136da33dad7952f010e5807d5c2878c4091e
Harshad Modi <hmo@tinyerp.com>
2008-03-01 10:36:31 +0000

    new module for separate surname from contact name of partner

---
+++ base_partner_surname/__terp__.py
@@ -0,0 +1,16 @@
+{
+    "name" : "Base Partner Surname",
+    "version" : "1.0",
+    "author" : "Tiny",
+    "website" : "http://tinyerp.com",
+    "category" : "Generic Modules/Base",
+    "description": """
+This module use for seperate surname from contact name of partner. Now You can give first name & last name on contact Name
+
+    """,
+    "depends" : ["base"],
+    "init_xml" : [ ],
+    "demo_xml" : [ ],
+    "update_xml" : [ "partner_view.xml" ],
+    "installable": True
+}
