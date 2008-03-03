c2f8867d6fd42011f68af50107b9008663679426
Harshad Modi <hmo@tinyerp.com>
2008-03-03 09:34:42 +0000

    product_listprice_upgrade : The aim of this module is to allow the automatic upgrade of the field 'List Price' on each product.     * added a new price type called 'Internal Pricelist' (currently, we have only 2 price types: Sale and Purchase Pricelist)     * Created a wizard button in the menu Products>Pricelist called 'Upgrade Product List Price'     * When we have completed the wizard and click on 'Upgrade', it will change the field 'List Price' for all products contained in the categories that we have selected in the wizard

---
+++ product_listprice_upgrade/__terp__.py
@@ -0,0 +1,17 @@
+{
+    "name":"Product listprice upgrade",
+    "version":"1.0",
+    "author":"Tiny",
+    "category":"Generic Modules/Inventory Control",
+    "description": """
+    The aim of this module is to allow the automatic upgrade of the field 'List Price' on each product.
+    * added a new price type called 'Internal Pricelist' (currently, we have only 2 price types: Sale and Purchase Pricelist)
+    * Created a wizard button in the menu Products>Pricelist called 'Upgrade Product List Price'
+    * When we have completed the wizard and click on 'Upgrade', it will change the field 'List Price' for all products contained in the categories that we have selected in the wizard
+    """,
+    "depends":["base","product"],
+    "demo_xml":[],
+    "update_xml":['product_wizard.xml','product_data.xml'],
+    "active":False,
+    "installable":True,
+}
---
+++ product_listprice_upgrade/wizard/wizard_product_listprice.py
@@ -0,0 +1,107 @@
+##############################################################################
+#
+# Copyright (c) 2005-2006 TINY SPRL. (http://tiny.be) All Rights Reserved.
+#                    Fabien Pinckaers <fp@tiny.Be>
+#
+# WARNING: This program as such is intended to be used by professional
+# programmers who take the whole responsability of assessing all potential
+# consequences resulting from its eventual inadequacies and bugs
+# End users who are looking for a ready-to-use solution with commercial
+# garantees and support are strongly adviced to contract a Free Software
+# Service Company
+#
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+##############################################################################
+
+
+
+import pooler
+import time
+import wizard
+
+_pricelist_form = '''<?xml version="1.0"?>
+<form string="Upgrade list price">
+    <separator string="Select a pricelist " colspan="4"/>
+    <field name="pricelist" colspan="4" nolabel="1"/>
+    <separator string="Select Product Categories " colspan="4"/>
+    <field name="product_category" colspan="4" nolabel="1"/>
+    <newline/>
+    <field name="upgrade" />
+</form>'''
+
+_done_form = '''<?xml version="1.0"?>
+<form string="Upgraded list price of prodcuts">
+    <field name="update_products"/>
+
+</form>'''
+
+_done_fields = {
+    'update_products': {'string':'Upgraded list price of Products', 'type':'float', 'readonly': True},
+
+
+}
+
+class wizard_product_pricelist(wizard.interface):
+
+
+    def _get_pricelist(self, cr, uid, context):
+        pricelist_obj=pooler.get_pool(cr.dbname).get('product.pricelist')
+        ids=pricelist_obj.search(cr, uid, [('type', '=', 'internal'),])
+        pricelists=pricelist_obj.browse(cr, uid, ids)
+        return [(pricelist.id, pricelist.name ) for pricelist in pricelists]
+
+    def _upgrade_listprice(self, cr, uid, data, context):
+        self.update_products=0
+        categories_ids=data['form']['product_category']
+        pricelist_obj=pooler.get_pool(cr.dbname).get('product.pricelist')
+        cat_obj = pooler.get_pool(cr.dbname).get('product.category')
+        product_obj = pooler.get_pool(cr.dbname).get('product.product')
+        pricelist_id=data['form']['pricelist']
+
+        def _upgrade(category_id):
+            if data['form']['upgrade']==True:
+                child_ids=cat_obj.search(cr, uid, [('parent_id', '=', category_id),])
+                for child_id in child_ids:
+                    _upgrade(child_id)
+            product_ids=product_obj.search(cr, uid, [('categ_id', '=', category_id),])
+            for product_id in product_ids:
+                list_price=pricelist_obj.price_get(cr, uid, [pricelist_id], product_id, 1)
+                product_obj.write(cr, uid, [product_id], {
+                            'list_price': list_price[pricelist_id]})
+                self.update_products += 1
+
+        for category_id in categories_ids[0][2]:
+            _upgrade(category_id)
+
+        return {'update_products':self.update_products}
+
+    _pricelist_fields = {
+        'pricelist': {'string':'Pricelist', 'type':'many2one', 'relation': 'product.pricelist','domain':[('type','=','internal')], 'required':True},
+        'product_category': {'string':'Product Category', 'type':'many2many', 'relation': 'product.category', 'required':True},
+        'upgrade' : {'string':'Upgrade Child categories', 'type':'boolean', 'default': lambda x,y,z: True}
+    }
+
+    states = {
+        'init': {
+            'actions': [],
+            'result': {'type':'form', 'arch':_pricelist_form, 'fields':_pricelist_fields, 'state':[('end','Cancel'),('upgrade','Upgrade')]}
+        },
+        'upgrade':{
+             'actions':[_upgrade_listprice],
+             'result':{'type': 'form', 'arch': _done_form, 'fields': _done_fields,'state':[('end', 'End')]}
+       }
+    }
+wizard_product_pricelist('product.listprice')
