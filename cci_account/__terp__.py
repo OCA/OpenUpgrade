cd22b9d3f6276d5fbf78154f9452ed15c2d31b82
Mustufa Rangwala <mra@tinyerp.com>
2008-03-07 06:01:30 +0000

    cci account

---
+++ cci_account/__terp__.py
@@ -0,0 +1,16 @@
+{
+    "name" : "CCI Account",
+    "version" : "1.0",
+    "author" : "Tiny",
+    "website" : "http://tinyerp.com",
+    "category" : "Generic Modules/CCI Account",
+    "description": """
+        specific module for cci project which will use account module (Reports).
+    """,
+    "depends" : ["base","account"],
+    "init_xml" : [],
+    "demo_xml" : [],
+    "update_xml" : [],
+    "active": False,
+    "installable": True
+}
