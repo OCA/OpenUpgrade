de54f77f21fcb780a7009ba2cd113b4e5a9805aa
Jay vora <jvo@tinyerp.com>
2008-03-08 07:50:54 +0000

    Added an object of cci_account_message ,its view,wizard and report that prompts user for a message that has to be printed at the bottom of the invoices report.Changed the list view of statement lines:moving 'name' field as the last field.

--- cci_account/__terp__.py
+++ cci_account/__terp__.py
@@ -10,7 +10,7 @@
     "depends" : ["base","account"],
     "init_xml" : [],
     "demo_xml" : [],
-    "update_xml" : ["cci_account_wizard.xml"],
+    "update_xml" : ["cci_account_wizard.xml","cci_account_view.xml","cci_account_report.xml"],
     "active": False,
     "installable": True
 }
