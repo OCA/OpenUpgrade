8bb0d4c130b07bfae8ad13198f37528db7f7f514
Dhara Shah <dsh@tinyerp.com>
2008-03-10 06:47:05 +0000

    some imp change

--- Translation/translation.py
+++ Translation/translation.py
@@ -3,28 +3,46 @@ import netsvc
 from osv import fields, osv
 import ir
 from mx import DateTime
-from tools import config
+import tools 
+import xmlrpclib
+s = xmlrpclib.Server("http://192.168.0.4:8000")
+
+def get_language(cr,uid,context,user=None):
+    if user:
+        if user=='contributor':
+            file_list = s.get_publish_list()
+            print file_list
+        else:
+            file_list = s.get_contrib_list()
+        list = map(lambda x: x.replace('.csv',''),file_list)
+        print "================================",list
+    else:
+        sql = "select distinct lang from ir_translation"
+        cr.execute(sql)
+        list = map(lambda x: x[0],cr.fetchall())
+    lang_dict = tools.get_languages()
+    return [(lang, lang_dict.get(lang, lang)) for lang in list]
 
 class ir_translation_contribution(osv.osv):
     _name = "ir.translation.contribution"
-    _inherits = {'ir.translation': 'translation_id'}
+    _inherit = 'ir.translation'
     _description = "Translation Contribution"
     _columns = {
-            'translation_id' : fields.many2one('ir.translation','Original Transaltion'),
+#            'transaltion_id'  : fields.many2one('ir_translation','Parent Transaltion'),
             'contributor_email'  : fields.char('Email Id of Contibutor',size=128),      
             'state': fields.selection(
                     [('draft','Draft'),
                      ('propose','Propose for Change'),
                      ('unchange',"Don't change"),
-                     ('accpet','Accept'),
+                     ('accept','Accept'),
                      ('deny','Deny'),
-                     ('done','Done'),],
+                     ],
                      'Translation State', readonly=True, select=True), 
                 }   
     
-#    _sql = """
-#        create index ir_translation_ltn1 on ir_translation (lang,type,name);
-#        create index ir_translation_res_id1 on ir_translation (res_id);
-#    """
-    
+    _sql = """
+        create index ir_translation_ltn1 on ir_translation (lang,type,name);
+        create index ir_translation_res_id1 on ir_translation (res_id);
+    """
+
 ir_translation_contribution()
