7cdcadd7e54e1d1ce3a25dc63700333290d4f5ec
qdp <>
2008-03-10 13:50:54 +0000

    *spaces replaced by tabulations *cci_mission replaced by cci_missions

--- profile_cci/__terp__.py
+++ profile_cci/__terp__.py
@@ -1,15 +1,15 @@
 {
-    "name":"Profile Chamber of Commerce",
-    "version":"1.0",
-    "author":"Tiny",
-    "category":"Profile",
-    "depends":[
-        "base_contact", "cci_partner", "cci_base_contact", "event", "cci_event",
-        "cci_crm_profile", "account_payment", "membership", "base_module_record", "l10n_be",
-		"base_report_creator" , "cci_mission"
-    ],
-    "demo_xml":[],
-    "update_xml":[],
-    "active":False,
-    "installable":True,
+	"name":"Profile Chamber of Commerce",
+	"version":"1.0",
+	"author":"Tiny",
+	"category":"Profile",
+	"depends":[
+		"base_contact", "cci_partner", "cci_base_contact", "event", "cci_event",
+		"cci_crm_profile", "account_payment", "membership", "base_module_record", "l10n_be",
+		"base_report_creator" , "cci_missions"
+	],
+	"demo_xml":[],
+	"update_xml":[],
+	"active":False,
+	"installable":True,
 }
