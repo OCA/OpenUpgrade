ad8eb0dd2ff135d3f2e2a538739ded3713cceffc
Dhara Shah <dsh@tinyerp.com>
2008-03-13 13:22:14 +0000

    change for password and other

--- Translation/translation.py
+++ Translation/translation.py
@@ -7,7 +7,7 @@ import tools
 import xmlrpclib
 s = xmlrpclib.Server("http://192.168.0.4:8000")
 
-def get_language(cr,uid,context,user=None):
+def get_language(cr,uid,context,user=None,model=None):
     if user:
         if user=='contributor':
             file_list = s.get_publish_list()
@@ -15,12 +15,12 @@ def get_language(cr,uid,context,user=None):
         else:
             file_list = s.get_contrib_list()
         list = map(lambda x: x.replace('.csv',''),file_list)
-        print "================================",list
     else:
-        sql = "select distinct lang from ir_translation"
+        sql = "select distinct lang from %s"%model
         cr.execute(sql)
         list = map(lambda x: x[0],cr.fetchall())
     lang_dict = tools.get_languages()
+    print "list :",list
     return [(lang, lang_dict.get(lang, lang)) for lang in list]
 
 class ir_translation_contribution(osv.osv):
@@ -28,17 +28,17 @@ class ir_translation_contribution(osv.osv):
     _inherit = 'ir.translation'
     _description = "Translation Contribution"
     _columns = {
-#            'transaltion_id'  : fields.many2one('ir_translation','Parent Transaltion'),
             'contributor_email'  : fields.char('Email Id of Contibutor',size=128),      
             'state': fields.selection(
                     [('draft','Draft'),
                      ('propose','Propose for Change'),
                      ('unchange',"Don't change"),
-                     ('accept','Accept'),
+                     ('done','Accept'),
                      ('deny','Deny'),
                      ],
-                     'Translation State', readonly=True, select=True), 
-                }   
+                     'Translation State', readonly=True, select=True),
+            'upload':fields.boolean('upload') 
+                }
     
     _sql = """
         create index ir_translation_ltn1 on ir_translation (lang,type,name);
