7258864980008f41f517148b7aa10f4ad1f89a39
Dhara Shah <dsh@tinyerp.com>
2008-03-14 12:48:09 +0000

    It shows the view for Bug Tracking

--- crm_configuration/__terp__.py
+++ crm_configuration/__terp__.py
@@ -1,12 +1,20 @@
 {
     "name" : "Customer Relationship Management",
     "version" : "1.0",
-    "author" :"Tiny", 
-    "website": "http://tinyerp.com/module_crm.html",
-    "depends":["base"],
+    "author" : "Tiny",
+    "website" : "http://tinyerp.com/module_crm.html",
+    "category" : "Generic Modules/CRM & SRM",
+    "description": """ The Tiny ERP case and request tracker enables a group of
+people to intelligently and efficiently manage tasks, issues, and requests.
+It manages key tasks such as communication, identification, prioritization,
+assignment, resolution and notification.
+
+The CRM module has a email gateway for the synchronisation interface
+between mails and Tiny ERP.""",
+    "depends" : ["base","crm"],
     "init_xml" : [],
-    "demo_xml" : [],
-    "update_xml": ["employee_view.xml","crm_view.xml","crm_workflow.xml","crm_demo.xml"],
+    "demo_xml" : ["crm_demo.xml"],
+    "update_xml" : ["crm_view.xml"],
     "active": False,
     "installable": True
 }
