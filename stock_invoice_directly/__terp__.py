2ea858d30a245cb25d20fbc94cc8b275452ded28
Fabien Pinckaers <fp@tinyerp.com>
2008-03-14 07:52:17 +0000

    New module

---
+++ stock_invoice_directly/__terp__.py
@@ -0,0 +1,17 @@
+{
+	"name" : "Invoice Picking Directly",
+	"version" : "1.0",
+	"author" : "Tiny",
+	"website" : "http://tinyerp.com/module_sale.html",
+	"depends" : ["delivery"],
+	"category" : "Generic Modules/Sales & Purchases",
+	"description": """
+		When you send or deliver goods, this module automatically launch
+		the invoicing wizard if the delivery is to be invoiced.
+	""",
+	"init_xml" : [],
+	"demo_xml" : [],
+	"update_xml" : [],
+	"active": False,
+	"installable": True
+}
