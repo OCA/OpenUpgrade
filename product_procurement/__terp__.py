c5be2debd91b3f651ad78196269ab94900865eed
Fabien Pinckaers <fp@tinyerp.com>
2008-03-14 05:28:05 +0000

    Added a new module that adds a procurement request on a product form

---
+++ product_procurement/__terp__.py
@@ -0,0 +1,17 @@
+{
+	"name" : "Product Prodcurement",
+	"version" : "1.0",
+	"author" : "Tiny",
+	"website" : "http://tinyerp.com/module_sale.html",
+	"depends" : ["mrp"],
+	"category" : "Generic Modules/Sales & Purchases",
+	"description": """
+	This module adds a shortcut on the product form to create automatically
+	a procurement.
+	""",
+	"init_xml" : [],
+	"demo_xml" : [],
+	"update_xml" : ["product_procurement_wizard.xml"],
+	"active": False,
+	"installable": True
+}
