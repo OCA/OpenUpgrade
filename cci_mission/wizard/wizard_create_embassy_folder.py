d9db8ebf370ede666053037adc0bdf4603a70c99
Mustufa Rangwala <mra@tinyerp.com>
2008-03-18 08:58:53 +0000

    remove duplication of code : 	- merge two files for creating embassy folder (leg and certificate)
    	so give same wizard ref to both the leg and certificate wizard (creat embassy folder)

--- cci_mission/wizard/wizard_create_embassy_folder.py
+++ cci_mission/wizard/wizard_create_embassy_folder.py
@@ -17,27 +17,27 @@ fields = {
           }
 def _create_embassy_folder(self, cr, uid, data, context):
     obj_pool = pooler.get_pool(cr.dbname)
-    obj_certificate = obj_pool.get('cci_missions.certificate')
-    data_certificate = obj_certificate.browse(cr,uid,data['ids'])
+    obj_dossier = obj_pool.get(data['model'])
+    data_dossier = obj_dossier.browse(cr,uid,data['ids'])
     list_folders = []
     folder_create = 0
     folder_reject = 0
     folder_rej_reason = ""
-    for certificate in data_certificate:
-        if certificate.embassy_folder_id:
+    for data in data_dossier:
+        if data.embassy_folder_id:
             folder_reject = folder_reject + 1
-            folder_rej_reason += "ID "+str(certificate.id)+": Already Has an Folder Linked \n"
+            folder_rej_reason += "ID "+str(data.id)+": Already Has an Folder Linked \n"
             continue
         folder_create = folder_create + 1
         folder_id =obj_pool.get('cci_missions.embassy_folder').create(cr, uid, {
-                    'name': certificate.name,
-                    'date': certificate.date,
-                    'partner_id': certificate.order_partner_id.id,
-                    'destination_id': certificate.destination_id.id,
-                    'invoice_note' : certificate.text_on_invoice
+                    'name': data.name,
+                    'date': data.date,
+                    'partner_id': data.order_partner_id.id,
+                    'destination_id': data.destination_id.id,
+                    'invoice_note' : data.text_on_invoice
             })
         list_folders.append(folder_id)
-        obj_certificate.write(cr, uid,certificate.id, {'embassy_folder_id' : folder_id})
+        obj_dossier.write(cr, uid,data.id, {'embassy_folder_id' : folder_id})
     return {'folder_created' : str(folder_create) , 'folder_rejected' : str(folder_reject) , 'folder_rej_reason': folder_rej_reason }
 
 class create_embassy_folder(wizard.interface):
