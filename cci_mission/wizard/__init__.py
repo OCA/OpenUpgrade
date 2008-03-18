ab4ff2394aeed361b1107b1d2affa7968bec27b1
Mustufa Rangwala <mra@tinyerp.com>
2008-03-18 08:59:32 +0000

    remove duplication of code : 	- merge two files for creating embassy folder (leg and certificate)
    	so give same wizard ref to both the leg and certificate wizard (creat embassy folder)

--- cci_mission/wizard/__init__.py
+++ cci_mission/wizard/__init__.py
@@ -2,7 +2,6 @@ import create_invoice
 import wizard_create_embassy_folder
 import create_invoice_carnet
 import wizard_print_carnet
-import create_embassy_folder_legalization
 import wizard_federation_sending
 import create_invoice_embassy
 import wizard_simulation_carnet
