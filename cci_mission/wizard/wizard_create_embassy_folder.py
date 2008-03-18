a77cec81c41b065f6e2faee22c531edc46367f92
qdp <>
2008-03-18 12:58:26 +0000

    *improvements of the wizard for creating embassy folder from legalization and certificates + bug corrected *small improvement of some views *wizard xml reorganization

--- cci_mission/wizard/wizard_create_embassy_folder.py
+++ cci_mission/wizard/wizard_create_embassy_folder.py
@@ -1,6 +1,16 @@
 import wizard
 import pooler
 import netsvc
+
+preform = """<?xml version="1.0"?>
+<form string="Choose Site">
+    <field name="site_id" width="400"/>
+</form>
+"""
+prefields = {
+    'site_id': {'string':'Site', 'type':'many2one', 'relation':'cci_missions.site' ,'required':True},
+          }
+
 form = """<?xml version="1.0"?>
 <form string="Create Embassy Folder">
     <field name="folder_created"/>
@@ -16,6 +26,7 @@ fields = {
     'folder_rej_reason': {'string':'Error Messages', 'type':'text', 'readonly':True},
           }
 def _create_embassy_folder(self, cr, uid, data, context):
+    site_id = data['form']['site_id']
     obj_pool = pooler.get_pool(cr.dbname)
     obj_dossier = obj_pool.get(data['model'])
     data_dossier = obj_dossier.browse(cr,uid,data['ids'])
@@ -32,6 +43,7 @@ def _create_embassy_folder(self, cr, uid, data, context):
         folder_id =obj_pool.get('cci_missions.embassy_folder').create(cr, uid, {
                     'name': data.name,
                     'date': data.date,
+                    'site_id': site_id,
                     'partner_id': data.order_partner_id.id,
                     'destination_id': data.destination_id.id,
                     'invoice_note' : data.text_on_invoice
@@ -44,6 +56,12 @@ class create_embassy_folder(wizard.interface):
 
     states = {
         'init' : {
+            'actions' : [],
+            'result' : {'type' : 'form' ,   'arch' : preform,
+                    'fields' : prefields,
+                    'state' : [('go','Next')]}
+        },
+        'go' : {
             'actions' : [_create_embassy_folder],
             'result' : {'type' : 'form' ,   'arch' : form,
                     'fields' : fields,
