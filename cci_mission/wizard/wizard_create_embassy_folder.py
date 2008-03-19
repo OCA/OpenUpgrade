55c2e353055a23da79301e4243e2d9faddda8ac6
Mustufa Rangwala <mra@tinyerp.com>
2008-03-19 10:25:54 +0000

    - now wizard open embassy folder from wizard, - remove name from creation of embassy folder and put it on write method becaz it did not work becaz of sequence field

--- cci_mission/wizard/wizard_create_embassy_folder.py
+++ cci_mission/wizard/wizard_create_embassy_folder.py
@@ -41,18 +41,32 @@ def _create_embassy_folder(self, cr, uid, data, context):
             continue
         folder_create = folder_create + 1
         folder_id =obj_pool.get('cci_missions.embassy_folder').create(cr, uid, {
-                    'name': data.name,
                     'date': data.date,
                     'site_id': site_id,
                     'partner_id': data.order_partner_id.id,
                     'destination_id': data.destination_id.id,
                     'invoice_note' : data.text_on_invoice
             })
+        pooler.get_pool(cr.dbname).get('cci_missions.embassy_folder').write(cr,uid,folder_id,{'name' : data.name})
         list_folders.append(folder_id)
         obj_dossier.write(cr, uid,data.id, {'embassy_folder_id' : folder_id})
-    return {'folder_created' : str(folder_create) , 'folder_rejected' : str(folder_reject) , 'folder_rej_reason': folder_rej_reason }
+    return {'folder_created' : str(folder_create) , 'folder_rejected' : str(folder_reject) , 'folder_rej_reason': folder_rej_reason , 'folder_ids' : list_folders }
 
 class create_embassy_folder(wizard.interface):
+    def _open_folder(self, cr, uid, data, context):
+        mission_datas = pooler.get_pool(cr.dbname).get('cci_missions.embassy_folder').read(cr,uid,data['form']['folder_ids'],fields=['crm_case_id'])
+        crm_ids = ','.join([str(x['crm_case_id'][0]) for x in mission_datas if x['crm_case_id'] and x['crm_case_id'][0]])
+        val = {
+            'domain': "[('id','in', ["+crm_ids+"])]",
+            'name': 'Folder',
+            'view_type': 'form',
+            'view_mode': 'tree,form',
+            'res_model': 'cci_missions.embassy_folder',
+            'view_id': False,
+            'context' : "{}",
+            'type': 'ir.actions.act_window'
+            }
+        return val
 
     states = {
         'init' : {
@@ -65,8 +79,12 @@ class create_embassy_folder(wizard.interface):
             'actions' : [_create_embassy_folder],
             'result' : {'type' : 'form' ,   'arch' : form,
                     'fields' : fields,
-                    'state' : [('end','Ok')]}
+                    'state' : [('end','Ok'),('open','Open')]}
         },
+        'open': {
+            'actions': [],
+            'result': {'type':'action', 'action':_open_folder, 'state':'end'}
+        }
     }
 
 create_embassy_folder("cci_mission.create_embassy_folder")
