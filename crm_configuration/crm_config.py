a86bbc9caa0ff842ceacaa11a488c4d4418e8f6b
Dhara Shah <dsh@tinyerp.com>
2008-03-20 12:37:21 +0000

    made some changes for Bug Tracking

--- crm_configuration/crm_config.py
+++ crm_configuration/crm_config.py
@@ -5,11 +5,10 @@ from osv import fields,osv,orm
 import mx.DateTime
 import base64
 AVAILABLE_STATES = [
-    ('draft','Draft'),
-    ('unreviewed','Unreviewed'),
+    ('draft','Unreviewed'),
     ('open','Open'),
-    ('cancel', 'Cancel'),
-    ('done', 'Close'),
+    ('cancel', 'Refuse Bug'),
+    ('done', 'Done'),
     ('pending','Pending')
 ]
 class crm_case_category2(osv.osv):
@@ -35,10 +34,8 @@ class crm_case_stage(osv.osv):
 crm_case_stage()
 
 class crm_cases(osv.osv):
-    
-    
-    
-    _name = "crm.cases"
+     
+    _name = "crm.case"
     _inherit = "crm.case"
     def get_section(self, cr, uid, context={}):
          user = self.pool.get('crm.case.section').search(cr, uid, [('name', '=', 'Bug Tracking')])
@@ -46,7 +43,7 @@ class crm_cases(osv.osv):
     _columns = {
         'stage_id': fields.many2one ('crm.case.stage', 'Stage', domain="[('section_id','=',section_id)]"),
         'category2_id': fields.many2one('crm.case.category2','Category Name', domain="[('section_id','=',section_id)]"),
-        'duration': fields.time ('Duration', type=float),
+        'duration': fields.float('Duration', size=16),
         'note': fields.text('Note'),
         'partner_name': fields.char('Employee Name', size=64),
         'partner_name2': fields.char('Last Name', size=64),
@@ -56,59 +53,20 @@ class crm_cases(osv.osv):
          
         
                 }
-    _defaults = {
-             'section_id' :get_section,
-                 }
-    def menu_create1(self, cr, uid, ids, name, menu_parent_id=False, context={}):
-        menus = {}
-        menus[-1] = menu_parent_id
-        for section in self.browse(cr, uid, ids, context):
-            for (index, mname, mdomain, latest) in [
-                (0,'',"[('section_id','=',"+str(section.id)+")]", -1),
-                (1,'My ',"[('section_id','=',"+str(section.id)+"),('user_id','=',uid)]", 0),
-                (2,'My Unclosed ',"[('section_id','=',"+str(section.id)+"),('user_id','=',uid), ('state','<>','cancel'), ('state','<>','done')]", 1),
-                (3,'My Open ',"[('section_id','=',"+str(section.id)+"),('user_id','=',uid), ('state','=','open')]", 2),
-                (4,'My Pending ',"[('section_id','=',"+str(section.id)+"),('user_id','=',uid), ('state','=','pending')]", 2),
-                (5,'My Unreviewed ',"[('section_id','=',"+str(section.id)+"),('user_id','=',uid), ('state','=','draft'), ('state','=','unreviewed')]", 2),
-                (6,'All ',"[('section_id','=',"+str(section.id)+"),]", 0),
-                (7,'All Unclosed ',"[('section_id','=',"+str(section.id)+"),('state','<>','cancel'), ('state','<>','done')]", 6),
-                (8,'All Open ',"[('section_id','=',"+str(section.id)+"),('state','=','open')]", 7),
-                (9,'All Pending ',"[('section_id','=',"+str(section.id)+"),('state','=','pending')]", 7),
-                (10,'All Unreviewed ',"[('section_id','=',"+str(section.id)+"),('state','=','draft'), ('state','=','unreviewed')]", 7),
-                
-            ]:
-                view_mode = 'tree,form'
-                icon = 'STOCK_JUSTIFY_FILL'
-                if index==0:
-                    view_mode = 'form,tree'
-                    icon = 'STOCK_NEW'
-                menu_id=self.pool.get('ir.ui.menu').create(cr, uid, {
-                    'name': mname+name,
-                    'parent_id': menus[latest],
-                    'icon': icon
-                })
-                menus[index] = menu_id
-                action_id = self.pool.get('ir.actions.act_window').create(cr,uid, {
-                    'name': mname+name+' Cases',
-                    'res_model': 'crm.cases',
-                    'domain': mdomain,
-                    'view_type': 'form',
-                    'view_mode': view_mode,
-                })
-                self.pool.get('ir.values').create(cr, uid, {
-                    'name': 'Open Cases',
-                    'key2': 'tree_but_open',
-                    'model': 'ir.ui.menu',
-                    'res_id': menu_id,
-                    'value': 'ir.actions.act_window,%d'%action_id,
-                    'object': True
-                })
-        return True
-    #end def
+#    _defaults = {
+#             'section_id' :get_section,
+#                 }
+
     def case_unreview(self, cr, uid, ids, *args):
         cases = self.browse(cr, uid, ids)
         cases[0].state # to fill the browse record cache
         self.write(cr, uid, ids, {'state':'unreviewed', 'active':True})
         return True
-
+    
+    def case_draft(self, cr, uid, ids, *args):
+        cases = self.browse(cr, uid, ids)
+        cases[0].state # to fill the browse record cache
+        self.write(cr, uid, ids, {'state':'cancel', 'active':True})
+        self._action(cr,uid, cases, 'cancel')
+        return True
 crm_cases()
