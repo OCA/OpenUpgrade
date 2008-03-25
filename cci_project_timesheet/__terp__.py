c8b67ae4b151f94b56542e414541518dcc4fcdaa
Jay vora <jvo@tinyerp.com>
2008-03-25 12:43:37 +0000

    New module for auto entry from tasks to timesheet lines.

---
+++ cci_project_timesheet/__terp__.py
@@ -0,0 +1,16 @@
+{
+    "name" : "CCI Project Timesheet",
+    "version" : "1.0",
+    "author" : "Tiny",
+    "website" : "http://tinyerp.com",
+    "category" : "Generic Modules/CCI Project Timesheet",
+    "description": """
+        specific module for cci project which will use project module and hr_timesheet_sheet.
+    """,
+    "depends" : ["base","project"],
+    "init_xml" : [],
+    "demo_xml" : [],
+    "update_xml" : [],
+    "active": False,
+    "installable": True
+}
