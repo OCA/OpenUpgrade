fbc07f5b156a5a4defbe0b1c92c0019bf8422b7e
Dhara Shah <dsh@tinyerp.com>
2008-03-26 08:56:50 +0000

    added profile , version and revision ,complete last wizard for maintainer

--- base_translation/translation.py
+++ base_translation/translation.py
@@ -10,42 +10,43 @@ import pooler
 from wizard import SERVER,PORT
 s = xmlrpclib.Server("http://"+server+":"+str(PORT))
 
-def get_language(cr,uid,context,user=None,model=None):
+def get_version(cr, uid,context):
+    user = pooler.get_pool(cr.dbname).get('res.users').read(cr,uid,uid,['login'])['login']
+    return s.version_list(user)
+    
+def get_profile(cr,uid,context):
+    user = pooler.get_pool(cr.dbname).get('res.users').read(cr,uid,uid,['login'])['login']
+    return s.profile_list(user)
+
+def get_language(cr,uid,context,user=None,model=None,lang=None):
     if user:
         if user=='contributor':
-            file_list = s.get_publish_list()
+            list = s.get_lang_list()
         else:
             login = pooler.get_pool(cr.dbname).get('res.users').read(cr,uid,uid,['login'])['login']
-            if user=='maintainer_publish':
-                user_list = s.get_publish_list(login)
-                cr.execute("select distinct(lang) from ir_translation_contribution where state='accept'")
-                db_lang_list =map(lambda x: x[0],cr.fetchall())
-                file_list = filter(lambda x:x in user_list,db_lang_list)
-            else:
-                
-#                code should be optimise
-
-                file_list = s.get_contrib_list(login)
-                lang_list=[]
-                lang_dict = tools.get_languages()
-                for f in file_list:
-                    if f.find('AT')>0:
-                        splited_name = f.split('-')
-                        name = splited_name[1].split('_AT')[0]
-                        vn = splited_name[-1].replace('.csv','')
-                        lang_name = lang_dict.get(splited_name[0],splited_name[0])
-                        lang_list.append((f,lang_name+' by '+name+' V'+vn))
-                    else :
-                        f = f.replace('.csv','')
-                        lang_list.append((f, lang_dict.get(f,f)))
-                return lang_list
-        list = map(lambda x: x.replace('.csv',''),file_list)
-    else:
-        sql = "select distinct lang from %s"%model
+            list = s.get_lang_list(login)
+    elif model:
+        if model=='ir_translation_contrib':
+            sql = "select distinct lang from ir_translation_contribution where state='propose'"
+        else:
+            sql = "select distinct lang from ir_translation"
         cr.execute(sql)
         list = map(lambda x: x[0],cr.fetchall())
+    else :
+        sql = "select distinct lang from ir_translation_contribution where state='accept'"
+        cr.execute(sql)
+        list = map(lambda x: x[0],cr.fetchall())        
     lang_dict = tools.get_languages()
     return [(lang, lang_dict.get(lang, lang)) for lang in list]
+
+#
+#class ir_translation(osv.osv):
+#    _inherit = 'ir.translation'
+#    _columns = {
+#                'version':fields.char('Version',size=64),
+#                'profile':fields.char('profile',size=64),
+#            }
+#ir_translation()
 
 class ir_translation_contribution(osv.osv):
     _name = "ir.translation.contribution"
@@ -65,12 +66,8 @@ class ir_translation_contribution(osv.osv):
 #            so same contribution doesnt upload more than once
             'upload':fields.boolean('upload') 
                 }
-    
     _sql = ''    
-#    _sql = """
-#        create index ir_translation_ltn1 on ir_translation (lang,type,name);
-#        create index ir_translation_res_id1 on ir_translation (res_id);
-#    """
+
     def write(self, cr, uid, ids, vals, context=None):
         if self.read(cr,uid,ids,['upload']):
             vals['upload']=False
