72575e0b14d28dd99a23fa350806149d71f77524
qdp <>
2008-03-26 12:52:54 +0000

    removed CCI

--- project_timesheet/__terp__.py
+++ project_timesheet/__terp__.py
@@ -1,15 +1,15 @@
 {
-    "name" : "CCI Project Timesheet",
+    "name" : "Project Timesheet",
     "version" : "1.0",
     "author" : "Tiny",
     "website" : "http://tinyerp.com",
-    "category" : "Generic Modules/CCI Project Timesheet",
+    "category" : "Generic Modules/HR",
     "description": """
         This module lets you transfer the entries under tasks defined for Project Management to
-        the Timesheet line entries for particular date and particular user  with the effect of creating,editing and deleting either ways.
+        the Timesheet line entries for particular date and particular user  with the effect of creating, editing and deleting either ways.
 
     """,
-    "depends" : ["base","project"],
+    "depends" : ["base", "project", "hr_timesheet"],
     "init_xml" : [],
     "demo_xml" : [],
     "update_xml" : [],
