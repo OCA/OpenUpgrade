d04e05fb8d94faa6b9e92ed71f49a0c8647a83f6
Dhara Shah <dsh@tinyerp.com>
2008-03-26 09:38:08 +0000

    added profile , version and revision ,complete last wizard for maintainer

--- base_translation/wizard/wizard_upload_contribution.py
+++ base_translation/wizard/wizard_upload_contribution.py
@@ -4,10 +4,11 @@ import pooler
 import xmlrpclib
 import csv
 import re
-from base_translation.translation import get_language
+import random
+import base_translation.translation
 
 import config
-s = xmlrpclib.Server("http://"config.SERVER+":"+str(config.PORT))
+s = xmlrpclib.Server("http://"+config.SERVER+":"+str(config.PORT))
 
 view_form_end = """<?xml version="1.0"?>
     <form string="Information For Contribution">
@@ -26,7 +27,9 @@ view_form = """<?xml version="1.0"?>
         <label align="0.0" string="Choose a language to upload:" colspan="4"/>
         <field name="lang" colspan="4"/>
         <field name="email_id" colspan="4"/>
-        <field name="note" colspan="4"/>        
+        <field name="note" colspan="4"/>   
+        <field name="version" colspan="4"/>
+        <field name="profile" colspan="4"/>     
         <label align="0.0" string="Note that this operation may take a few minutes." colspan="4"/>
     </group>
 </form>"""
@@ -36,42 +39,52 @@ class wizard_upload_contrib(wizard.interface):
     def _upload_contrib(self, cr, uid, data, context):
         lang = data['form']['lang']
         email_id =data['form']['email_id']
+        version = data['form']['version']
+        profile = data['form']['profile']
         pattern = '^([a-zA-Z])[a-zA-Z0-9_\.]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,3}$'
         reg = re.compile(pattern)
         reg.search(email_id)
         if not reg.search(email_id):
             raise wizard.except_wizard('Error !', 'Your Email Id is not well-formed')
         ir_translation_contrib = pooler.get_pool(cr.dbname).get('ir.translation.contribution')
-        ids = ir_translation_contrib.search(cr,uid,[('lang','=',lang),('state','=','propose'),('upload','=',False)])
-        print ids        
+        sql = "select id from ir_translation_contribution where lang='%s' and state='propose' and upload=False"%lang
+        cr.execute(sql)
+        ids = map(lambda x:x[0],cr.fetchall())
+        print ids
         if ids:
             contrib =ir_translation_contrib.read(cr,uid,ids)
             title = ['type','name','res_id','src','value']
             content = map(lambda x:[x['type'],x['name'],x['res_id'],x['src'].decode('utf8').encode('utf8'),x['value'].decode('utf8').encode('utf8')],contrib)
-            file_list = s.get_contrib_list()
-            n_file = filter(lambda x: not x.find(lang),file_list)
             email_id = email_id.replace('@','_AT_')
             email_id = email_id.replace('.','_DOT_')
-            filename = lang+'-'+email_id+'-'+str(len(n_file)+1)+'.csv'
+            filename = lang+'-'+email_id+'-'+str(random.randint(0,100))+'.csv'
             content.insert(0,title)
-            for id in ids:
-                ir_translation_contrib.write(cr,uid,id,{'upload':True})
             try :
-                s.publish_contrib(content,filename)
+                s.publish_contrib(lang,version,profile,filename,content)
             except Exception,e:
                 print e
                 raise wizard.except_wizard('Error !',"server is not properly configuraed")
+            sql = "UPDATE ir_translation_contribution SET upload=True where id in %s"%str(tuple(ids))
+            cr.execute(sql)
         ids = ir_translation_contrib.search(cr,uid,[('lang','=',lang)])
         contrib =ir_translation_contrib.read(cr,uid,ids)
         return {'total':len(ids),'draft':len(filter(lambda x:x['state'] =='draft',contrib)),'propose':len(filter(lambda x:x['state'] =='propose',contrib))}
 
     def _get_language(sel, cr, uid, context):
-        return get_language(cr,uid,context,model='ir_translation_contribution')
+        return base_translation.translation.get_language(cr,uid,context,model='ir_translation_contribution')
+    
+    def _get_version(self, cr, uid,context):
+        return base_translation.translation.get_version(cr,uid,context)
+    
+    def _get_profile(self,cr,uid,context):
+        return base_translation.translation.get_profile(cr,uid,context)
 
     fields_form = {
         'lang': {'string':'Language', 'type':'selection', 'selection':_get_language,'required':True},
         'note': {'string':'Note','type':'text'},
         'email_id':{'string':"Contributor's Email-ID",'type':'char','size':64,'required':True},
+        'version':{'string':"Version",'type':'selection', 'selection':_get_version,'required':True},
+        'profile':{'string':"Profile",'type':'selection', 'selection':_get_profile,'required':True},        
     }
     
     fields_form_end = {
