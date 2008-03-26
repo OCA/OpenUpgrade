6c7d97fb993cadbf6651976cb0f4519a6c62fe20
qdp <>
2008-03-26 10:12:27 +0000

    *corrected issues and bugs *views improvements

--- event/event.py
+++ event/event.py
@@ -29,6 +29,14 @@
 from osv import fields, osv
 import time
 
+class crm_case_log(osv.osv):
+	_inherit = 'crm.case.log'
+	_description = 'crm.case.log'
+	_defaults = {
+		'user_id': lambda self,cr,uid,context: uid,
+	}
+crm_case_log()
+
 class one2many_mod_task(fields.one2many):
 	def get(self, cr, obj, ids, name, user=None, offset=0, context=None, values=None):
 		if not context:
@@ -123,7 +131,7 @@ class event_registration(osv.osv):
 		self.write(cr, uid, ids, {'state':'open',})
 		return True
 
-	def button_reg_done(self, cr, uid, ids, *args):
+	def button_reg_close(self, cr, uid, ids, *args):
 		self.write(cr, uid, ids, {'state':'done',})
 		return True
 
@@ -230,15 +238,15 @@ class report_event_registration(osv.osv):
 				c.name as name,
 				e.date_begin as date_begin,
 				e.date_end as date_end,
-				(SELECT count(*) FROM event_registration x , crm_case c  WHERE x.case_id=c.id and c.section_id=e.section_id and state in ('draft')) as draft_state,
-				(SELECT count(*) FROM event_registration x , crm_case c  WHERE x.case_id=c.id and c.section_id=e.section_id and state in ('open')) as confirm_state,
+				(SELECT sum(nb_register) FROM event_registration x , crm_case c  WHERE x.case_id=c.id and c.section_id=e.section_id and state in ('draft')) as draft_state,
+				(SELECT sum(nb_register) FROM event_registration x , crm_case c  WHERE x.case_id=c.id and c.section_id=e.section_id and state in ('open')) as confirm_state,
 				e.register_max as register_max
 				from
 				event_event e,crm_case_section c
 				where
 				e.section_id=c.id
-
 				)""")
+
 report_event_registration()
 
 class report_event_type_registration(osv.osv):
@@ -257,8 +265,8 @@ class report_event_type_registration(osv.osv):
 				select
 				count(t.id) as id,
 				t.name as name,
-				(select count(*) from event_registration r , crm_case c , event_event e  where c.section_id=e.section_id and r.case_id=c.id and c.state='draft' and e.type=t.id ) as draft_state ,
-				(select count(*) from event_registration r , crm_case c , event_event e  where c.section_id=e.section_id and r.case_id=c.id and c.state='open' and e.type=t.id ) as  confirm_state,
+				(select sum(nb_register) from event_registration r , crm_case c , event_event e  where c.section_id=e.section_id and r.case_id=c.id and c.state='draft' and e.type=t.id ) as draft_state ,
+				(select sum(nb_register) from event_registration r , crm_case c , event_event e  where c.section_id=e.section_id and r.case_id=c.id and c.state='open' and e.type=t.id ) as  confirm_state,
 				count(t.id) as nbevent
 				from
 					event_event e
