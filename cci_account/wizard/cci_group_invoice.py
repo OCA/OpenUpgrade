9745a16a01ecc4ace5e40dd5902826fdacaa4dd3
Mustufa Rangwala <mra@tinyerp.com>
2008-03-28 11:49:24 +0000

    * enhance code : 	- now it create invocie with invoice line in grouping by partner 	- open list of invoices

--- cci_account/wizard/cci_group_invoice.py
+++ cci_account/wizard/cci_group_invoice.py
@@ -40,13 +40,13 @@ form = """<?xml version="1.0"?>
 fields = {
       'nbr_invoice': {'string':'Invoice Grouped', 'type':'char', 'readonly':True},
           }
-
 def _group_invoice(self, cr, uid, data, context):
     pool_obj=pooler.get_pool(cr.dbname)
     obj_inv=pool_obj.get('account.invoice')
     state = 'draft'
-    cr.execute("select partner_id,id,date_invoice from account_invoice where state=%s group by partner_id,id,date_invoice", ('draft',))
+    cr.execute("select partner_id,date_invoice,id from account_invoice where state=%s and type in ('out_invoice','in_invoice') group by partner_id,id,date_invoice", ('draft',))
     a=cr.fetchall()
+    a.sort()
     ls=([i[0] for i in a])
     ls=list(set([i for i in ls]))
     ls.sort()
@@ -61,19 +61,19 @@ def _group_invoice(self, cr, uid, data, context):
         for t in a:
             if id==t[0]:
                 if flag:
-                    in_dict['date']=t[2]
-                    in_lst.append(t[1])
+                    in_dict['date']=t[1]
+                    in_lst.append(t[2])
                     flag=0
                 else:
-                    if in_dict['date']==t[2]:
-                        in_lst.append(t[1])
+                    if in_dict['date']==t[1]:
+                        in_lst.append(t[2])
                     else:
                         in_dict['inv_id']=in_lst
                         t_in_dict=in_dict.copy()
                         in_lst2.append(t_in_dict)
-                        in_dict['date']=t[2]
+                        in_dict['date']=t[1]
                         in_lst=[]
-                        in_lst.append(t[1])
+                        in_lst.append(t[2])
             else:
                 if id>t[0]:
                     continue
@@ -86,14 +86,89 @@ def _group_invoice(self, cr, uid, data, context):
     in_lst2.append(in_dict)
     data['invoices']=in_lst2
     lst.append(data)
-    print lst
-    return {'nbr_invoice' : str(10)}
+    part_count = 0
+    list_invoice = []
+    for l in lst:
+        part_count = part_count + 1
+        line_list =[]
+        count = 0
+        for data in l['invoices']:
+            id_note=pool_obj.get('account.invoice.line').create(cr,uid,{'name':'customer ref','state':'text','sequence':count}) # a new line of type 'note' with all the customer reference (if there is/are one/several on some invoices of this bloc)
+            count=count+1
+            line_list.append(id_note)
+            data_inv=obj_inv.browse(cr,uid,data['inv_id'])
+            notes = ''
+            for m in data_inv:
+                for line in m.invoice_line:
+                    #all the lines copied and ordered by name (the new name)
+                    if m.name:
+                        name = m.name +' '+ line.name
+                    else:
+                        name = line.name
+                    #pool_obj.get('account.invoice.line').write(cr,uid,line.id,{'name':name,'sequence':count})
+                    inv_line = pool_obj.get('account.invoice.line').create(cr, uid, {
+                    'name': name,
+                    'account_id':line.account_id.id,
+                    'price_unit': line.price_unit,
+                    'quantity': line.quantity,
+                    'discount': False,
+                    'uos_id': line.uos_id.id,
+                    'product_id':line.product_id.id,
+                    'invoice_line_tax_id': [(6,0,line.invoice_line_tax_id)],
+                    'note':False,
+                        })
+                    count=count+1
+                    line_list.append(inv_line)
+                    if m.comment:
+                        notes = (notes + m.comment)
+                obj_inv.write(cr,uid,m.id,{'state':'cancel'})
+            id_note1=pool_obj.get('account.invoice.line').create(cr,uid,{'name':notes,'state':'text','sequence':count})# a new line of type 'note' with all the old invoice note
+            count=count+1
+            line_list.append(id_note1)
+            id_linee=pool_obj.get('account.invoice.line').create(cr,uid,{'state':'line','sequence':count}) #a new line of type 'line'
+            count=count+1
+            line_list.append(id_linee)
+            id_stotal=pool_obj.get('account.invoice.line').create(cr,uid,{'name':'Subtotal','state':'subtotal','sequence':count})#a new line of type 'subtotal'
+            count=count+1
+            line_list.append(id_stotal)
+        inv = {
+                'name': 'Group invocie',
+                'origin': 'Group invocie',
+                'type': 'out_invoice',
+                'reference': False,
+                'account_id': m.account_id.id,
+                'partner_id': m.partner_id.id,
+                'address_invoice_id':m.address_invoice_id.id,
+                'address_contact_id':m.address_contact_id.id,
+                'invoice_line': [(6,0,line_list)],
+                'currency_id' :m.currency_id.id,# 1
+                'comment': "",
+                'payment_term':m.payment_term.id,
+            }
+        inv_id = obj_inv.create(cr, uid, inv)
+        list_invoice.append(inv_id)
+    return {'nbr_invoice' : str(part_count),'invoice_ids':list_invoice}
 
 class group_invoice(wizard.interface):
+    def _list_invoice(self, cr, uid, data, context):
+        return {
+            'domain': "[('id','in', ["+','.join(map(str,data['form']['invoice_ids']))+"])]",
+            'name': 'Invoices',
+            'view_type': 'form',
+            'view_mode': 'tree,form',
+            'res_model': 'account.invoice',
+            'views': False,
+            'context': "{'type':'out_invoice'}",
+            'type': 'ir.actions.act_window'
+        }
     states = {
         'init' : {
                'actions' : [_group_invoice],
-               'result': {'type': 'form', 'arch': form, 'fields': fields, 'state':[('end','Ok')]}
+               'result': {'type': 'form', 'arch': form, 'fields': fields, 'state':[('end','Ok'),('open','Open')]}
             },
+        'open': {
+            'actions': [],
+            'result': {'type':'action', 'action':_list_invoice, 'state':'end'}
+            }
     }
 group_invoice("account.group_invoice")
