2e22c7479810c0c316299afcb71073221ddd0c5e
Mustufa Rangwala <mra@tinyerp.com>
2008-03-28 11:56:22 +0000

    add dependace to account_invoice_layout

--- cci_account/__terp__.py
+++ cci_account/__terp__.py
@@ -7,7 +7,7 @@
     "description": """
         specific module for cci project which will use account module (Reports).
     """,
-    "depends" : ["base","account"],
+    "depends" : ["base","account","account_invoice_layout"],
     "init_xml" : [],
     "demo_xml" : [],
     "update_xml" : ["cci_account_wizard.xml","cci_account_view.xml","cci_account_report.xml"],
