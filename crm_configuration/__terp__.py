255e8b732cfc0806da0b60efa581ad54a9b785c9
sso <>
2008-04-08 03:05:01 +0000

    The demo and view files of all the cases are separated.

--- crm_configuration/__terp__.py
+++ crm_configuration/__terp__.py
@@ -10,8 +10,8 @@
                    identification, prioritization, assignment, resolution and notification.""",
     "depends" : ["base","crm"],
     "init_xml" : [],
-    "demo_xml" : [],
-    "update_xml" : ["crm_view.xml","crm_demo.xml","crm_data.xml"],
+    "demo_xml" : ["crm_bugs_demo.xml","crm_jobs_demo.xml","crm_lead_demo.xml","crm_meeting_demo.xml"],
+    "update_xml" : ["crm_bugs_view.xml","crm_jobs_view.xml","crm_lead_view.xml","crm_meeting_view.xml"],
     "active": False,
     "installable": True
 }
