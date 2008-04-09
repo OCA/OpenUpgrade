2f71c1253612012e82c92828f2cd3b190282cefa
Fabien Pinckaers <fp@tinyerp.com>
2008-04-09 12:58:15 +0000

    Adding vertical module for associations

---
+++ association_vertical/__terp__.py
@@ -0,0 +1,13 @@
+{
+		"name" : "Association Verticalisation",
+		"version" : "Association Verticalisation",
+		"author" : "Tiny",
+		"website" : "http://tinyerp.com",
+		"category" : "Vertical Modules/Parametrization",
+		"description": """Simplification of the interface for associations.""",
+		"depends" : ["hr","crm","event"],
+		"init_xml" : [ ],
+		"demo_xml" : [ ],
+		"update_xml" : ["aso_data.xml"],
+		"installable": True
+} 
