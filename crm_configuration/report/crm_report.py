1ce9453de4f63213590edb7aeb9050f1c85320b5
sso <>
2008-04-11 11:06:55 +0000

    Included the objects for the reports.

--- crm_configuration/report/crm_report.py
+++ crm_configuration/report/crm_report.py
@@ -12,6 +12,7 @@ class report_crm_case_section_categ2(osv.osv):
     _description = "Cases by section and category2"
     _auto = False
     _columns = {
+        'name': fields.date('Month', readonly=True),
         'user_id':fields.many2one('res.users', 'User', readonly=True),
         'section_id':fields.many2one('crm.case.section', 'Section', readonly=True),
         'category2_id':fields.many2one('crm.case.category2', 'Type', readonly=True),
@@ -27,6 +28,7 @@ class report_crm_case_section_categ2(osv.osv):
               create or replace view report_crm_case_section_categ2 as (
                 select
                     min(c.id) as id,
+                    substring(c.create_date for 7)||'-01' as name,
                     c.user_id,
                     c.state,
                     c.category2_id,
@@ -37,6 +39,115 @@ class report_crm_case_section_categ2(osv.osv):
                 from
                     crm_case c
                 where c.category2_id is not null
-                group by c.user_id, c.state, c.stage_id, c.category2_id, c.section_id)""")
+                group by substring(c.create_date for 7), c.user_id, c.state, c.stage_id, c.category2_id, c.section_id)""")
 
 report_crm_case_section_categ2()
+
+class report_crm_case_section_stage(osv.osv):
+    _name = "report.crm.case.section.stage"
+    _description = "Cases by section and stage"
+    _auto = False
+    _columns = {
+        'name': fields.date('Month', readonly=True),
+        'user_id':fields.many2one('res.users', 'User', readonly=True),
+        'section_id':fields.many2one('crm.case.section', 'Section', readonly=True),
+        'categ_id':fields.many2one('crm.case.categ', 'Category', readonly=True),
+        'stage_id':fields.many2one('crm.case.stage', 'Stage', readonly=True),
+        'nbr': fields.integer('# of Cases', readonly=True),
+        'state': fields.selection(AVAILABLE_STATES, 'State', size=16, readonly=True),        
+        'delay_close': fields.char('Delay Close', size=20, readonly=True),
+                }
+    _order = 'stage_id, section_id'
+    
+    def init(self, cr):
+        cr.execute("""
+              create or replace view report_crm_case_section_stage as (
+                select
+                    min(c.id) as id,
+                    substring(c.create_date for 7)||'-01' as name,
+                    c.user_id,
+                    c.state,
+                    c.stage_id,
+                    c.section_id,
+                    count(*) as nbr,
+                    to_char(avg(date_closed-c.create_date), 'DD"d" HH24:MI:SS') as delay_close
+                from
+                    crm_case c
+                where c.stage_id is not null
+                group by substring(c.create_date for 7), c.user_id, c.state, c.stage_id, c.section_id)""")
+
+report_crm_case_section_stage()
+
+class report_crm_case_section_categ_stage(osv.osv):
+    _name = "report.crm.case.section.categ.stage"
+    _description = "Cases by section, Category and stage"
+    _auto = False
+    _columns = {
+        'name': fields.date('Month', readonly=True),
+        'user_id':fields.many2one('res.users', 'User', readonly=True),
+        'categ_id':fields.many2one('crm.case.categ', 'Category', readonly=True),
+        'section_id':fields.many2one('crm.case.section', 'Section', readonly=True),
+        'stage_id':fields.many2one('crm.case.stage', 'Stage', readonly=True),
+        'nbr': fields.integer('# of Cases', readonly=True),
+        'state': fields.selection(AVAILABLE_STATES, 'State', size=16, readonly=True),        
+        'delay_close': fields.char('Delay Close', size=20, readonly=True),
+                }
+    _order = 'stage_id, section_id, categ_id'
+    
+    def init(self, cr):
+        cr.execute("""
+              create or replace view report_crm_case_section_categ_stage as (
+                select
+                    min(c.id) as id,
+                    substring(c.create_date for 7)||'-01' as name,
+                    c.user_id,
+                    c.categ_id,
+                    c.state,
+                    c.stage_id,
+                    c.section_id,
+                    count(*) as nbr,
+                    to_char(avg(date_closed-c.create_date), 'DD"d" HH24:MI:SS') as delay_close
+                from
+                    crm_case c
+                where c.categ_id is not null AND c.stage_id is not null
+                group by substring(c.create_date for 7), c.user_id, c.categ_id, c.state, c.stage_id, c.section_id)""")
+
+report_crm_case_section_categ_stage()
+
+class report_crm_case_section_categ_categ2(osv.osv):
+    _name = "report.crm.case.section.categ.categ2"
+    _description = "Cases by section, Category and Category2"
+    _auto = False
+    _columns = {
+        'name': fields.date('Month', readonly=True),
+        'user_id':fields.many2one('res.users', 'User', readonly=True),
+        'categ_id':fields.many2one('crm.case.categ', 'Category', readonly=True),
+        'category2_id':fields.many2one('crm.case.category2', 'Type', readonly=True),
+        'section_id':fields.many2one('crm.case.section', 'Section', readonly=True),
+        'stage_id':fields.many2one('crm.case.stage', 'Stage', readonly=True),
+        'nbr': fields.integer('# of Cases', readonly=True),
+        'state': fields.selection(AVAILABLE_STATES, 'State', size=16, readonly=True),        
+        'delay_close': fields.char('Delay Close', size=20, readonly=True),
+                }
+    _order = 'section_id, categ_id, category2_id'
+    
+    def init(self, cr):
+        cr.execute("""
+              create or replace view report_crm_case_section_categ_categ2 as (
+                select
+                    min(c.id) as id,
+                    substring(c.create_date for 7)||'-01' as name,
+                    c.user_id,
+                    c.categ_id,
+                    c.category2_id,
+                    c.state,
+                    c.stage_id,
+                    c.section_id,
+                    count(*) as nbr,
+                    to_char(avg(date_closed-c.create_date), 'DD"d" HH24:MI:SS') as delay_close
+                from
+                    crm_case c
+                where c.categ_id is not null AND c.category2_id is not null
+                group by substring(c.create_date for 7), c.user_id, c.categ_id, c.category2_id, c.state, c.stage_id, c.section_id)""")
+
+report_crm_case_section_categ_categ2()
