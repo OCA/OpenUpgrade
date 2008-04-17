7fc154e1fb5980aadf950bda3987ae763df17df1
Mustufa Rangwala <mra@tinyerp.com>
2008-04-17 07:15:30 +0000

    * override name_search method on account.move.line 	- if there is no partner on statement lines then it wll display 	  all partner's move.line on reconcile..

--- cci_account/cci_account.py
+++ cci_account/cci_account.py
@@ -9,3 +9,30 @@ class cci_account_message(osv.osv):
     }
 cci_account_message()
 
+class account_move_line(osv.osv):
+    _inherit = "account.move.line"
+    _description = "account.move.line"
+
+    def name_search(self, cr, user, name='', args=None, operator='ilike', context=None, limit=80):
+        if not args:
+            args=[]
+        if not context:
+            context={}
+        # will check if the partner exists in statement lines if not then display all partner's account.move.line
+        if not context['partner_id']:
+            args.pop(0)
+            if not context['account_id']:
+                args=[]
+                args.append(['reconcile_id', '=', False])
+                args.append(['state', '=', 'valid'])
+        else:
+            if not context['account_id']:
+                args.pop(2)
+
+        args=args[:]
+        if name:
+            args += [(self._rec_name,operator,name)]
+        ids = self.search(cr, user, args, limit=limit, context=context)
+        return self.name_get(cr, user, ids, context)
+
+account_move_line()
