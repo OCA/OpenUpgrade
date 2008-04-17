693f0b1bcf930975ea17d64b14f514880858b788
Jay vora <jvo@tinyerp.com>
2008-04-17 10:48:05 +0000

    Added 'Save as' option and modified as per the requirement.put the static list.

--- cci_account/wizard/cci_account_vat_declaration.py
+++ cci_account/wizard/cci_account_vat_declaration.py
@@ -2,46 +2,45 @@ import wizard
 import time
 import datetime
 import pooler
-import tools
+import base64
 
 form = """<?xml version="1.0"?>
 <form string="Notification">
      <separator string="XML Flie has been Created."  colspan="4"/>
      <field name="msg" colspan="4" nolabel="1"/>
+     <field name="file_save" />
 </form>"""
 
 fields = {
     'msg': {'string':'File created', 'type':'text', 'size':'100','readonly':True},
+    'file_save':{'string': 'Save File',
+        'type': 'binary',
+        'readonly': True,},
 }
 
 class wizard_vat_declaration(wizard.interface):
 
     def _create_xml(self, cr, uid, data, context):
 
-        datas=[]
-        cr.execute('select count(*) from account_tax_code')
-        rows=cr.fetchone()
-
-        if rows[0]==0:
-            raise wizard.except_wizard('Data Insufficient!','No Records found in Tax Codes.')
+        list_of_tags=['00','01','02','03','45','46','47','48','49','54','55','56','57','59','61','62','63','64','71','81','82','83','84','85','86','87','91']
 
         tax_ids=pooler.get_pool(cr.dbname).get('account.tax.code').search(cr,uid,[])
         tax_info=pooler.get_pool(cr.dbname).get('account.tax.code').read(cr,uid,tax_ids,['code','sum_period'])
 
-        root_path=tools.config.options['root_path']
-        file=open(root_path+'/vat_declaration.xml', 'w')
-        file.write('<?xml version="1.0"?>\n<header>\n\tWelcome To TinyERP.Think Big, Use TinyERP.\n</header>\n<DATA>\n\t<DATA_ELEM>')
+        data_of_file='<?xml version="1.0"?>\n<header>\n\tWelcome To TinyERP.Think Big, Use TinyERP.\n</header>\n<DATA>\n\t<DATA_ELEM>'
 
         for item in tax_info:
             if item['code']:
                 if item['code'] == '71-72':
-                    item['code']=71
-                file.write('\n\t\t<D'+str(item['code']) +'>' + str(item['sum_period'])+  '</D'+str(item['code']) +'>')
+                    item['code']='71'
+                if item['code'] in list_of_tags:
+                    data_of_file +='\n\t\t<D'+str(item['code']) +'>' + str(item['sum_period'])+  '</D'+str(item['code']) +'>'
 
-        file.write('\n\t</DATA_ELEM>\n</DATA>')
-        file.close()
+        data_of_file +='\n\t</DATA_ELEM>\n</DATA>'
+        data['form']['msg']='Save the File with '".xml"' extension.'
+        data['form']['file_save']=base64.encodestring(data_of_file)
+        return data['form']
 
-        return {'msg':'vat_declaration.xml file has been created at '+root_path+'.' }
 
     states = {
         'init': {
