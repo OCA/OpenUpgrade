6e7c72129fd1cadbf109fb280d8cb6088e216f32
Jay vora <jvo@tinyerp.com>
2008-04-18 08:39:13 +0000

    Changes Header info and displayed country code instead of name.

--- cci_account/wizard/cci_partner_vat_listing.py
+++ cci_account/wizard/cci_partner_vat_listing.py
@@ -9,10 +9,13 @@ form = """<?xml version="1.0"?>
     <label string="This wizard will create an XML file for Vat details and total invoiced amounts per human entity(Partner)."  colspan="4"/>
     <newline/>
     <field name="fyear" />
+    <newline/>
+    <field name="mand_id" />
 </form>"""
 
 fields = {
     'fyear': {'string': 'Fiscal Year', 'type': 'many2one', 'relation': 'account.fiscalyear', 'required': True},
+    'mand_id':{'string':'MandatarieId','type':'char','size':'30','required': True},
    }
 msg_form = """<?xml version="1.0"?>
 <form string="Notification">
@@ -33,6 +36,11 @@ class wizard_vat(wizard.interface):
     def _create_xml(self, cr, uid, data, context):
 
         datas=[]
+        obj_company=pooler.get_pool(cr.dbname).get('res.company').browse(cr,uid,1)
+        vat_company=obj_company.partner_id.vat
+        if not vat_company:
+            raise wizard.except_wizard('Data Insufficient','No VAT Number Associated with Main Company!')
+
         p_id_list=pooler.get_pool(cr.dbname).get('res.partner').search(cr,uid,[('vat','!=',False)])
 
         if not p_id_list:
@@ -51,7 +59,7 @@ class wizard_vat(wizard.interface):
             for ads in obj_partner.address:
                 if ads.type=='default':
                     if ads.country_id:
-                        record.append(ads.country_id.name)
+                        record.append(ads.country_id.code)
                     else:
                         record.append('None')
             cr.execute('select a.type,sum(debit)-sum(credit) from account_move_line l left join account_account a on (l.account_id=a.id) where a.type in ('"'income'"','"'tax'"') and l.partner_id=%d and l.date between %s group by a.type'%(p_id,period))
@@ -71,14 +79,13 @@ class wizard_vat(wizard.interface):
                 for item in line_info:
                     record.append(item[1])
             datas.append(record)
-
-        data_file='<?xml version="1.0"?>\n<header>\n\tWelcome To TinyERP.Think Big, Use TinyERP.\n</header>\n'
+        sender_date=time.strftime('%Y-%m-%d')
+        data_file='<?xml version="1.0"?>\n<VatList SenderId="'+ str(vat_company) + '" MandataireId="'+ data['form']['mand_id'] + '" SenderDate="'+ str(sender_date)+'" />\n'
 
         seq=0
         for line in datas:
             seq +=1
             data_file +='<ClientList SequenceNum="'+str(seq)+'">\n\t<CompanyInfo>\n\t\t<VATNum>'+line[0] +'</VATNum>\n\t\t<Country>'+line[1] +'</Country>\n\t</CompanyInfo>\n\t<Amount>'+str(line[2]) +'</Amount>\n\t<TurnOver>'+str(line[3]) +'</TurnOver>\n</ClientList>\n'
-
         data['form']['msg']='Save the File with '".xml"' extension.'
         data['form']['file_save']=base64.encodestring(data_file)
         return data['form']
