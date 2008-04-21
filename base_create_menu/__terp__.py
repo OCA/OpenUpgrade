52bc047689d84093c2823e5cd95d7467ed34822f
Amit Mendapara <ame@tinyerp.com>
2008-04-21 07:22:06 +0000

    * added `base_create_menu` module

---
+++ base_create_menu/__terp__.py
@@ -0,0 +1,20 @@
+{
+    "name" : "Menu Wizard ",
+    "version" : "1.0",
+    "depends" : ["base"],
+    "author" : "Tiny",
+    "description": """
+    """,
+    "website" : "http://tinyerp.com",
+    "category" : "Generic Modules/Base",
+    "init_xml" : [
+    ],
+    "demo_xml" : [
+
+    ],
+    "update_xml" : [
+                    "menu_wizard_view.xml"
+                    ],
+    "active": True,
+    "installable": True
+}
