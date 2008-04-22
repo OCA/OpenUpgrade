ec05376da43313d23c76679ddc91bb76ac2bb9db
Mantavya Gajjar <mga@tinyerp.com>
2008-04-22 07:44:04 +0000

    Implement _set_operator that will send the operator list according to the field type

--- base_report_creator/wizard/wiz_set_filter_fields.py
+++ base_report_creator/wizard/wiz_set_filter_fields.py
@@ -59,19 +59,8 @@ set_value_form = """<?xml version="1.0"?>
 mapping_fields = {'$': 'End With', 'not in': 'Not Contains', '<>': 'Not Equals', 'is': 'Is Empty', 'in': 'Contains', '>': 'Bigger', '=': 'Equals', '<': 'Smaller', 'is not': 'Is Not Empty', '^': 'Start With'}
 
 set_value_fields = {
-					'field_id':{'type':'many2one','relation':'ir.model.fields','string':'Field Name','required':True,'readonly':True},
-					'operator':{'type':'selection','selection':[
-											 ('=','Equals'),
-											 ('in','Contains'),
-											 ('<>','Not Equals'),
-											 ('not in','Not Contains'),
-											 ('^','Start With'),
-											 ('$','End With'),
-											 ('is','Is Empty'),
-											 ('is not','Is Not Empty'),
-											 ('<','Smaller'),
-											 ('>','Bigger')
-											 ],'string':'Operator'},
+					'field_id':{'type':'many2one', 'relation':'ir.model.fields','string':'Field Name','required':True,'readonly':True},
+					'operator':{'type':'selection','selection':[],'string':'Operator'},
 					'value':{'type':'char','string':'Values','size':256},
 					'condition' : {'type':'selection','string':'Condition', 'selection':[('and','AND'),('or','OR')]}
 				}
@@ -81,12 +70,6 @@ def _set_field_domain(self,cr,uid,data,context):
 	this_data = this_pooler.read(cr,uid,data.get('ids'),['model_ids'],context)[0]
 	select_field_fields['field_id']['domain'] = [('model_id','in',this_data.get('model_ids')),('ttype','<>','many2many'),('ttype','<>','one2many')] 
 	return {'field_id':False}
-
-#def _set_field_value(self, cr, uid, data, context):
-#	field_id = data['form']['field_id']
-#	
-#	return {}
-#
 
 def set_field_operator(self,field_name,field_type,search_operator,search_value):
 		field_search = [field_name,search_operator,search_value]		
@@ -107,22 +90,19 @@ def set_field_operator(self,field_name,field_type,search_operator,search_value):
 				field_search[2] = "("+','.join([str(x) for x in search_value])+")"
 			else:
 				field_search[1] = 'ilike'
-				field_search[2] = "'%"+search_value+"%'"
-			#end if field_type=='one2many':
+				field_search[2] = "'%"+str(search_value)+"%'"
 		elif search_operator == 'not in':
 			if field_type=='many2one':
 				field_search[2] = "("+','.join([str(x) for x in search_value])+")"
 			else:
 				field_search[1] = 'not ilike'
-				field_search[2] = "'%"+search_value+"%'"
-			#end if field_type=='one2many':
+				field_search[2] = "'%"+str(search_value)+"%'"
 		elif search_operator == '^':
 			if field_type in char_type:
 				field_search[1]='~'
-				field_search[2]="'"+search_operator+search_value+"'"
+				field_search[2]="'"+str(search_operator)+str(search_value)+"'"
 			else:
 				return False
-			#end if field_type in char_type:
 		elif search_operator == '$':
 			if field_type in char_type:
 				field_search[1]='~'
@@ -131,13 +111,12 @@ def set_field_operator(self,field_name,field_type,search_operator,search_value):
 				return False
 			#end if field_type in char_type:
 		elif search_operator in ('is','is not'):
-			field_search[2]=False
+			field_search[2] = 'null'
 		elif search_operator in ('<','>'):
 			if field_type in date_type:
 				field_search[2] = "'"+field_search[2]+"'"
 			elif field_type not in int_type:
 				return False
-			#end if field_type not in int_type:
 		return field_search
 def _set_filter_value(self, cr, uid, data, context):
 	form_data = data['form']
@@ -149,7 +128,6 @@ def _set_filter_value(self, cr, uid, data, context):
 	model_pool = pooler.get_pool(cr.dbname).get(model_name)
 	table_name = model_pool._table
 	model_name = model_pool._description
-	
 	if field_type:
 		if field_type == 'many2many' and value_data and len(value_data):
 			fields_list = set_field_operator(self,table_name+"."+field_data['name'],field_data['ttype'],form_data['operator'],value_data[0][2])
@@ -158,11 +136,12 @@ def _set_filter_value(self, cr, uid, data, context):
 			
 		if fields_list and value_data:
 			create_dict = {
-						   'name':model_name + "/" +field_data['field_description'] +" "+ mapping_fields[form_data['operator']] + " " + fields_list[2],
+						   'name':model_name + "/" +field_data['field_description'] +" "+ mapping_fields[form_data['operator']] + " " + str(fields_list[2]),
 						   'expression':' '.join(fields_list),
 						   'report_id':data['id'],
 						   'condition' : form_data['condition']
 						   }
+			
 			pooler.get_pool(cr.dbname).get('base_report_creator.report.filter').create(cr,uid,create_dict)
 		#end if field_type == 'many2many' and value_data and len(value_data):
 #		pooler.get_pool(cr.dbname).get('custom.report.filter').create(cr,uid,form_data)
@@ -191,6 +170,39 @@ def _set_form_value(self, cr, uid, data, context):
 	ret_dict={'field_id':field_id,'operator':'=', 'condition':'and','value':False}	
 	return ret_dict
 
+def _set_operator(self, cr, uid, data, context):
+	field = pooler.get_pool(cr.dbname).get('ir.model.fields').browse(cr, uid, data['form']['field_id'])
+	print field.ttype
+	operator = set_value_fields['operator']['selection']
+	while operator: 
+		operator.pop(operator.__len__()-1)
+		
+	if field.ttype == 'many2one':
+		operator.append(('in','Equals'))
+		operator.append(('in','Contains'))
+		operator.append(('not in','Not Contains'))
+		operator.append(('is','Is Empty'))
+		operator.append(('is not','Is Not Empty'))
+	elif field.ttype in ('char', 'text'):
+		operator.append(('=','Equals'))
+		operator.append(('in','Contains'))
+		operator.append(('<>','Not Equals'))
+		operator.append(('not in','Not Contains'))
+		operator.append(('^','Start With'))
+		operator.append(('$','End With'))
+		operator.append(('is','Is Empty'))
+		operator.append(('is not','Is Not Empty'))
+	elif field.ttype in ('date', 'datetime', 'integer', 'float'):
+		operator.append(('=','Equals'))
+		operator.append(('<>','Not Equals'))
+		operator.append(('is','Is Empty'))
+		operator.append(('is not','Is Not Empty'))
+		operator.append(('<','Smaller'))
+		operator.append(('>','Bigger'))
+	elif field.ttype in ('boolean', 'selection'):
+		operator.append(('=','Equals'))
+		operator.append(('<>','Not Equals'))
+	return {}
 
 class set_filter_fields(wizard.interface):
 	states = {
@@ -199,7 +211,7 @@ class set_filter_fields(wizard.interface):
 			'result': {'type':'form', 'arch':select_field_form, 'fields':select_field_fields, 'state':[('end','Cancel'),('set_value_select_field','Continue')]}			
 		},
 		'set_value_select_field':{
-			'actions': [_set_form_value],
+			'actions': [_set_form_value, _set_operator],
 			'result': {'type' : 'form', 'arch' : set_value_form, 'fields' : set_value_fields, 'state' : [('end', 'Cancel'),('set_value', 'Confirm Filter') ]}
 		},
 		'set_value':{
