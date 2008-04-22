6a7413a8fc624056f69ddfdc98ac972cf856dbc3
Mantavya Gajjar <mga@tinyerp.com>
2008-04-22 11:57:01 +0000

    A new Module that based on the "base","hr" a new way to ask Holiday and manage Leaves
    * employee Hierarchy
    * manage the workflow 
    * New Reports

---
+++ hr_holidays_request/__init__.py
@@ -0,0 +1,3 @@
+import hr_holidays_request
+import wizard
+import report
---
+++ hr_holidays_request/__terp__.py
@@ -0,0 +1,13 @@
+{
+    "name" : "HR Holiday Request",
+    "version" : "1.0",
+    "author" : "Axelor",
+    "category" : "Generic Modules/Human Resources",
+    "website": "http://www.axelor.com",
+    "depends" : ["base","hr"],
+    "init_xml" : [],
+    "update_xml" : ["holiday_demo_data.xml","hr_holidays_request_view.xml","hr_holiday_wizard.xml","hr_workflow.xml"],
+    "demo_xml" : [],
+    "installable": True,
+    "active" : False,
+}
