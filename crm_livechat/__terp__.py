4837fe8204c4e31f5a5643422d021ceb25bdda1f
Fabien Pinckaers <fp@tinyerp.com>
2008-04-28 07:19:23 +0000

    Improved __terp__ description

--- crm_livechat/__terp__.py
+++ crm_livechat/__terp__.py
@@ -5,7 +5,18 @@
 	"author" : "Tiny",
 	"description": """
 This module allows you to configure and manage a livechat on your website.
-So that your salesman can directly interrogate your users in your website.
+So that your salesman can directly talk with your users in your website, using
+their normal jabber account. This project includes two parts:
+* An OpenERP module to manage everything
+* A python Ajax client to set on your website for the end-user interface.
+
+It allows you to define:
+* XMPP (Jabber) Accounts for your users
+* XMPP Accounts for anonymous customers
+
+Then based one some events (a customer visiting some pages), it can open a
+window so that the visitor can directly talk with your teams. It goes to a
+jabber user according to what you configured in the OpenERP interface.
 	""",
 	"website" : "http://tinyerp.com/",
 	"category" : "Generic Modules/CRM & SRM",
