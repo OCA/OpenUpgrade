fc2e6bcf09e7ecc54613dcc899fdc8441368ef41
Mantavya Gajjar <mga@tinyerp.com>
2008-05-01 05:05:52 +0000

    change in Relation for the company and Email Address

--- smtpclient/res_company.py
+++ smtpclient/res_company.py
@@ -1,18 +1,19 @@
 from osv import fields,osv
 import tools
 
-class res_company(osv.osv):
-    _inherit = "res.company"
-
-    _columns = {        
-        'addresses': fields.one2many('res.company.address', 'name', 'Email Addresses'),
-    }
-    
-res_company()
 class res_company_address(osv.osv):
     _name = "res.company.address"
     _columns = {        
-        'name': fields.char('Email Address', size=64,required=True),
-        'type' : fields.selection([("default", "Default"),("inoice", "Invoice"),("sale","Sale"),("delivery","Delivery")], "Address Type",required=True)
+        'company_id' : fields.many2one('res.company', 'Company' , required=True),
+        'email': fields.many2one('email.smtpclient', 'Email Address',  required=True),
+        'name' : fields.selection([("default", "Default"),("inoice", "Invoice"),("sale","Sale"),("delivery","Delivery")], "Address Type",required=True)
     }
 res_company_address()
+
+class res_company(osv.osv):
+    _inherit = "res.company"
+    _columns = {        
+        'addresses': fields.one2many('res.company.address', 'company_id', 'Email Addresses'),
+    }
+res_company()
+
