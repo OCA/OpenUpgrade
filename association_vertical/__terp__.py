0de9a6be57eb10df01247660dc156c23f9cd226a
sso <>
2008-05-08 07:00:38 +0000

    Added 2 files related to Fund Raising:: 1) Demo data 2) Menus

--- association_vertical/__terp__.py
+++ association_vertical/__terp__.py
@@ -6,8 +6,11 @@
 		"category" : "Vertical Modules/Parametrization",
 		"description": """Simplification of the interface for associations.""",
 		"depends" : ["hr","crm_configuration","event"],
-		"init_xml" : [ ],
-		"demo_xml" : [ ],
-		"update_xml" : ["aso_data.xml","aso_vertical_view.xml"],
+		"init_xml" : [],
+		"demo_xml" : ["crm_fund_data.xml"],
+		"update_xml" : ["crm_fund_demo.xml",
+					    "aso_data.xml",
+					    "aso_vertical_view.xml",
+					   ],
 		"installable": True
 } 
