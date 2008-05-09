ae5eace625ffee72aaa615622ea52d717a165384
Fabien Pinckaers <fp@tinyerp.com>
2008-05-09 03:42:58 +0000

    Modifs

--- sale_margin/invoice_margin.py
+++ sale_margin/invoice_margin.py
@@ -39,7 +39,7 @@ class account_invoice_line(osv.osv):
 		'cost_price': fields.float('Cost Price', digits=(16, 2)),
 	}
 	def write(self, cr, uid, ids, vals, context={}):
-		if vals.get('product_id' , False)
+		if vals.get('product_id' , False):
 			res=self.pool.get('product.product').read(cr, uid, [vals['product_id']], ['standard_price'])
 			vals['cost_price']=res[0]['standard_price']
 		return super(account_invoice_line, self).write(cr, uid, ids, vals, context)
