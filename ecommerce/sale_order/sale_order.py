b4e670da959dea049fddae523846f6025a264c74
Sananaz Mansuri <sma@tinyerp.com>
2008-05-12 04:13:10 +0000

    * chnges in the actions

--- ecommerce/sale_order/sale_order.py
+++ ecommerce/sale_order/sale_order.py
@@ -5,44 +5,50 @@ import datetime
 import time
 from tools import config
 
-class sale_order(osv.osv):
+class ecommerce_sale_order(osv.osv):
+
     _name='ecommerce.saleorder'
-    _description="ecommerce sale order"
     _columns = {
         'name': fields.char('Order Description',size=64, required=True),
         'state': fields.selection([
             ('draft','Draft'),
             ('done','Done'),
             ('cancel','Cancel')
-        ],'OrderState', readonly=True),
+        ], 'Order State',readonly=True),
         'date_order':fields.date('Date Ordered', required=True),
-        'epartner_shipping_id':fields.many2one('ecommerce.partner', 'Shipping Address', required=True),
-        'epartner_invoice_id':fields.many2one('ecommerce.partner', 'Invoice Address', required=True),
+
+        'epartner_shipping_id':fields.many2one('ecommerce.partner', 'Ecommerce Shipping Address', required=True),
+        'epartner_invoice_id':fields.many2one('ecommerce.partner', 'Ecommerce Invoice Address', required=True),
+
         'partner_id':fields.many2one('res.partner', 'Contact Address'),
         'partner_shipping_id':fields.many2one('res.partner.address', 'Shipping Address'),
-        'partner_invoice_id':fields.many2one('res.partner.address', 'Invoice Address'),      
+        'partner_invoice_id':fields.many2one('res.partner.address', 'Invoice Address'),
+
         'web_id':fields.many2one('ecommerce.shop', 'Web Shop', required=True),
         'order_lines': fields.one2many('ecommerce.order.line', 'order_id', 'Order Lines'),
         'order_id': fields.many2one('sale.order', 'Sale Order'),
         'note': fields.text('Notes'),
     }
     _defaults = {
-        'name': lambda obj, cr, uid, context: obj.pool.get('ir.sequence').get(cr, uid, 'ecommerce.saleorder'),                 
         'date_order': lambda *a: time.strftime('%Y-%m-%d'),
         'state': lambda *a: 'draft',
     }
 
-
-    def action_draft(self,cr,uid,ids):
+    def order_draft(self,cr,uid,ids):
         self.write(cr, uid, ids, {'state':'draft'})
         return True
     
-    def action_done(self,cr,uid,ids):
-        for order in self.browse(cr, uid, ids):
+    def order_cancel(self, cr, uid, ids, context={}):
+        self.write(cr, uid, ids, {'state':'cancel'})
+        return True
+    
+    def order_create_function(self, cr, uid, ids, context={}):
+       
+        for order in self.browse(cr, uid, ids, context):
             if not (order.partner_id and order.partner_invoice_id and order.partner_shipping_id):
                 raise osv.except_osv('No addresses !', 'You must assign addresses before creating the order.')
-            
-            pricelist_id=order.partner_id.property_product_pricelist.id
+
+            pricelist_id=order.partner_id.property_product_pricelist
             order_lines = []
             for line in order.order_lines:
                 val = {
@@ -58,42 +64,54 @@ class sale_order(osv.osv):
                 val_new['product_uos'] = 'product_uos' in val_new and val_new['product_uos'] and val_new['product_uos'][0] or False
                 val.update( val_new )
                 val['tax_id'] = 'tax_id' in val and [(6,0,val['tax_id'])] or False
-                order_lines.append( (0,0,val))
-                order_id = self.pool.get('sale.order').create(cr, uid, {
+                order_lines.append((0,0,val))
+              
+            order_id = self.pool.get('sale.order').create(cr, uid, {
+                                                                    
                 'name': order.name,
-                'shop_id': order.web_id.id,
-                'origin': 'WEB:'+str(order.web_ref),
+                'shop_id': order.web_id.shop_id.id,
                 'user_id': uid,
                 'note': order.note or '',
                 'partner_id': order.partner_id.id,
                 'partner_invoice_id':order.partner_invoice_id.id,
                 'partner_order_id':order.partner_invoice_id.id,
                 'partner_shipping_id':order.partner_shipping_id.id,
-                'pricelist_id': pricelist_id,
-                'order_line': order_lines
+                'pricelist_id': pricelist_id.id,
+                'order_line':order_lines
             })
             self.write(cr, uid, [order.id], {'state':'done', 'order_id': order_id})
+
         return True
-    
-    def action_cancel(self,cr,uid,ids):
-            self.write(cr, uid, ids, {'state':'cancel'})
-            return True
 
-sale_order()
-
+    def address_set(self, cr, uid, ids, *args):
+        done = []
+        for order in self.browse(cr, uid, ids):
+            for a in [order.epartner_shipping_id.id,order.epartner_invoice_id.id]:
+                if a not in done:
+                    done.append(a)
+                    self.pool.get('ecommerce.partner').address_set(cr, uid, [a] )
+            self.write(cr, uid, [order.id], {
+                'partner_shipping_id': order.epartner_invoice_id.address_id.id,
+                'partner_id': order.epartner_invoice_id.address_id.partner_id.id,
+                'partner_invoice_id': order.epartner_shipping_id.address_id.id,
+           })
+        return True
+   
+ecommerce_sale_order()
 
 class ecommerce_order_line(osv.osv):
     _name = 'ecommerce.order.line'
-    _description = 'ecomemrce sale order'
+    _description = 'eSale Order line'
     _columns = {
-                'name': fields.char('Order Line', size=64, required=True),
-                'order_id': fields.many2one('ecommerce.saleorder', 'Eorder Ref.'),
-                'product_qty': fields.float('Quantity', digits=(16,2), required=True),
-                'product_id': fields.many2one('product.product', 'Product'),
-                'product_uom_id': fields.many2one('product.uom', 'Unit of Measure',required=True),
-                'price_unit': fields.float('Unit Price',digits=(16,2), required=True),
+        'name': fields.char('Order Line', size=64, required=True),
+        'order_id': fields.many2one('ecommerce.saleorder', 'eOrder Ref'),
+        'product_qty': fields.float('Quantity', digits=(16,2), required=True),
+        'product_id': fields.many2one('product.product', 'Product', domain=[('sale_ok','=',True)], change_default=True),
+        'product_uom_id': fields.many2one('product.uom', 'Unit of Measure',required=True),
+        'price_unit': fields.float('Unit Price',digits=(16, int(config['price_accuracy'])), required=True),
     }
-    
+    _defaults = {
+    }
 ecommerce_order_line()
 
 
