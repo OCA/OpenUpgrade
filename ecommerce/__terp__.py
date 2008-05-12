8dfb4c305b810413a697f6234fb588f84fb263b8
Sananaz Mansuri <sma@tinyerp.com>
2008-05-12 04:07:04 +0000

    * add demo data file

--- ecommerce/__terp__.py
+++ ecommerce/__terp__.py
@@ -6,10 +6,12 @@
     "depends" : ["base", "product","sale"],
     "category" : "Generic Modules/E-Commerce Shop",
     "init_xml" : [],
-    "demo_xml" : [],
+    "demo_xml" : ["ecommerce_demo_data.xml"],
     "description": """
     """,
-    "update_xml" : ["basic_info_view.xml",
+    "update_xml" : [
+                    "tools/ecom_product_view.xml",
+                    "basic_info_view.xml",
                     "catalog/catalog_view.xml",
                     "tools/tools_wizard.xml",
                     "partner/partner_new_view.xml",
