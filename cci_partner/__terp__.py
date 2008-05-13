ce0bc18228267b2d9a0713e53ddb454af276e34f
Jay vora <jvo@tinyerp.com>
2008-05-13 06:58:31 +0000

    Added report for counting all kind of invoice for customer and supplier type invoice.

--- cci_partner/__terp__.py
+++ cci_partner/__terp__.py
@@ -1,17 +1,17 @@
 {
-    "name" : "CCI partner",
+    "name" : "CCI Partner",
     "version" : "1.0",
     "author" : "Tiny",
     "website" : "http://tinyerp.com",
     "category" : "Generic Modules/CCI Partner",
     "description": """
-        specific module for cci project which will inherit partner module
+        Specific module for cci project which will inherit partner module
     """,
     "depends" : ["base","cci_base_contact"],
     "init_xml" : [],
     "demo_xml" : [ "cci_data.xml", "user_data.xml","zip_data.xml", "courtesy_data.xml","links_data.xml", "activity_data.xml", "states_data.xml", "category_data.xml", "function_data.xml"],
 
-    "update_xml" : ['cci_partner_view.xml','article_sequence.xml'],
+    "update_xml" : ['cci_partner_view.xml','article_sequence.xml','cci_partner_report.xml'],
     "active": False,
-    "installable": True
+    "installable": True,
 }
