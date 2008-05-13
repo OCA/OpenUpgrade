3b776e086f21dc049b757ba02e2c23ed1cae673d
Mustufa Rangwala <mra@tinyerp.com>
2008-05-13 08:25:25 +0000

    * misc changes

--- cci_account/wizard/open_closed_fiscalyear.py
+++ cci_account/wizard/open_closed_fiscalyear.py
@@ -1,24 +1,28 @@
 import wizard
 import netsvc
 import pooler
-
 from osv import fields, osv
 
-form_msg = """<?xml version="1.0"?>
-<form string="Inovoice Grouped">
-    <field name="message"  colspan="5"/>
+form = """<?xml version="1.0"?>
+<form string="Entries">
+    <label string="Entries Removed"/>
 </form>
 """
-fields_msg = {
-      'message': {'string':'Entries Deleted', 'type':'text', 'readonly':True, 'size':'500'},
-}
+
+def _remove_entries(self, cr, uid, data, context):
+    pool = pooler.get_pool(cr.dbname)
+    ids_journal = pool.get('account.journal').search(cr, uid, [('centralisation', '=', True)])
+    if not ids_journal:
+        raise wizard.except_wizard('Warning', 'There is no centralised counterpart journal available ')
+    ids_move = pool.get('account.move.line').search(cr,uid,[('journal_id','in',ids_journal)])
+    pool.get('account.move.line').unlink(cr,uid,ids_move)
+    return {}
 
 class open_closed_fiscal(wizard.interface):
     states = {
         'init' : {
-               'actions' : [],
-               'result': {'type': 'form', 'arch': form_msg, 'fields': fields_msg, 'state':[('end','Ok')]}
+               'actions' : [_remove_entries],
+               'result': {'type': 'form', 'arch': form, 'fields': {}, 'state':[('end','Ok')]}
             },
-
     }
 open_closed_fiscal("cci_account.open_closed_fiscal")
