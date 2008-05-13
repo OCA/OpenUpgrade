b046cbc7f0e7835d27fb5202dd31e31c83afc437
sso <>
2008-05-13 08:31:34 +0000

    Includes the view files and .py files for Direct Marketing

---
+++ dm/__terp__.py
@@ -0,0 +1,19 @@
+{
+    "name" : "Direct Marketing",
+    "version" : "1.0",
+    "author" : "Tiny",
+    "website" : "http://tinyerp.com",
+    "category" : "Generic Modules/Direct Marketing",
+    "description": """
+    
+                    """,
+    "depends" : ["project"],
+    "init_xml" : [ ],
+    "demo_xml" : [ ],
+    "update_xml" : ["campaign_view.xml",
+                    "offer_step_view.xml",
+                    "offer_view.xml",
+                    "trademark_view.xml"],
+    "active": False,
+    "installable": True
+}
---
+++ dm/trademark.py
@@ -0,0 +1,17 @@
+import time
+
+from osv import fields
+from osv import osv
+
+
+class dm_trademark(osv.osv):
+    _name = "dm.trademark"
+    _columns = {
+        'name' : fields.char('Name', size=64),
+        'partner_id' : fields.many2one('res.partner', 'Partner Name'),
+        'header' : fields.binary('Header (.odt)'),
+        'logo' : fields.binary('Logo'),
+        'signature' : fields.binary('Signature')
+                }
+    
+dm_trademark()
