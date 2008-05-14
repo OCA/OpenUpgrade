215134a1640008f970a9f460613062dfefe94b1b
Mustufa Rangwala <mra@tinyerp.com>
2008-05-14 11:03:59 +0000

    remove cci_group_invoice.py

--- cci_account/wizard/__init__.py
+++ cci_account/wizard/__init__.py
@@ -2,7 +2,6 @@ import cci_wizard_aged_trial_balance
 import cci_account_special_message
 import cci_wizard_partner_balance_report
 import cci_wizard_third_party_ledger
-import cci_group_invoice
 import cci_partner_vat_listing
 import cci_account_vat_declaration
 import cci_switched_debit_credit
