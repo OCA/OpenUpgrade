505b75efa8a17a377f9d575ceb7a9597557c233a
qdp <>
2008-05-20 05:47:18 +0000

    *implemented tags, open answers... *created views accordingly *overwritten build_form() and _questionnaire_compute() functions to fetch requirements and the use of a teext field as answer

--- cci_crm_profile/cci_crm_profile.py
+++ cci_crm_profile/cci_crm_profile.py
@@ -28,16 +28,87 @@
 
 from osv import fields,osv
 from osv import orm
+#Old version
+#-----------
+#class cci_partner_profile_answer(osv.osv):#added for partner.ods
+#    _name="cci_partner_profile.answer"
+#    _description="Partner Answer"
+#    _columns={
+#        "partner_id": fields.many2one('res.partner','Partner'),
+#        "address_id": fields.many2one('res.partner.address','Address'),
+#        "contact_id": fields.many2one('res.partner.contact','Contact'),
+#        "question_id": fields.many2one('crm_profiling.question','Question'),
+#        "answer_id": fields.many2one('crm_profiling.answer','Answer'),
+#        "answer_text":fields.char('Answer Text',size=20)#should be corect
+#        }
+#cci_partner_profile_answer()
 
-class cci_partner_profile_answer(osv.osv):#added for partner.ods
-    _name="cci_partner_profile.answer"
-    _description="Partner Answer"
-    _columns={
-        "partner_id": fields.many2one('res.partner','Partner'),
-        "address_id": fields.many2one('res.partner.address','Address'),
-        "contact_id": fields.many2one('res.partner.contact','Contact'),
-        "question_id": fields.many2one('crm_profiling.question','Question'),
-        "answer_id": fields.many2one('crm_profiling.answer','Answer'),
-        "answer_text":fields.char('Answer Text',size=20)#should be corect
-        }
-cci_partner_profile_answer()
+
+class question(osv.osv):
+	_inherit="crm_profiling.question"
+	_columns={
+		'target':fields.selection([('res.partner','Partner'),('res.partner.contact','Contact'),('res.partner.address','Address')], 'Target', required=True),
+		'open_question':fields.boolean('Open Question'),
+		}
+	_defaults = {
+		'target': lambda *args: 'res.partner',
+		'open_question': lambda *args : False,
+	}
+question()
+
+class answer(osv.osv):
+	_inherit="crm_profiling.answer"
+	_columns={
+		"text": fields.text("Open Answer"),
+		}
+answer()
+
+class res_partner_contact(osv.osv):
+	_inherit="res.partner.contact"
+	_columns={
+		"answers_ids": fields.many2many("crm_profiling.answer","contact_question_rel","contact","answer","Answers", domain=[('question_id.target','=','res.partner.contact')]),
+		}
+res_partner_contact()
+
+class res_partner_address(osv.osv):
+	_inherit="res.partner.address"
+	_columns={
+		"answers_ids": fields.many2many("crm_profiling.answer","address_question_rel","address","answer","Answers",domain=[('question_id.target','=','res.partner.address')]),
+		}
+res_partner_address()
+
+class partner(osv.osv):
+	_inherit="res.partner"
+
+	def _questionnaire_compute(self, cr, uid, data, context):
+		temp = []
+		for field in data['form']:
+			if field.startswith("quest_form") and data['form'][field] != 0 :
+				question_id = field.lstrip('quest_form')
+				question_rec = self.pool.get('crm_profiling.question').browse(cr, uid, int(question_id))
+				if question_rec.open_question:
+					vals = {
+						'name': '/',
+						'question_id': question_rec.id,
+						'text': data['form'][field]
+					}
+					data['form'][field] = self.pool.get('crm_profiling.answer').create(cr, uid, vals, context)
+		return super(partner,self)._questionnaire_compute(cr, uid, data, context)
+
+	_columns={
+		"answers_ids": fields.many2many("crm_profiling.answer","partner_question_rel","partner","answer","Answers", domain=[('question_id.target','!=','res.partner.address'),('question_id.target','!=','res.partner.contact')]),
+		}
+partner()
+
+class questionnaire(osv.osv):
+	_inherit="crm_profiling.questionnaire"
+
+	def build_form(self, cr, uid, data, context):
+		quest_form,quest_fields = super(questionnaire,self).build_form(cr, uid, data, context)
+		for field in quest_fields:
+			question_id = field.lstrip('quest_form')
+			question_rec = self.pool.get('crm_profiling.question').browse(cr, uid, int(question_id))
+			if question_rec.open_question:
+				quest_fields[field] = {'string': question_rec.name, 'type': 'text',}
+		return quest_form, quest_fields
+questionnaire()
