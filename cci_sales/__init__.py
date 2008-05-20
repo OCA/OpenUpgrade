d2be3611df8e20e9a99d866ac4cfbfb0d6079165
Jay vora <jvo@tinyerp.com>
2008-05-20 11:40:39 +0000

    Added 2 new fields on sale order.

---
+++ cci_sales/__init__.py
@@ -0,0 +1,1 @@
+import cci_sales
---
+++ cci_sales/__terp__.py
@@ -0,0 +1,16 @@
+{
+    "name" : "CCI Sales",
+    "version" : "1.0",
+    "author" : "Tiny",
+    "website" : "http://www.tinyerp.com",
+    "category" : "Generic Modules/CCI Sales",
+    "description": """
+        A Specific Module for CCI which will make the use of Sale module..
+    """,
+    "depends" : ["sale",],
+    "init_xml" : [],
+    "demo_xml" : [],
+    "update_xml" : ["cci_sales_view.xml"],
+    "active": False,
+    "installable": True
+}
