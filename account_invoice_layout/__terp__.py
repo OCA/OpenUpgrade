e0930b20e65e41e78ba22c3a745124c9f5ebf6e4
Mustufa Rangwala <mra@tinyerp.com>
2008-05-21 10:36:48 +0000

    add description of special message report on invoice

--- account_invoice_layout/__terp__.py
+++ account_invoice_layout/__terp__.py
@@ -10,6 +10,7 @@
 		* order all the lines of an invoice
 		* add titles, comment lines, sub total lines
 		* draw horizontal lines and put page breaks
+		* report to print invoices with given spcial message at the bottom of invoice
 
 
 	""",
