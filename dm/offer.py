21e47ffa78ab8f83587a07aa61806b7a67f5700e
Dhara Shah <dsh@tinyerp.com>
2008-05-23 08:22:49 +0000

    step_id  removed

--- dm/offer.py
+++ dm/offer.py
@@ -92,7 +92,7 @@ class dm_offer(osv.osv):
         'code' : fields.char('Code', size=16, required=True),
         'lang_orig' : fields.many2one('res.lang', 'Original Language'),
         'copywriter_id' : fields.many2one('res.partner', 'Copywriter'),
-        'step_ids' : fields.one2many('dm.offer.step','offer_id','Offer Steps'),
+#        'step_ids' : fields.one2many('dm.offer.step','offer_id','Offer Steps'),
 #        'mark_id' : fields.
         'offer_origin_id' : fields.many2one('dm.offer', 'Original Offer'),
         'quotation' : fields.char('Quotation', size=16),
