914855ebf4a0d454db73f4882594c2720d89fd87
Mustufa Rangwala <mra@tinyerp.com>
2008-05-24 09:17:43 +0000

    * purchase tendor

---
+++ purchase_tender/__terp__.py
@@ -0,0 +1,15 @@
+{
+    "name" : "Purchase - Purchase Tendor",
+    "version" : "0.1",
+    "author" : "Tiny",
+    "category" : "Generic Modules/Purchase",
+    "website" : "http://tinyerp.com/",
+    "description": """ Purchase Tendor
+""",
+    "depends" : ["purchase"],
+    "init_xml" : [],
+    "demo_xml" : [],
+    "update_xml" : ["purchase_tendor_view.xml",],
+    "active": False,
+    "installable": True
+}
