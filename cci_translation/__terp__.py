de2828554d58ccf5ca7e60e581ea22d4e0333695
Mustufa Rangwala <mra@tinyerp.com>
2008-05-27 07:42:23 +0000

    cci translation 	- add view 	- add data_xml

---
+++ cci_translation/__terp__.py
@@ -0,0 +1,17 @@
+{
+    "name" : "CCI Translation",
+    "version" : "1.0",
+    "author" : "Tiny",
+    "website" : "http://tinyerp.com",
+    "category" : "Generic Modules/CCI Translation",
+    "description": """
+        cci translation
+    """,
+    "depends" : ["base","cci_mission"],
+    "init_xml" : [],
+    "demo_xml" : ["cci_translation_data.xml"],
+
+    "update_xml" : ["cci_translation_view.xml"],
+    "active": False,
+    "installable": True
+}
