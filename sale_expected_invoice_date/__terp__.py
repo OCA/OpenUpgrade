2105aea85b05f8581d8d7b096d2b84e3ec55ac31
Mustufa Rangwala <mra@tinyerp.com>
2008-05-28 08:19:16 +0000

    * sale.order.line 	- add field on it (inherit)

---
+++ sale_expected_invoice_date/__terp__.py
@@ -0,0 +1,16 @@
+{
+    "name" : "Sale Expected Invoice Date",
+    "version" : "0.1",
+    "author" : "Tiny",
+    "category" : "Generic Modules/Sales",
+    "website" : "http://tinyerp.com/",
+    "description": """
+     * add one field on sale order line
+""",
+    "depends" : ["sale"],
+    "init_xml" : [],
+    "demo_xml" : [],
+    "update_xml" : ["sale_expected_invoice_date_view.xml",],
+    "active": False,
+    "installable": True
+}
