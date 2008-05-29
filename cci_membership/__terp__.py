bd4ec55d49efa4e3966ef7a794454dd2b7920518
Mustufa Rangwala <mra@tinyerp.com>
2008-05-29 09:49:12 +0000

    cci membership

---
+++ cci_membership/__terp__.py
@@ -0,0 +1,17 @@
+{
+    "name" : "CCI Membership",
+    "version" : "1.0",
+    "author" : "Tiny",
+    "website" : "http://tinyerp.com",
+    "category" : "Generic Modules/CCI Membership",
+    "description": """
+        cci membership
+            - vcs functionality for cci
+    """,
+    "depends" : ["base","membership"],
+    "init_xml" : [],
+    "demo_xml" : [],
+    "update_xml" : ["cci_membership_view.xml"],
+    "active": False,
+    "installable": True
+}
