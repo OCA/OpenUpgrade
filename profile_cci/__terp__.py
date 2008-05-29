0d3d616dc763e36ce7392714e63bbfb2a24e8887
Mustufa Rangwala <mra@tinyerp.com>
2008-05-29 10:27:46 +0000

    * cci_membership added

--- profile_cci/__terp__.py
+++ profile_cci/__terp__.py
@@ -6,7 +6,7 @@
 	"depends":[
 		"base_contact", "cci_partner", "cci_base_contact", "event", "cci_event",
 		"cci_crm_profile", "account_payment", "membership", "base_module_record", "l10n_be",
-		"base_report_creator" , "cci_mission","cci_account","cci_sales","cci_purchase",
+		"base_report_creator" ,"cci_membership", "cci_mission","cci_account","cci_sales","cci_purchase",
 	],
 	"demo_xml":[],
 	"update_xml":[],
