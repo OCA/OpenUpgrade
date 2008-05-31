57f61bf15a524ebf6a4aa37c5bcddd58dddcdac9
Sananaz Mansuri <sma@tinyerp.com>
2008-05-31 09:38:27 +0000

    * Add shipping_method class * Add shipping_service class

--- ecommerce/catalog/catalog.py
+++ ecommerce/catalog/catalog.py
@@ -11,7 +11,8 @@ class product_product(osv.osv):
     _inherit = "product.product"
     _columns = {
                 'product_logo': fields.binary('Product Logo'),
-                 'shop_ids': fields.many2many('ecommerce.shop','ecommerce_new_product_rel','ecommerce_product','product','Web shop'),
+                'shop_ids': fields.many2many('ecommerce.shop','ecommerce_new_product_rel','ecommerce_product','product','Web shop'),
+                'currency_id': fields.many2one('res.currency', 'Currency')
 }
 product_product()
 
@@ -40,3 +41,26 @@ class reviews(osv.osv):
     }
     
 reviews()
+
+class shipping_method(osv.osv):
+    _name = "ecommerce.shipping.method"
+    _description = "Ecommerce Shipping Method"
+    _columns = {
+                'state_id': fields.many2one("res.country.state", 'State', domain="[('country_id','=',country_id)]"),
+                'country_id': fields.many2one('res.country', 'Country', required=True),
+                'shipping':fields.one2many('ecommerce.shipping.service','shipp_method','Shipping')
+                }
+shipping_method()
+
+class shipping_service(osv.osv):
+    _name = "ecommerce.shipping.service"
+    _rec_name = 'shipping_type'
+    _description = "Ecommerce Shipping Service"
+    _columns ={
+               'shipping_type':fields.selection([('byair','ByAir'), ('ground','Ground'), ('free shipping','Free Shipping'), ('flat rate','Flat Rate'),('home delivery','Home Delivery')], 'Shipping Type', required=True),
+               'shipping_charge':fields.float('Charges',required=True),
+               'currency':fields.many2one('res.currency','Currency',required=True),
+               'country_id': fields.many2one('res.country', 'Country', required=True),
+               'shipp_method':fields.many2one('ecommerce.shipping.method','Shipping Method')
+               }
+shipping_service()
