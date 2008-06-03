455507a9616b5e1f3a0489f7283cf111e97b2f61
nch <>
2008-06-03 11:22:36 +0000

    Form and Tree View of md_hr_contract and md_hr_contract_availability

---
+++ md_hr_contract/__init__.py
@@ -0,0 +1,1 @@
+import pilot_hr_contract
---
+++ md_hr_contract/__terp__.py
@@ -0,0 +1,12 @@
+{
+    "name" : "Pilot Human Resource Management",
+    "version" : "1.0",
+    "author" : "Tiny",
+    "depends" : ["base","hr","hr_contract", ],
+    "category" : "Generic Modules",
+    "init_xml" : [],
+    "demo_xml" : [],
+    "update_xml" : ["pilot_hr_contract_view.xml"],
+    "active": False,
+    "installable": True
+}
