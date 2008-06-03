26a6b63630c0fdc8ec51d531d341fa2be5285b39
stw <>
2008-06-03 13:57:22 +0000

    change the category of the module

--- idea/__terp__.py
+++ idea/__terp__.py
@@ -3,7 +3,7 @@
 		"version" : "0.1",
 		"author" : "Tiny",
 		"website" : "http://openerp.com",
-		"category" : "Unknown",
+		"category" : "Tools",
 		"description": """This module allows your user to easily and efficiently participate in the innovation of the enterprise. It allows everybody to express ideas about different subjects. Then, others userscan comment these ideas and vote for particular ideas. Each idea as a score based on the different votes. The managers can obtain an easy view on best ideas from all the users. Once installed, check the menu 'Ideas' in the 'Tools' main menu.""", 
 		"depends" : ['base'],
 		"init_xml" : [ ],
