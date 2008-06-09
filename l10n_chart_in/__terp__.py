a7d5afd67512eb8537d2c956a9f8bfe04869131c
vra <>
2008-06-09 05:14:35 +0000

    Added Indian Accounting Chart Xml.

--- l10n_chart_in/__terp__.py
+++ l10n_chart_in/__terp__.py
@@ -5,7 +5,7 @@
 	"category" : "Localisation/Account charts",
 	"depends" : ["account", "account_chart"],
 	"demo_xml" : [],
-	"update_xml" : ['account_tax_code.xml',"account_chart.xml",'account_tax.xml'],
+	"update_xml" : ['account_tax_code.xml',"account_chart.xml",'account_tax.xml',"account_chart_new.xml"],
 	"active": False,
 	"installable": True
 }
