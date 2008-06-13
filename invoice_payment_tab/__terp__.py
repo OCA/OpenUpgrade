89fad242c628cdd7cca23aeb973791028eaabb11
qdp <>
2008-06-13 09:54:23 +0000

    *replaced spaces by tabs *completed description of the module in __terp__.py file

--- invoice_payment_tab/__terp__.py
+++ invoice_payment_tab/__terp__.py
@@ -1,17 +1,17 @@
 {
-    "name" : "Invoice payment tab",
-    "version" : "1.0",
-    "author" : "Tiny",
-    "website" : "http://tinyerp.com",
-    "category" : "Generic Modules/Invoice payment tab",
-    "description": """
-        - it defines many2many(account.move.line) field on account.invoice
-    """,
-    "depends" : ["base","account"],
-    "init_xml" : [],
-    "demo_xml" : [],
+	"name" : "Invoice payment tab",
+	"version" : "1.0",
+	"author" : "Tiny",
+	"website" : "http://openerp.com",
+	"category" : "Generic Modules/Accounting",
+	"description": """
+		This module defines a new tab on invoices, which contains all the payments made for this invoice. This improvement will allow accountants to easily manage due amounts and prepayments.
+	""",
+	"depends" : ["base","account"],
+	"init_xml" : [],
+	"demo_xml" : [],
 
-    "update_xml" : ["invoice_payment_tab_view.xml"],
-    "active": False,
-    "installable": True
+	"update_xml" : ["invoice_payment_tab_view.xml"],
+	"active": False,
+	"installable": True
 }
