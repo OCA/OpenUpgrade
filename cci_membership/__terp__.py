9545e9c7a7bad598919efe6339d81761fef858c4
Mustufa Rangwala <mra@tinyerp.com>
2008-06-13 10:23:12 +0000

    * added wizard 	- will print list of partners with sum of its sold value of product 	- compute the difference between the total sold to them if they weren't member or if they were member for a certain period.

--- cci_membership/__terp__.py
+++ cci_membership/__terp__.py
@@ -11,7 +11,7 @@
     "depends" : ["base","membership"],
     "init_xml" : [],
     "demo_xml" : [],
-    "update_xml" : ["cci_membership_view.xml"],
+    "update_xml" : ["cci_membership_view.xml","cci_membership_report.xml"],
     "active": False,
     "installable": True
 }
