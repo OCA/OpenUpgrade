4a709b75d624505290c67a99303f021a798cbd41
Mustufa Rangwala <mra@tinyerp.com>
2008-06-16 10:49:05 +0000

    *  It will overwrite the project object: if the stop date of a project is changed, all the tasks of the project should be moved of the same number of days too.

---
+++ project_retro_planning/__terp__.py
@@ -0,0 +1,16 @@
+{
+    "name" : "Project Retro planning",
+    "version" : "0.1",
+    "author" : "Tiny",
+    "category" : "Generic Modules/Project Retro Planning",
+    "description": """
+        - Stop date of project is change will change all the tasks of that project to same number of days """,
+    "depends" : [
+        "base", "project"
+        ],
+    "demo_xml" : [],
+    "init_xml" : [],
+    "update_xml" : ["project_retro_planning_view.xml"],
+    "active" : False,
+    "installable" : True,
+}
