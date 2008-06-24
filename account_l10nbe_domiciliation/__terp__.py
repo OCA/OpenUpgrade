3566a2a578b38789bba0d4659c34f581c7d5f7b3
Mustufa Rangwala <mra@tinyerp.com>
2008-06-24 09:23:03 +0000

    * l10be_domiciliation

---
+++ account_l10nbe_domiciliation/__terp__.py
@@ -0,0 +1,17 @@
+{
+    "name" : "Account l10nbe Domiciliation",
+    "version" : "1.0",
+    "author" : "Tiny",
+    "website" : "http://tinyerp.com",
+    "category" : "Generic Modules/Account l10nbe Domiciliation",
+    "description": """
+        related with l10n_be module
+    """,
+    "depends" : ["base","account"],
+    "init_xml" : [],
+    "demo_xml" : [],
+
+    "update_xml" : ["l10nbe_domiciliation_view.xml"],
+    "active": False,
+    "installable": True
+}
