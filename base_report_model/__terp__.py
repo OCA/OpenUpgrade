31c01cabe017eb3f4baa3ef3bf909d3abeabbb6a
stw <>
2008-06-24 04:43:30 +0000

    add the base_report_model module, this module helps to define a list of models which can have an attachment from openoffice report

---
+++ base_report_model/__terp__.py
@@ -0,0 +1,13 @@
+{
+		"name" : "Base Report Model", 
+		"version" : "0.1",
+		"author" : "Tiny",
+		"website" : "http://openerp.com",
+		"category" : "Tools",
+		"description": """This module allows you to specify the models which can have an attachment from OpenOffice Report""",
+		"depends" : ['base'],
+		"init_xml" : [ ],
+		"demo_xml" : [ ],
+		"update_xml" : ['base_report_model_view.xml'],
+		"installable": True
+}
