3147fa925542866e6ad76d6586915d1323074715
Mustufa Rangwala <mra@tinyerp.com>
2008-06-25 04:00:19 +0000

    * move wizards to l10n_be module to make it generic 	- vat listing and declaration wizards

--- cci_account/wizard/__init__.py
+++ cci_account/wizard/__init__.py
@@ -1,8 +1,6 @@
 import cci_wizard_aged_trial_balance
 import cci_wizard_partner_balance_report
 import cci_wizard_third_party_ledger
-import cci_partner_vat_listing
-import cci_account_vat_declaration
 import cci_switched_debit_credit
 import cci_use_model
 import cci_wizard_vcs
