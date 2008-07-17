779674ab3e9a707ee1591983670b408af96922ac
Mustufa Rangwala <mra@tinyerp.com>
2008-07-17 15:22:27 +0530

    account bob

---
+++ account_bob_import/__terp__.py
@@ -0,0 +1,13 @@
+
+{
+    'name': 'cci Data',
+    'category': 'Data Module',
+     'init_xml':[],
+     'author': 'Tiny',
+     'depends': [],
+     'version': '1.0',
+     'active': False,
+     'demo_xml': [],
+     'update_xml':[],
+     'installable': True
+}
