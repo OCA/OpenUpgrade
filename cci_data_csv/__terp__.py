9093760f94489ae8d35f4e1f10723f0956d43974
qdp <qdp@tinyerp.com>
2008-07-30 14:56:32 +0200

    *added cci_data_csv for import

---
+++ cci_data_csv/__terp__.py
@@ -0,0 +1,16 @@
+# -*- encoding: utf-8 -*-
+
+{
+    'name': 'cci Data',
+    'category': 'Data Module',
+     'init_xml':[],
+     'author': 'OpenERP',
+     'depends': ['base'],
+     'version': '1.0',
+     'active': False,
+     'demo_xml': [],
+     'update_xml':['res.users.csv'],
+     'installable': True
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+
