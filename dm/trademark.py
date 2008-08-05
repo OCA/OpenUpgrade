34009c8aa9f099196ad61d5511cf312a49983dee
Dhara Shah <dsh@tinyerp.com>
2008-08-05 16:57:28 +0530

    changes for trademark

--- dm/trademark.py
+++ dm/trademark.py
@@ -7,10 +7,8 @@ from osv import osv
 
 class dm_trademark(osv.osv):
     _name = "dm.trademark"
+    _inherit = "res.partner"
     _columns = {
-        'name' : fields.char('Name', size=64, required=True),
-        'code' : fields.char('Code', size=6, required=True),
-        'partner_id' : fields.many2one('res.partner', 'Partner'),
         'header' : fields.binary('Header (.odt)'),
         'logo' : fields.binary('Logo'),
         'signature' : fields.binary('Signature')
