7f293b598b6f1043d81a0376e415b5d349972de2
Mustufa Rangwala <mra@tinyerp.com>
2008-08-05 11:14:32 +0530

    security module for cci ..give access to diff groups for cci objects

---
+++ cci_security/__terp__.py
@@ -0,0 +1,19 @@
+# -*- encoding: utf-8 -*-
+{
+    "name" : "CCI Security",
+    "version" : "1.0",
+    "author" : "Tiny",
+    "website" : "http://www.tinyerp.com",
+    "category" : "Generic Modules/CCI Security",
+    "description": """
+        give access of objects to different groups of cci using csv
+    """,
+    "depends" : ["base"],
+    "init_xml" : [],
+    "demo_xml" : [],
+    "update_xml" : ["ir.model.access.csv"],
+    "active": False,
+    "installable": True
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+
