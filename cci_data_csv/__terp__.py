ab19ddcb74922a7d01deaea5f8a1244f8a395d1a
qdp <qdp-launchpad@tinyerp.com>
2008-08-06 17:29:52 +0200

    *removed old csv file *added csv files for res.partner.state and res.partner.state2

--- cci_data_csv/__terp__.py
+++ cci_data_csv/__terp__.py
@@ -1,16 +1,23 @@
 # -*- encoding: utf-8 -*-
 
 {
-    'name': 'cci Data',
-    'category': 'Data Module',
-     'init_xml':[],
-     'author': 'OpenERP',
-     'depends': ['base'],
-     'version': '1.0',
-     'active': False,
-     'demo_xml': [],
-     'update_xml':['res.users.csv'],
-     'installable': True
+    'name': 'cci Data 1',
+    'category': 'Data Module 1',
+    'init_xml':[],
+    'author': 'OpenERP',
+    'depends': ['base'],
+    'version': '1.0',
+    'active': False,
+    'demo_xml': [],
+    'update_xml':[
+#        'res.groups.csv',
+#        'res.users.csv',
+#        'ir.model.access.csv',
+        'res.partner.state.csv',
+        'res.partner.state2.csv',
+
+    ],
+    'installable': True
 }
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
 
