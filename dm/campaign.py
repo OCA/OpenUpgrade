44a5f86d1f676e73019b162cf776fb5dbe8656df
Fabian Semal <fabian@tinyerp.com>
2008-08-13 16:02:28 +0200

    Changed domain for offers

--- dm/campaign.py
+++ dm/campaign.py
@@ -59,7 +59,7 @@ class dm_campaign(osv.osv):
 
     _columns = {
         'code1' : fields.function(_campaign_code,string='Code',type="char",method=True,readonly=True),                
-        'offer_id' : fields.many2one('dm.offer', 'Offer',domain=[('state','=','open')]),
+        'offer_id' : fields.many2one('dm.offer', 'Offer',domain=[('state','=','open'),('type','in',['new','standart','rewrite'])]),
         'country_id' : fields.many2one('res.country', 'Country',required=True),
         'lang_id' : fields.many2one('res.lang', 'Language'),
         'trademark_id' : fields.many2one('res.partner', 'Trademark', help="TO CHECK : trademark"),
