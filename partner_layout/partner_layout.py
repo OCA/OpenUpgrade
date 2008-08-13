2d9ffaf6beed75d17b1f2239f64ab8cdf5e84677
Sofia <sahibsofia@gmail.com>
2008-08-13 19:26:14 +0530

    function for m2m domain

--- partner_layout/partner_layout.py
+++ partner_layout/partner_layout.py
@@ -14,4 +14,13 @@ class res_partner(osv.osv):
         'signature' : fields.binary('Signature')
     }
     
+    def _default_company(self, cr, uid, context={}):
+        if 'category_id' in context and context['category_id']:
+            id_cat = self.pool.get('res.partner.category').search(cr,uid,[('name','ilike',context['category_id'])])[0]
+            return [id_cat]
+        return []
+   
+    _defaults = {
+        'category_id': _default_company,
+    }
 res_partner()
