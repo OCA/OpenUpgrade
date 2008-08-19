b632310f7364d57e9ea7b011050dde675be026bd
Christophe Simonis <christophe@tinyerp.com>
2008-08-19 13:23:19 +0200

    retake empty files from svn

---
+++ commission_pricelist/pricelist.py
@@ -0,0 +1,15 @@
+import math
+from osv import fields,osv
+import tools
+import ir
+import pooler
+
+class product_pricelist(osv.osv):
+	_name = "product.pricelist"
+        _description = "sale agent commission Rate"
+        _inherit="product.pricelist"
+	_columns = {
+		'commrate':fields.integer('Commission Rate(%)'),
+                }
+product_pricelist()
+
