e10473d56e8395e28edcab36e001acaa9ee12860
Fabian Semal <fabian@tinyerp.com>
2008-09-03 14:46:36 +0200

    Added po generation (not finished) Removed demo datas from dm_data.xml fixed proposition product wizard

--- dm/campaign.py
+++ dm/campaign.py
@@ -2,6 +2,7 @@
 import time
 import datetime
 import offer
+from mx import DateTime
 
 from osv import fields
 from osv import osv
@@ -118,6 +119,7 @@ class dm_campaign(osv.osv):
         'currency_id' : fields.many2one('res.currency','Currency',ondelete='cascade'),
         'manufacturing_costs': fields.float('Manufacturing Costs',digits=(16,2)),
         'manufacturing_product': fields.many2one('product.product','Manufacturing Product'),
+        'purchase_line_ids': fields.one2many('dm.campaign.purchase_line', 'campaign_id', 'Purchase Lines'),
     }
 
     _defaults = {
@@ -173,7 +175,7 @@ class dm_campaign(osv.osv):
             if camp.offer_id.forbidden_country_ids and camp.country_id.id  in  forbidden_country_ids :
                 raise osv.except_osv("Error!!","This offer is not valid in this country")
         if not camp.date_start or not camp.dealer_id or not camp.trademark_id :
-            raise osv.except_osv("Error!!","Informations are missing.Check Date Start, Dealer and Trademark")
+            raise osv.except_osv("Error!!","Informations are missing. Check Date Start, Dealer and Trademark")
         super(dm_campaign,self).write(cr, uid, ids, {'state':'open'})
         return True
 
@@ -234,12 +236,6 @@ class dm_campaign(osv.osv):
             super(dm_campaign, self).write(cr, uid, cmp_id, {'date_start':0})
         return cmp_id
 
-    def po_generate(self,cr, uid, ids, *args):
-
-
-
-        return True
-
 dm_campaign()
 
 #Postgres view
@@ -253,21 +249,6 @@ class dm_campaign_statistics(osv.osv):
     }
 dm_campaign_statistics()
 
-
-#class dm_campaign_pricelist(osv.osv):
-#    _name = "dm.campaign.pricelist"
-#    _description = "Pricelist"
-#    _columns = {
-#        'name': fields.char('Name',size=64, required=True),
-#        'active': fields.boolean('Active'),
-#        'type': fields.selection([('customer','Customer'),('requirer','Requirer')], 'Pricelist Type', required=True),
-#        'currency_id': fields.many2one('res.currency', 'Currency', required=True),
-#    }
-#
-#    _defaults = {
-#        'active': lambda *a: 1,
-#    }
-#dm_campaign_pricelist()
 
 class dm_campaign_proposition(osv.osv):
     _name = "dm.campaign.proposition"
@@ -337,6 +318,8 @@ class dm_campaign_proposition(osv.osv):
 #        'product_ids' : fields.many2many('dm.product', 'proposition_product_rel', 'proposition_id', 'product_id', 'Catalogue'),
         'payment_methods' : fields.many2many('account.journal','campaign_payment_method_rel','proposition_id','journal_id','Payment Methods',domain=[('type','=','cash')]),
         'keep_segments' : fields.boolean('Keep Segments'),
+        'force_sm_price' : fields.boolean('Force Starting Mail Price'),
+        'sm_price' : fields.float('Starting Mail Price', digits=(16,2)),
 #        'prices_prog_id' : fields.many2one('dm.campaign.proposition.prices_progression', 'Prices Progression'),
         'manufacturing_costs': fields.float('Manufacturing Costs',digits=(16,2)),
     }
@@ -408,14 +391,97 @@ class dm_campaign_delay(osv.osv):
 
 dm_campaign_delay()
 
-class Country(osv.osv):
-    _name = 'res.country'
-    _inherit = 'res.country'
+
+PURCHASE_LINE_TRIGGERS = [
+    ('draft','At Draft'),
+    ('open','At Open'),
+    ('planned','At Planning'),
+    ('close','At Close'),
+    ('manual','Manual'),
+]
+
+PURCHASE_LINE_STATES = [
+    ('pending','Pending'),
+    ('done','Ordered'),
+]
+
+PURCHASE_LINE_CONSTRAINTS = [
+    ('manufacturing','Manufacturing'),
+    ('items','Items'),
+    ('customer_file','Customer Files'),
+]
+
+class dm_campaign_purchase_line(osv.osv):
+    _name = 'dm.campaign.purchase_line'
+    _rec_name = 'product_id'
+
+    def _get_uom_id(self, cr, uid, *args):
+        cr.execute('select id from product_uom order by id limit 1')
+        res = cr.fetchone()
+        return res and res[0] or False
+
+    def po_generate(self,cr, uid, ids, *args):
+        plines = self.browse(cr, uid ,ids)
+
+        for pline in plines:
+            if pline.state != 'done':
+                for supplier in pline.product_id.seller_ids:
+                    partner_id = supplier.id
+                    partner = supplier.name
+
+                    address_id = self.pool.get('res.partner').address_get(cr, uid, [partner_id], ['delivery'])['delivery']
+                    pricelist_id = partner.property_product_pricelist_purchase.id
+                    price = self.pool.get('product.pricelist').price_get(cr, uid, [pricelist_id], pline.product_id.id, pline.quantity, False, {'uom': pline.uom_id.id})[pricelist_id]
+                    newdate = DateTime.strptime(pline.date_planned, '%Y-%m-%d') - DateTime.RelativeDateTime(days=pline.product_id.product_tmpl_id.seller_delay or 0.0)
+
+                    print "DEBUG - newdate : ",newdate.strftime('%Y-%m-%d')
+
+                    line = {
+                       'name': pline.product_id.name,
+                       'product_qty': pline.quantity,
+                       'product_id': pline.product_id.id,
+                       'product_uom': pline.uom_id,
+                       'price_unit': price,
+                       'date_planned': newdate.strftime('%Y-%m-%d'),
+#                       'taxes_id': [(6, 0, [x.id for x in pline.product_id.product_tmpl_id.supplier_taxes_id])],
+#                       'move_dest_id': res_id,
+                    }
+
+                    print "DEBUG - partner_id",partner_id
+                    print "DEBUG - pricelist_id",pricelist_id
+                    print 'partner_address_id',address_id
+                    purchase_id = self.pool.get('purchase.order').create(cr, uid, {
+#                        'origin': procurement.origin,
+                        'partner_id': partner_id,
+                        'partner_address_id': address_id,
+                        'location_id': 1,
+                        'pricelist_id': pricelist_id,
+                        'order_line': [(0,0,line)]
+                    })
+                    print "DEBUG - purchase_id",purchase_id
+#                    self.write(cr, uid, [pline.id], {'state':'done'})
+
+        return purchase_id
+
     _columns = {
-                'main_language' : fields.many2one('res.lang','Main Language',ondelete='cascade',),
-                'main_currency' : fields.many2one('res.currency','Main Currency',ondelete='cascade'),
+        'campaign_id': fields.many2one('dm.campaign', 'Campaign'),
+        'product_id' : fields.many2one('product.product', 'Product', required=True),
+        'quantity' : fields.integer('Quantity', required=True),
+        'uom_id' : fields.many2one('product.uom','UOM', required=True),
+        'date_planned': fields.date('Scheduled date', required=True),
+        'trigger' : fields.selection(PURCHASE_LINE_TRIGGERS, 'Trigger'),
+        'constraint' : fields.selection(PURCHASE_LINE_CONSTRAINTS, 'Constraints'),
+        'state' : fields.selection(PURCHASE_LINE_STATES, 'State'),
     }
-Country()
+
+    _defaults = {
+        'quantity' : lambda *a : 0,
+        'uom_id' : _get_uom_id,
+        'trigger': lambda *a : 'manual',
+        'state': lambda *a : 'pending',
+    }
+dm_campaign_purchase_line()
+
 
 class dm_campaign_proposition_prices_progression(osv.osv):
     _name = 'dm.campaign.proposition.prices_progression'
@@ -425,6 +491,16 @@ class dm_campaign_proposition_prices_progression(osv.osv):
         'percent_prog' : fields.float('Percentage Prices Progression', digits=(16,2)),
     }
 dm_campaign_proposition_prices_progression()
+
+
+class Country(osv.osv):
+    _name = 'res.country'
+    _inherit = 'res.country'
+    _columns = {
+                'main_language' : fields.many2one('res.lang','Main Language',ondelete='cascade',),
+                'main_currency' : fields.many2one('res.currency','Main Currency',ondelete='cascade'),
+    }
+Country()
 
 class res_partner(osv.osv):
     _name = "res.partner"
@@ -442,5 +518,3 @@ class res_partner(osv.osv):
 res_partner()
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
-
-
