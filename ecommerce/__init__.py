4b3fca8b3c4fefd3313e1344ae2b5216ca172342
Sananaz Mansuri <mansuri.sananaz@gmail.com>
2008-09-22 17:04:30 +0530

    * Add Ecommerce Module.

---
+++ ecommerce/__init__.py
@@ -0,0 +1,9 @@
+# -*- encoding: utf-8 -*-
+import tools
+import basic_info
+import partner
+import catalog
+import sale_order
+import report
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+
