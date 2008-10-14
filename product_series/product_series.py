d7ec10f78a10c7cd8151825354299a17375735c5
P. Christeas <p_christ@hol.gr>
2008-10-14 15:30:20 +0300

    Import 'product_series' from Tiny modules.

---
+++ product_series/product_series.py
@@ -0,0 +1,52 @@
+##############################################################################
+#
+# Copyright (c) 2005 TINY SPRL. (http://tiny.be) All Rights Reserved.
+#
+# $Id: product_expiry.py 2103 2006-01-11 21:01:14Z pinky $
+#
+# WARNING: This program as such is intended to be used by professional
+# programmers who take the whole responsability of assessing all potential
+# consequences resulting from its eventual inadequacies and bugs
+# End users who are looking for a ready-to-use solution with commercial
+# garantees and support are strongly adviced to contract a Free Software
+# Service Company
+#
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+##############################################################################
+
+from osv import fields,osv
+class product_series(osv.osv):
+	_name = "product.series"
+	_description = "Partner Product Series"
+	_columns = {
+		'partner_id' : fields.many2one('res.partner','Manufacturer',required=True,select='True'),
+		'code': fields.char('Code', size=64, select='True'),
+		'name': fields.char('Name', size=64, select='True'),
+		'description': fields.char('Description', size=128, select='True'),
+	}
+
+
+product_series()
+
+
+class product_product(osv.osv):
+	_inherit ="product.product"
+	_name = "product.product"
+	_columns = {
+		'series': fields.many2one('product.series','Series'),
+	}
+
+product_product()
