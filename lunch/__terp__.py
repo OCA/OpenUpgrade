c191912705e993d37ba5715736b3f41584022eb5
Priyesh <solanki.priyesh@gmail.com>
2008-10-23 11:19:52 +0530

    process files

--- lunch/__terp__.py
+++ lunch/__terp__.py
@@ -3,9 +3,11 @@
     "name": "Lunch Module",
     "author": "Tiny",
     "version": "0.1",
-    "depends": ["base"],
+    "depends": ["base", "process", "hr"],
     "init_xml": [],
-    "update_xml": ['lunch_wizard.xml', 'lunch_view.xml', 'lunch_report.xml'],
+    "update_xml": ['lunch_wizard.xml', 'lunch_view.xml', 'lunch_report.xml',
+                   'process/lunch_process.xml'
+                   ],
     "demo_xml": ['lunch_demo.xml'],
     "installable": True,
 }
