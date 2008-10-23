4a195b5c1c116c718b3859a8c098997898508ede
stw <>
2008-10-23 14:47:57 +0000

    Add some dependencies

--- profile_ifbl/__terp__.py
+++ profile_ifbl/__terp__.py
@@ -4,7 +4,7 @@
     'version' : '0.0.1',
     'website' : 'http://www.openerp.com',
     'description' : 'IFBL Profile',
-    'depends' : ['training'],
+    'depends' : ['training', 'crm', 'project', 'account_analytic_analysis'],
     'init_xml' : [],
     'update_xml' : [],
     'demo_xml' : [],
