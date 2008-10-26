73aa38c644c2cf9833a83e6249d0eaf8056743cf
stw <>
2008-10-26 18:02:04 +0000

    add the Profile category for this module

--- profile_ifbl/__terp__.py
+++ profile_ifbl/__terp__.py
@@ -4,6 +4,7 @@
     'version' : '0.0.1',
     'website' : 'http://www.openerp.com',
     'description' : 'IFBL Profile',
+    'category' : 'Profile',
     'depends' : ['training', 'crm', 'project', 'account_analytic_analysis'],
     'init_xml' : [],
     'update_xml' : [],
