445224ae9a1ce8fdcad49584a997d25f6f27e0a4
Stephane Wirtel <stephane@tinyerp.com>
2008-11-04 17:41:24 +0100

    Rename the object

--- base_contact_team/base_contact_team.py
+++ base_contact_team/base_contact_team.py
@@ -1,7 +1,7 @@
 from osv import osv, fields
 
-class quality_team(osv.osv):
-    _name = 'base.contact.team'
+class res_partner_team(osv.osv):
+    _name = 'res.partner.team'
 
     _columns = {
         'name' : fields.char('Name', size=64, required=True, select=True),
@@ -14,7 +14,7 @@ class res_partner_job(osv.osv):
     _inherit = 'res.partner.job'
 
     _columns = {
-        'quality_team_id' : fields.many2one('base.contact.team', 'Team'),
+        'quality_team_id' : fields.many2one('res.partner.team', 'Team'),
     }
 
 res_partner_job()
