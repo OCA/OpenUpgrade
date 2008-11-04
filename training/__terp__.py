402ba563644c441e2143741edd21a85bfa36ef43
stw <>
2008-11-04 13:56:31 +0000

    Add the product dependency

--- training/__terp__.py
+++ training/__terp__.py
@@ -4,7 +4,7 @@
     'author' : 'Tiny SPRL',
     'website' : 'http://www.openerp.com',
     'description' : 'Training',
-    'depends' : ['account', 'base_contact', 'base_iban'],
+    'depends' : ['account', 'base_contact', 'base_iban', 'product'],
     'init_xml' : [],
     'demo_xml' : [],
     'update_xml' : [
