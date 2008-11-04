baed205e8e7d364df8ef8a2c96e5a5e88bc24afe
Stephane Wirtel <stephane@tinyerp.com>
2008-11-04 17:34:24 +0100

    Add the description

--- base_contact_team/__terp__.py
+++ base_contact_team/__terp__.py
@@ -3,7 +3,7 @@
     'author' : 'Tiny SPRL',
     'version' : '0.1',
     'website' : 'http://www.openerp.com',
-    'description' : '',
+    'description' : 'With this module, you can add a team to your contacts',
     'depends' : ['base_contact'],
     'init_xml' : [],
     'update_xml' : ['base_contact_team_view.xml'],
