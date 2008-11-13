044c9421d7b6ce7263292b696c69bdbdbc7013c3
P. Christeas <p_christ@hol.gr>
2008-11-13 19:23:00 +0200

    Fix lunch, hr_holidays
    Let these modules load.

--- hr_holidays/report/__init__.py
+++ hr_holidays/report/__init__.py
@@ -19,6 +19,6 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-import sandwich_report
+import holidays_summary_report
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
 
---
+++ lunch/__init__.py
@@ -0,0 +1,5 @@
+# -*- encoding: utf-8 -*-
+
+import lunch
+import wizard
+import report
