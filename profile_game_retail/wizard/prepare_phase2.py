77a650e61f7661bf29fc8b2e175dcb0d9c71bf31
Naresh Choksy <nch@tinyerp.com>
2008-12-03 19:01:04 +0530

    business game further progress and solved a small bug in sale_forecast

--- profile_game_retail/wizard/prepare_phase2.py
+++ profile_game_retail/wizard/prepare_phase2.py
@@ -25,6 +25,7 @@ import netsvc
 
 def get_ready_phase2(self, cr, uid, data, context):
         pool = pooler.get_pool(cr.dbname)
+        lm_action = ['menu_stock_planning','menu_action_orderpoint_form']
         sm_group = ['Purchase / Manager','Purchase / User','Employee','Finance / Accountant','Finance / Invoice']
         sm_group_ids = pool.get('res.groups').search(cr,uid,[('name','in',sm_group)])
         sm_roles = ['Purchase','Invoice']
@@ -33,19 +34,54 @@ def get_ready_phase2(self, cr, uid, data, context):
         phase1_obj = pool.get('profile.game.retail.phase1')
         obj = phase1_obj.browse(cr,uid,data['id'])
 
-        user_ids=pool.get('res.users').search(cr,uid,[])
-        user_browse=pool.get('res.users').browse(cr,uid,user_ids)
+        user_ids = pool.get('res.users').search(cr,uid,[])
+        user_browse = pool.get('res.users').browse(cr,uid,user_ids)
+
+        sc_ids = pool.get('ir.ui.view_sc').search(cr,uid,[('user_id','in',user_ids)])
+        pool.get('ir.ui.view_sc').unlink(cr,uid,sc_ids)
 
         result = mod_obj._get_id(cr, uid, 'profile_game_retail', 'open_board_game2')
         id = mod_obj.read(cr, uid, [result], ['res_id'])[0]['res_id']
         for user in user_ids:
             pool.get('res.users').write(cr,uid,user,{'action_id':id})
 
-        sale_user=pool.get('res.users').search(cr,uid,[('login','=','sale')])
-        cr.execute('delete from res_groups_users_rel where uid =%d'%(sale_user[0]))
-        cr.execute('delete from res_roles_users_rel where uid =%d'%(sale_user[0]))
-        pool.get('res.users').write(cr,uid,sale_user[0],{'groups_id':[[6,0,sm_group_ids]],
-                                                    'roles_id':[[6,0,sm_role_ids]]})
+        for user in user_browse:
+            if user.login in ('sale','finance'):
+                val = {}
+                if user.login=='sale':
+                    cr.execute('delete from res_groups_users_rel where uid =%d'%(user.id))
+                    cr.execute('delete from res_roles_users_rel where uid =%d'%(user.id))
+                    pool.get('res.users').write(cr,uid,user.id,{'groups_id':[[6,0,sm_group_ids]],
+                                                                'roles_id':[[6,0,sm_role_ids]]})
+                    res=mod_obj._get_id(cr, uid, 'sale_forecast', 'menu_sale_forecast_my_managing')
+
+                if user.login=='finance':
+                    res=mod_obj._get_id(cr, uid, 'account_budget', 'menu_budget_post_form')
+
+                res_id = mod_obj.read(cr, uid, [res], ['res_id'])[0]['res_id']
+                val['res_id']=res_id
+                val['resource']='ir.ui.menu'
+                val['user_id'] =user.id
+                val['name']=pool.get('ir.ui.menu').read(cr,uid,[res_id],['name'])[0]['name']
+                pool.get('ir.ui.view_sc').create(cr,uid,val)
+
+            if user.login=='logistic':
+                for action in lm_action:
+                    val={}
+                    if action =='menu_action_orderpoint_form':
+                        module='mrp'
+                    else:
+                        module='stock_planning'
+                    res=mod_obj._get_id(cr, uid, module, action)
+                    res_id = mod_obj.read(cr, uid, [res], ['res_id'])[0]['res_id']
+                    val['res_id']=res_id
+                    val['resource']='ir.ui.menu'
+                    val['user_id'] =user.id
+                    val['name']=pool.get('ir.ui.menu').read(cr,uid,[res_id],['name'])[0]['name']
+                    pool.get('ir.ui.view_sc').create(cr,uid,val)
+
+
+
       #  phase1_obj.write(cr,uid,data['id'],{'state':'started_phase2'})
         return  {
         'name': 'Business Game',
