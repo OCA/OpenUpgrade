ddc6dfab6fee37ced2c9ed49a4b9b9773948f449
rvalyi <>
2008-12-19 01:57:11 +0000

    fixed issue http://code.google.com/p/magento-openerp-smile-synchro/issues/detail?id=23 thanks to eLBaddy

--- __terp__.py
+++ __terp__.py
@@ -4,7 +4,8 @@
     "author" : "Smile",
     "category" : "Interfaces/CMS & eCommerce",
     "website" : "http://code.google.com/p/magento-openerp-smile-synchro/",
-    "depends" : ["product", "stock", "sale", "account", "account_tax_include",],
+    #please notice that the delivery module is only used to add attach a shipping price line
+    "depends" : ["product", "stock", "sale", "account", "account_tax_include", "delivery"],
     "description": """Magento OpenERP Interface - This module allows product catalog 
 and sale orders synchronization between OpenERP and Magento.
 This module is contributed by the Smile French IT company
