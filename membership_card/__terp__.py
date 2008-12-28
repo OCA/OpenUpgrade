ac2e569b9aca01279f544f5a1a2393b810f41c2a
Jordi Esteve <jesteve@alba>
2008-12-28 22:06:42 +0100

    Added new module membership_card

---
+++ membership_card/__terp__.py
@@ -0,0 +1,18 @@
+# -*- encoding: utf-8 -*-
+{
+  "name" : "Membership card",
+  "version" : "1.0",
+  "author" : "Zikzakmedia SL",
+  "category" : "Generic Modules/Association",
+  "website": "http://www.zikzakmedia.com",
+  "description": """Adds a photo field to partner contact.
+Prints membership cards with name, number, bar code and photo of contacts.
+  """,
+  "depends" : ["base"],
+  "init_xml" : [],
+  "demo_xml" : [],
+  "update_xml" : ["membership_card_view.xml","membership_card_report.xml",],
+  "active": False,
+  "installable": True
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
