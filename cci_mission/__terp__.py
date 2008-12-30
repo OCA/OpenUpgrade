258f1f4ef6582143d6ed46f51aeec6182d232b7c
qdp <qdp@tinyerp.com>
2008-12-30 15:33:33 +0100

    *bgufix security

--- cci_mission/__terp__.py
+++ cci_mission/__terp__.py
@@ -31,7 +31,7 @@
     "depends" : ["base","crm","cci_partner","product","membership", "sale","cci_event","cci_account","cci_translation","cci_country"],
     "init_xml" : [],
     "demo_xml" : ["cci_mission_data.xml"],
-    "update_xml" : ["cci_mission_view.xml","cci_mission_wizard.xml","cci_mission_report.xml","cci_mission_workflow.xml",'security/ir.model.access.csv','security/security.xml','security/cci_missions_security.xml'],
+    "update_xml" : ["cci_mission_view.xml","cci_mission_wizard.xml","cci_mission_report.xml","cci_mission_workflow.xml",'security/security.xml','security/cci_missions_security.xml','security/ir.model.access.csv'],
     "active": False,
     "installable": True
 }
