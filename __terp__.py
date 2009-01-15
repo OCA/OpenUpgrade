644574f6164d91b3824bef9e8c58902e64672bca
rvalyi <>
2009-01-15 16:33:29 +0000

    no, actually the delivery module is NOT a required dependency as we add our own delivery product by default.

--- __terp__.py
+++ __terp__.py
@@ -5,7 +5,7 @@
     "category" : "Interfaces/CMS & eCommerce",
     "website" : "http://code.google.com/p/magento-openerp-smile-synchro/",
     #please notice that the delivery module is only used to add attach a shipping price line
-    "depends" : ["product", "stock", "sale", "account", "account_tax_include", "delivery"],
+    "depends" : ["product", "stock", "sale", "account", "account_tax_include"],
     "description": """Magento OpenERP Interface - This module allows product catalog 
 and sale orders synchronization between OpenERP and Magento.
 This module is contributed by the Smile French IT company
