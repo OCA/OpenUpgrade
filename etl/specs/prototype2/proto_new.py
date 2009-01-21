c746a05d6b0d4d7c37d5b02482c502df8131372f
Harry (Open ERP) <hmo@tinyerp.com>
2009-01-21 14:48:23 +0530

    diff etl component

--- etl/specs/prototype2/proto_new.py
+++ etl/specs/prototype2/proto_new.py
@@ -1,4 +1,4 @@
-#csv_in ---> logger
+
 import csv
 import sys
 import time
@@ -19,14 +19,14 @@ class component(object):
         return self.generator
 
     def channel_get(self, trans=None):
-        self.data.setdefault(trans, [])
+        self.data.setdefault(trans, [])        
         gen = self.generator_get(trans) or []
         while True:
             if self.data[trans]:
                 yield self.data[trans].pop(0)
                 continue
             elif self.data[trans] is None:
-                raise StopIteration
+                raise StopIteration            
             data, chan = gen.next()
             if data is None:
                 raise StopIteration
@@ -65,7 +65,7 @@ class csv_out(component):
         datas = []
         for channel,trans in self.input_get().items():
             for iterator in trans:
-                for d in iterator:
+                for d in iterator:                    
                     if not fp2:
                         fp2 = file(self.filename, 'wb+')
                         fieldnames = d.keys()
@@ -96,7 +96,7 @@ class logger_bloc(component):
         self.name = name
         self.output = output
         self.is_end = 'main'
-        super(logger, self).__init__(*args, **argv)
+        super(logger_bloc, self).__init__(*args, **argv)
 
     def process(self):
         datas=[]
@@ -131,9 +131,53 @@ class logger(component):
     def process(self):
         for channel,trans in self.input_get().items():
             for iterator in trans:
-                for d in iterator:
+                for d in iterator:                    
                     self.output.write('\tLog '+self.name+str(d)+'\n')
                     yield d, 'main'
+
+class diff(component):
+    def __init__(self, keys, *args, **argv):
+        self.keys = keys
+        self.row = {}
+        self.diff = []
+        self.same = []
+        super(diff, self).__init__(*args, **argv)
+
+    # Return the key of a row
+    def key_get(self, row):
+        result = []
+        for k in self.keys:
+            result.append(row[k])
+        return tuple(result)
+
+    def process(self):  
+        self.row = {}      
+        for channel,transition in self.input_get().items():            
+            if channel not in self.row:
+                self.row[channel] = {}
+            other = None            
+            for key in self.row.keys():
+                if key<>channel:
+                    other = key
+                    break
+            for iterator in transition:
+                for r in iterator: 
+                    key = self.key_get(r)
+                    if other and (key in self.row[other]):
+                        if self.row[other][key] == r:
+                            yield r, 'same'                            
+                        else:
+                            yield r, 'update' 
+                        del self.row[other][key]
+                    else:
+                        self.row[channel][key] = r         
+        todo = ['add','remove']
+        for k in self.row:     
+            channel= todo.pop()                   
+            for v in self.row[k].values():
+            	yield v,channel               
+    
+
 
 class transition(object):
     def __init__(self, source, destination,type='data_transition', status='open', channel_source='main', channel_destination='main'):
@@ -169,11 +213,39 @@ tran4=transition(sleep1,log2)
 tran5=transition(sort1,csv_out1)
 
 
-job1=job([csv_out1,log2])
-job1.run()
+#job1=job([csv_out1,log2])
+#job1.run()
 
-# this is not work, log2 can not get data
 
-#job2=job([log1,sort1,csv_out1,log2])
-#job2.run(0)
+in1 = csv_in('partner.csv')
+in2 = csv_in('partner2.csv')
+diff1 = diff(['id'])
+
+log_1 = logger_bloc(name="Original Data")
+log_2 = logger_bloc(name="Modified Data")
+
+log1 = logger(name="Log Same")
+log2 = logger(name="Log Add")
+log3 = logger(name="Log Remove")
+log4 = logger(name="Log Update")
+
+
+csv_out1 = csv_out('add.csv')
+
+transition(in1, log_1)
+transition(in2, log_2)
+
+transition(in1, diff1, channel_destination='original')
+transition(in2, diff1, channel_destination='modified')
+
+transition(diff1, log1, channel_source="same")
+transition(diff1, log3, channel_source="remove")
+transition(diff1, log2, channel_source="add")
+transition(diff1, csv_out1, channel_source="add")
+transition(diff1, log4, channel_source="update")
+
+job = job([log_1,log_2,diff1,log1,log2,log3,log4,csv_out1])
+job.run()
+
+
 
