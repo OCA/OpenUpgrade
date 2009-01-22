eed1996cc6174e29264cfc87369b5d362205a622
Harry (Open ERP) <hmo@tinyerp.com>
2009-01-22 18:07:59 +0530

    modif ETL

---
+++ etl/lib/etl/component/control/data_count.py
@@ -0,0 +1,42 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    ETL system- Extract Transfer Load system
+#    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>). All Rights Reserved
+#    $Id$
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+
+import etl
+
+class control_count(etl.component):
+    """
+    ETL Data Count Control Component:
+    * calculate total of tranfering data
+
+    """
+    def process(self):
+        datas = {}
+        for channel,trans in self.input_get().items():
+            for iterator in trans:
+                for d in iterator:
+                    datas.setdefault(channel, 0)
+                    datas[channel] += 1
+
+        for d in datas:
+            yield {'channel': d, 'count': datas[d]}, 'main'
+
---
+++ etl/lib/etl/component/input/csv_in.py
@@ -0,0 +1,45 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    ETL system- Extract Transfer Load system
+#    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>). All Rights Reserved
+#    $Id$
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+
+import etl
+import csv
+
+class csv_in(etl.component):
+    """
+        This is an ETL Component that use to read data from csv file.
+       
+		Type: Data Component
+		Computing Performance: Streamline
+		Input Flows: 0
+		* .* : nothing
+		Output Flows: 0-x
+		* .* : return the main flow with data from csv file
+    """
+    def __init__(self, filename, *args, **argv):
+        super(csv_in, self).__init__(*args, **argv)
+        self.filename = filename
+
+    def process(self):
+        fp = csv.DictReader(file(self.filename))
+        for row in fp:
+            yield row, 'main'
