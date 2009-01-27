23d86e7c63bf08aa5a9f9a8bb129ab40f850822a
P. Christeas <p_christ@hol.gr>
2009-01-27 12:52:32 +0200

    Timesheet invoice: map to unicode strings, not just str.
    This fixes invoice lines with non-latin characters.

--- hr_timesheet_invoice/wizard/hr_timesheet_invoice_create.py
+++ hr_timesheet_invoice/wizard/hr_timesheet_invoice_create.py
@@ -145,9 +145,9 @@ class invoice_create(wizard.interface):
                         details.append(line['name'])
                     #if data['form']['price']:
                     #   details.append(abs(line['amount']))
-                    note.append(' - '.join(map(str,details)))
+                    note.append(u' - '.join(map(unicode,details)))
 
-                curr_line['note'] = "\n".join(map(str,note))
+                curr_line['note'] = "\n".join(map(unicode,note))
                 pool.get('account.invoice.line').create(cr, uid, curr_line)
                 strids = ','.join(map(str, data['ids']))
                 cr.execute("update account_analytic_line set invoice_id=%%s WHERE account_id = %%s and id IN (%s)" % strids, (last_invoice,account.id,))
