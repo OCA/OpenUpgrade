12ae03cbcca79fc5ac18aed2a76e7ba7726581ff
Sofia (Open ERP) <sso@tinyerp.com>
2009-01-28 12:34:42 +0530

    migration of dm_customer_order to sale_order

--- dm/__terp__.py
+++ dm/__terp__.py
@@ -46,7 +46,7 @@
             - Manage copywriters, brokers, dealers, addresses deduplicators and cleaners
 
             """,
-    "depends" : ["project_retro_planning","purchase","purchase_tender","base_language","document"],
+    "depends" : ["project_retro_planning","purchase","purchase_tender","base_language","document","sale"],
     "init_xml" : [ ],
     "demo_xml" : [
                     "dm_demo.xml"
