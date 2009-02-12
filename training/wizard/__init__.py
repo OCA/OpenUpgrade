873bd57b5ff97964a9e3b83fb192a74dfafc1fcf
stw <>
2009-02-12 07:08:05 +0000

    [IMP] Create all directories (report,wizard,security,i18n)

--- training/wizard/__init__.py
+++ training/wizard/__init__.py
@@ -19,10 +19,5 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-import partner
-import training
-
-import report
-import wizard
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
