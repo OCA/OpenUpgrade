e58b645d66da6b4302286893b7827b5fc31f6157
CB <claude.brule@alternatique.com>
2009-02-16 04:57:37 +0100

    [IMP] use even with no partner

--- crm_telemarketing/wizard/direct_poll.py
+++ crm_telemarketing/wizard/direct_poll.py
@@ -71,15 +71,16 @@ class direct_poll(wizard.interface):
         temp.append(answer)
         crm_obj = pooler.get_pool(cr.dbname).get('crm.case')
         partner = crm_obj.read(cr, uid, data['id'], ['partner_id'], context)
-        partner_id = partner['partner_id'][0]
 
-        query = "select answer from partner_question_rel where partner=%s"
-        cr.execute(query, (partner_id,))
-        for x in cr.fetchall():
-            temp.append(x[0])
+        if partner['partner_id']:
+            partner_id = partner['partner_id'][0]
+            query = "select answer from partner_question_rel where partner=%s"
+            cr.execute(query, (partner_id,))
+            for x in cr.fetchall():
+                temp.append(x[0])
 
-        partner_obj = pooler.get_pool(cr.dbname).get('res.partner')
-        partner_obj.write(cr, uid, [partner_id],{'answers_ids':[[6,0,temp]]}, context)
+            partner_obj = pooler.get_pool(cr.dbname).get('res.partner')
+            partner_obj.write(cr, uid, [partner_id],{'answers_ids':[[6,0,temp]]}, context)
 
         # Check if the last answer attached to a new question.
         answer_obj = pooler.get_pool(cr.dbname).get('crm_profiling.answer')
