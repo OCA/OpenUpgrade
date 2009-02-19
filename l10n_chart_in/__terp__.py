dbcf0780ddb10863dae43d8cccde234b91dba68a
mga (Tiny/Axelor) <mga@tinyerp.com>
2009-02-19 14:26:57 +0530

    merging from the indian accounting development branch

--- l10n_chart_in/__terp__.py
+++ l10n_chart_in/__terp__.py
@@ -1,20 +1,14 @@
 # -*- encoding: utf-8 -*-
 {
-    "name" : "India - Chart of Accounts",
+    "name" : "India",
     "version" : "1.0",
     "author" : "Tiny",
-    "category" : "Localisation/Account Charts",
-    "depends" : ["account_chart"],
-    "description": "This is the module to manage the accounting chart for India in Open ERP.",
+    "category" : "Localisation/Account charts",
+    "depends" : ["account", "account_chart"],
     "demo_xml" : [],
-<<<<<<< TREE
-    "update_xml" : ["account_tax_code.xml","account_chart.xml",
-                    "account_tax.xml","l10n_chart_in_wizard.xml"
-=======
     "update_xml" : [
         "account_chart.xml",
         "l10n_in_wizard.xml",
->>>>>>> MERGE-SOURCE
     ],
     "active": False,
     "installable": True
