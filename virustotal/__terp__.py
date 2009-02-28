e73a06b4590b7e16dd9192f0956ff7962da2e1a6
Christophe Simonis <christophe@taupe>
2009-02-28 23:00:30 +0100

    [NEW] new module: virustotal

---
+++ virustotal/__terp__.py
@@ -0,0 +1,11 @@
+{
+    'name': "Allow attachments to be checked via virustotal.com",
+    'version': '1.0',
+    'author': 'Christophe Simonis',
+    'description': 'This module allow users to send any attachment to virustotal.com in order to be checked with latest antivirus engines',
+    'depends': ['base'],
+    'init_xml': [],
+    'update_xml': ['attachment_view.xml'],
+    'active': False,
+    'installable': True,
+}
