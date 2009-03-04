94562c6d9068487fabe6970d1b70f15cd090dccb
rpa (Open ERP) <rpa@tinyerp.com>
2009-03-04 19:14:29 +0530

    Added module etl in etl: Under development

---
+++ etl/__terp__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'new',
+    'version': '1.0',
+    'category': 'Generic Modules/Others',
+    'description': """
+    The base module to manage of the  documents """,
+    'author': 'Tiny & Axelor',
+    'website': 'http://openerp.com',
+    'depends': ['base'],
+    'init_xml': [],
+    'update_xml': ['etl.xml'],
+    'installable': True,
+    'active': False,
+    }
