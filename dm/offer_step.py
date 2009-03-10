4dbee0192f8ea7881338e70b8ceb9bb86aa9a05b
Sofia (Open ERP) <sso@tinyerp.com>
2009-03-10 21:30:00 +0530

    changes for the offer step action by mpa

--- dm/offer_step.py
+++ dm/offer_step.py
@@ -59,6 +59,16 @@ class dm_offer_step_type(osv.osv):
 
 dm_offer_step_type()
 
+class dm_offer_step_action(osv.osv):
+    _name = 'dm.offer.step.action'
+    _inherits = {'ir.actions.server':'server_action_id'}
+    _columns = {
+        'server_action_id' : fields.many2one('ir.actions.server','Server Action'),
+        'media_id' : fields.many2one('dm.media','Media')
+    }
+    
+dm_offer_step_action()
+
 class dm_offer_step(osv.osv):
     _name = "dm.offer.step"
 
@@ -105,7 +115,9 @@ class dm_offer_step(osv.osv):
         'split_mode' : fields.selection([('and','And'),('or','Or'),('xor','Xor')],'Split mode'),
         'doc_number' : fields.integer('Number of documents of the mailing', states={'closed':[('readonly',True)]}),
         'manufacturing_constraint_ids' : fields.many2many('product.product','dm_offer_step_manufacturing_product_rel','product_id','offer_step_id','Mailing Manufacturing Products',domain=[('categ_id', 'ilike', 'Mailing Manufacturing')], states={'closed':[('readonly',True)]}),
-        'action' : fields.many2one('ir.actions.server', string="Action", required=True, domain="[('dm_action','=',True)]"),
+#        use this line if step action wont wrk
+#        'action' : fields.many2one('actions_server', string="Action", required=True, domain="[('dm_action','=',True)]"),
+        'action' : fields.many2one('dm.offer.step.action', string="Action", required=True, domain="[('dm_action','=',True)]"),
     }
 
     _defaults = {
@@ -230,5 +242,8 @@ class actions_server(osv.osv):
     }
     
 actions_server()
+
+
+    
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
 
