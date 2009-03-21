e5cce7f227c7890f7c1da3c5fbe8761bc8011a94
P. Christeas <p_christ@hol.gr>
2009-03-21 09:51:45 +0200

    Product EAN always module.
    Since the EAN is the field to search for with Barcode, it should
    be there even in the simplified view. Promote it.

---
+++ product_ean_always/__terp__.py
@@ -0,0 +1,14 @@
+{
+        "name" : "Product EAN always",
+        "version" : "0.1",
+        "author" : "P. Christeas",
+        "website" : "http://www.openerp.com",
+        "category" : "Generic Modules/Others",
+        "description": """Make product EAN always visible and selectable, even in
+simplified view.""",
+        "depends" : ["product","base"],
+        "init_xml" : [ ],
+        "demo_xml" : [ ],
+        "update_xml" : ["product_ean_always_data.xml"],
+        "installable": True
+} 
