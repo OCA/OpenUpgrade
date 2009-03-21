bfe1fa2eae33d204e1c71ce0f8180121773a6269
P. Christeas <p_christ@hol.gr>
2009-03-21 09:58:02 +0200

    POS print: code the algorithm.

--- point_of_sale_print/test_posprint.py
+++ point_of_sale_print/test_posprint.py
@@ -2,16 +2,48 @@
 
 dict = { 'a' : 'Alpha', 'b' : 'Beta', 'g': 'Gamma' }
 
-report = '''
-
+report = '''$\\
 	$a\t bc $$ \vssd\n
+	
+	$other
+	$other
+
+$\other
+	some other string
 '''
 
 from string import Template
 
 s = Template(report.decode('string_escape'))
 
+def Do_report(report, dict):
+	sections= {}
+	main_section = None
+	dict2= dict
+	if report.startswith('$\\'):
+		cur_sec='';
+		for line in report.splitlines(True):
+			if line.startswith('$\\'):
+				#current line
+				cur_args=line[2:].strip().split(' ')
+				cur_sec=cur_args[0]
+				if len(cur_sec):
+					sections[cur_sec]= ''
+				else:
+					main_section=''
+				#TODO: process args[1..]
+			else:
+				if len(cur_sec):
+					sections[cur_sec]+=line.decode('string_escape')
+				else:
+					main_section +=line.decode('string_escape')
+		
+	else:
+		main_section = report.decode('string_escape')
+	
+	dict2.update(sections)
+	return Template(main_section).substitute(dict2)
 
-print s.substitute(dict)
+print Do_report(report,dict)
 
 #eof
