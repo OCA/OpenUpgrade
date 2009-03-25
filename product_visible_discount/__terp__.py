034845e3866997f63ff584f7b24b27cdf5cd2390
Numerigraphe - Lionel Sausin <ls@numerigraphe.fr>
2009-03-25 13:20:40 +0100

    product_visible_discount : improved module description, moved to the category sales&purchase.

--- product_visible_discount/__terp__.py
+++ product_visible_discount/__terp__.py
@@ -20,18 +20,17 @@
 #
 ##############################################################################
 {
-    "name":"Visible Discount Module",
+    "name":"Visible Discount",
     "version":"1.0",
     "author":"Tiny",
-    "category":"Generic Modules/Inventory Control",
+    "category":"Generic Modules/Sales & Purchases",
     "description": """
-    This module use for calculate discount amount on Sale order line and invoice line  base on partner's pricelist
-    For that,On the pricelists form, new check box called "Visible Discount" is added.
+    This module lets you calculate discounts on Sale Order lines and Invoice lines base on the partner's pricelist.
+    To this end, a new check box named "Visible Discount" is added to the pricelist form.
     Example:
-        For product PC1, listprice=450, for partner Asustek, his pricelist calculated is 225 for PC1
-        If the check box is ticked, we will have on the SO line (and so also on invoice line): Unit price=450, Discount=50,00, Net price=225
-        If the check box is NOT ticked, we will have on SO and Invoice lines: Unit price=225, Discount=0,00, Net price=225
-
+        For the product PC1 and the partner "Asustek": if listprice=450, and the price calculated using Asustek's pricelist is 225
+        If the check box is checked, we will have on the sale order line: Unit price=450, Discount=50,00, Net price=225
+        If the check box is unchecked, we will have on Sale Order and Invoice lines: Unit price=225, Discount=0,00, Net price=225
     """,
     "depends":["base","product","account","sale"],
     "demo_xml":[],
