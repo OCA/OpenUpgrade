9f72ec0324be01c6c6ecbcfabae7456c49ab17a8
rpa (Open ERP) <rpa@tinyerp.com>
2009-03-26 14:59:38 +0530

    [ETL_INTERFACE]: Minor Modifications

--- etl_interface/wizard/wizard_launch_process.py
+++ etl_interface/wizard/wizard_launch_process.py
@@ -28,36 +28,38 @@ import pooler
 def _open_process(self, cr, uid, data, context):
     pool = pooler.get_pool(cr.dbname)
     obj_process=pool.get('etl.job.process')
+    obj_job=pool.get('etl.job')
     process_ids = []
     for id in data['ids']:
-        name = pool.get('ir.sequence').get(cr, uid, 'etl.job.process')
-        process_id=obj_process.create(cr, uid, {'name': name, 'job_id': id})
-        process_ids.append(process_id)
-        wf_service = netsvc.LocalService("workflow")
-        wf_service.trg_validate(uid, 'etl.job.process', process_id, 'action_open', cr)
+        job = obj_job.browse(cr, uid, id)
+        if job.state == 'open':
+            name = pool.get('ir.sequence').get(cr, uid, 'etl.job.process')
+            process_id=obj_process.create(cr, uid, {'name': name, 'job_id': id})
+            process_ids.append(process_id)
+            wf_service = netsvc.LocalService("workflow")
+            wf_service.trg_validate(uid, 'etl.job.process', process_id, 'action_open', cr)
     cr.execute('select id,name from ir_ui_view where model=%s and type=%s', ('etl.job.process', 'tree'))
     view_res = cr.fetchone()
     return {
-    'name': 'Job Process',
-    'view_type': 'form',
-    'view_mode': 'tree,form',
-    'res_model': 'etl.job.process',
-    'view_id': view_res,
-    'type': 'ir.actions.act_window',
+    'name': 'Job Process', 
+    'view_type': 'form', 
+    'view_mode': 'tree,form', 
+    'res_model': 'etl.job.process', 
+    'view_id': view_res, 
+    'type': 'ir.actions.act_window', 
     'domain' : "[('id','in',%s)]" % process_ids
     }
 
 class open_process(wizard.interface):
     states = {
         'init' : {
-            'actions' : [],
-            'result' : {'type' : 'action',
-                    'action' : _open_process,
+            'actions' : [], 
+            'result' : {'type' : 'action', 
+                    'action' : _open_process, 
                     'state' : 'end'}
-        },
+        }, 
     }
 open_process("etl.launch.job.process")
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
-
 
