6cd3ebbbb19ed591fbc9254aefc21e2a9d9a5f28
Stephane Wirtel <stephane@tinyerp.com>
2009-03-30 10:47:07 +0200

    [IMP] Add a dependency to the document_lock module

--- profile_ifbl/__init__.py
+++ profile_ifbl/__init__.py
@@ -19,7 +19,6 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-import document
 import training
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
