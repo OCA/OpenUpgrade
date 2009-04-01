322c787846b855a77d10bd1ae73348282a1109a9
Harry (Open ERP) <hmo@tinyerp.com>
2009-04-01 19:39:52 +0530

    [ETL]improve

--- etl/lib/etl/component/output/csv_out.py
+++ etl/lib/etl/component/output/csv_out.py
@@ -32,75 +32,60 @@ import datetime
 
 class csv_out(component):
     """
-        This is an ETL Component that use to write data to csv file.
+    This is an ETL Component that use to write data to csv file.
 
-        Type: Data Component
-        Computing Performance: Streamline
-        Input Flows: 0-x
-        * .* : the main data flow with input data
-        Output Flows: 0-1
-        * main : return all data
+    Type: Data Component
+    Computing Performance: Streamline
+    Input Flows: 0-x
+    * .* : the main data flow with input data
+    Output Flows: 0-1
+    * main : return all data
     """   
 
     def __init__(self,fileconnector,transformer=None,name='component.output.csv_out',row_limit=0, csv_params={}):
-
-	""" 
-    Parameters ::
-	fileconnector : Provides  localfile connector to connect with file
-	transformer   : Provides transformer object to transform string data into  particular object
-	row_limit     : Limited records send to destination if row limit specified. If row 	limit is 0,all records are send.
-	csv_param     : To specify other csv parameter like fieldnames , restkey , restval etc. 
-	"""
+        """ 
+        Parameters ::
+        fileconnector : Provides  localfile connector to connect with file
+        transformer   : Provides transformer object to transform string data into  particular object
+        row_limit     : Limited records send to destination if row limit specified. If row     limit is 0,all records are send.
+        csv_param     : To specify other csv parameter like fieldnames , restkey , restval etc. 
+        """
         super(csv_out, self).__init__(name,transformer=transformer)      
           
         self.fileconnector = fileconnector 
         self.csv_params=csv_params       
-        self.row_limit=row_limit 
-        self.row_count=0                                
-        self.fp=None
-        self.writer=None   
-        self.name = name
+        self.row_limit=row_limit                                  
 
-    def action_end(self,key,singal_data={},data={}):        
-        super(csv_out, self).action_end(key,singal_data,data)
-        if self.fp:     
-             self.fp.close() 
-        if self.fileconnector:    
-             self.fileconnector.close()        
+    
 
     def process(self):  
         #TODO : proper handle exception. not use generic Exception class      
         datas = []        
+        fp=False
+        writer=False
         for channel,trans in self.input_get().items():
             for iterator in trans:
                 for d in iterator:
                     try:                    
-                        if not self.fp:
-                            self.fp=self.fileconnector.open('wb+')
+                        if not fp:
+                            fp=self.fileconnector.open('wb+')
                             fieldnames = d.keys()
-                            self.writer = csv.DictWriter(self.fp, fieldnames)
-                            self.writer.writerow(dict(map(lambda x: (x,x), fieldnames)))
-                        self.writer.writerow(d)
+                            writer = csv.DictWriter(fp, fieldnames)
+                            writer.writerow(dict(map(lambda x: (x,x), fieldnames)))
+                        writer.writerow(d)
                         yield d, 'main'
+                    except StopIteration,e:
+                        self.fileconnector.close(fp)
+                        self.signal('end')
                     except IOError,e:  
-                        self.action_error(e)
+                        self.signal('error',{'data':self.data,'type':'exception','error':str(e)})
 
-    def __copy__(self):
-        """
-        Overrides copy method
-        """
+    def __copy__(self):        
         res= csv_out(self.fileconnector , self.name, self.transformer, self.row_limit, self.csv_params)
         return res
 
 def test():
-    from etl_test import etl_test
-    import etl
-    file_conn=etl.connector.file_connector('http://localhost:8069', 'dms_20090204', 'admin', 		              
-    'admin',con_type='xmlrpc')
-    test=etl_test.etl_component_test(csv_out('test',file_conn,'res.partner'))
-    test.check_input({'main':[{'name':'OpenERP1'},{'name':'Fabien1'}]})
-    test.check_output([{'name':'OpenERP1'},{'name':'Fabien1'}],'main')
-    res=test.output()
+    pass
 if __name__ == '__main__':
     test()
 
