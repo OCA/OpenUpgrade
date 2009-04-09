a4fa00e2d7b575abe3009c5e789fcba065ad2457
Sofia (Open ERP) <sso@tinyerp.com>
2009-04-09 12:03:27 +0530

    modifs for offer step code

--- dm/offer_step.py
+++ dm/offer_step.py
@@ -126,7 +126,7 @@ class dm_offer_step(osv.osv):
     def onchange_code(self,cr,uid,ids,type_id):
         step_type = self.pool.get('dm.offer.step.type').browse(cr,uid,[type_id])[0]
         value = {
-                    'code':step_type['name'],
+                    'code':step_type['code'],
                 }
         return {'value':value}
     
