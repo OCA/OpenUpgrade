737412b9d75c86bc1aadd14b60bdc181b1680a46
mra (Open ERP) <mra@tinyerp.com>
2009-04-13 18:49:47 +0530

    google_calendar: new module send events to google calendar

---
+++ google_calendar/__terp__.py
@@ -0,0 +1,16 @@
+{
+    "name" : "Google calendar",
+    "version" : "1.0",
+    "author" : "Tiny",
+    "website" : "http://www.openerp.com",
+    "category" : "Generic Modules",
+    "description": """
+        Export events (crm) to google calendar
+    """,
+    "depends" : ["event"],
+    "init_xml" : [],
+    "demo_xml" : [],
+    "update_xml" : ['google_calendar_view.xml'],
+    "active": False,
+    "installable": True,
+}
