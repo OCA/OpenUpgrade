f4d9796dc1499a862d4e6f6efe6dfdb09a5a1763
Sbh (Open ERP) <sbh@tinyerp.com>
2009-04-28 19:02:59 +0530

    [ETL] modfiy the test

--- etl/lib/etl/connector/sql_connector.py
+++ etl/lib/etl/connector/sql_connector.py
@@ -21,17 +21,17 @@
 ##############################################################################
 
 """
- To provide connectivity with SQL database server. 
+ To provide connectivity with SQL database server.
  supported connection with :
      * postgres server
      * mysql server
      * oracle server
 
- Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>). 
+ Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>).
  GNU General Public License.
 """
 from etl.connector import connector
-class sql_connector(connector):   
+class sql_connector(connector):
     def __init__(self, host, port, db, uid, passwd, sslmode='allow', con_type='postgres', name='sql_connector'):
         """
         Required Parameters
@@ -40,8 +40,8 @@ class sql_connector(connector):
         db     : Database name.
         uid    : User name to login into Database.
         passwd : Password of the user.
-                
-        Extra Parameters 
+
+        Extra Parameters
         sslmode  : For SSL connection.
         con_type : Type of connection (postgres, mysql, oracle).
         name     : Name of the connector.
@@ -51,17 +51,17 @@ class sql_connector(connector):
         self.uri = host + ':' + str(port)
         self.host = host
         self.port = port
-        self.sslmode = sslmode             
+        self.sslmode = sslmode
         self.db = db
         self.uid = uid
         self.con_type = con_type
-        self.passwd = passwd         
-        
-         
+        self.passwd = passwd
+
+
     def open(self):
-        """ 
+        """
         Opens a connection to Database server.
-        """ 
+        """
         super(sql_connector, self).open()
         connector = False
         if self.con_type == 'postgres':
@@ -74,27 +74,34 @@ class sql_connector(connector):
         elif self.con_type == 'oracle':
             import cx_Oracle
             dsn_tns = cx_Oracle.makedsn(self.host, self.port, self.db)
-            connector = cx_Oracle.connect(self.uid, self.passwd, dsn_tns)    
+            connector = cx_Oracle.connect(self.uid, self.passwd, dsn_tns)
         else:
-            raise Exception('Not Supported')           
-        return connector    
+            raise Exception('Not Supported')
+        return connector
 
-    def close(self, connector): 
-        """ 
+    def close(self, connector):
+        """
         Closes the connection made to Database Server.
-        """ 
-        super(sql_connector, self).close()          
+        """
+        super(sql_connector, self).close()
         return connector.close()
 
-    def __copy__(self): 
+    def __copy__(self):
         """
         Overrides copy method.
         """
-        res = sql_connector(self.host, self.port, self.db, self.uid, self.passwd, self.sslmode, self.con_type, self.name)        
+        res = sql_connector(self.host, self.port, self.db, self.uid, self.passwd, self.sslmode, self.con_type, self.name)
         return res
 
-def test():   
-    #TODO
+def test():
+
+    from etl_test import etl_test
+    import etl
+    sqlconnector_partner=sql_connector('localhost',5432, 'test', 'postgres', 'postgres')
+    test = etl_test.etl_component_test(etl.component.input.sql_in(sqlconnector_partner,'select * from res_partner where id<=10 order by id'))
+    res=test.output()
+    print res
+
     pass
 
 if __name__ == '__main__':
