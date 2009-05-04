1086ccfb3db44ec87df67ff586c48f6217e30ff0
JJA (OpenERP) <khatri.jatin7@gmail.com>
2009-05-04 11:44:57 +0530

    Modified : wizard layers_of_region_category of google_earth

--- google_earth/wizard/layers_of_region_category.py
+++ google_earth/wizard/layers_of_region_category.py
@@ -28,6 +28,8 @@ from xml.dom.ext import PrettyPrint
 
 import urllib
 import xml.dom.minidom
+import pyexpat
+#from xml.sax import ExpatParser
 
 import wizard
 import pooler
@@ -37,12 +39,12 @@ from osv import osv, fields
 
 _earth_form =  '''<?xml version="1.0"?>
         <form string="Google Map/Earth">
-        <separator string="Select Partner" colspan="4"/>
-        <field name="partner_id"/>
+        <separator string="Select Region(city/state/country)" colspan="4"/>
+        <field name="region"/>
         </form> '''
 
 _earth_fields = {
-            'partner_id': {'string': 'Partner', 'type': 'many2one', 'relation': 'res.partner', 'required': True,},
+            'region': {'string': 'Region', 'type': 'char','required': True,},
             }
 
 def geocode(address):
@@ -71,80 +73,128 @@ def create_kml(self, cr, uid, data, context={}):
     # KML elements.
 
     address = ' '
+    coordinates = []
+    addresslist = []
+    coordinates_text = ' '
     pool = pooler.get_pool(cr.dbname)
     partner_obj = pool.get('res.partner')
     path = tools.config['addons_path']
     fileName = path + '/google_earth/kml/partner_region.kml'
-    partner_data = partner_obj.browse(cr, uid, data['form']['partner_id'], context)
-
-#    cr.execute('select min(id) as id, sum(credit) as turnover, partner_id as partner_id from account_move_line group by partner_id')
-##    for id, turnover, partner_id in cr.fetchall():
-##        if not (partner_id == data['form']['partner_id']):
-##            raise wizard.except_wizard('Warning','This parnter has no turnover')
-##            return {}
     address_obj= pool.get('res.partner.address')
-    add = address_obj.browse(cr, uid, partner_data.address[0].id, context) # Todo: should be work for multiple address
-
-    if add.city:
-        address += str(add.city)
-    if add.state_id:
-        address += ', '
-        address += str(add.state_id.name)
-    if add.country_id:
-        address += ', '
-        address += str(add.country_id.name)
-
+    address_with_country_ids = address_obj.search(cr, uid, [('country_id.name','=', data['form']['region'])])
+    address_with_state_ids = address_obj.search(cr, uid, [('state_id.name','=', data['form']['region'])])
+    address_with_city_ids = address_obj.search(cr, uid, [('city','=', data['form']['region'])])
+    
+    if address_with_country_ids:
+        for id in address_with_country_ids:
+            add = address_obj.browse(cr, uid, id, context)
+            address = ' '
+            if add.city:
+                address += str(add.city)
+            if add.state_id:
+                address += ', '
+                address += str(add.state_id.name)
+            if add.country_id:
+                address += ', '
+                address += str(add.country_id.name)
+            addresslist.append(address)
+    elif address_with_state_ids:
+        for id in address_with_state_ids:
+            add = address_obj.browse(cr, uid, id, context)
+            address = ' '
+            if add.city:
+                address += str(add.city)
+            if add.state_id:
+                address += ', '
+                address += str(add.state_id.name)
+            if add.country_id:
+                address += ', '
+                address += str(add.country_id.name)
+            addresslist.append(address)
+    elif address_with_city_ids:
+        for id in address_with_city_ids:
+            add = address_obj.browse(cr, uid, id, context)
+            address = ' '
+            if add.city:
+                address += str(add.city)
+            if add.state_id:
+                address += ', '
+                address += str(add.state_id.name)
+            if add.country_id:
+                address += ', '
+                address += str(add.country_id.name)
+            addresslist.append(address)
+    else:
+        raise wizard.except_wizard('Warning','Region has not defined')
+        return {}
+    
+    for add in addresslist:
+        coordinates.append(geocode(add))
+    
+    for coordinate in coordinates:
+        coordinates_text += coordinate + '\n\t'
+    
+#    kml creation start
     kmlDoc = xml.dom.minidom.Document()
-    kmlElement = kmlDoc.createElementNS('http://maps.google.com/kml/2.2','kml')
+    kmlElement = kmlDoc.createElementNS('http://maps.google.com','kml')
     kmlElement = kmlDoc.appendChild(kmlElement)
 
     documentElement = kmlDoc.createElement('Document')
     documentElement = kmlElement.appendChild(documentElement)
 
-    RegionElement = kmlDoc.createElement('Region')
-
-    LatlonaltboxElement = kmlDoc.createElement('LatLonAltBox')
-    northElement = kmlDoc.createElement('north')
-    LatlonaltboxElement.appendChild(northElement)
-    southElement = kmlDoc.createElement('south')
-    LatlonaltboxElement.appendChild(southElement)
-    eastElement = kmlDoc.createElement('east')
-    LatlonaltboxElement.appendChild(eastElement)
-    westElement = kmlDoc.createElement('west')
-    LatlonaltboxElement.appendChild(westElement)
-    RegionElement.appendChild(LatlonaltboxElement)
-
-    LodElement = kmlDoc.createElement('Lod')
-    minlodpixelsElement = kmlDoc.createElement('minLodPixels')
-    LodElement.appendChild(minlodpixelsElement)
-    maxlodpixelsElement = kmlDoc.createElement('maxLodPixels')
-    LodElement.appendChild(maxlodpixelsElement)
-    RegionElement.appendChild(LodElement)
-
-    documentElement.appendChild(RegionElement)
+    styleElement = kmlDoc.createElement('Style')
+#    styleElement = kmlDoc.EndElement(style)
+    
+    styleElement.setAttribute('id','style15')
+    
+    linestyleElement = kmlDoc.createElement('LineStyle')
+    colorElement = kmlDoc.createElement('color')
+    colorElement.appendChild(kmlDoc.createTextNode('80000000'))
+    linestyleElement.appendChild(colorElement)
+    widthElement = kmlDoc.createElement('width')
+    widthElement.appendChild(kmlDoc.createTextNode('3'))
+    linestyleElement.appendChild(widthElement)
+    styleElement.appendChild(linestyleElement)
+    
+    polystyleElement = kmlDoc.createElement('PolyStyle')
+    polycolorElement = kmlDoc.createElement('color')
+    polycolorElement.appendChild(kmlDoc.createTextNode('59009900'))
+    polystyleElement.appendChild(polycolorElement)
+    fillElement = kmlDoc.createElement('fill')
+    fillElement.appendChild(kmlDoc.createTextNode('1'))
+    polystyleElement.appendChild(fillElement)
+    outlineElement = kmlDoc.createElement('outline')
+    outlineElement.appendChild(kmlDoc.createTextNode('1'))
+    polystyleElement.appendChild(outlineElement)
+    documentElement.appendChild(polystyleElement)
+    documentElement.appendChild(styleElement)
 
     placemarkElement = kmlDoc.createElement('Placemark')
-
     placemarknameElement = kmlDoc.createElement('name')
-    placemarknameText = kmlDoc.createTextNode(partner_data.name)
+    placemarknameText = kmlDoc.createTextNode(' ')
     placemarknameElement.appendChild(placemarknameText)
     placemarkElement.appendChild(placemarknameElement)
-
     descriptionElement = kmlDoc.createElement('description')
-    descriptionText = kmlDoc.createTextNode(address)
+    descriptionText = kmlDoc.createTextNode(' ')
     descriptionElement.appendChild(descriptionText)
-    placemarkElement.appendChild(descriptionElement)
-
-    linestringElement = kmlDoc.createElement('LineString')
+    styleurlElement = kmlDoc.createElement('styleUrl')
+    styleurlElement.appendChild(kmlDoc.createTextNode('#style15'))
+    placemarkElement.appendChild(styleurlElement)
+    polygonElement = kmlDoc.createElement('Polygon')
+    outerboundaryisElement = kmlDoc.createElement('outerBoundaryIs')
+    linearringElement = kmlDoc.createElement('LinearRing')
     tessellateElement = kmlDoc.createElement('tessellate')
-    tessellateText = kmlDoc.createTextNode('1')
-    tessellateElement.appendChild(tessellateText)
-    linestringElement.appendChild(tessellateElement)
-    coordinatesElement = kmlDoc.createElement('coordinates')
-    linestringElement.appendChild(coordinatesElement)
-    placemarkElement.appendChild(linestringElement)
-
+    tessellateElement.appendChild(kmlDoc.createTextNode('1'))
+    linearringElement.appendChild(tessellateElement)
+    coordinatesElemenent = kmlDoc.createElement('coordinates')
+    coordinatesElemenent.appendChild(kmlDoc.createTextNode(coordinates_text))
+    linearringElement.appendChild(coordinatesElemenent)
+    outerboundaryisElement.appendChild(linearringElement)
+    polygonElement.appendChild(outerboundaryisElement)
+    placemarkElement.appendChild(polygonElement)
+    placemarkElement.appendChild(descriptionElement)
     documentElement.appendChild(placemarkElement)
+    
 
     # This writes the KML Document to a file.
     kmlFile = open(fileName, 'w')
