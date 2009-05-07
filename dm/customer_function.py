66b6d12b261b66cf1ce4cb229c4d233d2eacc9ef
Sofia (Open ERP) <sso@tinyerp.com>
2009-05-07 18:26:17 +0530

    DTP Plugin - DM base contact

--- dm/customer_function.py
+++ dm/customer_function.py
@@ -12,11 +12,14 @@ def customer_function(cr,uid,ids,**args):
     model_name = args['model_name']
     model_object =  pool.get(model_name)
     res = pool.get('res.partner.address').read(cr,uid,ids)[0]
-    if model_name =='res.partner' :
-        ids  = res['partner_id'][0]
-        res = model_object.read(cr,uid,[ids])[0]
-   # else :
-#        defin res over  here or wuill get an erro
+    if model_name == 'res.partner' :
+        if res['partner_id']:
+            res = model_object.read(cr,uid,res['partner_id'])[0]
+    elif model_name in ['res.partner.contact','res.partner.job'] :
+        id = res['job_id']
+        if model_name == 'res.partner.contact' : 
+            id = pool.get('res.partner.job').read(cr,uid,id)[0]['contact_id']
+        res = model_object.read(cr,uid,id)[0]
     if args['field_type'] == 'selection':
         if res[args['field_name']]:
             if args['field_name'] == 'lang':
@@ -26,5 +29,18 @@ def customer_function(cr,uid,ids,**args):
                 res[args['field_name']] = str(read_name[0]['name'])
         value = res[args['field_name']]
         return value
-    elif args['field_type'] not in ['many2many','one2many','many2one']:
+    elif args['field_type'] == 'many2one':
+        if res[args['field_name']]:
+            if args['field_name'] == 'lang_id':
+                read_name = pool.get('res.lang').read(cr,uid,res['lang_id'][0],['name'])
+                res[args['field_name']] = str(read_name['name'])
+            elif args['field_name'] == 'country_id' or args['field_name'] == 'country':
+                id = res['country_id'][0] or res['country'][0]
+                read_name = pool.get('res.country').read(cr,uid,id,['name'])
+                res[args['field_name']] = str(read_name['name'])
+            elif args['field_name'] == 'name':
+                read_name = pool.get('res.partner').read(cr,uid,res['name'][0],['name'])
+                res[args['field_name']] = str(read_name['name'])
+        return res[args['field_name']]
+    elif args['field_type'] not in ['many2many','one2many']:
         return res[args['field_name']]
