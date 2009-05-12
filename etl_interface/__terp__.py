f045ba20b269a80e24d217c0e4fbf930e192f911
rpa (Open ERP) <rpa@tinyerp.com>
2009-05-12 16:15:33 +0530

    [ETL_INTERFACE]:Modifications+Added view and py for sugarcrm-in,xmlrpc-out

--- etl_interface/__terp__.py
+++ etl_interface/__terp__.py
@@ -39,15 +39,20 @@
     'update_xml': [
          'etl_interface_sequence.xml',
          'etl_interface_view.xml',
-         'etl_component/etl_component_view.xml',
-         'etl_connector/etl_connector_view.xml',
          'etl_interface_workflow.xml',
+         
          'etl_connector/localfile_view.xml',
          'etl_connector/openobject_connector_view.xml',
          'etl_connector/sql_connector_view.xml',
          'etl_connector/facebook_connector_view.xml',
+         'etl_connector/sugarcrm_connector_view.xml',
+         
+         'etl_component/etl_component_view.xml',
+         'etl_connector/etl_connector_view.xml',
+         
          'etl_component/control/data_count_view.xml',
          'etl_component/control/sleep_view.xml',
+         
          'etl_component/input/csv_in_view.xml',
          'etl_component/input/vcard_in_view.xml',
          'etl_component/input/facebook_in_view.xml',
@@ -55,22 +60,24 @@
          'etl_component/input/excel_in_view.xml',
          'etl_component/input/xml_in_view.xml',
          'etl_component/input/openobject_in_view.xml',
-        'etl_component/input/xmlrpc_in_view.xml',
+         'etl_component/input/xmlrpc_in_view.xml',
 	     'etl_component/input/gmail_in_view.xml',
-
+         'etl_component/input/sugarcrm_in_view.xml',
+         
 	     'etl_component/output/csv_out_view.xml',
          'etl_component/output/sql_out_view.xml',
          'etl_component/output/openobject_out_view.xml',
          'etl_component/output/excel_out_view.xml',
          'etl_component/output/xml_out_view.xml',
          'etl_component/output/vcard_out_view.xml',
+         'etl_component/output/xmlrpc_out_view.xml',
 
          'etl_component/transform/data_filter_view.xml',
          'etl_component/transform/sort_view.xml',
          'etl_component/transform/map_view.xml',
          'etl_component/transform/logger_view.xml',
          'etl_component/transform/logger_bloc_view.xml',
-         'etl_component/transform/merge_view.xml', # To be check
+         'etl_component/transform/merge_view.xml',
          'etl_component/transform/schema_validator_view.xml',
          'etl_component/transform/unique_view.xml',
 
