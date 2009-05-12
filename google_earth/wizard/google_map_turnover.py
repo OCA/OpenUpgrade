f2ad9dc44dd53a49639da627fafeb2f08b05fca9
JJA (OpenERP) <khatri.jatin7@gmail.com>
2009-05-12 11:22:43 +0530

    Modified : changes in google_map_turnover(google_earth)

--- google_earth/wizard/google_map_turnover.py
+++ google_earth/wizard/google_map_turnover.py
@@ -77,14 +77,18 @@ def create_kml(self, cr, uid, data, context={}):
 
     res = {}
 #    cr.execute('select min(id) as id, sum(credit) as turnover, partner_id as partner_id from account_move_line group by partner_id')
-    cr.execute("select min(aml.id) as id, sum(aml.credit) as turnover, aml.partner_id as partner_id from account_move_line aml, account_account ac, account_account_type actype where aml.account_id = ac.id and ac.user_type = actype.id and (actype.name = 'income' or ac.type = 'receivable') group by aml.partner_id")
+    cr.execute("select min(aml.id) as id, sum(aml.credit) as turnover, aml.partner_id as partner_id from account_move_line aml, account_account ac, account_account_type actype where aml.account_id = ac.id and ac.user_type = actype.id and (ac.type = 'receivable') group by aml.partner_id")
     res_partner = cr.fetchall()
     for part in partner_data:
-        for id, turnover, partner_id in res_partner:
-            if not (partner_id == part.id):
-                res[part.id] = 0
-            if partner_id == part.id:
-                res[part.id] = turnover
+        res[part.id]= 0
+    for ml_id, turnover, partner_id in res_partner:
+        res[partner_id] = turnover
+#    for part in partner_data:
+#        for id, turnover, partner_id in res_partner:
+#            if partner_id == part.id:
+#                res[part.id] = turnover
+#            if not (partner_id == part.id):
+#                res[part.id] = 0
     kmlDoc = xml.dom.minidom.Document()
     kmlElement = kmlDoc.createElementNS('http://maps.google.com/kml/2.2','kml')
     kmlElement = kmlDoc.appendChild(kmlElement)
