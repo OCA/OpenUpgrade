19071370cfa1e2aa4d7b240ba8f5933ecb3b70b9
mra (Open ERP) <mra@tinyerp.com>
2009-05-13 13:03:14 +0530

    etl lib

--- etl/lib/demo/test_server.py
+++ etl/lib/demo/test_server.py
@@ -9,7 +9,7 @@ fileconnector_partner=etl.connector.localfile('input/partner.csv')
 
 fileconnector_partner1=etl.connector.localfile('input/partner1.csv')
 fileconnector_partner3=etl.connector.localfile('input/partner3.csv')
-fileconnector_output=etl.connector.localfile('output/test1_partner55.csv','r+')
+fileconnector_output=etl.connector.localfile('output/test1_partner222.csv','r+')
 
 csv_in1= etl.component.input.csv_in(fileconnector_partner,name='Partner Data')
 csv_in2= etl.component.input.csv_in(fileconnector_partner1,name='Partner Data1')
@@ -27,55 +27,72 @@ tran4=etl.transition(sleep1,log2)
 tran5=etl.transition(sort1,csv_out1)
 
 
-job1=etl.job([csv_in1,csv_in2,csv_out1,sort1,log1,log2,sleep1])
+job1=etl.job([csv_in1,csv_in2,csv_out1,sort1,log1,log2,sleep1], name="Job1")
 
 
 
 import pickle
-class etl_server(threading.Thread):   
+class etl_server(threading.Thread):
     path = 'pickle.txt'
     job = False
 
     def write(self):
         """
         Store job instance into pickle object.
-        """        
+        """
         fp = open(self.path,'wb')
         pck_obj = pickle.dump(self.job,fp)
         fp.close()
-        
+
         return True
-        
-    def read(self): 
+
+    def read(self):
         """
         Read job instance value from pickle object.
         Parameter
-        value - pickle value  
-        """  
+        value - pickle value
+        """
         value = False
-        try:  
-            fp = open(self.path,'a+')                         
+        try:
+            fp = open(self.path,'a+')
             value = pickle.load(fp)
         except Exception,e:
             print "<<<<<<<<<<<",e
         return value
 
-        
-    def run(self):      
-        try:          
-            obj = self.read() 
-            print obj                                
+    def run(self):
+        try:
+            obj = self.read()
+            print "------------New job--------------"
+            print "job",self.job
+            print "job_id",self.job.job_id
+            print "------------New job-- end------------"
             if obj:
-                self.job = obj
+                print "=======old job data start========"
+                print "status",obj
+                print "job id",obj.job_id
+                print "======old job data end========="
+                print "****In if****old to take****"
+                if self.job.job_id == obj.job_id:
+                    if obj.status == 'end':
+                        self.job.run() # should be chk.. if in picke if our job is ended once..and try to run that same job with exception then it should be run...
+                        print "Job is already done...................."
+                    elif obj.status == 'pause':
+                        self.job = obj
+                        self.job.restart()
+                    else:
+                        pass
+                else:
+                    self.job.run()
+                    self.write()
             else:
+                print "^^^^^^In else new job^^^^^^"
                 self.job.run()
+                self.write()
         except Exception,e:
-            print ">>>>>>>>",e            
+            print ">>>Exception in run...............>>>>>",e
+#            self.job.pause()
             self.write()
-        
-    
-    
-
 
 server = etl_server()
 server.job = job1
@@ -83,5 +100,5 @@ server.start()
 
 
 
-       
+
 
