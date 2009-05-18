b7437f95e4f5725839fbcef769a09e1cebd23c5d
Mpa (open ERP) <mitesh.patel1119@gmail.com>
2009-05-18 17:19:58 +0530

    Added Field in document_report.py for Trademark

--- dm/wizard/document_report.py
+++ dm/wizard/document_report.py
@@ -28,6 +28,7 @@ class wizard_document_report(wizard.interface):
     <form string="Select Report">
         <field name="report" colspan="4"/>
         <field name="address_id" colspan="4"/>
+        <field name="trademark_id" colspan="4"/>
     </form>'''
     def execute(self, db, uid, data, state='init', context=None):
         self.dm_wiz_data = data
@@ -50,8 +51,8 @@ class wizard_document_report(wizard.interface):
     report_list_fields = {
                     
         'report': {'string': 'Select Report', 'type': 'selection', 'selection':_get_reports, },
-        'address_id': {'string': 'Select Customer Address', 'type': 'many2one','relation':'res.partner.address', 'selection':_get_reports, 'domain':[('partner_id.category_id','=','DTP Preview Customers')] }
-        
+        'address_id': {'string': 'Select Customer Address', 'type': 'many2one','relation':'res.partner.address', 'selection':_get_reports, 'domain':[('partner_id.category_id','=','DTP Preview Customers')] },
+        'trademark_id':{'string': 'Select Trademark', 'type': 'many2one','relation':'dm.trademark'}
         }
     
     states = {
