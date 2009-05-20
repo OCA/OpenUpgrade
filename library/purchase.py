024477e5ff774a0b1badc228ff268961fde890fe
Olivier Laurent <olt@tinyerp.com>
2009-05-20 13:38:17 +0200

    pep8

--- library/purchase.py
+++ library/purchase.py
@@ -1,7 +1,7 @@
 # -*- encoding: utf-8 -*-
 ##############################################################################
 #
-#    OpenERP, Open Source Management Solution	
+#    OpenERP, Open Source Management Solution
 #    Copyright (C) 2004-2008 Tiny SPRL (<http://tiny.be>). All Rights Reserved
 #    $Id$
 #
@@ -23,31 +23,29 @@
 from osv import fields, osv
 import netsvc
 
+
 class purchase_order_line(osv.osv):
     _inherit = 'purchase.order.line'
     _columns = {
         'origin': fields.char('Origin', size=1024),
-        'production_lot_id':fields.many2one('stock.production.lot', 'Production Lot'),
+        'production_lot_id': fields.many2one('stock.production.lot', 'Production Lot'),
         'customer_ref': fields.char('Customer reference', size=64),
     }
 
-
-
-
 purchase_order_line()
-
 
 
 class purchase_order(osv.osv):
     _inherit = 'purchase.order'
     _order = "create_date desc"
-    def action_picking_create(self,cr, uid, ids, *args):
+
+    def action_picking_create(self, cr, uid, ids, *args):
 
         picking_id = False
         for order in self.browse(cr, uid, ids):
             loc_id = order.partner_id.property_stock_supplier.id
             istate = 'none'
-            if order.invoice_method=='picking':
+            if order.invoice_method == 'picking':
                 istate = '2binvoiced'
             picking_id = self.pool.get('stock.picking').create(cr, uid, {
                 'origin': 'PO:%d:%s' % (order.id, order.name),
@@ -62,7 +60,7 @@ class purchase_order(osv.osv):
                 if order_line.product_id.product_tmpl_id.type in ('product', 'consu'):
                     dest = order.location_id.id
                     self.pool.get('stock.move').create(cr, uid, {
-                        'name': 'PO:'+order_line.name[:50],
+                        'name': 'PO:' + order_line.name[:50],
                         'product_id': order_line.product_id.id,
                         'product_qty': order_line.product_qty,
                         'product_uos_qty': order_line.product_qty,
@@ -80,32 +78,31 @@ class purchase_order(osv.osv):
                     })
 
                     if order_line.move_dest_id:
-                        self.pool.get('stock.move').write(cr, uid, [order_line.move_dest_id.id], {'location_id':order.location_id.id})
-            self.write(cr,uid,[order.id],{'picking_id':picking_id})
+                        self.pool.get('stock.move').write(cr, uid, [order_line.move_dest_id.id], {'location_id': order.location_id.id})
+            self.write(cr, uid, [order.id], {'picking_id': picking_id})
             wf_service = netsvc.LocalService("workflow")
             wf_service.trg_validate(uid, 'stock.picking', picking_id, 'button_confirm', cr)
         return picking_id
 
-
     def wkf_confirm_order(self, cr, uid, ids, context={}):
-        pol = self.pool.get('purchase.order').browse(cr,uid,ids)
+        pol = self.pool.get('purchase.order').browse(cr, uid, ids)
         for order in pol:
-            l_id= 0
+            l_id = 0
             for line in order.order_line:
-                if line.production_lot_id : continue
+                if line.production_lot_id:
+                    continue
                 l_id += 1
-                production_lot_id = self.pool.get('stock.production.lot').create(cr,uid,{
+                production_lot_id = self.pool.get('stock.production.lot').create(cr, uid, {
                     'name': line.order_id and (str(line.order_id.name)+'/Line'+str(l_id)) or False
                     })
-                self.pool.get('purchase.order.line').write(cr,uid,[line.id],{'production_lot_id':production_lot_id})
-            
-        super(purchase_order,self).wkf_confirm_order(cr,uid,ids,context)
+                self.pool.get('purchase.order.line').write(cr, uid, [line.id], {'production_lot_id': production_lot_id})
+
+        super(purchase_order, self).wkf_confirm_order(cr, uid, ids, context)
         return True
 
     _defaults = {
         'invoice_method': lambda *a: 'picking',
         }
-    
+
 purchase_order()
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
 
