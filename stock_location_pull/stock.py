f401c16bed9f5cf889b5506e61f61a5ecbbb6ff8
Najlaâ EL KHAYAT <nel@tinyerp.com>
2009-06-23 20:41:09 +0200

    tooltips

--- stock_location_pull/stock.py
+++ stock_location_pull/stock.py
@@ -54,15 +54,15 @@ class product_pulled_flow(osv.osv):
     _name = 'product.pulled.flow'
     _description = "Pulled Flows."
     _columns = {
-        'name': fields.char('Name', size=64, required=True),
-        'cancel_cascade': fields.boolean('Cascade'),
-        'location_id': fields.many2one('stock.location','Location', required=True),
-        'location_src_id': fields.many2one('stock.location','Location Source'),
+        'name': fields.char('Name', size=64, required=True, help="This field will fill the packing Origin and the name of its moves"),
+        'cancel_cascade': fields.boolean('Cancel Cascade', help="Allow you to cancel moves related to the product pull flow"),
+        'location_id': fields.many2one('stock.location','Location', required=True, help="Is the destination location that needs supplying"),
+        'location_src_id': fields.many2one('stock.location','Location Source', help="Location used by Destination Location to supply"),
         'procure_method': fields.selection([('make_to_stock','Make to Stock'),('make_to_order','Make to Order')], 'Procure Method', required=True, help="'Make to Stock': When needed, take from the stock or wait until re-supplying. 'Make to Order': When needed, purchase or produce for the procurement request."),
         'type_proc': fields.selection([('produce','Produce'),('buy','Buy'),('move','Move')], 'Type of Procurement', required=True),
-        'company_id': fields.many2one('res.company', 'Company'),
+        'company_id': fields.many2one('res.company', 'Company', help="Is used to know to which company belong packings and moves"),
         'partner_address_id': fields.many2one('res.partner.address', 'Partner Address'),
-        'picking_type': fields.selection([('out','Sending Goods'),('in','Getting Goods'),('internal','Internal'),('delivery','Delivery')], 'Shipping Type', required=True, select=True),
+        'picking_type': fields.selection([('out','Sending Goods'),('in','Getting Goods'),('internal','Internal'),('delivery','Delivery')], 'Shipping Type', required=True, select=True, help="Depending on the company, choose whatever you want to receive or send products"),
         'product_id':fields.many2one('product.product','Product'),
     }
     _defaults = {
