6ba3f0e58e409ac09d696f8997108914daa77fe7
Sofia (Open ERP) <sso@tinyerp.com>
2009-07-10 18:37:45 +0530

    [IMP] modifs in copy method, Offer report

--- dm/campaign.py
+++ dm/campaign.py
@@ -624,7 +624,6 @@ class dm_campaign(osv.osv):#{{{
         return super(dm_campaign,self).write(cr, uid, ids, vals, context)
     
     def create(self,cr,uid,vals,context={}):
-
         type_id = self.pool.get('dm.campaign.type').search(cr, uid, [('code','=','model')])[0]
         if context.has_key('campaign_type') and context['campaign_type']=='model':
             vals['campaign_type_id']=type_id
@@ -714,14 +713,17 @@ class dm_campaign(osv.osv):#{{{
         return True
 
     def copy(self, cr, uid, id, default=None, context={}):
-        cmp_id = super(dm_campaign, self).copy(cr, uid, id, default, context=context)
-        data = self.browse(cr, uid, cmp_id, context)
-        if 'name' in default:
-            name_default=default['name']
-        else:
-            name_default='Copy of %s' % data.name
-        super(dm_campaign, self).write(cr, uid, cmp_id, {'name':name_default, 'date_start':0, 'date':0, 'project_id':0})
-        return cmp_id
+        if not default: default = {}
+        campaign_id = self.browse(cr, uid, id)
+        if not default.get('name', False):
+            default['name'] = 'Copy of ' +campaign_id.name
+        default.update({'date_start': False, 'date': False, 'project_id': False, 'proposition_ids': []})
+        camp_copy_id = super(dm_campaign, self).copy(cr, uid, id, default, context)
+        prop_ids = [x.id for x in campaign_id.proposition_ids]
+        for proposition in campaign_id.proposition_ids:
+            default = {'camp_id': camp_copy_id}
+            prop_copy = self.pool.get('dm.campaign.proposition').copy(cr, uid, proposition.id, default, context)
+        return camp_copy_id
 
     def unlink(self, cr, uid, ids, context={}):
         for campaign in self.browse(cr, uid, ids, context):
@@ -771,15 +773,19 @@ class dm_campaign_proposition(osv.osv):#{{{
         Function to duplicate segments only if 'keep_segments' is set to yes else not to duplicate segments
         """
         proposition_id = super(dm_campaign_proposition, self).copy(cr, uid, id, default, context=context)
+        if 'camp_id' in default and default['camp_id']:
+            self.write(cr, uid, proposition_id, {'camp_id' : default['camp_id']})
+
         data = self.browse(cr, uid, proposition_id, context)
-        default='Copy of %s' % data.name
-        super(dm_campaign_proposition, self).write(cr, uid, proposition_id, {'name':default, 'date_start':0, 'initial_proposition_id':id})
+        default_name='Copy of %s' % data.name
+        super(dm_campaign_proposition, self).write(cr, uid, proposition_id, {'name':default_name, 'date_start':False, 'initial_proposition_id':id})
+
         if data.keep_segments == False:
             l = []
             for i in data.segment_ids:
                  l.append(i.id)
                  self.pool.get('dm.campaign.proposition.segment').unlink(cr,uid,l)
-                 super(dm_campaign_proposition, self).write(cr, uid, proposition_id, {'segment_ids':[(6,0,[])]})
+                 self.write(cr, uid, proposition_id, {'segment_ids':[(6,0,[])]})
         
         """
         Function to duplicate products only if 'keep_prices' is set to yes else not to duplicate products
@@ -789,7 +795,7 @@ class dm_campaign_proposition(osv.osv):#{{{
             for i in data.item_ids:
                  l.append(i.id)
                  self.pool.get('dm.campaign.proposition.item').unlink(cr,uid,l)
-                 super(dm_campaign_proposition, self).write(cr, uid, proposition_id, {'item_ids':[(6,0,[])]})
+                 self.write(cr, uid, proposition_id, {'item_ids':[(6,0,[])]})
         return proposition_id
 
     def _proposition_code(self, cr, uid, ids, name, args, context={}):
@@ -1221,7 +1227,7 @@ class dm_campaign_proposition_item(osv.osv):#{{{
         'price' : fields.float('Sale Price'),
         'proposition_id': fields.many2one('dm.campaign.proposition', 'Commercial Proposition'),
         'item_type': fields.selection(AVAILABLE_ITEM_TYPES, 'Product Type', size=64),
-        'offer_step_type_id': fields.many2one('dm.offer.step.type','Offer Step Type'), 
+        'offer_step_id': fields.many2one('dm.offer.step','Offer Step Name'), 
         'notes' : fields.text('Notes'),
         'forecasted_yield' : fields.float('Forecasted Yield'),
     }
