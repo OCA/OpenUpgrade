ca0789bcafe5f11349890563993f209f356f4493
Najlaâ EL KHAYAT <nel@tinyerp.com>
2009-07-13 11:21:16 +0200

    labo tools

---
+++ labo_tool/__terp__.py
@@ -0,0 +1,13 @@
+{
+    "name" : "Labo tools create Progenus project",
+    "version" : "1.0",
+    "author" : "Tiny",
+    "category" : "Enterprise Specific Modules/Food Industries",
+    "depends" : ["base", "account", "product", "stock","sale","crm"],
+    "init_xml" : [],
+    "demo_xml" : [],
+    "description": "Progenus project Labo tools object",
+    "update_xml" : ['labo_tools_view.xml','labo_tools_data.xml'],
+    "active": False,
+    "installable": True
+}
