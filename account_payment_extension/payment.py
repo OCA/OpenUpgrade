c25a2e5357012c94c756a38f8a24e276a1f492ea
Jordi Esteve <jesteve@fontanilles>
2009-08-14 10:33:36 +0200

    [IMP+FIX] account_payment_extension module: Improvements in views and minor fixes

--- account_payment_extension/payment.py
+++ account_payment_extension/payment.py
@@ -89,7 +89,7 @@ class payment_order(osv.osv):
 
     def _get_type(self, cr, uid, context={}):
         type = context.get('type', 'payable')
-        print "type",type
+        #print "type",type
         return type
 
     def _get_reference(self, cr, uid, context={}):
