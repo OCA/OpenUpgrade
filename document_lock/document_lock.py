5b3ce035424afecdd14c4008d34d49ca1e6a30f2
P. Christeas <p_christ@hol.gr>
2009-09-01 09:10:50 +0300

    Document lock: use true as the return value

--- document_lock/document_lock.py
+++ document_lock/document_lock.py
@@ -35,7 +35,7 @@ class ir_attachment(osv.osv):
 
     def check(self, cr, uid, ids, mode, context = None):
         if not ids:
-            return
+            return True
         ima = self.pool.get('ir.model.access')
         if isinstance(ids, (int, long)):
             ids = [ids]
@@ -53,6 +53,7 @@ class ir_attachment(osv.osv):
         cr.execute('select distinct res_model from ir_attachment where id in ('+','.join(map(str, ids))+')')
         for obj in cr.fetchall():
             ima.check(cr, uid, obj[0], mode)
+	return True
 
 ir_attachment()
 
