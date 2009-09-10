4efded750c8d08add291d834cb6c809f03f2505e
vra <vra@tinyerp.com>
2009-09-10 14:51:28 +0530

    [IMP] purchase_number, sale_number modules : Reports (Purchase order and sale order with excise).

--- sale_numbers/__terp__.py
+++ sale_numbers/__terp__.py
@@ -3,7 +3,7 @@
     "version" : "1.0",
     "author" : "Tiny",
     "website" : "http://tinyerp.com/module_sale.html",
-    "depends" : ["sale","account_base"],
+    "depends" : ["sale","account_base","base_vat"],
     "category" : "Generic Modules/Sales & Purchases",
     "description": """
     remove the drawback of the sales numbering, 
