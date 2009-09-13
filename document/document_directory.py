1115549c302409f2779f5cab2cd9ed6f55a43e16
P. Christeas <p_christ@hol.gr>
2009-09-13 17:53:08 +0300

    Document: break[1] the code into separate files.
    [1] literaly break it, too!

---
+++ document/document_directory.py
@@ -0,0 +1,212 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>). All Rights Reserved
+#    $Id$
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+import base64
+
+from osv import osv, fields
+from osv.orm import except_orm
+import urlparse
+
+import os
+
+class document_directory(osv.osv):
+    _name = 'document.directory'
+    _description = 'Document directory'
+    _columns = {
+        'name': fields.char('Name', size=64, required=True, select=1),
+        'write_date': fields.datetime('Date Modified', readonly=True),
+        'write_uid':  fields.many2one('res.users', 'Last Modification User', readonly=True),
+        'create_date': fields.datetime('Date Created', readonly=True),
+        'create_uid':  fields.many2one('res.users', 'Creator', readonly=True),
+        'file_type': fields.char('Content Type', size=32),
+        'domain': fields.char('Domain', size=128, help="Use a domain if you want to apply an automatic filter on visible resources."),
+        'user_id': fields.many2one('res.users', 'Owner'),
+        'group_ids': fields.many2many('res.groups', 'document_directory_group_rel', 'item_id', 'group_id', 'Groups'),
+        'parent_id': fields.many2one('document.directory', 'Parent Item'),
+        'child_ids': fields.one2many('document.directory', 'parent_id', 'Children'),
+        'file_ids': fields.one2many('ir.attachment', 'parent_id', 'Files'),
+        'content_ids': fields.one2many('document.directory.content', 'directory_id', 'Virtual Files'),
+        'type': fields.selection([('directory','Static Directory'),('ressource','Other Resources')], 'Type', required=True),
+        'ressource_type_id': fields.many2one('ir.model', 'Directories Mapped to Objects',
+            help="Select an object here and Open ERP will create a mapping for each of these " \
+                 "objects, using the given domain, when browsing through FTP."),
+        'ressource_parent_type_id': fields.many2one('ir.model', 'Parent Model',
+            help="If you put an object here, this directory template will appear bellow all of these objects. " \
+                 "Don't put a parent directory if you select a parent model."),
+        'ressource_id': fields.integer('Resource ID'),
+        'ressource_tree': fields.boolean('Tree Structure',
+            help="Check this if you want to use the same tree structure as the object selected in the system."),
+    }
+    _defaults = {
+        'user_id': lambda self,cr,uid,ctx: uid,
+        'domain': lambda self,cr,uid,ctx: '[]',
+        'type': lambda *args: 'directory',
+        'ressource_id': lambda *a: 0
+    }
+    _sql_constraints = [
+        ('dirname_uniq', 'unique (name,parent_id,ressource_id,ressource_parent_type_id)', 'The directory name must be unique !')
+    ]
+
+    def get_resource_path(self,cr,uid,dir_id,res_model,res_id):
+        # this method will be used in process module
+        # to be need test and Improvement if resource dir has parent resource (link resource)
+        path=[]
+        def _parent(dir_id,path):
+            parent=self.browse(cr,uid,dir_id)
+            if parent.parent_id and not parent.ressource_parent_type_id:
+                _parent(parent.parent_id.id,path)
+                path.append(parent.name)
+            else:
+                path.append(parent.name)
+                return path
+
+        directory=self.browse(cr,uid,dir_id)
+        model_ids=self.pool.get('ir.model').search(cr,uid,[('model','=',res_model)])
+        if directory:
+            _parent(dir_id,path)
+            path.append(self.pool.get(directory.ressource_type_id.model).browse(cr,uid,res_id).name)
+            #user=self.pool.get('res.users').browse(cr,uid,uid)
+            #return "ftp://%s:%s@localhost:%s/%s/%s"%(user.login,user.password,config.get('ftp_server_port',8021),cr.dbname,'/'.join(path))
+	    # No way we will return the password!
+	    return "ftp://user:pass@host:port/test/this"
+        return False
+
+    def _check_recursion(self, cr, uid, ids):
+        level = 100
+        while len(ids):
+            cr.execute('select distinct parent_id from document_directory where id in ('+','.join(map(str,ids))+')')
+            ids = filter(None, map(lambda x:x[0], cr.fetchall()))
+            if not level:
+                return False
+            level -= 1
+        return True
+
+    _constraints = [
+        (_check_recursion, 'Error! You can not create recursive Directories.', ['parent_id'])
+    ]
+    def __init__(self, *args, **kwargs):
+        res = super(document_directory, self).__init__(*args, **kwargs)
+        self._cache = {}
+
+    def onchange_content_id(self, cr, uid, ids, ressource_type_id):
+        return {}
+
+    """
+        PRE:
+            uri: of the form "Sales Order/SO001"
+        PORT:
+            uri
+            object: the object.directory or object.directory.content
+            object2: the other object linked (if object.directory.content)
+    """
+    def get_object(self, cr, uid, uri, context={}):
+        lang = context.get('lang',False)
+        if not lang:
+            user = self.pool.get('res.users').browse(cr, uid, uid)
+            lang = user.context_lang 
+        context['lang'] = lang
+        if not uri:
+            return node_class(cr, uid, '', False, context=context, type='database')
+        turi = tuple(uri)
+        if False and (turi in self._cache):
+            (path, oo, oo2, context, content,type,root) = self._cache[turi]
+            if oo:
+                object = self.pool.get(oo[0]).browse(cr, uid, oo[1], context)
+            else:
+                object = False
+            if oo2:
+                object2 = self.pool.get(oo2[0]).browse(cr, uid, oo2[1], context)
+            else:
+                object2 = False
+            node = node_class(cr, uid, '/', False, context=context, type='database')
+            return node
+
+        node = node_class(cr, uid, '/', False, context=context, type='database')
+        for path in uri[:]:
+            if path:
+                node = node.child(path)
+                if not node:
+                    return False
+        oo = node.object and (node.object._name, node.object.id) or False
+        oo2 = node.object2 and (node.object2._name, node.object2.id) or False
+        self._cache[turi] = (node.path, oo, oo2, node.context, node.content,node.type,node.root)
+        return node
+
+    def get_childs(self, cr, uid, uri, context={}):
+        node = self.get_object(cr, uid, uri, context)
+        if uri:
+            children = node.children()
+        else:
+            children= [node]
+        result = map(lambda node: node.path_get(), children)
+        return result
+
+    def copy(self, cr, uid, id, default=None, context=None):
+        if not default:
+            default ={}
+        name = self.read(cr, uid, [id])[0]['name']
+        default.update({'name': name+ " (copy)"})
+        return super(document_directory,self).copy(cr,uid,id,default,context)
+
+    def _check_duplication(self, cr, uid,vals,ids=[],op='create'):
+        name=vals.get('name',False)
+        parent_id=vals.get('parent_id',False)
+        ressource_parent_type_id=vals.get('ressource_parent_type_id',False)
+        ressource_id=vals.get('ressource_id',0)
+        if op=='write':
+            for directory in self.browse(cr,uid,ids):
+                if not name:
+                    name=directory.name
+                if not parent_id:
+                    parent_id=directory.parent_id and directory.parent_id.id or False
+                if not ressource_parent_type_id:
+                    ressource_parent_type_id=directory.ressource_parent_type_id and directory.ressource_parent_type_id.id or False
+                if not ressource_id:
+                    ressource_id=directory.ressource_id and directory.ressource_id or 0
+                res=self.search(cr,uid,[('id','<>',directory.id),('name','=',name),('parent_id','=',parent_id),('ressource_parent_type_id','=',ressource_parent_type_id),('ressource_id','=',ressource_id)])
+                if len(res):
+                    return False
+        if op=='create':
+            res=self.search(cr,uid,[('name','=',name),('parent_id','=',parent_id),('ressource_parent_type_id','=',ressource_parent_type_id),('ressource_id','=',ressource_id)])
+            if len(res):
+                return False
+        return True
+    def write(self, cr, uid, ids, vals, context=None):
+        if not self._check_duplication(cr,uid,vals,ids,op='write'):
+            raise osv.except_osv(_('ValidateError'), _('Directory name must be unique!'))
+        return super(document_directory,self).write(cr,uid,ids,vals,context=context)
+
+    def create(self, cr, uid, vals, context=None):
+        if not self._check_duplication(cr,uid,vals):
+            raise osv.except_osv(_('ValidateError'), _('Directory name must be unique!'))
+        if vals.get('name',False) and (vals.get('name').find('/')+1 or vals.get('name').find('@')+1 or vals.get('name').find('$')+1 or vals.get('name').find('#')+1) :
+            raise osv.except_osv(_('ValidateError'), _('Directory name contains special characters!'))
+        return super(document_directory,self).create(cr, uid, vals, context)
+
+document_directory()
+
+class document_directory_node(osv.osv):
+    _inherit = 'process.node'
+    _columns = {
+        'directory_id':  fields.many2one('document.directory', 'Document directory', ondelete="set null"),
+    }
+document_directory_node()
