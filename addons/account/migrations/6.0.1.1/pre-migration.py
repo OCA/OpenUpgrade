e8f52ab77f4e10b11bab4a12d0c006e1c5374c1c
Stefan Rijnhart <stefan@therp.nl>
2011-11-28 10:24:25 +0000

    [FIX] various bugfixes

--- addons/account/migrations/6.0.1.1/pre-migration.py
+++ addons/account/migrations/6.0.1.1/pre-migration.py
@@ -10,7 +10,7 @@ logger = logging.getLogger('migrate')
 column_renames = {
     # this is a mapping per table from old column name
     # to new column name
-    'account.invoice': [
+    'account_invoice': [
         ('partner_bank', 'partner_bank_id'),
         ('number', 'internal_number'),
         ],
@@ -18,15 +18,15 @@ column_renames = {
 
 def mgr_refund_journal_type(cr):
     # assign new refund journal types to journal.journal
-    cr.execute('UPDATE account_journal SET type = "purchase_refund" ' +
-               'where type = "purchase" and refund_journal = TRUE')
-    cr.execute('UPDATE account_journal SET type = "sale_refund" ' +
-               'where type = "sale" and refund_journal = TRUE')
+    cr.execute("UPDATE account_journal SET type = 'purchase_refund' " +
+               "where type = 'purchase' and refund_journal = TRUE")
+    cr.execute("UPDATE account_journal SET type = 'sale_refund' " +
+               "where type = 'sale' and refund_journal = TRUE")
 
 def migrate(cr, version):
     try:
         pool = pooler.get_pool(cr.dbname)
-        openupgrade.rename_columns(column_renames)
+        openupgrade.rename_columns(cr, column_renames)
         mgr_refund_journal_type(cr)
     except Exception, e:
         log.error("Migration: error in pre.py: %s" % e)
