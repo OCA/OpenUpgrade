18b79de901d7259c5816af1c720a75f049a00cbd
Holger Brunn (Therp) <no_email@example.org>
2012-06-24 13:18:42 +0000

    [ADD] base_report_creator: Set new 'result' model in existing act_windows

---
+++ addons/base_report_creator/migrations/6.0.1.0/pre-migration.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    This migration script copyright (C) 2012 Therp BV (<http://therp.nl>)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp.openupgrade import openupgrade
+
+@openupgrade.migrate()
+def migrate(cr, version):
+    #the object to show a report is now base_report_creator_report.result
+    #but reports are still configured with base_report_creator.report
+    cr.execute("""
+        update ir_act_window set 
+            res_model='base_report_creator_report.result' 
+        where 
+            res_model='base_report_creator.report' 
+        and
+            context like '{''report_id'':%}'""")
