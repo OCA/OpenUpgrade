7c43f65cec92e9f54423a299d41ed4f440016e11
Stefan Rijnhart <stefan@therp.nl>
2012-10-23 13:24:01 +0000

    [ADD] Wrap raw SQL query in res_currency in savepoints
    https://launchpad.net/bugs/1067874

--- openerp/addons/base/res/res_currency.py
+++ openerp/addons/base/res/res_currency.py
@@ -91,9 +91,16 @@ class res_currency(osv.osv):
         # we would allow duplicate "global" currencies (all having company_id == NULL) 
         cr.execute("""SELECT indexname FROM pg_indexes WHERE indexname = 'res_currency_unique_name_company_id_idx'""")
         if not cr.fetchone():
-            cr.execute("""CREATE UNIQUE INDEX res_currency_unique_name_company_id_idx
+            try:
+                cr.execute('SAVEPOINT index_currency');
+                cr.execute("""CREATE UNIQUE INDEX res_currency_unique_name_company_id_idx
                           ON res_currency
                           (name, (COALESCE(company_id,-1)))""")
+                cr.execute('RELEASE SAVEPOINT index_currency');
+            except Exception, e:
+                cr.execute('ROLLBACK TO SAVEPOINT index_currency');
+                import logging
+                logging.getLogger('OpenUpgrade').debug('Could not create currency unique index: %s', e)
 
     def read(self, cr, user, ids, fields=None, context=None, load='_classic_read'):
         res = super(res_currency, self).read(cr, user, ids, fields, context, load)
