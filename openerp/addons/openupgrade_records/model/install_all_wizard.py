74246cf552152fad861bd0bfdb1ed550c337c144
Stefan Rijnhart <stefan@therp.nl>
2012-11-24 21:32:46 +0000

    [ADD] Untested OpenUpgrade records addon

---
+++ openerp/addons/openupgrade_records/model/install_all_wizard.py
@@ -0,0 +1,113 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    This module Copyright (C) 2012 OpenUpgrade community
+#    https://launchpad.net/~openupgrade-committers
+#
+#    Contributors:
+#    Therp BV <http://therp.nl>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+import time
+import os
+from osv import osv, fields
+import pooler
+
+class install_all_wizard(osv.osv_memory):
+    _name = 'openupgrade.install.all.wizard'
+    _description = 'OpenUpgrade Install All Wizard'
+    _columns = {
+        'state': fields.selection([('init', 'init'), ('ready', 'ready')], 'State', readonly=True),
+        'to_install': fields.integer('Number of modules to install', readonly=True),
+        }
+    _defaults = {
+        'state': lambda *a: 'init',
+        }
+
+
+    def default_get(self, cr, uid, fields, context=None):
+        """
+        Update module list and retrieve the number
+        of installable modules
+        """
+        res = super(install_all_wizard, self).default_get(
+            cr, uid, fields, context=None)
+        module_obj = self.pool.get('ir.module.module')
+        update, add = module_obj.update_list(cr, uid,)
+        print "%s modules added" % add
+        module_ids = module_obj.search(
+            cr, uid, [('state', 'not in', ['installed', 'uninstallable', 'unknown'])])
+        res.update(
+            {'to_install': module_ids and len(module_ids) or False}
+            )
+        return res
+
+    def quirk_fiscalyear(self, cr, uid, ids, context=None):
+        """ 
+        Install account module first and create a fiscal year,
+        in order to prevent "No fiscal year defined" exception
+        during an upgrade or reinstallation of the account module.
+        
+        Refer to account_fiscalyear.find(), which is called as
+        a default function by the orm upon module upgrade.
+        """
+        module_obj = self.pool.get('ir.module.module')
+        pool = self.pool
+        # Retrieve status of the account module
+        account_module_id = module_obj.search(
+            cr, uid, [('name', '=', 'account')], context=context)[0]
+        state = module_obj.read(
+            cr, uid, account_module_id, ['state'], context=context)['state']
+        if state != 'installed':
+            # Cancel installation of other modules
+            module_ids = module_obj.search(
+                cr, uid, [('state', '=', 'to install')])
+            module_obj.write(cr, uid, module_ids, {'state': 'uninstalled'})
+            # Mark the module and its dependencies
+            module_obj.button_install(cr, uid, [account_module_id])
+            # Install account module
+            cr.commit()
+            _db, pool = pooler.restart_pool(cr.dbname, update_module=True)
+        # get or create today's fiscal year
+        fy_obj = pool.get('account.fiscalyear')
+        if not fy_obj.find(cr, uid, False, exception=False, context=context):
+            fy_obj.create(cr, uid, {
+                    'name': time.strftime('%Y'),
+                    'code': time.strftime('%Y'),
+                    'date_start': "%s-01-01" % time.strftime('%Y'),
+                    'date_stop': "%s-12-31" % time.strftime('%Y'),
+                    })
+        
+    def install_all(self, cr, uid, ids, context=None):
+        """
+        Main wizard step. Set all installable modules to install
+        and actually install them.
+        """
+        module_obj = self.pool.get('ir.module.module')
+        module_ids = module_obj.search(
+            cr, uid, [('state', 'not in', ['installed', 'uninstallable', 'unknown'])])
+        if module_ids:
+            module_obj.write(
+                cr, uid, module_ids, {'state': 'to install'})
+            cr.commit()
+            _db, pool = pooler.restart_pool(cr.dbname, update_module=True)
+            self.write(cr, uid, ids, {'state': 'ready'})
+        return True
+
+install_all_wizard()
+
---
+++ openerp/addons/openupgrade_records/model/openupgrade_record.py
@@ -0,0 +1,111 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    This module Copyright (C) 2012 OpenUpgrade community
+#    https://launchpad.net/~openupgrade-committers
+#
+#    Contributors:
+#    Therp BV <http://therp.nl>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from osv import osv, fields
+
+# Cannot use forward references in 6.0
+class openupgrade_record(osv.osv):
+    _name = 'openupgrade.record'
+openupgrade_record()
+
+class openupgrade_attribute(osv.osv):
+    _name = 'openupgrade.attribute'
+    _rec_name = 'attribute_id'
+    _columns = {
+        'name': fields.char(
+            'Name', size=24,
+            readonly=True,
+            ),
+        'value': fields.char(
+            'Value',
+            size=4096,
+            readonly=True,
+            ),
+        'record_id': fields.many2one(
+            'openupgrade.record', ondelete='CASCADE',
+            readonly=True,
+            ),
+        }
+openupgrade_attribute()
+
+class openupgrade_record(osv.osv):
+    _inherit = 'openupgrade.record'
+
+    _columns = {
+        'name': fields.char('Name', size=256, readonly=True),
+        'module': fields.char('Module', size=128, readonly=True),
+        'model': fields.char('Model', size=128, readonly=True),
+        'field': fields.char('Field', size=128, readonly=True),
+        'mode': fields.selection(
+            [('create', 'Create'), ('modify', 'Modify')],
+            'Mode',
+            help='Set to Create if a field is newly created '
+            'in this module. If this module modifies an attribute of an '
+            'exting field, set to Modify.',
+            readonly=True,
+             ),
+        'type': fields.selection(
+            [('field', 'Field'), ('xmlid', 'XML ID')],
+            'Type',
+            readonly=True,
+            ),
+        'attribute_ids': fields.one2many(
+            'openupgrade.attribute', 'record_id', 'Attributes',
+            readonly=True,
+            ),
+        }
+    def field_dump(self, cr, uid, context=None):
+        keys = [
+            'module',
+            'mode',
+            'model',
+            'field',
+            'type',
+            'isfunction',
+            'relation',
+            'required',
+            'selection_keys',
+            'req_default',
+            'inherits',
+            ]
+
+        template = dict([(x, False) for x in keys])
+        ids = self.search(cr, uid, [('type', '=', 'field')], context=context)
+        records = self.browse(cr, uid, ids, context=context)
+        data = []
+        for record in records:
+            repr = template.copy()
+            repr.update({
+                    'module': record.module,
+                    'model': record.model,
+                    'field': record.field,
+                    'mode': record.mode,
+                    })
+            repr.update(
+                dict([(x.name, x.value) for x in record.attribute_ids]))
+            data.append(repr)
+        return data
+
+openupgrade_record()
