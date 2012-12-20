f6ebdb05fc4de4d980a1b9fc7075da8bf6b44cc9
Stefan Rijnhart <stefan@therp.nl>
2012-12-20 13:02:54 +0100

    [UPD] Refresh analysis file for the base module [IMP] Add to the apriori knowledge

--- openerp/addons/openupgrade_records/lib/apriori.py
+++ openerp/addons/openupgrade_records/lib/apriori.py
@@ -3,9 +3,11 @@ to help the matching process
 """
 
 renamed_modules = {
+    'account_coda': 'l10n_be_coda',
+    'base_crypt': 'auth_crypt',
+    'mrp_subproduct': 'mrp_byproduct',
     'users_ldap': 'auth_ldap',
     'wiki': 'document_page',
-    'mrp_subproduct': 'mrp_byproduct',
     }
 
 renamed_models = {
