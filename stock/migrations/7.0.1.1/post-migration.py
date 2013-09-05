4dfc248b11c34033f10e0234d81d0728e2e2eeb4
Sylvain LE GAL <sylvain.legal@grap.coop>
2013-09-05 22:59:18 +0200

    [ADD] analysis and script migration for 'stock' module.

---
+++ stock/migrations/7.0.1.1/post-migration.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    This module copyright (C) 2013 Sylvain LE GAL
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp import pooler
+from openerp.openupgrade import openupgrade, openupgrade_70
+
+def migrate_partners(cr, pool):
+    openupgrade_70.set_partner_id_from_partner_address_id(
+        cr, pool, 'stock.warehouse',
+        'partner_id', openupgrade.get_legacy_name('partner_address_id'))
+    openupgrade_70.set_partner_id_from_partner_address_id(
+        cr, pool, 'stock.location',
+        'partner_id', openupgrade.get_legacy_name('address_id'))
+    openupgrade_70.set_partner_id_from_partner_address_id(
+        cr, pool, 'stock.move',
+        'partner_id', openupgrade.get_legacy_name('address_id'))
+    openupgrade_70.set_partner_id_from_partner_address_id(
+        cr, pool, 'stock.picking',
+        'partner_id', openupgrade.get_legacy_name('address_id'))
+
+@openupgrade.migrate()
+def migrate(cr, version):
+    pool = pooler.get_pool(cr.dbname)
+    migrate_partners(cr, pool)
