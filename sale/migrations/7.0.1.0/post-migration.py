cdc24d12d5a413e2d9e7142e3749a862f8377236
Sylvain LE GAL <sylvain.legal@grap.coop>
2013-09-24 14:03:08 +0200

    [FIX] disable two wkf_condition before execution of update 'sale.order' partners and enabled after.

--- sale/migrations/7.0.1.0/post-migration.py
+++ sale/migrations/7.0.1.0/post-migration.py
@@ -22,6 +22,25 @@ from openerp import pooler, SUPERUSER_ID
 from openerp.openupgrade import openupgrade, openupgrade_70
 
 def migrate_sale_order_addresses(cr, pool):
+    # getting object from 'sale_stock' module because the pre-migration script
+    # has renamed xml_id from 'sale' to 'sale_stock' previously
+    data_obj = pool.get("ir.model.data")
+    trans_1 = data_obj.read(cr, SUPERUSER_ID, 
+        [data_obj._get_id(cr, SUPERUSER_ID, 'sale_stock', 'trans_ship_ship_except')], 
+        ['res_id'])[0]['res_id']
+    trans_2 = data_obj.read(cr, SUPERUSER_ID, 
+        [data_obj._get_id(cr, SUPERUSER_ID, 'sale_stock', 'trans_ship_ship_end')], 
+        ['res_id'])[0]['res_id']
+    # backup values
+    cr.execute("""SELECT condition FROM wkf_transition WHERE id=%s""" %(trans_1))
+    value_1 = cr.fetchone()[0]
+    cr.execute('SELECT condition FROM wkf_transition WHERE id=%s' %(trans_2))
+    value_2 = cr.fetchone()[0]
+
+    # disabling two transitions during 'sale_order' update.
+    cr.execute("""UPDATE wkf_transition SET condition = 'False' WHERE id=%s or id=%s """ 
+        %(trans_1, trans_2))
+    
     # partner_address become partner
     openupgrade_70.set_partner_id_from_partner_address_id(
         cr, pool, 'sale.order',
@@ -34,6 +53,12 @@ def migrate_sale_order_addresses(cr, pool):
     openupgrade_70.set_partner_id_from_partner_address_id(
         cr, pool, 'sale.order',
         'partner_id', openupgrade.get_legacy_name('partner_order_id'))
+
+    # Rewriting good values
+    cr.execute("""update wkf_transition set condition = %s where id=%s""",
+        (value_1, trans_1))
+    cr.execute("""update wkf_transition set condition = %s where id=%s""",
+        (value_2, trans_2))
 
 def migrate_sale_order_line_addresses(cr, pool):
     # partner_address become partner
@@ -63,7 +88,7 @@ def migrate_sale_order_line_names(cr, pool):
 @openupgrade.migrate()
 def migrate(cr, version):
     pool = pooler.get_pool(cr.dbname)
-    # migrate_sale_order_addresses(cr, pool) TODO : Restore
+    migrate_sale_order_addresses(cr, pool)
     migrate_sale_order_line_addresses(cr, pool)
     migrate_sale_order_line_names(cr, pool)
 
