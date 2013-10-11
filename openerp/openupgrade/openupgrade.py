7ce4b8e2413cf8eb4821b0835db91030f2be5a39
Sandy Carter <sandy.carter@savoirfairelinux.com>
2013-10-11 14:17:05 -0400

    [IMP] update_module_names to update ir_module_module_dependency

--- openerp/openupgrade/openupgrade.py
+++ openerp/openupgrade/openupgrade.py
@@ -348,6 +348,9 @@ def update_module_names(cr, namespec):
         query = ("UPDATE ir_model_data SET module = %s "
                  "WHERE module = %s ")
         logged_query(cr, query, (new_name, old_name))
+        query = ("UPDATE ir_module_module_dependency SET name = %s "
+                 "WHERE name = %s")
+        logged_query(cr, query, (new_name, old_name))
 
 def add_ir_model_fields(cr, columnspec):
     """
