d662b7e3d30e8d208b453398123e6a97fdf72b9f
Sandy Carter <sandy.carter@savoirfairelinux.com>
2013-11-14 13:01:22 -0500

    [IMP] Many improvements to crm migration * leads migrate correctly * mail aliases related to crm.case.sections are created properly * fields depricated correctly * crm.lead.categ_id changed to many_to_many

--- crm/migrations/7.0.1.0/post-migration.py
+++ crm/migrations/7.0.1.0/post-migration.py
@@ -1,4 +1,5 @@
 # -*- encoding: utf-8 -*-
+##############################################################################
 #
 #    OpenERP, Open Source Management Solution
 #    This module copyright (C) 2013 Savoir-faire Linux
@@ -17,14 +18,47 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
+##############################################################################
 
+from openerp import pooler, SUPERUSER_ID
 from openupgrade import openupgrade
+
+
+def create_section_mail_aliases(cr, pool, uid=SUPERUSER_ID):
+    """
+    Create mail aliases for each existing crm.case.section
+    """
+    # Pools
+    crm_case_section = pool.get('crm.case.section')
+    mail_alias = pool.get('mail.alias')
+    # Get case_sections
+    openupgrade.logged_query(cr, """SELECT id, name from crm_case_section;""")
+    vals = cr.fetchall()
+    # Create alias for each section
+    for id, name in vals:
+        fields = {
+            'alias_name': name,
+            'alias_defaults': {'section_id': id, 'type': 'lead'},
+        }
+        alias_id = mail_alias.create_unique_alias(cr, uid, fields,
+                                                  model_name="crm.lead")
+        crm_case_section.write(cr, uid, [id], {'alias_id': alias_id})
 
 
 @openupgrade.migrate()
 def migrate(cr, version):
+    """
+    * Create mail aliases for crm case section,
+    * Load xml data (Mail alias and updates to CRM stages)
+    * Change categ_id to many to many
+    """
+    pool = pooler.get_pool(cr.dbname)
     openupgrade.load_xml(
         cr, 'crm',
         'migrations/7.0.1.0/data.xml')
+    create_section_mail_aliases(cr, pool)
+    # Migrate m2o categ_id to m2m categ_ids
+    openupgrade.m2o_to_m2m(cr, pool.get('crm.lead'), 'crm_lead', 'categ_ids',
+                           openupgrade.get_legacy_name('categ_id'))
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
