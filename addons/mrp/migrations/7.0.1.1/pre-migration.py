2c8212ce74f26221ed4850f852742722ef24c9e1
sylvain.legal@grap.coop <sylvain.legal@grap.coop>
2014-01-24 08:41:46 +0000

    [ADD] analysis and script migration for mrp module.

---
+++ addons/mrp/migrations/7.0.1.1/pre-migration.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    This module copyright (C) 2013 Sylvain LE GAL
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp.openupgrade import openupgrade
+
+xmlid_renames = [
+    ('mrp.trans_product_check_produce', 'mrp.trans_confirm_mto_need_production'),
+    ('procurement.trans_produce_make_done', 'mrp.trans_produce_finished_prod'),
+]
+
+@openupgrade.migrate()
+def migrate(cr, version):
+    openupgrade.rename_xmlids(cr, xmlid_renames)
