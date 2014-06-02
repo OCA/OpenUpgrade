dd941af6c6c5ad1fae3029b1ca8750a128c90979
Alexandre Fayolle <afayolle.ml@free.fr>
2014-06-02 18:38:49 +0200

    fix error in previous refactoring

--- addons/product/migrations/8.0.1.1/post-migration.py
+++ addons/product/migrations/8.0.1.1/post-migration.py
@@ -12,13 +12,13 @@ def move_fields(cr, pool):
                "          WHERE product_product.id=product_supplierinfo.%s) " \
                % openupgrade.get_legacy_name('product_id'),
                #
-               "UPDATE product_template as pt " 
+               "UPDATE product_template as pt "
                "SET color=(SELECT pp1.%s "
                "      FROM product_product as pp1 "
                "      WHERE pp1.product_tmpl_id=pt.id ORDER BY pp1.id LIMIT 1), "
                "    image=(SELECT pp2.image_variant "
                "      FROM product_product as pp2 "
-               "      WHERE pp2.product_tmpl_id=pt.id ORDER BY pp2.id LIMIT 1)" 
+               "      WHERE pp2.product_tmpl_id=pt.id ORDER BY pp2.id LIMIT 1)"
                "    packaging=(SELECT pp3.%s "
                "      FROM product_product as pp3 "
                "      WHERE pp3.product_tmpl_id=pt.id ORDER BY pp3.id LIMIT 1)" \
@@ -26,7 +26,7 @@ def move_fields(cr, pool):
                   openupgrade.get_legacy_name('packaging'),
                   )
                #
-               
+
                ]
     for sql in queries:
         execute(cr, sql)
@@ -51,12 +51,12 @@ def migrate_packaging(cr, pool):
     packaging_ids = packaging_obj.search(cr, SUPERUSER_ID, [])
     for packaging in packaging_obj.browse(cr, SUPERUSER_ID, packaging_ids):
         ul = packaging.ul
-        ul.write({'height': height,
-                  'width': width,
-                  'length': length,
-                  'weight_ul': weight,
+        ul.write({'height': packaging.height,
+                  'width': packaging.width,
+                  'length': packaging.length,
+                  'weight_ul': packaging.weight,
                   })
-        
+
 @openupgrade.migrate()
 def migrate(cr, version):
     pool = pooler.get_pool(cr.dbname)
@@ -64,4 +64,4 @@ def migrate(cr, version):
     copy_fields(cr, pool)
     migrate_packaging(cr, pool)
     load_data(cr)
-    
+
