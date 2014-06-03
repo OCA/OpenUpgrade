a1313d69b93c94aa7c4aa5e5ee3bf91037ebbf27
OpenERP File User <florian@Florian.(none)>
2014-06-03 12:32:30 +0200

    Add analytic file and pre-migration file for module hr (no post-migration file needed)

---
+++ addons/hr/migrations/8.0.1.1/pre-migration.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    This migration script copyright (C) 2010-2014 Akretion
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+
+from openerp.openupgrade import openupgrade, openupgrade_80
+from openerp import pooler, SUPERUSER_ID
+
+
+column_change_type = [
+        ('hr_job', 'expected_employees'),
+        ('hr_job', 'no_of_employee'),
+        ('hr_job', 'no_of_recruitment')
+        ]
+
+@openupgrade.migrate()
+def migrate(cr, version):
+    pool = pooler.get_pool(cr.dbname)
+    openupgrade_80.set_message_last_post(
+        cr, SUPERUSER_ID, pool, ['hr.job', 'hr.employee'])
+    for table, field in column_change_type:
+        openupgrade.float_to_integer(cr, table, field)
+
+
+
