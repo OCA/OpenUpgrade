aecd2d511167268dec46a4b1771661db3c9b43c2
Sylvain LE GAL <sylvain.legal@grap.coop>
2014-06-05 17:32:44 +0200

    [ADD] post and pre migration files. (work in progress)

---
+++ addons/point_of_sale/migrations/8.0.1.0.1/post-migration.py
@@ -0,0 +1,28 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    'Point Of Sale' migration module for Odoo
+#    copyright: 2014-Today GRAP
+#    @author: Sylvain LE GAL <https://twitter.com/legalsylvain>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp.openupgrade import openupgrade
+
+
+@openupgrade.migrate()
+def migrate(cr, version):
+    pass
