54294843941f7f9ea3c687fe22cada4509658f31
Guewen Baconnier <guewen@gmail.com>
2014-06-05 10:38:19 +0200

    a migration function in mrp was incomplete

--- addons/mrp/migrations/8.0.1.1/post-migrate.py
+++ addons/mrp/migrations/8.0.1.1/post-migrate.py
@@ -1,18 +1,23 @@
 from openerp.openupgrade import openupgrade
-from openerp import pooler, SUPERUSER_ID
+from openerp import pooler
+
 
 def move_fields(cr, pool):
     execute = openupgrade.logged_query
-    queries = [ """
-    UPDATE product_product
-    SET produce_delay=(SELECT pt.%s
-                       FROM product_template
-                       WHERE product_template.id=product_product.product_tmpl_id)
-    """ % openupgrade.get_legacy_name('produce_delay'),
+    queries = [
+        """
+        UPDATE product_product
+        SET produce_delay = (
+            SELECT pt.%s
+            FROM product_template
+            WHERE product_template.id = product_product.product_tmpl_id)
+        """ % openupgrade.get_legacy_name('produce_delay'),
+    ]
+    for sql in queries:
+        execute(cr, sql)
 
 
 @openupgrade.migrate()
 def migrate(cr, version):
     pool = pooler.get_pool(cr.dbname)
     move_fields(cr, pool)
-    
