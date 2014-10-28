bfd9421c660c696f84b5f287e45d91538e195349
Sylvain LE GAL <sylvain.legal@grap.coop>
2014-10-28 12:22:37 +0100

    [FIX] [8.0] 'base' skip error if not valid view;

--- openerp/addons/base/ir/ir_ui_view.py
+++ openerp/addons/base/ir/ir_ui_view.py
@@ -208,16 +208,7 @@ class view(osv.osv):
             if view.type != 'qweb':
                 view_doc = etree.fromstring(view_arch_utf8)
                 # verify that all fields used are valid, etc.
-                try:
-                    self.postprocess_and_fields(cr, uid, view.model, view_doc, view.id, context=context)
-                except Exception:
-                    # OpenUpgrade: We ignore all view errors as they are
-                    # caused by views introduced by models not year loaded
-                    _logger.warn(
-                        "Can't render view %s for model: %s. If you are "
-                        "migrating between major versions of OpenERP, "
-                        "this is to be expected (otherwise, do not run "
-                        "OpenUpgrade server).", view.xml_id, view.model)
+                self.postprocess_and_fields(cr, uid, view.model, view_doc, view.id, context=context)
                 # RNG-based validation is not possible anymore with 7.0 forms
                 view_docs = [view_doc]
                 if view_docs[0].tag == 'data':
@@ -229,23 +220,9 @@ class view(osv.osv):
                     if parse_version(version) < parse_version('7.0') and validator and not validator.validate(view_arch):
                         for error in validator.error_log:
                             _logger.error(tools.ustr(error))
-                        # OpenUpgrade: We ignore all view errors as they are
-                        # caused by views introduced by models not year loaded
-                        _logger.warn(
-                            "Can't render view %s for model: %s. If you are "
-                            "migrating between major versions of OpenERP, "
-                            "this is to be expected (otherwise, do not run "
-                            "OpenUpgrade server).", view.xml_id, view.model)
-                        return True
+                        return False
                     if not valid_view(view_arch):
-                        # OpenUpgrade: We ignore all view errors as they are
-                        # caused by views introduced by models not year loaded
-                        _logger.warn(
-                            "Can't render view %s for model: %s. If you are "
-                            "migrating between major versions of OpenERP, "
-                            "this is to be expected (otherwise, do not run "
-                            "OpenUpgrade server).", view.xml_id, view.model)
-                        return True
+                        return False
         return True
 
     _sql_constraints = [
