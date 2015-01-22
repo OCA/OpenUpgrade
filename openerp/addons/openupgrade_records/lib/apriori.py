51d8ea549347df0d5a31dd7d7f0e5a3187688395
Holger Brunn <hbrunn@therp.nl>
2015-01-22 09:58:47 +0100

    [ADD] rename letter_mgmt_v6

--- openerp/addons/openupgrade_records/lib/apriori.py
+++ openerp/addons/openupgrade_records/lib/apriori.py
@@ -8,6 +8,9 @@ renamed_modules = {
     'mrp_subproduct': 'mrp_byproduct',
     'users_ldap': 'auth_ldap',
     'wiki': 'document_page',
+    # https://github.com/OCA/crm/tree/7.0/lettermgmt
+    # this worked with 6.1 too and there's no specific 6.1 version
+    'letter_mgmt_v6': 'lettermgmt',
     }
 
 renamed_models = {
