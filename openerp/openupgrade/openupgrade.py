617b2f2fd8fa9fd6477fbead9c628019df12b73e
Pedro M. Baeza <pedro.baeza@gmail.com>
2015-02-08 11:55:55 +0100

    [IMP/FIX] m2o_to_m2m: Use SQL for performance and for avoiding side effects

--- openerp/openupgrade/openupgrade.py
+++ openerp/openupgrade/openupgrade.py
@@ -537,14 +537,27 @@ def m2o_to_x2m(cr, model, table, field, source_field):
 
     .. versionadded:: 7.0
     """
+    if not model._columns.get(field):
+        raise orm.except_orm(
+            "Error", "m2o_to_m2m: field %s doesn't exist in model %s" % (
+                field, model._name))
+    if not isinstance(model._columns[field], orm.fields.many2many):
+        raise orm.except_orm(
+            "Error", "m2o_to_m2m: field %s of model %s is not a many2many "
+                     "one" % (field, model._name))
     cr.execute('SELECT id, %(field)s '
                'FROM %(table)s '
                'WHERE %(field)s is not null' % {
                    'table': table,
                    'field': source_field,
                    })
+    rel, id1, id2 = orm.fields.many2many._sql_names(model._columns[field],
+                                                    model)
     for row in cr.fetchall():
-        model.write(cr, SUPERUSER_ID, row[0], {field: [(4, row[1])]})
+        cr.execute(
+            "INSERT INTO %s (%s, %s) "
+            "VALUES (%%s, %%s)" %
+            (rel, id1, id2), (row[0], row[1]))
 
 # Backwards compatibility
 m2o_to_m2m = m2o_to_x2m
