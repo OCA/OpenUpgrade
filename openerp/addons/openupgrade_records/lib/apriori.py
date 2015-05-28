b8459435e34057a1055d72308aa74955f240d398
Holger Brunn <hbrunn@therp.nl>
2015-05-28 16:34:01 +0200

    [ADD] rename some of the banking modules

--- openerp/addons/openupgrade_records/lib/apriori.py
+++ openerp/addons/openupgrade_records/lib/apriori.py
@@ -9,6 +9,11 @@ renamed_modules = {
     'invoice_validation_wkfl': 'account_invoice_validation_workflow',
     # OCA/server-tools
     'audittrail': 'auditlog',
+    # OCA/bank-statement-import
+    'account_banking': 'account_bank_statement_import',
+    'account_banking_camt': 'bank_statement_parse_camt',
+    'account_banking_nl_ing_mt940': 'bank_statement_parse_nl_ing_mt940',
+    'account_banking_nl_rabo_mt940': 'bank_statement_parse_nl_rabo_mt940',
 }
 
 renamed_models = {
