a7095730909d8f5fcc32bd4aed09bb9e0e09e10f
Holger Brunn <hbrunn@therp.nl>
2015-08-17 11:42:26 +0200

    [FIX] migrate categories of hr.holiday.status

--- addons/hr_holidays/migrations/7.0.1.5/post-migration.py
+++ addons/hr_holidays/migrations/7.0.1.5/post-migration.py
@@ -2,8 +2,7 @@
 ##############################################################################
 #
 #    OpenERP, Open Source Management Solution
-#    This migration script copyright (C) 2014
-#                    Pedro M. Baeza (pedro.baeza@serviciosbaeza.com)
+#    This migration script copyright (C) 2015 Therp BV
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -21,14 +20,22 @@
 ##############################################################################
 from openupgrade import openupgrade
 
-column_renames = {
-    'hr_holidays': [
-        ('case_id', 'meeting_id'),
-        ('categ_id', None),
-    ]
-}
-
 
 @openupgrade.migrate()
 def migrate(cr, version):
-    openupgrade.rename_columns(cr, column_renames)
+    # write categories from crm.case.categ
+    cr.execute('alter table crm_meeting_type add column %s integer',
+               openupgrade.get_legacy_name('crm_meeting_type_id'))
+    cr.execute(
+        'insert into crm_meeting_type (name, %s) '
+        'select name, id from crm_case_categ where object_id='
+        "(select id from ir_model where model='crm.meeting')" %
+        openupgrade.get_legacy_name('crm_meeting_type_id'))
+    cr.execute(
+        'update hr_holidays_status s '
+        'set categ_id=t.id '
+        'from crm_meeting_type t '
+        'where t.%s=s.%s' % (
+            openupgrade.get_legacy_name('crm_meeting_type_id'),
+            openupgrade.get_legacy_name('categ_id'),
+        ))
