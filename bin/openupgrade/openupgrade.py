da46ee751911cb3c77a1c82bac24f29f3df13cbd
Stefan Rijnhart <stefan@therp.nl>
2015-08-17 16:41:57 +0200

    [RFR] Warn, not log depreciation messages

--- bin/openupgrade/openupgrade.py
+++ bin/openupgrade/openupgrade.py
@@ -1,15 +1,13 @@
 # -*- coding: utf-8 -*-
-import logging
-logger = logging.getLogger('OpenUpgrade')
+import warnings
 
 _short_name = __name__.split(".")[-1]
-logger.warn(
+warnings.warn(
     "Importing %(full_name)s is deprecated. "
     "Use from openupgradelib import %(short_name)s" % {
         'full_name': __name__,
         'short_name': _short_name,
-    }
-)
+    }, DeprecationWarning, stacklevel=2)
 
 _new_name = "openupgradelib.%s" % _short_name
 
