432a104ba45a37a2f7f50f81f4eb0868493c930d
Stefan Rijnhart <stefan@opener.am>
2015-10-04 12:02:45 +0200

    [RFR] Relative imports

--- openerp/addons/openupgrade_records/model/__init__.py
+++ openerp/addons/openupgrade_records/model/__init__.py
@@ -1,5 +1,5 @@
-import openupgrade_record
-import comparison_config
-import analysis_wizard
-import generate_records_wizard
-import install_all_wizard
+from . import openupgrade_record
+from . import comparison_config
+from . import analysis_wizard
+from . import generate_records_wizard
+from . import install_all_wizard
