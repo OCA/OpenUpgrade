77964be55d91bf7b7124bec1cb144e4653367de8
M-E Leblanc <marc_e_leblanc@outlook.com>
2015-11-23 16:45:55 -0500

    First version

---
+++ addons/project/migrations/9.0.1.1/pre-migration.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenUpgrade module for Odoo
+#    @copyright 2014-Today: Odoo Community Association
+#    @author: Sylvain LE GAL <https://twitter.com/legalsylvain>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp.openupgrade import openupgrade
+from openerp.addons.openupgrade_records.lib import apriori
+
+
+column_copies = {
+    'project.task': [
+        ('description', None, None),
+    ],
+}
+
+column_renames = {
+    'res_partner_bank': [
+        ('bank', 'bank_id'),
+    ],
+}
+
+
+@openupgrade.migrate()
+def migrate(cr, version):
+    openupgrade.copy_columns(cr, column_copies)
+    openupgrade.rename_columns(cr, column_renames)
