4c7cf337f887b117bb630cf3bcbc4707c676a5b9
Vincent Coll <vincentgagnnoncoll@gmail.com>
2015-11-30 11:35:58 -0500

    First version

---
+++ addons/project_issue/migrations/9.0.1.0/post-migration.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenUpgrade module for Odoo
+#    @copyright 2014-Today: Odoo Community Association, Microcom
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+import logging
+from openerp import pooler, SUPERUSER_ID
+from openupgradelib import openupgrade
+logger = logging.getLogger('OpenUpgrade')
+
+# copied from pre-migration
+column_copies = {
+    'project.task': [
+        ('description', None, None),
+    ],
+}
+
+
+def map_priority(cr):
+    openupgrade.map_values(
+        cr,
+        openupgrade.get_legacy_name('priority'),
+        'priority',
+        [('2', '1')],
+        table='project_task', write='sql')
+
+
+def map_template_state(cr):
+    openupgrade.map_values(
+        cr,
+        openupgrade.get_legacy_name('state'),
+        'state',
+        [('template', 'draft')],
+        table='project_project', write='sql')
+
+
+@openupgrade.migrate()
+def migrate(cr, version):
+    pool = pooler.get_pool(cr.dbname)
+    map_priority(cr)
+    map_template_state(cr)
+    for table_name in column_spec.keys():
+        for (old, new, field_type) in column_spec[table_name]:
+            convert_field_to_html(cr, table_name, get_legacy_name(old), old)
+
---
+++ addons/project_issue/migrations/9.0.1.0/pre-migration.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenUpgrade module for Odoo
+#    @copyright 2014-Today: Odoo Community Association, Microcom
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openupgradelib import openupgrade
+
+
+column_copies = {
+    'project.task': [
+        ('description', None, None),
+    ],
+}
+
+# Column is kept even though it was removed in v9, as to allow recovery by a future OCA module
+column_renames = {
+    'project.task': [
+        ('reviewer_id', None),
+    ],
+}
+
+
+@openupgrade.migrate()
+def migrate(cr, version):
+    openupgrade.copy_columns(cr, column_copies)
+    openupgrade.rename_columns(cr, column_renames)
+    openupgrade.rename_tables(
+        cr, [('project_category', 'project_tags')])
