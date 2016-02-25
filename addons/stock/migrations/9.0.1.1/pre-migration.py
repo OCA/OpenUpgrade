74e9311f4a29eddde415dfd143a9bd56cd35c455
Stephane Le Cornec <stephane.lecornec@gmail.com>
2016-02-25 16:36:24 -0500

    9.0 - stock

---
+++ addons/stock/migrations/9.0.1.1/pre-migration.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenUpgrade module for Odoo
+#    @copyright 2014-Today: Odoo Community Association
+#    @author: Sylvain LE GAL <https://twitter.com/legalsylvain>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openupgradelib import openupgrade
+
+
+xmlids = [
+    ('stock.view_partner_property_form', 'stock.view_partner_stock_form'),
+]
+
+
+@openupgrade.migrate()
+def migrate(cr, version):
+    openupgrade.rename_xmlids(cr, xmlids)
