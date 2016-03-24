eea29dda9b1e5f1fd42cd6599aaf8eb7ade663b2
Pedro M. Baeza <pedro.baeza@gmail.com>
2016-03-24 03:50:18 +0100

    [FIX] crm_helpdesk: Correct priority mapping

--- addons/crm_helpdesk/migrations/8.0.1.0/post-migration.py
+++ addons/crm_helpdesk/migrations/8.0.1.0/post-migration.py
@@ -13,4 +13,4 @@ def migrate(cr, version):
     openupgrade_80.set_message_last_post(cr, uid, pool, ['crm.helpdesk'])
     openupgrade.map_values(
         cr, 'priority', openupgrade.get_legacy_name('priority'),
-        [('1', '0'), ('2', '1'), ('3', '2'), ('4', '2')], table='crm_helpdesk')
+        [('1', '2'), ('3', '1'), ('4', '0'), ('5', '0')], table='crm_helpdesk')
