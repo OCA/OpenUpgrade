5be486f2bd60dad21365a9c70f5b9e87888aabf3
Stefan Rijnhart <stefan@opener.am>
2016-06-28 21:36:40 +0200

    [UPD] Migration script for sales_team

--- addons/sales_team/migrations/9.0.1.0/pre-migration.py
+++ addons/sales_team/migrations/9.0.1.0/pre-migration.py
@@ -2,12 +2,18 @@
 # © 2015 Eficent Business and IT Consulting Services S.L. -
 # Jordi Ballester Alomar
 # © 2015 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
+# © 2016 Opener B.V. - Stefan Rijnhart
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-
 from openupgradelib import openupgrade
 
-table_renames = [('crm_case_section', 'crm_team'),]
 
 @openupgrade.migrate()
 def migrate(cr, version):
-    openupgrade.rename_tables(cr, table_renames)
+    openupgrade.rename_xmlids(cr, [
+        ('sales_team.section_sales_department',
+         'sales_team.team_sales_department'),
+    ])
+    openupgrade.rename_tables(cr, [('crm_case_section', 'crm_team')])
+    openupgrade.rename_models(cr, [('crm.case.section', 'crm.team')])
+    openupgrade.rename_columns(cr, {
+        'res_users': [('default_section_id', 'sale_team_id')]})
