dc4e92f291f9f47718dd57eca35ee2b53c08f787
Stephane Le Cornec <stephane.lecornec@gmail.com>
2016-06-29 11:10:51 -0400

    [ADD] fix copyright notice

--- addons/hr/migrations/9.0.1.1/pre-migration.py
+++ addons/hr/migrations/9.0.1.1/pre-migration.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    Copyright (C) 2014 Akretion (http://www.akretion.com/)
-#    @author: Alexis de Lattre <alexis.delattre@akretion.com>
+#    OpenUpgrade module for Odoo
+#    @copyright 2014-Today: Odoo Community Association, Microcom
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
