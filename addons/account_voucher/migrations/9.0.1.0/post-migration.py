0754312409104d6c239aab6e8b6e861c00e24669
Holger Brunn <hbrunn@therp.nl>
2016-07-28 17:28:33 +0200

    [ADD] account_voucher migration

---
+++ addons/account_voucher/migrations/9.0.1.0/post-migration.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# © 2016 Therp BV <http://therp.nl>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrate()
+def migrate(cr, version):
+    openupgrade.map_values(
+        cr, 'voucher_type', 'voucher_type',
+        [('receipt', 'sale'), ('payment', 'purchase')],
+        table='account_voucher')
+    cr.execute(
+        "insert into account_tax_account_voucher_line_rel "
+        "(account_tax_id, account_voucher_line_id) "
+        "select v.tax_id, l.id "
+        "from account_voucher v "
+        "join account_voucher_line l on l.voucher_id=v.id")
