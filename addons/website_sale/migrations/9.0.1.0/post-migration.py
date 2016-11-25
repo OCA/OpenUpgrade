8e012400f4bc63cd4c7d67fe34359864c145bbb4
cubells <vicent@vcubells.net>
2016-11-25 17:55:25 +0100

    Updated post-script migration and module coverage document

--- addons/website_sale/migrations/9.0.1.0/post-migration.py
+++ addons/website_sale/migrations/9.0.1.0/post-migration.py
@@ -3,20 +3,21 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from openupgradelib import openupgrade
-from openerp import api, SUPERUSER_ID
 
 
-def set_last_sale_on_partner(env):
-    partners = env['res.partner'].search([])
-    for partner in partners:
-        sale = env['sale.order'].search([
-            ('website_order_line', '!=', False),
-            ('parner_id', '=', partner.id)
-        ], order='date_order desc', limit=1)
-        partner.write({'last_website_so_id': sale.id})
+def set_last_sale_on_partner(cr):
+    openupgrade.logged_query(cr, """
+            UPDATE res_partner rp
+            SET last_website_so_id = so.id
+            FROM sale_order so
+            WHERE so.id = (
+               SELECT max(id) FROM sale_order
+               WHERE partner_id = rp.id
+            )
+            AND rp.last_website_so_id IS NULL;
+            """)
 
 
 @openupgrade.migrate()
 def migrate(cr, version):
-    env = api.Environment(cr, SUPERUSER_ID, {})
-    set_last_sale_on_partner(env)
+    set_last_sale_on_partner(cr)
