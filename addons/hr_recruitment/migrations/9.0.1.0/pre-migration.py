76f333ed279e3072ecb723a73f5841c3225db005
cubells <vicent@vcubells.net>
2016-11-28 09:59:09 +0100

    [MIG] hr_recruitment module migration script  * Added analisys  * Created pre and post files

---
+++ addons/hr_recruitment/migrations/9.0.1.0/pre-migration.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# © 2016 Tecnativa - Vicent Cubells <vicent.cubells@tecnativa.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from openupgradelib import openupgrade
+
+
+column_renames = {
+    'hr_applicant': [('priority', None)]}
+
+
+@openupgrade.migrate()
+def migrate(cr, version):
+    openupgrade.rename_columns(cr, column_renames)
