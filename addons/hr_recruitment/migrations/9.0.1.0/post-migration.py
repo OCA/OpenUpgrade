04c6d893318982ec3370e33b8880278034bd63dc
cubells <vicent@vcubells.net>
2016-11-29 17:40:34 +0100

    Updated availability and source_id of hr.applicant

--- addons/hr_recruitment/migrations/9.0.1.0/post-migration.py
+++ addons/hr_recruitment/migrations/9.0.1.0/post-migration.py
@@ -2,6 +2,27 @@
 # © 2016 Tecnativa <vicent.cubells@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from openupgradelib import openupgrade
+from openerp import fields
+from datetime import timedelta
+
+
+def update_applicant_availability(cr):
+    cr.execute(
+        """
+            SELECT id, create_date, %s
+            FROM hr_applicant
+        """ % (openupgrade.get_legacy_name('availability'),)
+    )
+    for applicant in cr.fetchall():
+        new_date = fields.Date.from_string(applicant[1])
+        if applicant[2]:
+            new_date += timedelta(days=applicant[2])
+        openupgrade.logged_query(
+            cr,
+            """
+            UPDATE hr_applicant SET availability = %s
+            WHERE id = %s
+            """ % (new_date, applicant[0]))
 
 
 @openupgrade.migrate()
@@ -14,3 +35,4 @@ def migrate(cr, version):
         'priority',
         [('4', '3'), ('3', '1'), ('2', '0'), ('1', '0')],
         table='hr_applicant', write='sql')
+    update_applicant_availability(cr)
--- addons/hr_recruitment/migrations/9.0.1.0/pre-migration.py
+++ addons/hr_recruitment/migrations/9.0.1.0/pre-migration.py
@@ -5,9 +5,31 @@ from openupgradelib import openupgrade
 
 
 column_renames = {
-    'hr_applicant': [('priority', None)]}
+    'hr_applicant': [
+        ('priority', None),
+        ('availability', None),
+    ]}
+
+
+def migrate_applicant_source(cr):
+    cr.execute(
+        """
+            SELECT id
+            FROM hr_recruitment_source
+        """)
+    for old_id in cr.fetchall():
+        cr.execute("INSERT_INTO utm_source (name) "
+                   "SELECT name "
+                   "FROM hr_recruitment_source "
+                   "RETURNING id")
+        new_id = cr.fetchone()[0]
+        cr.execute("UPDATE hr_applicant "
+                   "SET source_id = %s "
+                   "WHERE source_id = %s",
+                   (new_id, old_id[0]))
 
 
 @openupgrade.migrate()
 def migrate(cr, version):
     openupgrade.rename_columns(cr, column_renames)
+    migrate_applicant_source(cr)
