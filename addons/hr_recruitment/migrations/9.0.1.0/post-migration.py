92cc0f5d6ec8ac3147f9664054d5144e192b8b7d
cubells <vicent@vcubells.net>
2016-11-30 07:06:57 +0100

    FIX move update source to post-migration

--- addons/hr_recruitment/migrations/9.0.1.0/post-migration.py
+++ addons/hr_recruitment/migrations/9.0.1.0/post-migration.py
@@ -17,12 +17,31 @@ def update_applicant_availability(cr):
         new_date = fields.Date.from_string(applicant[1])
         if applicant[2]:
             new_date += timedelta(days=applicant[2])
+        new_date = fields.Date.to_string(new_date)
         openupgrade.logged_query(
             cr,
             """
             UPDATE hr_applicant SET availability = %s
             WHERE id = %s
             """ % (new_date, applicant[0]))
+
+
+def migrate_applicant_source(cr):
+    cr.execute(
+        """
+            SELECT id
+            FROM hr_recruitment_source
+        """)
+    for old_id in cr.fetchall():
+        cr.execute("INSERT_INTO utm_source (name) "
+                   "SELECT name "
+                   "FROM hr_recruitment_source "
+                   "RETURNING id")
+        new_id = cr.fetchone()[0]
+        cr.execute("UPDATE hr_applicant "
+                   "SET source_id = %s "
+                   "WHERE source_id = %s",
+                   (new_id, old_id[0]))
 
 
 @openupgrade.migrate()
@@ -36,3 +55,4 @@ def migrate(cr, version):
         [('4', '3'), ('3', '1'), ('2', '0'), ('1', '0')],
         table='hr_applicant', write='sql')
     update_applicant_availability(cr)
+    migrate_applicant_source(cr)
