f1283ac43c8915cf3976bd4d6c8c666cb9b6502b
Holger Brunn <hbrunn@therp.nl>
2017-04-05 19:55:02 +0200

    [ADD] mail

---
+++ addons/mail/migrations/10.0.1.0/post-migration.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# © 2017 Therp BV <http://therp.nl>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrate()
+def migrate(cr, version):
+    # the table exists already, so the ORM doesn't create an id column
+    cr.execute(
+        'alter table mail_message_res_partner_needaction_rel '
+        'add column id serial not null primary key'
+    )
+    # as 9.0 deleted notifications for read messages, all existing
+    # notifications are unread by definition
+    cr.execute(
+        'update mail_message_res_partner_needaction_rel set is_read=False'
+    )
+    # set email properties
+    cr.execute(
+        """update mail_message_res_partner_needaction_rel rel
+        set is_email=True, email_status=case
+            when m.state in ('sent', 'exception') then m.state
+            else 'ready'
+        end
+        from mail_mail m
+        where rel.mail_message_id=m.mail_message_id"""
+    )
---
+++ addons/mail/migrations/10.0.1.0/pre-migration.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# © 2017 Therp BV <http://therp.nl>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrate()
+def migrate(cr, version):
+    # create an xmlid for mail.bounce.alias is it exists
+    cr.execute(
+        """insert into ir_model_data
+        (module, name, model, res_id)
+        select 'mail', 'icp_mail_bounce_alias', 'ir.config_parameter', id
+        from ir_config_parameter
+        where key='mail.bounce.alias' limit 1"""
+    )
