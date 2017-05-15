9d7cd57d77ea1e989e5fe0dc6e8a22c402abd587
Pedro M. Baeza <pedro.baeza@gmail.com>
2017-05-15 17:33:30 +0200

    [FIX] analytic: PEP8

--- addons/analytic/migrations/10.0.1.1/post-migration.py
+++ addons/analytic/migrations/10.0.1.1/post-migration.py
@@ -3,14 +3,16 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 from openupgradelib import openupgrade
 
+
 def deactivate_closed_accounts(cr):
     # not using sql to restore account_type but map values
     openupgrade.map_values(
         cr, openupgrade.get_legacy_name('account_type'),
-        'active',  [('closed' ,  False) , ('normal', True)],
+        'active',  [('closed',  False), ('normal', True)],
         table='account_analytic_account',
         write='sql'
     )
+
 
 @openupgrade.migrate(use_env=True)
 def migrate(env, version):
