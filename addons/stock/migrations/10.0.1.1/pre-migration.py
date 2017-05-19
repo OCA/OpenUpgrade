8ed9285ceac3cf33af56519d81766dfb7d928ca3
Andres Calle <andres.calle@trescloud.com>
2017-05-18 19:06:06 -0500

    [ADD] stock: Added migration scripts for stock module

---
+++ addons/stock/migrations/10.0.1.1/pre-migration.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Trescloud <http://trescloud.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from openupgradelib import openupgrade
+
+column_copies = {
+    'procurement_rule': [
+        ('route_sequence', None, None),
+        ],
+    'stock_location_path': [
+        ('auto', None, None),
+        ('route_sequence', None, None),
+        ],
+    }
+
+@openupgrade.migrate(use_env=True)
+def migrate(env, version):    
+    openupgrade.copy_columns(env.cr, column_copies)
+    openupgrade.float_to_integer(env.cr, 'stock_location_path', 'route_sequence')
+    openupgrade.float_to_integer(env.cr, 'procurement_rule', 'route_sequence')
+    env.cr.execute(
+        """
+        UPDATE stock_location_path SET auto = 'manual' WHERE auto = 'auto';
+        """)
