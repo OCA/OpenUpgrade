f62e0875889841848e48ffdac015b9cf322b7fed
cubells <vicent@vcubells.net>
2017-05-25 09:33:53 +0200

    Rename account_analytic_plan_required

--- odoo/addons/openupgrade_records/lib/apriori.py
+++ odoo/addons/openupgrade_records/lib/apriori.py
@@ -3,6 +3,8 @@ to help the matching process
 """
 
 renamed_modules = {
+    # OCA/account-analytic
+    'account_analytic_plan_required': 'account_analytic_distribution_required',
 }
 
 renamed_models = {
