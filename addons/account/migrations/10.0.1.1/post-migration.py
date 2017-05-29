f91f2c2e5a3c122026e433c297800502728e4be6
Ronald Portier <ronald@therp.nl>
2017-05-29 17:35:41 +0200

    [FIX] pip/flake errors.

--- addons/account/migrations/10.0.1.1/post-migration.py
+++ addons/account/migrations/10.0.1.1/post-migration.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 # © 2017 Therp BV
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from openupgradelib import openupgrade
+
 
 @openupgrade.migrate(use_env=True)
 def migrate(env, version):
--- addons/account/migrations/10.0.1.1/pre-migration.py
+++ addons/account/migrations/10.0.1.1/pre-migration.py
@@ -3,12 +3,13 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 from openupgradelib import openupgrade
 
+
 @openupgrade.migrate(use_env=True)
 def migrate(env, version):
     cr = env.cr
     # copy columns good practice in pre-mig script format of colum_spec should
     # be: { Table_name_in_db : [( old_column_name , new_column_name, type)]
-    # defaults of new columname are fetchable via method get_legacy_name 
+    # defaults of new columname are fetchable via method get_legacy_name
     openupgrade.rename_tables(
         cr,
         [
