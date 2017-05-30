d6515a22330173e036d9232a7f1ce5135bf35684
Stefan Rijnhart (Opener) <stefan@opener.am>
2017-05-30 23:06:22 +0200

    [FIX] Remove implicit tuples from assert arguments

--- addons/crm/migrations/10.0.1.0/tests/test_crm.py
+++ addons/crm/migrations/10.0.1.0/tests/test_crm.py
@@ -8,17 +8,17 @@ class TestCrm(common.TransactionCase):
         # Check single team is assigned
         self.assertEqual(
             self.env.ref('crm.stage_lead2').team_id,
-            self.env.ref('sales_team.team_sales_department'),
+            self.env.ref('sales_team.team_sales_department')
         )
         # Check next activities field is filled
         activity = self.env.ref('crm.crm_activity_demo_make_quote')
         self.assertEqual(
             activity.recommended_activity_ids,
-            self.env.ref('crm.crm_activity_demo_followup_quote'),
+            self.env.ref('crm.crm_activity_demo_followup_quote')
         )
         activity = self.env.ref('crm.crm_activity_data_meeting')
         self.assertEqual(
             activity.recommended_activity_ids,
             self.env.ref('crm.crm_activity_data_call') +
-            self.env.ref('crm.crm_activity_data_email'),
+            self.env.ref('crm.crm_activity_data_email')
         )
