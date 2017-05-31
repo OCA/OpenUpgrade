ce2292576b7a663c4fe9db92eb2bba2e4ebb6917
Ronald Portier <ronald@therp.nl>
2017-05-31 14:59:16 +0200

    [FIX] Proper editting of copyright header.

--- openerp/addons/openupgrade_records/model/install_all_wizard.py
+++ openerp/addons/openupgrade_records/model/install_all_wizard.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# © 2012-2017 Therp BV <http://therp.nl>
+# © 2012-2017 2012-2014 OpenUpgrade community <https://odoo-community.org/>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 import time
 try:
