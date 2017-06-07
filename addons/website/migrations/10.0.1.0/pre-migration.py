23bb324c4b2f6225c8cc261bc7b9001d6117e76a
Holger Brunn <hbrunn@therp.nl>
2017-06-07 14:32:19 +0200

    [ADD] migrate website

---
+++ addons/website/migrations/10.0.1.0/pre-migration.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# © 2017 Therp BV <http://therp.nl>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrate()
+def migrate(env):
+    openupgrade.rename_xmlids(env.cr, [
+        ('base.group_website_designer', 'website.group_website_designer'),
+        ('base.group_website_publisher', 'website.group_website_publisher'),
+    ])
