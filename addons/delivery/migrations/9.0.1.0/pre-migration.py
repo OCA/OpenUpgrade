2a2dd280fc1166636b58ba0c71b8de028f4dd913
Dane Lowe <dane@avidonline.co.nz>
2017-06-20 17:02:28 +1200

    Revert "[IMP] delivery: Alternative migration"

--- addons/delivery/migrations/9.0.1.0/pre-migration.py
+++ addons/delivery/migrations/9.0.1.0/pre-migration.py
@@ -1,24 +1,11 @@
 # -*- coding: utf-8 -*-
 # © 2016 Therp BV <http://therp.nl>
-# Copyright 2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
 from openupgradelib import openupgrade
 
 column_renames = {
     'delivery_grid_line': [
         ('type', 'variable'),
-        ('grid_id', 'carrier_id'),
-        ('price_type', None),
-    ],
-    'delivery_grid': [
-        ('carrier_id', None),
-    ],
-    'delivery_grid_country_rel': [
-        ('grid_id', 'carrier_id'),
-    ],
-    'delivery_grid_state_rel': [
-        ('grid_id', 'carrier_id'),
     ],
 }
 
@@ -29,62 +16,12 @@ column_copies = {
 }
 
 table_renames = [
-    ('delivery_carrier', None),
-    ('delivery_grid', 'delivery_carrier'),
     ('delivery_grid_line', 'delivery_price_rule'),
-    ('delivery_grid_country_rel', 'delivery_carrier_country_rel'),
-    ('delivery_grid_state_rel', 'delivery_carrier_state_rel'),
 ]
-
-
-def correct_object_references(cr):
-    """Point sale order and stock picking to grid records (that will be
-    renamed as carrier objects)."""
-    openupgrade.lift_constraints(cr, 'sale_order', 'carrier_id')
-    openupgrade.logged_query(
-        cr, """
-        UPDATE sale_order so SET carrier_id = dg.id
-        FROM delivery_grid dg, delivery_carrier dc
-        WHERE dg.carrier_id = dc.id AND so.carrier_id = dc.id
-        """,
-    )
-    openupgrade.lift_constraints(cr, 'stock_picking', 'carrier_id')
-    openupgrade.logged_query(
-        cr, """
-        UPDATE stock_picking sp SET carrier_id = dg.id
-        FROM delivery_grid dg, delivery_carrier dc
-        WHERE dg.carrier_id = dc.id AND sp.carrier_id = dc.id
-        """,
-    )
-
-
-def correct_rule_prices(cr):
-    """Version 8 only allows to put a fixed or variable price, not both.
-    Now version 9 have a field for each option. We copy the value on both
-    fields and zero the field that doesn't correspond to the version in 8.
-    """
-    openupgrade.copy_columns(cr, column_copies)
-    cr.execute(
-        """
-        UPDATE delivery_grid_line
-        SET list_price = 0
-        WHERE {0} = 'fixed'
-        """.format(openupgrade.get_legacy_name('price_type'))
-    )
-    cr.execute(
-        """
-        UPDATE delivery_grid_line
-        SET list_base_price = 0
-        WHERE {0} = 'variable'
-        """.format(openupgrade.get_legacy_name('price_type'))
-    )
 
 
 @openupgrade.migrate()
 def migrate(cr, version):
-    correct_object_references(cr)
     openupgrade.rename_columns(cr, column_renames)
-    correct_rule_prices(cr)
+    openupgrade.copy_columns(cr, column_copies)
     openupgrade.rename_tables(cr, table_renames)
-    # TODO: if the same product is used for multiple carriers, duplicate it
-    # for having a correct structure of 1 product = 1 carrier
