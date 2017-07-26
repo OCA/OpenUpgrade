6ca4bd66ed20a7331157a166e8ea21746b6e995d
mreficent <miquel.raich@eficent.com>
2017-07-26 12:18:03 +0200

    [10.0][MIG] l10n_ch

---
+++ addons/l10n_ch/migrations/10.0.9.0/pre-migration.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Eficent <http://www.eficent.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    env.ref('l10n_ch.action_client_l10n_ch_menu').unlink()
