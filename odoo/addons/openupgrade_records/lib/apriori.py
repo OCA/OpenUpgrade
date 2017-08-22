720a32467a3dd3ec373081c475f806cc0772a3fb
mreficent <miquel.raich@eficent.com>
2017-08-22 14:06:23 +0200

    [MIG] Rename of hr_holiday_notify_employee_manager

--- odoo/addons/openupgrade_records/lib/apriori.py
+++ odoo/addons/openupgrade_records/lib/apriori.py
@@ -8,6 +8,9 @@ renamed_modules = {
     # connector and queue_job. We need to do this to correct upgrade both
     # modules.
     'connector': 'queue_job',
+    # OCA/hr
+    # The OCA extensions of the hr_holidays module are 'hr_holidays_something'
+    'hr_holiday_notify_employee_manager': 'hr_holidays_notify_employee_manager'
 }
 
 renamed_models = {
