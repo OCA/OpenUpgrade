a7e913be40b69d7233b8ebecb32dd826c6c6ae52
Pedro M. Baeza <pedro.baeza@gmail.com>
2017-08-22 17:56:13 +0200

    [FIX] mass_mailing: Fill empty source_id in mass mailings

---
+++ addons/mass_mailing/migrations/10.0.2.0/tests/test_mass_mailing.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Tecnativa - Pedro M. Baeza
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp.tests.common import TransactionCase
+
+
+class TestMassMailing(TransactionCase):
+
+    def test_mass_mailing_source_id(self):
+        mm = self.env.ref('mass_mailing.mass_mail_1')
+        self.assertTrue(mm.source_id)
+        self.assertEqual(mm.source_id.name, "OpenUpgrade wildcard source")
