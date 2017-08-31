ac4dfa47b57762974f7e756beb01592a1a8e4d8f
mreficent <miquel.raich@eficent.com>
2017-08-31 18:37:56 +0200

    [ADD][no_update_records] website_project_issue

---
+++ addons/website_project_issue/migrations/10.0.0.1/post-migration.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Eficent <http://www.eficent.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrate(use_env=False)
+def migrate(cr, version):
+    openupgrade.load_data(
+        cr, 'website_project_issue',
+        'migrations/10.0.0.1/noupdate_changes.xml',
+    )
