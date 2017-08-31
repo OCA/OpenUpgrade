708c4a2cc8dd0d09a3f22c34966a3a7bc89e2bc4
mreficent <miquel.raich@eficent.com>
2017-08-31 17:51:14 +0200

    [ADD][no_update_records] event

---
+++ addons/event/migrations/10.0.1.0/post-migration.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Eficent <http://www.eficent.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrate(use_env=False)
+def migrate(cr, version):
+    openupgrade.load_data(
+        cr, 'event', 'migrations/10.0.1.0/noupdate_changes.xml',
+    )
