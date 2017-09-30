ba39e26d2245b83f7a5931fabe1767973a865dd0
Pedro M. Baeza <pedro.baeza@gmail.com>
2017-09-30 16:35:30 +0200

    [FIX] website_blog: Change opacity for cover images
    Switch opacity value for posts, as now in v10 the meaning of that
    property is for an upper solid frame, not for the image itself:
    
    * opacity=1.0 in v9 means solid image
    * opacity=1.0 in v10 means solid color frame.
    
    So what we do is to get the inverse percentage of the opacity: abs(1 -opacity).

---
+++ addons/website_blog/migrations/10.0.1.0/post-migration.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Tecnativa - Pedro M. Baeza
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openupgradelib import openupgrade
+from odoo.tools import safe_eval
+
+
+def convert_blog_post_cover(env):
+    """Switch opacity value for posts, as now in v10 the meaning of that
+    property is for an upper solid frame, not for the image itself.
+    """
+    post_obj = env['blog.post'].with_context(active_test=False)
+    for post in post_obj.search([('cover_properties', '!=', False)]):
+        props = safe_eval(post.cover_properties)
+        if 'opacity' in props:
+            props['opacity'] = str(abs(1 - float(props['opacity'])))
+        post.cover_properties = str(props).replace("'", '"')
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    convert_blog_post_cover(env)
