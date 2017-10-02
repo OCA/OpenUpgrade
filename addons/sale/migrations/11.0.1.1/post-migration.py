bc9e45fc79ab2aa9d60a1e8219c013f781ecf05c
Benjamin Bachmann <bbachmann@bloopark.de>
2017-10-02 15:22:35 +0200

    [11.0][MIG] sale , moved group_display_incoterm to sale_stock

--- addons/sale/migrations/11.0.1.1/post-migration.py
+++ addons/sale/migrations/11.0.1.1/post-migration.py
@@ -3,6 +3,10 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from openupgradelib import openupgrade
 
+_xmlid_renames = [
+    ('sale.group_display_incoterm', 'sale_stock.group_display_incoterm'),
+]
+
 
 @openupgrade.migrate()
 def migrate(env, version):
