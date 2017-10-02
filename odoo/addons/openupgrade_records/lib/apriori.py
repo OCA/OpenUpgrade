80604ee97a89f972745fc4ce38adc8e993c0be68
mreficent <miquel.raich@eficent.com>
2017-10-02 12:28:25 +0200

    [11.0][ADD] Renames

--- odoo/addons/openupgrade_records/lib/apriori.py
+++ odoo/addons/openupgrade_records/lib/apriori.py
@@ -3,6 +3,12 @@ to help the matching process
 """
 
 renamed_modules = {
+    # Odoo
+    'base_action_rule': 'base_automation',
+    'crm_project_issue': 'crm_project',
+    'stock_picking_wave': 'stock_picking_batch',
+    'website_issue': 'website_form_project',
+    'website_rating_project_issue': 'website_rating_project',
 }
 
 renamed_models = {
