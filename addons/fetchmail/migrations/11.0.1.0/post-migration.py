fb6dbaf2c82fbd3ec6dc0b8c6fffe186e72aea5e
cubells <vicent@vcubells.net>
2017-10-23 10:59:07 +0200

    [MIG] fetchmail: Migrations scripts

---
+++ addons/fetchmail/migrations/11.0.1.0/post-migration.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Tecnativa - Vicent Cubells
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    openupgrade.load_data(
+        env.cr, 'fetchmail', 'migrations/11.0.1.0/noupdate_changes.xml',
+    )
