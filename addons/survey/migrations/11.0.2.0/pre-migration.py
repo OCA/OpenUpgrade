c01b409ce54f474f889530af94f41bc4e0f4ff01
cubells <vicent@vcubells.net>
2017-11-05 18:09:20 +0100

    Mapped type field and improve comment

--- addons/survey/migrations/11.0.2.0/pre-migration.py
+++ addons/survey/migrations/11.0.2.0/pre-migration.py
@@ -8,6 +8,7 @@ _column_copies = {
     'survey_question': [
         ('validation_max_date', None, None),
         ('validation_min_date', None, None),
+        ('type', None, None),
     ],
     'survey_user_input_line': [
         ('value_date', None, None),
@@ -18,3 +19,7 @@ _column_copies = {
 @openupgrade.migrate()
 def migrate(env, version):
     openupgrade.copy_columns(env.cr, _column_copies)
+    openupgrade.map_values(
+        env.cr, openupgrade.get_legacy_name('type'), 'type',
+        [('datetime', 'date')], table='survey_question',
+    )
