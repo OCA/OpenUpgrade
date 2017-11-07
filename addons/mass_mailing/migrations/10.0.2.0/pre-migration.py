ead8ac14333ecdbb857c89c5e3403a8f0b8bc39a
Jairo Llopis <yajo.sk8@gmail.com>
2017-11-07 12:02:21 +0100

    [FIX] mass_mailing: Link `mail.mass_mailing.campaign` to `utm.campaign` records

--- addons/mass_mailing/migrations/10.0.2.0/pre-migration.py
+++ addons/mass_mailing/migrations/10.0.2.0/pre-migration.py
@@ -32,7 +32,41 @@ def fill_mass_mailing_sources(env):
         )
 
 
+def convert_campaigns_to_utm(env):
+    """Link ``mail.mass_mailing.campaign`` to ``utm.campaign`` records.
+
+    Odoo 8.0 had the ``name`` field directly in ``mail.mass_mailing.campaign``
+    model. Odoo 9.0 had it there too, but it added ``_inherits`` from
+    ``utm.campaign``, a weird move. Odoo 10.0 removed ``name`` from the model
+    and relied on the linked ``utm.campaign``'s name, a more sensible move.
+
+    The result is that if you had records coming from v8, they may appear with
+    ``display_name`` as ``False`` to the superuser, and they may raise fake
+    ``AccessError`` to other users, due to missing records in some low level
+    inner joins that the ORM performs.
+
+    This method creates one ``utm.campaign`` record for each
+    ``mail.mass_mailing.campaign`` that has none, and fills its name.
+    """
+    mass = env["mail.mass_mailing.campaign"].search([
+        "|", ("campaign_id", "=", False), ("campaign_id.name", "=", False),
+    ])
+    for one in mass:
+        env.cr.execute(
+            "SELECT name FROM mail_mass_mailing_campaign WHERE id = %s",
+            (one.id,)
+        )
+        name = env.cr.fetchone()[0]
+        if not one.campaign_id:
+            one.campaign_id = env["utm.campaign"].create({
+                "name": name,
+            })
+        elif not one.campaign_id.name:
+            one.campaign_id.name = name
+
+
 @openupgrade.migrate()
 def migrate(env, version):
     openupgrade.rename_xmlids(env.cr, XMLID_RENAMES)
     fill_mass_mailing_sources(env)
+    convert_campaigns_to_utm(env)
