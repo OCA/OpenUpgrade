4ca082bad1918344f06f76299dcae7c70f067df6
Jairo Llopis <yajo.sk8@gmail.com>
2017-11-07 12:31:19 +0100

    fixup! [FIX] mass_mailing: Link `mail.mass_mailing.campaign` to `utm.campaign` records

--- addons/mass_mailing/migrations/10.0.2.0/post-migration.py
+++ addons/mass_mailing/migrations/10.0.2.0/post-migration.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2017 Tecnativa - Pedro M. Baeza
+# Copyright 2017 Tecnativa - Jairo Llopis
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from openupgradelib import openupgrade
