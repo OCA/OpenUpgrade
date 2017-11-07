efd79eae8e488544bce10b0462dbaec2c10cf911
Jairo Llopis <yajo.sk8@gmail.com>
2017-11-07 12:31:19 +0100

    fixup! [FIX] mass_mailing: Link `mail.mass_mailing.campaign` to `utm.campaign` records

--- addons/mass_mailing/migrations/10.0.2.0/pre-migration.py
+++ addons/mass_mailing/migrations/10.0.2.0/pre-migration.py
@@ -32,32 +32,7 @@ def fill_mass_mailing_sources(env):
         )
 
 
-def convert_campaigns_to_utm(env):
-    """Link ``mail.mass_mailing.campaign`` to ``utm.campaign`` records.
-
-    This method creates one ``utm.campaign`` record for each
-    ``mail.mass_mailing.campaign`` that has none, and fills its name if it had
-    it empty.
-    """
-    mass = env["mail.mass_mailing.campaign"].search([
-        "|", ("campaign_id", "=", False), ("campaign_id.name", "=", False),
-    ])
-    for one in mass:
-        env.cr.execute(
-            "SELECT name FROM mail_mass_mailing_campaign WHERE id = %s",
-            (one.id,)
-        )
-        name = env.cr.fetchone()[0]
-        if not one.campaign_id:
-            one.campaign_id = env["utm.campaign"].create({
-                "name": name,
-            })
-        elif not one.campaign_id.name:
-            one.campaign_id.name = name
-
-
 @openupgrade.migrate()
 def migrate(env, version):
     openupgrade.rename_xmlids(env.cr, XMLID_RENAMES)
     fill_mass_mailing_sources(env)
-    convert_campaigns_to_utm(env)
