6da4e6e2151ddfef6579ca650fc2d446818326b5
Yuri Quintana <yuriqp@gmail.com>
2017-11-16 15:52:41 +0100

    [11.0][MIG] calendar. Removing old translations

--- addons/calendar/migrations/11.0.1.0/pre-migration.py
+++ addons/calendar/migrations/11.0.1.0/pre-migration.py
@@ -5,12 +5,42 @@
 from openupgradelib import openupgrade
 
 
-@openupgrade.migrate()
-def migrate(env, version):
-    # Now user_id is required, set it to the one who created the entry
-    env.cr.execute(
-        """
-        UPDATE calendar_contacts SET user_id = create_uid 
+def update_user_ids(cr):
+    """
+    Now user_id is required, set it to the one who created the entry
+    """
+    cr.execute("""
+        UPDATE calendar_contacts SET user_id = create_uid
         WHERE user_id IS NULL;
-        """
+    """)
+
+
+def cleanup_translations(cr):
+    """
+    Cleanup translations of adopted templates
+    """
+
+    updated_templates = (
+        'calendar_template_meeting_reminder',
+        'calendar_template_meeting_changedate',
+        'calendar_template_meeting_invitation',
     )
+
+    cr.execute("""
+        SELECT res_id
+        FROM ir_model_data
+        WHERE module = 'calendar' AND model = 'mail.template' AND name in %s
+    """, (updated_templates,))
+    record_ids = tuple([r[0] for r in cr.fetchall()])
+
+    query = ("""
+        DELETE FROM ir_translation
+        WHERE module = 'calendar' AND res_id IN %s
+    """)
+    openupgrade.logged_query(cr, query, (record_ids,))
+
+
+@openupgrade.migrate(use_env=False)
+def migrate(cr, version):
+    update_user_ids(cr)
+    cleanup_translations(cr)
