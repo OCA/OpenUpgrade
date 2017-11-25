37ec561a43643b700e88897d0f851f4b2ca5a4c8
archetipo <alessio.gerace@gmail.com>
2017-11-25 19:14:03 +0100

    [FIX] add rename xmlids for ir.rule [ADD] openupgrade_analysis_work.txt

--- addons/fleet/migrations/10.0.0.1/pre-migration.py
+++ addons/fleet/migrations/10.0.0.1/pre-migration.py
@@ -7,6 +7,55 @@ from openupgradelib import openupgrade
 _xmlid_renames = [
     ('fleet.group_fleet_manager', 'fleet.fleet_group_manager'),
     ('fleet.group_fleet_user', 'fleet.fleet_group_user'),
+    (
+        'fleet.fleet_user_contract_visibility',
+        'fleet.fleet_rule_contract_visibility_user'
+    ),
+    (
+        'fleet.fleet_user_contract_visibility_manager',
+        'fleet.fleet_rule_contract_visibility_manager'
+    ),
+    (
+        'fleet.fleet_user_cost_visibility',
+        'fleet.fleet_rule_cost_visibility_user'
+    ),
+    (
+        'fleet.fleet_user_cost_visibility_manager',
+        'fleet.fleet_rule_cost_visibility_manager'
+    ),
+    (
+        'fleet.fleet_user_fuel_log_visibility',
+        'fleet.fleet_rule_fuel_log_visibility_user'
+    ),
+    (
+        'fleet.fleet_user_fuel_log_visibility_manager',
+        'fleet.fleet_rule_fuel_log_visibility_manager'
+    ),
+    (
+        'fleet.fleet_user_odometer_visibility',
+        'fleet.fleet_rule_odometer_visibility_user'
+    ),
+    (
+        'fleet.fleet_user_odometer_visibility_manager',
+        'fleet.fleet_rule_odometer_visibility_manager'
+    ),
+    (
+        'fleet.fleet_user_service_visibility',
+        'fleet.fleet_rule_service_visibility_user'
+    ),
+    (
+        'fleet.fleet_user_service_visibility_manager',
+        'fleet.fleet_rule_service_visibility_manager'
+    ),
+    (
+        'fleet.fleet_user_vehicle_visibility',
+        'fleet.fleet_rule_vehicle_visibility_user'
+    ),
+    (
+        'fleet.fleet_user_vehicle_visibility_manager',
+        'fleet.fleet_rule_vehicle_visibility_manager'
+    ),
+
 ]
 
 
