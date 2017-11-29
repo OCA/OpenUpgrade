61943027f95f40e1833c6415638ccd6d3022412a
Pedro M. Baeza <pedro.baeza@gmail.com>
2017-11-29 19:18:11 +0100

    [FIX] sale_service: Correct track_service assignation
    map_values method doesn't handle boolean columns.

--- addons/sale_service/migrations/9.0.1.0/post-migration.py
+++ addons/sale_service/migrations/9.0.1.0/post-migration.py
@@ -1,17 +1,20 @@
 # -*- coding: utf-8 -*-
 # Copyright 2017 Tecnativa - Vicent Cubells
+# Copyright 2017 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from openupgradelib import openupgrade
+from psycopg2.extensions import AsIs
 
 
 def map_product_template(cr):
-    openupgrade.map_values(
-        cr,
-        openupgrade.get_legacy_name('auto_create_task'),
-        'track_service',
-        [(True, 'task')],
-        table='product_template', write='sql')
+    openupgrade.logged_query(
+        cr, """
+        UPDATE product_template
+        SET track_service = 'task'
+        WHERE %s
+        """, (AsIs(openupgrade.get_legacy_name('auto_create_task')), )
+    )
 
 
 @openupgrade.migrate()
