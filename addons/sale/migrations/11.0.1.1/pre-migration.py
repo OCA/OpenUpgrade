a2b916facad1f1f3a3ef0b783e3e87abfb6f3d57
Nicolas JEUDY <nicolas@panda-chi.io>
2018-01-09 11:10:37 +0100

    [FIX] typo in version and security group renamed

--- addons/sale/migrations/11.0.1.1/pre-migration.py
+++ addons/sale/migrations/11.0.1.1/pre-migration.py
@@ -1,16 +1,14 @@
 # -*- coding: utf-8 -*-
-# © 2017 bloopark systems (<http://bloopark.de>)
+# Copyright 2018 - Nicolas JEUDY
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
 from openupgradelib import openupgrade
 
 _xmlid_renames = [
-    ('sale.group_display_incoterm', 'sale_stock.group_display_incoterm'),
+    ('account.group_proforma_invoices', 'sale.group_proforma_sales'),
 ]
 
 
 @openupgrade.migrate()
 def migrate(env, version):
     openupgrade.rename_xmlids(env.cr, _xmlid_renames)
-    openupgrade.load_data(
-        env.cr, 'sale', 'migrations/11.0.1.1/noupdate_changes.xml',
-    )
