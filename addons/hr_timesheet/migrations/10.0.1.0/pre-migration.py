606bec78ea28275842e1a3d483d9592ee8380025
Manuel Vázquez Acosta <manuel@merchise.org>
2018-01-10 18:47:21 -0500

    [IMP] [FIX] hr_timesheet: Precompute department_id in analytic lines.

--- addons/hr_timesheet/migrations/10.0.1.0/pre-migration.py
+++ addons/hr_timesheet/migrations/10.0.1.0/pre-migration.py
@@ -11,6 +11,25 @@ COLUMN_RENAMES = {
 }
 
 
+def create_and_populate_department(cr):
+    cr.execute('''
+       ALTER TABLE account_analytic_line ADD COLUMN department_id INT;
+       ALTER TABLE account_analytic_line
+          ADD CONSTRAINT account_analytic_line_department_id_fkey
+          FOREIGN KEY (department_id) REFERENCES hr_department (id);
+
+       WITH departments AS (
+          SELECT r.user_id AS user_id, MAX(e.department_id) AS dpt_id
+             FROM hr_employee e JOIN resource_resource r
+                                ON e.resource_id = r.id
+          GROUP BY user_id
+       )
+       UPDATE account_analytic_line aal SET department_id=departments.dpt_id
+       FROM departments WHERE aal.user_id = departments.user_id;
+    ''')
+
+
 @openupgrade.migrate()
 def migrate(env, version):
     openupgrade.rename_columns(env.cr, COLUMN_RENAMES)
+    create_and_populate_department(env.cr)
