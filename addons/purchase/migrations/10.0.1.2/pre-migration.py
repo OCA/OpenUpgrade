8563f13930a667d2926f2d9e484edcf5bb79ed1a
Manuel Vázquez Acosta <manuel@merchise.org>
2018-01-11 10:58:46 -0500

    [IMP] purchase: Speed up copying of 'state' field in purchase lines.

---
+++ addons/purchase/migrations/10.0.1.2/pre-migration.py
@@ -0,0 +1,23 @@
+from __future__ import (division as _py3_division,
+                        print_function as _py3_print,
+                        absolute_import as _py3_abs_import)
+
+from openupgradelib import openupgrade
+
+
+def copy_state_to_purchase_lines(cr):
+    cr.execute('''
+       ALTER TABLE purchase_order_line ADD COLUMN state VARCHAR;
+
+       WITH purchases AS (
+          SELECT id, state FROM purchase_order
+       )
+       UPDATE purchase_order_line line SET state=po.state
+       FROM purchases po WHERE line.order_id=po.id;
+
+    ''')
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    copy_state_to_purchase_lines(env.cr)
