00934e1c5cbd101d6d3803a7214af718be8e4148
Manuel Vázquez Acosta <manuel@merchise.org>
2018-01-12 11:10:04 -0500

    [IMP] purchase: Remove futures.

--- addons/purchase/migrations/10.0.1.2/pre-migration.py
+++ addons/purchase/migrations/10.0.1.2/pre-migration.py
@@ -1,7 +1,3 @@
-from __future__ import (division as _py3_division,
-                        print_function as _py3_print,
-                        absolute_import as _py3_abs_import)
-
 from openupgradelib import openupgrade
 
 
