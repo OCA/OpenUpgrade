a9af1edc03342e12f3a88eef108aa03c155ce203
Pedro M. Baeza <pedro.baeza@gmail.com>
2018-04-07 16:52:25 +0200

    [FIX] base_automation: Fix migration
    It turns out that migration was not correct all:
    
    * Checking if module "base_action_rule" is installed will return
      always False, as the module has been renamed. Any way, we don't
      need to check it, as if the script is being executed is because
      the module was installed.
    * Query for inserting ir_action_server record was not correct.
    * The column for referencing the delegate field was not created.
    * By default, queries don't return dictionaries with column names.
    * Associated server actions to run weren't linked.
    
    In addition, I have added:
    
    * More tests
    * If the fields for assigning a responsible or for adding followers
      are set, this is taken into account and translated to new system,
      so we can consider this now as a "pixel perfect" conversion.

--- addons/base_automation/migrations/11.0.1.0/tests/test_base_automation.py
+++ addons/base_automation/migrations/11.0.1.0/tests/test_base_automation.py
@@ -5,11 +5,27 @@ from odoo.tests import common
 
 
 class TestBaseAutomation(common.TransactionCase):
-    def test_rule_on_create(self):
-        record = self.env['base.automation'].search([
-            ('name', '=', 'Test rule on write'),
-        ])
+    def test_rule_on_write(self):
+        record = self.env.ref('base_automation.test_rule_on_write')
         self.assertTrue(record)
         self.assertEquals(record.filter_domain, "[('state', '=', 'done')]")
         self.assertEquals(record.filter_pre_domain, "[('state', '=', 'open')]")
         self.assertEquals(record.trigger, "on_write")
+        self.assertEquals(record.state, "object_write")
+        self.assertTrue(record.fields_lines)
+        self.assertEquals(record.fields_lines.value,
+                          self.env.ref('base.user_demo'))
+
+    def test_rule_followers_multi(self):
+        record = self.env.ref('base_automation.test_rule_followers_multi')
+        self.assertTrue(record)
+        self.assertTrue(record.child_ids)
+        rules = self.env['base.automation'].search([
+            ('name', '=', 'Test rule followers multi'),
+        ])
+        self.assertEquals(len(rules), 2)
+        new_record = rules - record
+        self.assertEquals(new_record.state, "followers")
+        followers = new_record.partners
+        self.assertIn(self.env.ref('base.partner_root'), followers)
+        self.assertIn(self.env.ref('base.partner_demo'), followers)
