67ff021dba635ae2e680209ebe1c376d48c96ade
Pedro M. Baeza <pedro.baeza@gmail.com>
2018-04-09 21:14:12 +0200

    [IMP] mass_mailing: Use m2o_to_x2m + single query

--- addons/mass_mailing/migrations/11.0.2.0/post-migration.py
+++ addons/mass_mailing/migrations/11.0.2.0/post-migration.py
@@ -1,40 +1,31 @@
 # -*- coding: utf-8 -*-
-# © 2017 bloopark systems (<http://bloopark.de>)
+# Copyright 2017 bloopark systems (<http://bloopark.de>)
+# Copyright 2018 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
 from openupgradelib import openupgrade
 
 
-def map_list_id_to_list_ids(cr):
+def map_list_id_to_list_ids(env):
     """Set the list_ids for the list_id in mail.mass_mailing.contact"""
-    cr.execute(
-        """
-        SELECT id, list_id FROM mail_mass_mailing_contact;
-        """)
-    rows = cr.fetchall()
-    for r in rows:
-        cr.execute(
-            """
-            INSERT INTO mail_mass_mailing_contact_list_rel (contact_id,
-            list_id) values(%s,%s)
-            """, (r[0], r[1],))
+    openupgrade.m2o_to_x2m(
+        env.cr, env['mail.mass_mailing.contact'], 'mail_mass_mailing_contact',
+        'list_ids', 'list_id',
+    )
 
 
 def map_mailing_model_to_mailing_model_id(cr):
     """Set the mailing_model_id for the mailing_model in mail.mass_mailing"""
-    cr.execute(
-        """
-        SELECT mm.id, m.id FROM mail_mass_mailing AS mm
-        LEFT JOIN ir_model AS m ON m.model = mm.mailing_model;
-        """)
-    rows = cr.fetchall()
-    for r in rows:
-        cr.execute(
-            """
-            UPDATE mail_mass_mailing SET mailing_model_id = %s WHERE id = %s
-            """, (r[1], r[0],))
+    openupgrade.logged_query(
+        cr, """
+        UPDATE mail_mass_mailing mmm
+        SET mailing_model_id = im.id
+        FROM ir_model im
+        WHERE im.model = mmm.mailing_model"""
+    )
 
 
 @openupgrade.migrate()
 def migrate(env, version):
-    map_list_id_to_list_ids(env.cr)
+    map_list_id_to_list_ids(env)
     map_mailing_model_to_mailing_model_id(env.cr)
