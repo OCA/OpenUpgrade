06beec16db5aadd91d6e46c6fcdc1706144cb78c
cubells <vicent@vcubells.net>
2018-05-17 12:20:21 +0200

    [MIG][11.0] sale_timesheet: migration scripts

---
+++ addons/sale_timesheet/migrations/11.0.1.0/post-migration.py
@@ -0,0 +1,46 @@
+# Copyright 2018 Tecnativa - Vicent Cubells
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openupgradelib import openupgrade
+from psycopg2.extensions import AsIs
+
+
+def migrate_sale_line_tasks(env):
+    openupgrade.logged_query(
+        env.cr, """
+            UPDATE sale_order_line sol
+            SET task_id = po.task_id
+            FROM procurement_order po
+            WHERE sol.id = po.sale_line_id
+        """)
+
+
+def migrate_project_sale_line(env):
+    service_column = openupgrade.get_legacy_name('service_type')
+    openupgrade.logged_query(
+        env.cr, """
+            UPDATE project_project pj
+            SET sale_line_id = sol.id
+            FROM sale_order_line sol, product_product pd, sale_order so,
+              product_template pt
+            WHERE sol.product_id = pd.id
+            AND sol.order_id = so.id
+            AND so.analytic_account_id = pj.id
+            AND pt.id = pd.product_tmpl_id
+            AND so.state in ('sale', 'done')
+            AND pt.%s in ('task', 'timesheet')
+        """, (AsIs(service_column),))
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    openupgrade.map_values(
+        env.cr, openupgrade.get_legacy_name('service_type'), 'service_type',
+        [('task', 'timesheet')], table='product_template',
+    )
+    # Update invoice_policy
+    env['product.template'].search([
+        ('type', '=', 'service'),
+    ])._compute_service_policy()
+    migrate_sale_line_tasks(env)
+    migrate_project_sale_line(env)
