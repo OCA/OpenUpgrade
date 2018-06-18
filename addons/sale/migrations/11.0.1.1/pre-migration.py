d79b110b0cb15ca7d549ac8fe600406283304ae9
cubells <vicent@vcubells.net>
2018-06-18 20:51:49 +0200

    [MIG][11.0] website_portal_sale: merged into website_sale

--- addons/sale/migrations/11.0.1.1/pre-migration.py
+++ addons/sale/migrations/11.0.1.1/pre-migration.py
@@ -8,7 +8,24 @@ _xmlid_renames = [
     ('account.group_proforma_invoices', 'sale.group_proforma_sales'),
 ]
 
+_portal_xmlid_renames = [
+    ('website_portal_sale.portal_my_home_menu_sale',
+     'sale.portal_my_home_menu_sale'),
+    ('website_portal_sale.portal_my_home_sale',
+     'sale.portal_my_home_sale'),
+    ('website_portal_sale.portal_my_quotations', 'sale.portal_my_quotations'),
+    ('website_portal_sale.portal_my_orders', 'sale.portal_my_orders'),
+]
+
 
 @openupgrade.migrate()
 def migrate(env, version):
     openupgrade.rename_xmlids(env.cr, _xmlid_renames)
+    openupgrade.rename_xmlids(env.cr, _portal_xmlid_renames)
+    try:
+        with env.cr.savepoint():
+            env.ref('website_portal_sale.orders_followup').unlink()
+    except Exception:
+        pass
+
+
