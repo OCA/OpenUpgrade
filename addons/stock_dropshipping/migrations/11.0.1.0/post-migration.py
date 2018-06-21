9c9eb9dde482894d9fbd4833da87186b9b93f9bb
cubells <vicent@vcubells.net>
2018-06-21 16:08:24 +0200

    [MIG][11.0] stock_dropshipping: no update changes

---
+++ addons/stock_dropshipping/migrations/11.0.1.0/post-migration.py
@@ -0,0 +1,25 @@
+# Copyright 2018 Tecnativa - Vicent Cubells
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from openupgradelib import openupgrade
+
+
+def update_sale_line_id(env):
+    openupgrade.logged_query(
+        env.cr, """
+          UPDATE purchase_order_line pol
+          SET sale_line_id = (
+            SELECT sale_line_id
+            FROM procurement_order po
+            WHERE po.purchase_line_id = pol.id
+            LIMIT 1)
+        """
+    )
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    openupgrade.load_data(
+        env.cr, 'stock_dropshipping', 'migrations/11.0.1.0/noupdate_changes.xml',
+    )
+    update_sale_line_id(env)
