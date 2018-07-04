a359b3296fc42b5be680b750045c60e36837939e
Stefan Rijnhart <stefan@opener.am>
2018-07-04 22:52:11 +0200

    [FIX] Purchase requisition setting on products The previously used method openupgrade.map_values currently does not support mapping away from boolean values as these are mapped to 'is (not) set'.

--- addons/purchase_requisition/migrations/9.0.0.1/post-migration.py
+++ addons/purchase_requisition/migrations/9.0.0.1/post-migration.py
@@ -3,14 +3,17 @@
 # Jordi Ballester Alomar
 # © 2015 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-
+from psycopg2.extensions import AsIs
 from openupgradelib import openupgrade
 
 
 @openupgrade.migrate()
 def migrate(cr, version):
     # Mapping new values for purchase_requisition
-    openupgrade.map_values(
-        cr, openupgrade.get_legacy_name('purchase_requisition'),
-        'purchase_requisition', [(True, 'tenders'), (False, 'rfq')],
-        table='product_template')
+    column = openupgrade.get_legacy_name('purchase_requisition')
+    openupgrade.logged_query(
+        cr, """UPDATE product_template SET purchase_requisition = 'rfq'
+        WHERE %s IS NOT TRUE""", (AsIs(column),))
+    openupgrade.logged_query(
+        cr, """UPDATE product_template SET purchase_requisition = 'tenders'
+        WHERE %s""",  (AsIs(column),))
