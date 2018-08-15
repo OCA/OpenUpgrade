4e2fe0d3fc82971e1d08db71551773f5e9a0344f
Pedro M. Baeza <pedro.baeza@gmail.com>
2018-08-15 21:18:50 +0200

    [ADD] hr_recruitment: Migration scripts (#1474)
    * Analysis work file
    * Migration scripts
    * No-update records
    * No-update translations deleted
    * Update documentation

---
+++ addons/hr_recruitment/migrations/11.0.1.0/post-migration.py
@@ -0,0 +1,37 @@
+# Copyright 2018 Tecnativa - Pedro M. Baeza
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openupgradelib import openupgrade
+
+
+def fill_applicant_activities(env):
+    """Convert fields about next action to do to activities records."""
+    mail_type = env.ref('mail.mail_activity_data_todo')
+    openupgrade.logged_query(
+        env.cr, """
+        INSERT INTO mail_activity
+            (res_id, res_model_id, res_model, res_name, summary,
+             activity_type_id, date_deadline, create_uid, create_date,
+             write_uid, write_date,
+             user_id)
+         SELECT
+            ha.id,  im.id, im.model, ha.name, ha.title_action,
+            %s, ha.date_action, ha.create_uid, ha.create_date,
+            ha.write_uid, ha.write_date,
+            COALESCE(ha.user_id, hj.user_id, hd.manager_id, ha.create_uid)
+         FROM hr_applicant AS ha
+            INNER JOIN ir_model AS im ON im.model = 'hr.applicant'
+            LEFT JOIN hr_department hd ON hd.id = ha.department_id
+            LEFT JOIN hr_job hj ON hj.id = ha.job_id
+         WHERE
+            title_action IS NOT NULL AND
+            date_action IS NOT NULL""", (mail_type.id, ),
+    )
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    fill_applicant_activities(env)
+    openupgrade.load_data(
+        env.cr, 'hr_recruitment', 'migrations/11.0.1.0/noupdate_changes.xml',
+    )
