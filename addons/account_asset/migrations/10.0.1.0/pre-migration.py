a52d01507b86735d30860ce408bb453423500ed6
Pedro M. Baeza <pedro.baeza@gmail.com>
2018-10-11 17:33:28 +0200

    [FIX] account_asset: Fill expense account from asset one
    Following the behavior in previos version, if you don't have expense account,
    then the asset one is used. As now the field is required, we copy the asset account
    in those empty expense accounts

--- addons/account_asset/migrations/10.0.1.0/pre-migration.py
+++ addons/account_asset/migrations/10.0.1.0/pre-migration.py
@@ -19,7 +19,18 @@ field_renames = [
 ]
 
 
+def assign_expense_account(env):
+    """Put the asset account where you don't have any expense account."""
+    openupgrade.logged_query(
+        env.cr, """
+        UPDATE account_asset_category
+        SET account_depreciation_expense_id = account_asset_id
+        WHERE account_depreciation_expense_id IS NULL""",
+    )
+
+
 @openupgrade.migrate(use_env=True)
 def migrate(env, version):
     openupgrade.rename_columns(env.cr, column_renames)
     openupgrade.rename_fields(env, field_renames)
+    assign_expense_account(env)
