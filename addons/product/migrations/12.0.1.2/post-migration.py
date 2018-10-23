f4060a7a0c19b276e45e2aa55bdbe6602539aebd
mreficent <miquel.raich@eficent.com>
2018-10-23 13:55:34 +0200

    [MIG] product

--- addons/product/migrations/12.0.1.2/post-migration.py
+++ addons/product/migrations/12.0.1.2/post-migration.py
@@ -1,12 +1,27 @@
+# Copyright 2018 Eficent <http://www.eficent.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from openupgradelib import openupgrade
 
-@openupgrade.migrate()
+
+def map_product_attribute_create_variant(cr):
+    # cannot use map_values method because it cannot map from a boolean
+    cr.execute(
+        """
+        UPDATE product_attribute
+        SET create_variant = 'always'
+        WHERE create_variant = TRUE
+        """
+    )
+    cr.execute(
+        """
+        UPDATE product_attribute
+        SET create_variant = 'no_variant'
+        WHERE create_variant != 'always'
+        """
+    )
+
+
+@openupgrade.migrate(use_env=True)
 def migrate(env, version):
-
-    default_spec = {
-        'product.attribute': [('create_variant', 'always')]
-    }
-
-    openupgrade.set_defaults(env.cr, env, default_spec)
-
     env['product.category']._parent_store_compute()
+    map_product_attribute_create_variant(env.cr)
--- addons/product/migrations/12.0.1.2/pre-migration.py
+++ addons/product/migrations/12.0.1.2/pre-migration.py
@@ -1,18 +1,30 @@
+# Copyright 2018 Paul Catinean <https://github.com/PCatinean>
+# Copyright 2018 Eficent <http://www.eficent.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from openupgradelib import openupgrade
 
-@openupgrade.migrate()
-def migrate(env, version):
+_column_renames = {
+    'res_partner': [
+        ('create_variant', None),
+    ],
+    'product_attribute_price': [
+        ('value_id', 'product_attribute_value_id'),
+    ],
+}
 
-    model_rename = [
-        ('product.attribute.line', 'product.template.attribute.line'),
-        ('product.attribute.price', 'product.template.attribute.value')
-    ]
-    table_rename = [('product_attribute_line', 'product_template_attribute_line')]
+_model_renames = [
+    ('product.attribute.line', 'product.template.attribute.line'),
+    ('product.attribute.price', 'product.template.attribute.value'),
+]
 
-    column_rename = {
-        'product_attribute_price': [('value_id', 'product_attribute_value_id')]
-    }
+_table_renames = [
+    ('product_attribute_line', 'product_template_attribute_line'),
+    ('product_attribute_price', 'product_template_attribute_value'),
+]
 
-    openupgrade.rename_columns(env.cr, column_rename)
-    openupgrade.rename_models(env.cr, model_rename)
-    openupgrade.rename_tables(env.cr, table_rename)
+
+@openupgrade.migrate(use_env=False)
+def migrate(cr, version):
+    openupgrade.rename_columns(cr, _column_renames)
+    openupgrade.rename_models(cr, _model_renames)
+    openupgrade.rename_tables(cr, _table_renames)
