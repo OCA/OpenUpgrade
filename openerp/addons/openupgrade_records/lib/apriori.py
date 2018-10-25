30128b75fccedcb5ba3f1e8c28d652a1dffa26fb
Sylvain LE GAL <sylvain.legal@grap.coop>
2018-10-25 14:29:17 +0200

    [REF] refactor renamed_modules and merged_modules

--- openerp/addons/openupgrade_records/lib/apriori.py
+++ openerp/addons/openupgrade_records/lib/apriori.py
@@ -2,10 +2,17 @@
 to help the matching process
 """
 
-renamed_modules = {
+_oca_odoo_renamed_modules = {
+    # Odoo SA
     'portal_claim': 'website_crm_claim',
-    # OCA/product-attribute
-    'product_m2m_categories': 'product_multi_category',
+    'website_mail_group': 'website_mail_channel',
+    # not exactly a module rename, but they do the same
+    'account_check_writing': 'account_check_printing',
+
+    # Odoo SA -> OCA/bank-payment
+    # not exactly a module rename, but they do the same
+    'account_payment': 'account_payment_order',
+
     # OCA/e-commerce
     'product_links': 'product_multi_link',
     # OCA/sale-workflow
@@ -18,10 +25,6 @@ renamed_modules = {
     'inactive_session_timeout': 'auth_session_timeout',
     # OCA/runbot-addons
     'runbot_secure': 'runbot_relative',
-    # not exactly a module rename, but they do the same
-    'account_check_writing': 'account_check_printing',
-    # same here, whoever want this will also need bank-payment
-    'account_payment': 'account_payment_order',
     # OCA/account-invoicing
     'account_refund_original': 'account_invoice_refund_link',
     'invoice_fiscal_position_update': 'account_invoice_fiscal_position_update',
@@ -30,45 +33,62 @@ renamed_modules = {
     # OCA/purchase-workflow
     'purchase_order_reorder_lines': 'purchase_order_line_sequence',
     # OCA/social
-    'mail_message_name_search': 'base_search_mail_content',
     'marketing_security_group': 'mass_mailing_security_group',
-    # odoomrp/odoomrp-wip
-    'product_variants_no_automatic_creation': 'product_variant_configurator',
-    'sale_product_variants': 'sale_variant_configurator',
-    'sale_stock_product_variants': 'sale_stock_variant_configurator',
-    'purchase_product_variants': 'purchase_variant_configurator',
     # OCA/account-financial-reporting
     'account_financial_report_webkit': 'account_financial_report_qweb',
     'account_journal_report_xls': 'account_journal_report',
     # OCA/contract
     'account_analytic_analysis': 'contract',
     'contract_recurring_plans': 'contract_recurring_analytic_distribution',
-    # OCA/website
-    'website_disable_odoo': 'website_odoo_debranding',
     # OCA/account-analytic
     'account_analytic_plans': 'account_analytic_distribution',
-    'account_analytic_plan_required': 'account_analytic_distribution_required',
     'sale_analytic_plans': 'sale_analytic_distribution',
     'purchase_analytic_plans': 'purchase_analytic_distribution',
+    # OCA/account-analytic 8.0 -> 10.0
+    'account_analytic_plan_required': 'account_analytic_distribution_required',
     # OCA/hr
     'hr_expense_analytic_plans': 'hr_expense_analytic_distribution',
-    # OCA/project
-    'project_work_time_control': 'project_timesheet_time_control',
     # OCA/manufacture-reporting
     'mrp_bom_structure_xls': 'mrp_bom_structure_xlsx',
-    'mrp_bom_structure_xls_level_1': 'mrp_bom_structure_xlsx_level_1',
     # OCA/l10n-spain
     'l10n_es_fiscal_year_closing': 'l10n_es_account_fiscal_year_closing',
+    # OCA/l10n-spain 8.0 -> 10.0
     'account_balance_reporting_xls': 'account_balance_reporting_xlsx',
-    # core
-    'website_mail_group': 'website_mail_channel',
-    # stock-logistics-barcode
+    # OCA/stock-logistics-barcode
     'product_barcode_generator': 'barcodes_generator_product',
     # OCA/web
     'web_shortcuts': 'web_shortcut',
 }
 
-merged_modules = [
+_other_renamed_modules = {
+    # ??? -> OCA/manufacture-reporting
+    'mrp_bom_structure_xls_level_1': 'mrp_bom_structure_xlsx_level_1',
+
+    # ??? -> OCA/product-attribute
+    'product_m2m_categories': 'product_multi_category',
+
+    # ??? -> OCA/product-variant
+    'product_variants_no_automatic_creation': 'product_variant_configurator',
+    'purchase_product_variants': 'purchase_variant_configurator',
+
+    # ??? -> OCA/project
+    'project_work_time_control': 'project_timesheet_time_control',
+
+    # ??? -> OCA/social
+    'mail_message_name_search': 'base_search_mail_content',
+
+    # ??? -> OCA/website
+    'website_disable_odoo': 'website_odoo_debranding',
+
+    # ??? -> ???
+    'sale_product_variants': 'sale_variant_configurator',
+    'sale_stock_product_variants': 'sale_stock_variant_configurator',
+}
+
+renamed_modules = _oca_odoo_renamed_modules.copy()
+renamed_modules.update(_other_renamed_modules)
+
+_oca_odoo_merged_modules = [
     ('account_chart', 'account'),
     ('account_followup', 'account_credit_control'),
     # We convert deprecated Odoo module into OCA one - The rest of
@@ -122,8 +142,8 @@ merged_modules = [
     ('account_invoice_reorder_lines', 'account'),
     ('sale_order_back2draft', 'sale'),
     ('partner_prepayment', 'sale_delivery_block'),
+    # from OCA/sale-workflow (7.0)- included in core
     ('sale_fiscal_position_update', 'sale'),
-    ('sale_documents_comments', 'sale_comment_propagation'),
     # from OCA/bank-payment
     ('account_payment_sale_stock', 'account_payment_sale'),
     # from OCA/website
@@ -137,6 +157,14 @@ merged_modules = [
     # OCA/stock-logistics-workflow
     ('stock_dropshipping_dual_invoice', 'stock_dropshipping'),
 ]
+
+_other_merged_modules = [
+    # ???. sale_comment_propagation doesn't exist in V9.
+    ('sale_documents_comments', 'sale_comment_propagation'),
+]
+
+merged_modules = _oca_odoo_merged_modules + _other_merged_modules
+
 
 renamed_models = {
 }
