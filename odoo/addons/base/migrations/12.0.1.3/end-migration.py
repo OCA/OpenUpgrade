9d1bec0d8fa060f94c8128f0f21a3d47c0fe1ac0
Stefan Rijnhart <stefan@opener.am>
2018-11-13 17:55:39 +0100

    [ADD] Migrate model_terms translations

--- odoo/addons/base/migrations/12.0.1.3/end-migration.py
+++ odoo/addons/base/migrations/12.0.1.3/end-migration.py
@@ -5,6 +5,42 @@ from openupgradelib import openupgrade, openupgrade_merge_records
 
 @openupgrade.migrate()
 def migrate(env, version):
+    update_model_terms_translations(env)
+    fork_off_system_user(env)
+
+
+def update_model_terms_translations(env):
+    """ Adapt to changes in https://github.com/odoo/odoo/pull/26925, that
+    introduces a separate translation type for xml structured fields. First,
+    deduplicate existing model translations with new model_terms translations
+    that were loaded during the migration. """
+    openupgrade.logged_query(
+        env.cr, """ DELETE FROM ir_translation WHERE id IN (
+        SELECT it2.id FROM ir_translation it1
+        JOIN ir_translation it2 ON it1.type in ('model', 'model_terms')
+            AND it2.type in ('model', 'model_terms')
+            AND it1.name = it2.name
+            AND it1.res_id = it2.res_id
+            AND it1.lang = it2.lang
+            AND it1.id < it2.id); """)
+    names = []
+    for rec in env['ir.model.fields'].search([('translate', '=', True)]):
+        try:
+            field = env[rec.model]._fields[rec.name]
+        except KeyError:
+            continue
+        if callable(field.translate):
+            names.append('%s,%s' % (rec.model, rec.name))
+    if names:
+        openupgrade.logged_query(
+            env.cr,
+            """ UPDATE ir_translation
+            SET type = 'model_terms'
+            WHERE type = 'model' AND name IN %s """,
+            (tuple(names),))
+
+
+def fork_off_system_user(env):
     """ Fork user admin off from user system. User admin keeps the original
     partner, and user system gets a new partner. """
     user_root = env.ref('base.user_root')
