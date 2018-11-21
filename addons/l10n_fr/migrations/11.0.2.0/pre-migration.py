dc1321e64bacf8f24951435185d344e4b727e88c
Raphaël Valyi <rvalyi@akretion.com>
2018-11-21 12:12:27 -0200

    rename_xmlids now takes a list

--- addons/l10n_fr/migrations/11.0.2.0/pre-migration.py
+++ addons/l10n_fr/migrations/11.0.2.0/pre-migration.py
@@ -8,8 +8,8 @@ def migrate_l10n_fr_siret_field(env):
     """If l10n_fr_siret is installed in version 10.0, we must rename xmlid"""
     if openupgrade.is_module_installed(env.cr, 'l10n_fr_siret'):
         openupgrade.rename_xmlids(
-            env.cr, ('l10n_fr_siret.field_res_partner_siret',
-                     'l10n_fr.field_res_partner_siret'))
+            env.cr, [('l10n_fr_siret.field_res_partner_siret',
+                     'l10n_fr.field_res_partner_siret')])
 
 
 @openupgrade.migrate()
