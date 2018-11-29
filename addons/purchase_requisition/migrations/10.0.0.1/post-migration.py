90b558e1c5759932f3bf9616a4c9c74238b5927b
mourad-ehm <mourad.elhadj.mimoune@akretion.com>
2018-11-29 12:31:32 +0100

    [FIX] purchase_requisition post-migration create product only if necessry & fix bug categ_id required & multi-company context (#1637)

--- addons/purchase_requisition/migrations/10.0.0.1/post-migration.py
+++ addons/purchase_requisition/migrations/10.0.0.1/post-migration.py
@@ -8,16 +8,24 @@ from openupgradelib import openupgrade
 
 def fill_product(env):
     """Fill with an pre-created product as it's required."""
-    product = env['product.product'].create({
-        'active': False,
-        'name': 'OU purchase requisition line wildcard',
-    })
-    openupgrade.logged_query(
-        env.cr, """
-        UPDATE purchase_requisition_line
-        SET product_id = %s
-        WHERE product_id IS NULL;""", (product.id, ),
-    )
+    if env['purchase.requisition.line'].search_count(
+            [('product_id', '=', False)]) > 0:
+        categ_id = env["product.template"]._get_default_category_id()
+        if not categ_id:
+            categ_id = env["product.category"].search(
+                [], limit=1, order='id asc').id
+        product = env['product.product'].create({
+            'active': False,
+            'categ_id': categ_id,  # categ_id is required
+            'company_id': False,  # tacke into account multi-company context
+            'name': 'OU purchase requisition line wildcard',
+        })
+        openupgrade.logged_query(
+            env.cr, """
+            UPDATE purchase_requisition_line
+            SET product_id = %s
+            WHERE product_id IS NULL;""", (product.id, ),
+        )
 
 
 @openupgrade.migrate()
