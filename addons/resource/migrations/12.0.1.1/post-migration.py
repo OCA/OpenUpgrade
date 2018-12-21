b7aefecbe7abda0201713d99366a0897dd8cb6a9
Pedro M. Baeza <pedro.baeza@gmail.com>
2018-12-21 20:01:42 +0100

    [IMP] resource: Complete migration scripts for v12

--- addons/resource/migrations/12.0.1.1/post-migration.py
+++ addons/resource/migrations/12.0.1.1/post-migration.py
@@ -1,23 +1,25 @@
 # Copyright 2018 Eficent <http://www.eficent.com>
+# Copyright 2018 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
 from openupgradelib import openupgrade
 from psycopg2.extensions import AsIs
 
 
 def fill_resource_calendar_attendance_day_period(cr):
-    cr.execute(
-        """
-        UPDATE resource_calendar_attendance
-        SET day_period = CASE WHEN date_to < ???? THEN 'morning'
-                              ELSE 'afternoon' END
-        """
+    openupgrade.logged_query(
+        cr, """UPDATE resource_calendar_attendance
+        SET day_period = CASE
+            WHEN hour_from < 12
+            THEN 'morning'
+            ELSE 'afternoon' END
+        """,
     )
 
 
 def fill_resource_calendar_tz(cr):
-    cr.execute(
-        """
-        UPDATE resource_calendar rc
+    openupgrade.logged_query(
+        cr, """UPDATE resource_calendar rc
         SET tz = rcl.%s
         FROM resource_calendar_leaves rcl
         WHERE rcl.calendar_id = rc.id AND rcl.%s IS NOT NULL
@@ -29,28 +31,29 @@ def fill_resource_calendar_tz(cr):
 
 
 def fill_resource_resource_tz(cr):
-    cr.execute(
-        """
-        UPDATE resource_resource rr
+    openupgrade.logged_query(
+        cr, """UPDATE resource_resource rr
         SET tz = rp.tz
         FROM res_users ru
         LEFT JOIN res_partner rp ON ru.partner_id = rp.id
         WHERE rr.user_id = ru.id AND rp.tz IS NOT NULL
-        """
+        """,
     )
 
 
+@openupgrade.logging()
 def fill_resource_calendar_hours_per_day(env):
-    records = env['resource_calendar'].search([])
-    records._onchange_hours_per_day()
+    for calendar in env['resource.calendar'].search([]):
+        calendar._onchange_hours_per_day()
 
 
-@openupgrade.migrate(use_env=True)
+@openupgrade.migrate()
 def migrate(env, version):
     cr = env.cr
+    fill_resource_calendar_attendance_day_period(cr)
     fill_resource_resource_tz(cr)
     fill_resource_calendar_tz(cr)
-    # fill_resource_calendar_hours_per_day(env)
+    fill_resource_calendar_hours_per_day(env)
     openupgrade.load_data(
         cr, 'resource', 'migrations/12.0.1.1/noupdate_changes.xml',
     )
