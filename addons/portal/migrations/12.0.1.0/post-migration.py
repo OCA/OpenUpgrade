078b62d2fa1b998b8bcdba1ee38980243a6d448e
Pedro M. Baeza <pedro.baeza@gmail.com>
2018-12-21 21:29:49 +0100

    [FIX] portal: Delete translations for new mail template

--- addons/portal/migrations/12.0.1.0/post-migration.py
+++ addons/portal/migrations/12.0.1.0/post-migration.py
@@ -1,9 +1,15 @@
 # Copyright 2018 Eficent <http://www.eficent.com>
+# Copyright 2018 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
 from openupgradelib import openupgrade
 
 
-@openupgrade.migrate(use_env=False)
-def migrate(cr, version):
+@openupgrade.migrate()
+def migrate(env, version):
     openupgrade.load_data(
-        cr, 'portal', 'migrations/12.0.1.0/noupdate_changes.xml')
+        env.cr, 'portal', 'migrations/12.0.1.0/noupdate_changes.xml')
+    openupgrade.delete_record_translations(
+        env.cr, 'portal',
+        [env.ref('portal.mail_template_data_portal_welcome').id],
+    )
