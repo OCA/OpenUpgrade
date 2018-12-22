f1ba82d5656cc2ea1caa91c35454e3082417cf3e
Pedro M. Baeza <pedro.baeza@gmail.com>
2018-12-22 12:48:51 +0100

    [FIX] analytic: Finish v12 migration scripts

--- addons/analytic/migrations/12.0.1.1/post-migration.py
+++ addons/analytic/migrations/12.0.1.1/post-migration.py
@@ -1,20 +1,17 @@
 # Copyright 2018 Eficent <http://www.eficent.com>
+# Copyright 2018 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
 from openupgradelib import openupgrade
-from psycopg2.extensions import AsIs
 
 
 def fill_account_analytic_line_company_id(cr):
-    cr.execute(
-        """
-        UPDATE account_analytic_line aal
-        SET company_id = aaa.%s 
+    openupgrade.logged_query(
+        cr, """UPDATE account_analytic_line aal
+        SET company_id = aaa.company_id
         FROM account_analytic_account aaa
-        WHERE aal.account_id = aaa.id AND aaa.%s IS NOT NULL
-        """, (
-            AsIs(openupgrade.get_legacy_name('company_id')),
-            AsIs(openupgrade.get_legacy_name('company_id')),
-        ),
+        WHERE aal.account_id = aaa.id
+        """,
     )
 
 
--- addons/analytic/migrations/12.0.1.1/pre-migration.py
+++ addons/analytic/migrations/12.0.1.1/pre-migration.py
@@ -1,9 +1,11 @@
 # Copyright 2018 Eficent <http://www.eficent.com>
+# Copyright 2018 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
 from openupgradelib import openupgrade
 
 column_copies = {
-    'account_analytic_account': [
+    'account_analytic_line': [
         ('company_id', None, None),
     ],
 }
