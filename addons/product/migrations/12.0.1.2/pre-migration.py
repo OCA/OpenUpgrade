3c6f35aa2161d4544d4499d008fc89b71b453004
Pedro M. Baeza <pedro.baeza@gmail.com>
2018-12-24 14:26:51 +0100

    [FIX] product: Finish v12 migration scripts

--- addons/product/migrations/12.0.1.2/pre-migration.py
+++ addons/product/migrations/12.0.1.2/pre-migration.py
@@ -1,16 +1,20 @@
 # Copyright 2018 Paul Catinean <https://github.com/PCatinean>
 # Copyright 2018 Eficent <http://www.eficent.com>
+# Copyright 2018 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
 from openupgradelib import openupgrade
 
 _column_renames = {
-    'res_partner': [
+    'product_attribute': [
         ('create_variant', None),
     ],
-    'product_attribute_price': [
-        ('value_id', 'product_attribute_value_id'),
-    ],
 }
+
+_field_renames = [
+    ('product.attribute.price', 'product_attribute_price', 'value_id',
+     'product_attribute_value_id'),
+]
 
 _model_renames = [
     ('product.attribute.line', 'product.template.attribute.line'),
@@ -23,8 +27,10 @@ _table_renames = [
 ]
 
 
-@openupgrade.migrate(use_env=False)
-def migrate(cr, version):
+@openupgrade.migrate()
+def migrate(env, version):
+    cr = env.cr
     openupgrade.rename_columns(cr, _column_renames)
+    openupgrade.rename_fields(env, _field_renames)
     openupgrade.rename_models(cr, _model_renames)
     openupgrade.rename_tables(cr, _table_renames)
