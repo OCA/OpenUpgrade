a3211c6ca3308cc621c2e544eebfdaf3663cdcc3
Pedro M. Baeza <pedro.baeza@gmail.com>
2019-01-12 13:15:53 +0100

    [FIX] mass_mailing: Finish migration scripts
    * Mass mailings don't use blacklist
    * Use logged_query for better log

--- addons/mass_mailing/migrations/12.0.2.0/post-migration.py
+++ addons/mass_mailing/migrations/12.0.2.0/post-migration.py
@@ -1,46 +1,36 @@
 # Copyright 2018 Eficent <http://www.eficent.com>
+# Copyright 2019 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
 from openupgradelib import openupgrade
 from psycopg2.extensions import AsIs
 
 
-def fill_mail_blacklist_mass_mailing_contact(cr):
-    cr.execute(
-        """
-        INSERT INTO mail_blacklist (email, active,
-            create_uid, create_date, write_uid, write_date)
-        SELECT email, TRUE as active, create_uid, create_date,
-            write_uid, write_date
-        FROM mail_mass_mailing_contact mmmc
-        WHERE mmmc.%s
-        ON CONFLICT DO NOTHING
-        """, (AsIs(openupgrade.get_legacy_name('opt_out')), ),
-    )
-
-
-def fill_mass_mailing_list_contact_rel_unsubscription_date(cr):
-    cr.execute(
-        """
+def fill_mass_mailing_list_contact_rel_data(cr):
+    openupgrade.logged_query(
+        cr, """
         UPDATE mail_mass_mailing_contact_list_rel mmmclr
-        SET unsubscription_date = mmmc.%s
+        SET unsubscription_date = mmmc.%s,
+            opt_out = mmmc.%s
         FROM mail_mass_mailing_contact mmmc
         WHERE mmmclr.contact_id = mmmc.id
-        """, (AsIs(openupgrade.get_legacy_name('unsubscription_date')), ),
+        """, (
+            AsIs(openupgrade.get_legacy_name('unsubscription_date')),
+            AsIs(openupgrade.get_legacy_name('opt_out')),
+        ),
     )
 
 
 def fill_mass_mailing_user_id(cr):
-    cr.execute(
-        """
+    openupgrade.logged_query(
+        cr, """
         UPDATE mail_mass_mailing
         SET user_id = create_uid
-        WHERE user_id IS NULL
-        """
+        WHERE user_id IS NULL""",
     )
 
 
 @openupgrade.migrate(use_env=False)
 def migrate(cr, version):
-    fill_mail_blacklist_mass_mailing_contact(cr)
-    fill_mass_mailing_list_contact_rel_unsubscription_date(cr)
+    fill_mass_mailing_list_contact_rel_data(cr)
     fill_mass_mailing_user_id(cr)
