235b2acfeb259d8008db272c8a66608814ac8f0d
Pedro M. Baeza <pedro.baeza@gmail.com>
2019-01-12 13:40:29 +0100

    [FIX] event: Finish migration scripts
    * Delete noupdate mail templates translations

--- addons/event/migrations/12.0.1.0/post-migration.py
+++ addons/event/migrations/12.0.1.0/post-migration.py
@@ -1,5 +1,7 @@
 # Copyright 2018 Eficent <http://www.eficent.com>
+# Copyright 2019 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
 from openupgradelib import openupgrade
 
 
@@ -7,3 +9,10 @@ from openupgradelib import openupgrade
 def migrate(cr, version):
     openupgrade.load_data(
         cr, 'event', 'migrations/12.0.1.0/noupdate_changes.xml')
+    openupgrade.delete_record_translations(
+        cr, 'event', [
+            'event_reminder',
+            'event_registration_mail_template_badge',
+            'event_subscription',
+        ],
+    )
