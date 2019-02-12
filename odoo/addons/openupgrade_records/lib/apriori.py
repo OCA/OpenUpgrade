8095005379a256c0d562338a0e43c081696e8907
Ernesto Tejeda <ernesto.tejeda87@gmail.com>
2019-02-12 06:33:31 -0500

    unnecessary auth_brute_force (#1741)

--- odoo/addons/openupgrade_records/lib/apriori.py
+++ odoo/addons/openupgrade_records/lib/apriori.py
@@ -24,6 +24,8 @@ merged_modules = {
     'website_rating_project': 'project',
     'website_sale_options': 'website_sale',
     'website_sale_stock_options': 'website_sale_stock',
+    # OCA/server-auth
+    'auth_brute_force': 'base',
     # OCA/web
     'web_sheet_full_width': 'web_responsive',
 }
