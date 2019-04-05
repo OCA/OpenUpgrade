9de9877b10842f048754ad43a4ad619d4df0c669
Pedro M. Baeza <pedro.baeza@gmail.com>
2019-04-05 20:10:37 +0200

    [FIX] fleet: Map without error fleet.vehicle.log.contract state
    It avoids:
    
      File "/opt/odoo/OpenUpgrade/addons/fleet/migrations/11.0.0.1/post-migration.py", line 14, in migrate
        env['fleet.vehicle.log.contract'].scheduler_manage_contract_expiration()
      File "/opt/odoo/OpenUpgrade/addons/fleet/models/fleet_vehicle_cost.py", line 278, in scheduler_manage_contract_expiration
        expired_contracts.write({'state': 'expired'})
      File "/opt/odoo/OpenUpgrade/addons/mail/models/mail_thread.py", line 291, in write
        track_self.message_track(tracked_fields, initial_values)
      File "/opt/odoo/OpenUpgrade/addons/mail/models/mail_thread.py", line 494, in message_track
        tracking = self._message_track_get_changes(tracked_fields, initial_values)
      File "/opt/odoo/OpenUpgrade/addons/mail/models/mail_thread.py", line 444, in _message_track_get_changes
        result[record.id] = record._message_track(tracked_fields, initial_values[record.id])
      File "/opt/odoo/OpenUpgrade/addons/mail/models/mail_thread.py", line 467, in _message_track
        tracking = self.env['mail.tracking.value'].create_tracking_values(initial_value, new_value, col_name, col_info)
      File "/opt/odoo/OpenUpgrade/addons/mail/models/mail_tracking_value.py", line 57, in create_tracking_values
        'old_value_char': initial_value and dict(col_info['selection'])[initial_value] or '',
    KeyError: 'toclose'

--- addons/fleet/migrations/11.0.0.1/post-migration.py
+++ addons/fleet/migrations/11.0.0.1/post-migration.py
@@ -1,4 +1,5 @@
 # Copyright 2017 Tecnativa - Vicent Cubells
+# Copyright 2019 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from openupgradelib import openupgrade
@@ -10,5 +11,11 @@ def migrate(env, version):
         env.cr, openupgrade.get_legacy_name('category'), 'category',
         [('both', 'contract')], table='fleet_service_type',
     )
-    # Update state of all the contracts
+    openupgrade.map_values(
+        env.cr,
+        openupgrade.get_legacy_name('state'),
+        'state',
+        [('toclose', 'diesoon')],
+        table='fleet_vehicle_log_contract',
+    )
     env['fleet.vehicle.log.contract'].scheduler_manage_contract_expiration()
--- addons/fleet/migrations/11.0.0.1/pre-migration.py
+++ addons/fleet/migrations/11.0.0.1/pre-migration.py
@@ -1,4 +1,5 @@
 # Copyright 2018 Tecnativa - Vicent Cubells
+# Copyright 2019 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from openupgradelib import openupgrade
@@ -7,6 +8,9 @@ COLUMN_COPIES = {
     'fleet_service_type': [
         ('category', None, None),
     ],
+    'fleet_vehicle_log_contract': [
+        ('state', None, None),
+    ]
 }
 
 
