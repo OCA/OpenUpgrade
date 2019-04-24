95cddfb3f326eaaee1aef64131643bb3aef1ac46
Andrea <a.stirpe@onestein.nl>
2019-04-24 10:52:31 +0200

    [MIG] l10n_nl

---
+++ addons/l10n_nl/migrations/12.0.3.0/post-migration.py
@@ -0,0 +1,15 @@
+# Copyright 2019 Onestein (<https://www.onestein.eu>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    openupgrade.delete_records_safely_by_xml_id(
+        env, [
+            'l10n_nl.transfer_account_id',
+            'l10n_nl.tag_nl_13',
+            'l10n_nl.tag_nl_34',
+            'l10n_nl.btw_X0',
+        ],
+    )
