045562f524e73eabbe8e9025aa065481a7961919
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2019-05-16 18:23:07 +0200

    [MIG] hr_holidays: Refine migration
    - Move model data
    - Subtypes handling
    - Better _work explanations
    - Minor tweaks

--- addons/hr_holidays/migrations/12.0.1.5/post-migration.py
+++ addons/hr_holidays/migrations/12.0.1.5/post-migration.py
@@ -1,4 +1,5 @@
 # Copyright 2019 Eficent <http://www.eficent.com>
+# Copyright 2019 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from openupgradelib import openupgrade
 from psycopg2.extensions import AsIs
@@ -8,14 +9,12 @@ def fill_hr_holidays_dates(cr):
     openupgrade.logged_query(
         cr, """
         UPDATE hr_holidays
-        SET date_from = COALESCE(date_from, create_date)
-        """
+        SET date_from = COALESCE(date_from, create_date)""",
     )
     openupgrade.logged_query(
         cr, """
         UPDATE hr_holidays
-        SET date_to = COALESCE(date_to, create_date)
-        """
+        SET date_to = COALESCE(date_to, create_date)""",
     )
 
 
@@ -24,8 +23,8 @@ def fill_hr_leave_type_validation_type(cr):
         cr, """
         UPDATE hr_leave_type
         SET validation_type = 'both'
-        WHERE %s = TRUE
-        """, (AsIs(openupgrade.get_legacy_name('double_validation')), ),
+        WHERE %s""",
+        (AsIs(openupgrade.get_legacy_name('double_validation')), ),
     )
 
 
@@ -34,228 +33,138 @@ def fill_hr_leave_type_allocation_type(cr):
         cr, """
         UPDATE hr_leave_type
         SET allocation_type = 'no'
-        WHERE %s = TRUE
-        """, (AsIs(openupgrade.get_legacy_name('limit')), ),
+        WHERE %s""", (AsIs(openupgrade.get_legacy_name('limit')), ),
     )
+
+
+def _move_model_in_data(env, ids, old_model, new_model):
+    renames = [
+        ('mail_message', 'model', 'res_id'),
+        ('mail_followers', 'res_model', 'res_id'),
+        ('ir_attachment', 'res_model', 'res_id'),
+        ('mail_activity', 'res_model', 'res_id'),
+        ('ir_model_data', 'model', 'res_id'),
+    ]
+    for rename in renames:
+        openupgrade.logged_query(
+            env.cr, """
+            UPDATE %s
+            SET %s = %s
+            WHERE %s IN %s AND %s = %s""",
+            (
+                AsIs(rename[0]),
+                AsIs(rename[1]), new_model,
+                AsIs(rename[2]), tuple(ids), AsIs(rename[1]), old_model,
+            ),
+        )
 
 
 def fill_hr_leave(env):
     # In pre-migration the hr_leave table still doesn't exist
-    env.cr.execute(
-        """
-        ALTER TABLE hr_leave
-        ADD COLUMN {} integer;
-        """.format(openupgrade.get_legacy_name('holidays_id'))
-    )
     openupgrade.logged_query(
         env.cr, """
-        INSERT INTO hr_leave (category_id, date_from, date_to, department_id,
+        INSERT INTO hr_leave (
+            id, category_id, date_from, date_to, department_id,
             employee_id, first_approver_id, holiday_status_id, holiday_type,
             manager_id, meeting_id, name, notes, number_of_days,
             payslip_status, report_note, second_approver_id, state, user_id,
-            create_uid, create_date, write_uid, write_date, %s)
-        SELECT category_id, date_from, date_to, department_id, employee_id,
+            create_uid, create_date, write_uid, write_date
+        )
+        SELECT id, category_id, date_from, date_to, department_id, employee_id,
             first_approver_id, holiday_status_id, holiday_type, manager_id,
             meeting_id, name, notes, number_of_days, payslip_status,
             report_note, second_approver_id, state, user_id, create_uid,
-            create_date, write_uid, write_date, id
+            create_date, write_uid, write_date
         FROM hr_holidays
         WHERE %s = 'remove'
-        RETURNING id, %s
-        """, (
-            AsIs(openupgrade.get_legacy_name('holidays_id')),
-            AsIs(openupgrade.get_legacy_name('type')),
-            AsIs(openupgrade.get_legacy_name('holidays_id')),
-        ),
+        RETURNING id""", (AsIs(openupgrade.get_legacy_name('type')), ),
     )
-    new_rows = env.cr.fetchall()
-    # fix parent_id:
-    openupgrade.logged_query(
-        env.cr, """
-        UPDATE hr_leave hl1
-        SET parent_id = hl2.id
-        FROM hr_holidays hh1
-        INNER JOIN hr_holidays hh2 ON hh1.parent_id = hh2.id
-        INNER JOIN hr_leave hl2 ON hh2.id = hl2.%s
-        WHERE hl1.%s = hh1.id
-        """, (
-            AsIs(openupgrade.get_legacy_name('holidays_id')),
-            AsIs(openupgrade.get_legacy_name('holidays_id')),
-        ),
-    )
-    # update many2ones of other models
-    env.cr.execute("""
-        SELECT model, name
-        FROM ir_model_fields
-        WHERE relation = 'hr_leave' AND ttype = 'many2one'
-            AND model NOT IN ('hr.holidays', 'hr.leave')
-    """)
-    for field in env.cr.fetchall():
-        for row in new_rows:
-            table = field[0].replace('.', '_')
-            env.cr.execute(
-                """
-                UPDATE %s
-                SET %s = %s
-                WHERE %s = %s
-                """ % (table, field[1], field[1], row[0], row[1])
-            )
-    # update mail_message
-    openupgrade.logged_query(
-        env.cr, """
-        UPDATE mail_message mm
-        SET model = 'hr.leave', res_id = hl.id
-        FROM hr_leave hl
-        WHERE mm.res_id = hl.%s AND mm.model = 'hr.holidays'
-        """, (
-            AsIs(openupgrade.get_legacy_name('holidays_id')),
-        ),
-    )
-    # update mail_followers
-    openupgrade.logged_query(
-        env.cr, """
-        UPDATE mail_followers mf
-        SET res_model = 'hr.leave', res_id = hl.id
-        FROM hr_leave hl
-        WHERE mf.res_id = hl.%s AND mf.res_model = 'hr.holidays'
-        """, (
-            AsIs(openupgrade.get_legacy_name('holidays_id')),
-        ),
-    )
-    # update attachments
-    openupgrade.logged_query(
-        env.cr, """
-        UPDATE ir_attachment ia
-        SET res_model = 'hr.leave', res_id = ia.id
-        FROM hr_leave hl
-        WHERE ia.res_id = hl.%s AND ia.res_model = 'hr.holidays'
-        """, (
-            AsIs(openupgrade.get_legacy_name('holidays_id')),
-        ),
-    )
+    ids = [x[0] for x in env.cr.fetchall()]
+    _move_model_in_data(env, ids, 'hr.holidays', 'hr.leave')
 
 
 def fill_hr_leave_request_dates(cr):
     openupgrade.logged_query(
         cr, """
         UPDATE hr_leave
-        SET request_date_from = COALESCE(request_date_from, date_from)
-        """
+        SET request_date_from = COALESCE(request_date_from, date_from)"""
     )
     openupgrade.logged_query(
         cr, """
         UPDATE hr_leave
-        SET request_date_to = COALESCE(request_date_to, date_to)
-        """
+        SET request_date_to = COALESCE(request_date_to, date_to)"""
     )
 
 
 def fill_hr_leave_allocation(env):
-    # In pre-migration the hr_leave_allocation table still doesn't exist
-    env.cr.execute(
-        """
-        ALTER TABLE hr_leave_allocation
-        ADD COLUMN {} integer;
-        """.format(openupgrade.get_legacy_name('holidays_id'))
-    )
     openupgrade.logged_query(
         env.cr, """
-        INSERT INTO hr_leave_allocation (category_id, date_from, date_to,
+        INSERT INTO hr_leave_allocation (
+            id, category_id, date_from, date_to,
             department_id, employee_id, first_approver_id, holiday_status_id,
             holiday_type, name, notes, number_of_days, second_approver_id,
-            state, create_uid, create_date, write_uid, write_date, %s)
-        SELECT category_id, date_from, date_to, department_id, employee_id,
+            state, create_uid, create_date, write_uid, write_date)
+        SELECT id, category_id, date_from, date_to, department_id, employee_id,
             first_approver_id, holiday_status_id, holiday_type, name, notes,
             number_of_days, second_approver_id, state, create_uid,
-            create_date, write_uid, write_date, id
+            create_date, write_uid, write_date
         FROM hr_holidays
         WHERE %s = 'add'
-        RETURNING id, %s
-        """, (
-            AsIs(openupgrade.get_legacy_name('holidays_id')),
-            AsIs(openupgrade.get_legacy_name('type')),
-            AsIs(openupgrade.get_legacy_name('holidays_id')),
-        ),
+        RETURNING id""", (AsIs(openupgrade.get_legacy_name('type')), ),
     )
-    new_rows = env.cr.fetchall()
-    # fix parent_id:
+    ids = [x[0] for x in env.cr.fetchall()]
+    _move_model_in_data(env, ids, 'hr.holidays', 'hr.leave.allocation')
+
+
+def set_max_sequences(env):
+    """Set for both new tables the next id val as the maximum of the previous
+    table + 1. This way, we don't have any of the old IDs duplicated in new
+    tables.
+    """
     openupgrade.logged_query(
-        env.cr, """
-        UPDATE hr_leave_allocation hla1
-        SET parent_id = hla2.id
-        FROM hr_holidays hh1
-        INNER JOIN hr_holidays hh2 ON hh1.parent_id = hh2.id
-        INNER JOIN hr_leave_allocation hla2 ON hh2.id = hla2.%s
-        WHERE hla1.%s = hh1.id
-        """, (
-            AsIs(openupgrade.get_legacy_name('holidays_id')),
-            AsIs(openupgrade.get_legacy_name('holidays_id')),
-        ),
+        env.cr,
+        "SELECT setval('hr_leave_id_seq', (SELECT MAX(id) FROM hr_holidays))"
     )
-    # update many2ones of other models
-    env.cr.execute("""
-        SELECT model, name
-        FROM ir_model_fields
-        WHERE relation = 'hr_leave_allocation' AND ttype = 'many2one'
-            AND model NOT IN ('hr.holidays', 'hr.leave.allocation')
-    """)
-    for field in env.cr.fetchall():
-        for row in new_rows:
-            table = field[0].replace('.', '_')
-            env.cr.execute(
-                """
-                UPDATE %s
-                SET %s = %s
-                WHERE %s = %s
-                """ % (table, field[1], field[1], row[0], row[1])
+    openupgrade.logged_query(
+        env.cr,
+        "SELECT setval('hr_leave_allocation_id_seq', "
+        "(SELECT MAX(id) FROM hr_holidays))"
+    )
+
+
+def subscribe_new_subtypes(env):
+    """Subscribe to the new subtypes to those that were subscribed to the
+    old ones.
+    """
+    subtype_mapping = [
+        ('mt_department_holidays_approved',
+         'mt_department_leave_allocation_approved',
+         'mt_department_leave_approved'),
+        ('mt_department_holidays_refused',
+         'mt_department_leave_allocation_refused',
+         'mt_department_leave_refused'),
+        ('mt_holidays_approved',
+         'mt_leave_allocation_approved',
+         'mt_leave_approved'),
+        ('mt_holidays_refused',
+         'mt_leave_allocation_refused',
+         'mt_leave_refused'),
+    ]
+    Followers = env['mail.followers']
+    for old_id, new_id1, new_id2 in subtype_mapping:
+        old_mt = env.ref('hr_holidays.' + old_id)
+        followers = Followers.search([('subtype_ids', '=', old_mt.id)])
+        if followers:
+            new_mt1 = env.ref('hr_holidays.' + new_id1)
+            new_mt2 = env.ref('hr_holidays.' + new_id2)
+            subtype_ids = (new_mt1 + new_mt2).ids
+            partner_ids = followers.mapped('partner_id').ids
+            Followers._insert_followers(
+                followers[0].res_model, followers.mapped('res_id'),
+                partner_ids,
+                dict((pid, subtype_ids) for pid in partner_ids),
+                [], False, check_existing=True, existing_policy='update',
             )
-    # update mail_message
-    openupgrade.logged_query(
-        env.cr, """
-        UPDATE mail_message mm
-        SET model = 'hr.leave.allocation', res_id = hla.id
-        FROM hr_leave_allocation hla
-        WHERE mm.res_id = hla.%s AND mm.model = 'hr.holidays'
-        """, (
-            AsIs(openupgrade.get_legacy_name('holidays_id')),
-        ),
-    )
-    # update mail_followers
-    openupgrade.logged_query(
-        env.cr, """
-        UPDATE mail_followers mf
-        SET res_model = 'hr.leave.allocation', res_id = hla.id
-        FROM hr_leave_allocation hla
-        WHERE mf.res_id = hla.%s AND mf.res_model = 'hr.holidays'
-        """, (
-            AsIs(openupgrade.get_legacy_name('holidays_id')),
-        ),
-    )
-    # update attachments
-    openupgrade.logged_query(
-        env.cr, """
-        UPDATE ir_attachment ia
-        SET res_model = 'hr.leave.allocation', res_id = ia.id
-        FROM hr_leave_allocation hla
-        WHERE ia.res_id = hla.%s AND ia.res_model = 'hr.holidays'
-        """, (
-            AsIs(openupgrade.get_legacy_name('holidays_id')),
-        ),
-    )
-
-
-def delete_hr_holidays_model(cr):
-    """Delete old hr.holidays model."""
-    openupgrade.logged_query(
-        cr,
-        "DELETE FROM ir_model WHERE model = %s",
-        ('hr.holidays', )
-    )
-    openupgrade.logged_query(
-        cr,
-        "DELETE FROM ir_model_data WHERE model = %s",
-        ('hr.holidays', )
-    )
 
 
 @openupgrade.migrate()
@@ -267,8 +176,10 @@ def migrate(env, version):
     fill_hr_leave(env)
     fill_hr_leave_request_dates(cr)
     fill_hr_leave_allocation(env)
+    set_max_sequences(env)
     openupgrade.load_data(
         cr, 'hr_holidays', 'migrations/12.0.1.5/noupdate_changes.xml')
+    subscribe_new_subtypes(env)
     openupgrade.delete_records_safely_by_xml_id(
         env, [
             'hr_holidays.mt_department_holidays_approved',
@@ -281,4 +192,3 @@ def migrate(env, version):
             'hr_holidays.mt_holidays_refused',
         ],
     )
-    delete_hr_holidays_model(cr)
--- addons/hr_holidays/migrations/12.0.1.5/pre-migration.py
+++ addons/hr_holidays/migrations/12.0.1.5/pre-migration.py
@@ -1,4 +1,5 @@
 # Copyright 2019 Eficent <http://www.eficent.com>
+# Copyright 2019 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from openupgradelib import openupgrade
 
@@ -22,12 +23,10 @@ _field_renames = [
 ]
 
 _model_renames = [
-    # ('hr.holidays', 'hr.leave'),
     ('hr.holidays.status', 'hr.leave.type'),
 ]
 
 _table_renames = [
-    # ('hr_holidays', 'hr_leave'),
     ('hr_holidays_status', 'hr_leave_type'),
 ]
 
@@ -39,3 +38,6 @@ def migrate(env, version):
     openupgrade.rename_fields(env, _field_renames)
     openupgrade.rename_models(cr, _model_renames)
     openupgrade.rename_tables(cr, _table_renames)
+    openupgrade.logged_query(
+        env.cr, "ALTER TABLE hr_leave_type ADD validity_start DATE"
+    )
