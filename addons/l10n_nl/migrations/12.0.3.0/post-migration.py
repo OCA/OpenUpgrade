3c54fff030b5a84c95b670d3a569d668eb6c76ba
Andrea Stirpe <a.stirpe@onestein.nl>
2019-05-28 17:15:54 +0200

    [ADD] l10n_nl (#1802)

---
+++ addons/l10n_nl/migrations/12.0.3.0/post-migration.py
@@ -0,0 +1,13 @@
+# Copyright 2019 Onestein (<https://www.onestein.eu>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    openupgrade.delete_records_safely_by_xml_id(
+        env, [
+            'l10n_nl.tag_nl_13',
+            'l10n_nl.tag_nl_34',
+        ],
+    )
---
+++ addons/l10n_nl/migrations/12.0.3.0/pre-migration.py
@@ -0,0 +1,28 @@
+# Copyright 2019 Onestein (<https://www.onestein.eu>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from openupgradelib import openupgrade
+
+
+def merge_res_country_states(cr):
+    openupgrade.logged_query(
+        cr, """
+        INSERT INTO ir_model_data (module, name, model, res_id, noupdate)
+          SELECT 'l10n_nl',
+                 CONCAT('state_nl_', LOWER(s.code)),
+                 'res.country.state',
+                 s.id,
+                 false
+          FROM res_country_state s
+              JOIN res_country c ON s.country_id = c.id
+              WHERE c.code LIKE 'NL'
+                AND UPPER(s.code) IN ('DR','FL','FR','GE','GR','LI','NB','NH',
+                                      'OV','UT','ZE','ZH','BQ1','BQ2','BQ3')
+        ON CONFLICT DO NOTHING
+        """
+    )
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    cr = env.cr
+    merge_res_country_states(cr)
