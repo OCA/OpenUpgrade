31e78a0ec0a4c8aa95acfa522fc0b666f06b3320
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2019-06-05 19:53:47 +0200

    [IMP] sale: Don't modify actual records in recompute qty_delivered

--- addons/sale/migrations/12.0.1.1/end-migration.py
+++ addons/sale/migrations/12.0.1.1/end-migration.py
@@ -6,22 +6,23 @@ from psycopg2.extensions import AsIs
 
 
 def recompute_sale_order_line_qty_delivered(env):
+    """Detect lines with a non coincident computed qty_delivered for assigning
+    the previous value manually.
+    """
     sale_lines = env['sale.order.line'].search([])
-    sale_lines._compute_qty_delivered()
-    # assure same values
-    openupgrade.logged_query(
-        env.cr, """
-        UPDATE sale_order_line sol
-        SET qty_delivered_method = 'manual',
-            qty_delivered_manual = %(qty_d)s
-        WHERE qty_delivered != %(qty_d)s OR qty_delivered_method = 'manual'
-        RETURNING id""",
-        {'qty_d': AsIs(openupgrade.get_legacy_name('qty_delivered'))}
-    )
-    manual_so_lines = env['sale.order.line'].browse(
-        [row[0] for row in env.cr.fetchall()])
-    for sol in manual_so_lines:
-        sol.qty_delivered = sol.qty_delivered_manual
+    for sol in sale_lines:
+        # Create an identical virtual record for not affecting dependent fields
+        # on original record, and compute expected qty_delivered in it
+        sol2 = sol.new(sol2.read()[0])
+        sol2._compute_qty_delivered()
+        if sol2.qty_delivered != sol.qty_delivered:
+            # Save changes in SQL for avoiding recomputations
+            env.cr.execute(
+                """UPDATE sale_order_line
+                SET qty_delivered_method = 'manual',
+                    qty_delivered_manual = %s""",
+                (sol2.qty_delivered, ),
+            )
 
 
 @openupgrade.migrate()
