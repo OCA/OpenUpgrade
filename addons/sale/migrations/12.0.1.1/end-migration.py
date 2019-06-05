f15a9f6a500eec3a6b04b6092e59621b6d36fef9
mreficent <miquel.raich@eficent.com>
2019-06-05 20:08:27 +0200

    [FIX] sale: recompute_sale_order_line_qty_delivered fixes

--- addons/sale/migrations/12.0.1.1/end-migration.py
+++ addons/sale/migrations/12.0.1.1/end-migration.py
@@ -13,16 +13,25 @@ def recompute_sale_order_line_qty_delivered(env):
     for sol in sale_lines:
         # Create an identical virtual record for not affecting dependent fields
         # on original record, and compute expected qty_delivered in it
-        sol2 = sol.new(sol2.read()[0])
+        sol2 = sol.new(sol.read()[0])
         sol2._compute_qty_delivered()
         if sol2.qty_delivered != sol.qty_delivered:
             # Save changes in SQL for avoiding recomputations
             env.cr.execute(
                 """UPDATE sale_order_line
                 SET qty_delivered_method = 'manual',
-                    qty_delivered_manual = %s""",
-                (sol2.qty_delivered, ),
+                    qty_delivered_manual = %s
+                WHERE id = %s""",
+                (sol2.qty_delivered, sol.id, ),
             )
+    openupgrade.logged_query(
+        env.cr, """
+            UPDATE sale_order_line sol
+            SET qty_delivered_manual = %(qty_d)s
+            WHERE qty_delivered_method = 'manual'
+                AND qty_delivered_manual != %(qty_d)s""",
+        {'qty_d': AsIs(openupgrade.get_legacy_name('qty_delivered')), }
+    )
 
 
 @openupgrade.migrate()
