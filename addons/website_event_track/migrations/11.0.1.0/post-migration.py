25cec6a8f5d0603b35865f1a6c5dd8cd63816339
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2019-06-18 00:09:13 +0200

    [FIX] website_event_track: Change stage assignation code
    For not having problems with translations

--- addons/website_event_track/migrations/11.0.1.0/post-migration.py
+++ addons/website_event_track/migrations/11.0.1.0/post-migration.py
@@ -1,18 +1,33 @@
 # Copyright 2019 Eficent <http://www.eficent.com>
+# Copyright 2019 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from psycopg2.extensions import AsIs
 from openupgradelib import openupgrade
 
 
 def fill_event_track_stage_id(env):
-    openupgrade.logged_query(
-        env.cr, """
-        UPDATE event_track et
-        SET stage_id = ets.id
-        FROM event_track_stage ets
-        WHERE et.%s = lower(ets.name)
-        """, (AsIs(openupgrade.get_legacy_name('state')),)
-    )
+    """Map to the new stage each of the old values of the state field."""
+    state_mapping = {
+        'draft': 'event_track_stage0',
+        'confirmed': 'event_track_stage1',
+        'announced': 'event_track_stage2',
+        'published': 'event_track_stage3',
+        'refused': 'event_track_stage4',
+        'cancel': 'event_track_stage5',
+    }
+    for state, xml_id in state_mapping.items():
+        openupgrade.logged_query(
+            env.cr, """
+            UPDATE event_track et
+            SET stage_id = %s
+            FROM event_track_stage ets
+            WHERE et.%s = %s
+            """, (
+                env.ref('website_event_track.%s' % xml_id).id,
+                AsIs(openupgrade.get_legacy_name('state')),
+                state,
+            ),
+        )
 
 
 def fill_event_track_partner_id(cr):
