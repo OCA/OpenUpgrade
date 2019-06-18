2a2a91a4aeb01ab5783cc35eb807ead69fd29650
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2019-06-18 21:16:26 +0200

    [IMP] stock: Missing copyright

--- addons/stock/migrations/12.0.1.1/pre-migration.py
+++ addons/stock/migrations/12.0.1.1/pre-migration.py
@@ -1,4 +1,5 @@
 # Copyright 2019 Eficent <http://www.eficent.com>
+# Copyright 2019 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 from openupgradelib import openupgrade
 
