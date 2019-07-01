399bf9dfa7a1ff2a6eb9eef912d26dc950237319
Miquel Raïch <miquel.raich@eficent.com>
2019-07-01 12:49:35 +0200

    [MIG] account_check_printing (#1873)

---
+++ addons/account_check_printing/migrations/11.0.1.0/post-migration.py
@@ -0,0 +1,11 @@
+# Copyright 2019 Eficent <http://www.eficent.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    openupgrade.load_data(
+        env.cr, 'account_check_printing',
+        'migrations/11.0.1.0/noupdate_changes.xml',
+    )
