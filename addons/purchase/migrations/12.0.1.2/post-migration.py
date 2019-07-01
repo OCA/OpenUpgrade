28fe7dbf1efc4634894e44600044b12dfd0c7aeb
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2019-07-01 21:17:12 +0200

    [FIX] purchase: Refine migration
    - Log querys
    - Delete mail templates translations
    - product_uom_qty computation rewritten.
      * Field added with openupgradelib method.
      * Performance optimization via SQL.
      * You can't change without user knowledge UoM of already confirmed POs, as
        stock.move UoM won't match.
    - purchase.route_warehouse0_buy XML-ID renamed on purchase, not on
      purchase_stock, as being new module, migration scripts are not executed.
    - Nothing to do on purchase_stock

--- addons/purchase/migrations/12.0.1.2/post-migration.py
+++ addons/purchase/migrations/12.0.1.2/post-migration.py
@@ -1,36 +1,35 @@
 # Copyright 2019 Eficent <http://www.eficent.com>
+# Copyright 2019 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
 from openupgradelib import openupgrade
 
 
 def fill_purchase_order_user_id(cr):
-    cr.execute(
-        """
+    openupgrade.logged_query(
+        cr, """
         UPDATE purchase_order
         SET user_id = create_uid
-        WHERE user_id IS NULL
-        """
+        WHERE user_id IS NULL""",
     )
 
 
-def reset_domain_purchase_rfq(cr):
-    cr.execute(
-        """
-        UPDATE ir_act_window iaw
-        SET domain = NULL
-        FROM ir_model_data imd
-        WHERE imd.name = 'purchase_rfq' AND imd.res_id = iaw.id
-            AND imd.module = 'purchase'
-        """
-    )
+def reset_domain_purchase_rfq(env):
+    env.ref('purchase.purchase_rfq').domain = False
 
 
 @openupgrade.migrate()
 def migrate(env, version):
     fill_purchase_order_user_id(env.cr)
-    reset_domain_purchase_rfq(env.cr)
+    reset_domain_purchase_rfq(env)
     openupgrade.load_data(
         env.cr, 'purchase', 'migrations/12.0.1.2/noupdate_changes.xml')
+    openupgrade.delete_record_translations(
+        env.cr, 'purchase', [
+            'email_template_edi_purchase',
+            'email_template_edi_purchase_done',
+        ],
+    )
     openupgrade.delete_records_safely_by_xml_id(
         env, [
             'purchase.mail_template_data_notification_email_purchase_order',
--- addons/purchase/migrations/12.0.1.2/pre-migration.py
+++ addons/purchase/migrations/12.0.1.2/pre-migration.py
@@ -1,28 +1,60 @@
 # Copyright 2019 Eficent <http://www.eficent.com>
+# Copyright 2019 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
 from openupgradelib import openupgrade
 
+xmlid_renames = [
+    ('purchase.route_warehouse0_buy', 'purchase_stock.route_warehouse0_buy'),
+]
 
-def fix_bad_pol_uom_data(env):
+
+def precompute_pol_product_uom_qty(env):
     """when computing product_uom_qty in _compute_product_uom_qty(),
     we need to avoid the UserError in _compute_quantity()"""
-    env.cr.execute(
-        """ALTER TABLE purchase_order_line
-        ADD COLUMN product_uom_qty DOUBLE PRECISION""",
+    openupgrade.add_fields(
+        env, [
+            ('product_uom_qty', 'purchase.order.line', 'purchase_order_line',
+             'float', 'double precision', 'purchase'),
+        ],
     )
+    # On first place, assign the same value when UoM has not changed
+    openupgrade.logged_query(
+        env.cr, """
+        UPDATE purchase_order_line
+        SET product_uom_qty = product_qty""",
+    )
+    # Then, only auto-switch UoM of lines when they are in draft
     openupgrade.logged_query(
         env.cr, """
         UPDATE purchase_order_line upd_pol
-        SET product_uom = pt.uom_id, product_uom_qty = pol.product_qty
+        SET product_uom = pt.uom_id
         FROM purchase_order_line pol
         JOIN product_product pp ON pol.product_id = pp.id
         JOIN product_template pt ON pp.product_tmpl_id = pt.id
-        JOIN uom_uom uom1 ON pol.product_uom = uom1.id
-        JOIN uom_uom uom2 ON pt.uom_id = uom2.id
-        WHERE uom1.category_id != uom2.category_id AND upd_pol.id = pol.id"""
+        JOIN uom_uom uu ON pol.product_uom = uu.id
+        JOIN uom_uom uu2 ON pt.uom_id = uu2.id
+        WHERE uu.category_id != uu2.category_id
+            AND pol.state = 'draft'
+            AND upd_pol.id = pol.id""",
+    )
+    # Lastly, perform conversion when different UoMs, ignoring incompatible
+    # UoM categories
+    openupgrade.logged_query(
+        env.cr, """
+        UPDATE purchase_order_line upd_pol
+        SET product_uom_qty = pol.product_qty / uu.factor * uu2.factor
+        FROM purchase_order_line pol
+        JOIN product_product pp ON pol.product_id = pp.id
+        JOIN product_template pt ON pp.product_tmpl_id = pt.id
+        JOIN uom_uom uu on uu.id = pol.product_uom
+        JOIN uom_uom uu2 on uu2.id = pt.uom_id
+        WHERE upd_pol.id = pol.id
+            AND pol.product_uom != pt.uom_id""",
     )
 
 
 @openupgrade.migrate()
 def migrate(env, version):
-    fix_bad_pol_uom_data(env)
+    precompute_pol_product_uom_qty(env)
+    openupgrade.rename_xmlids(env.cr, xmlid_renames)
