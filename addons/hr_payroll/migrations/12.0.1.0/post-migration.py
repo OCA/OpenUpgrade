626dc27bad550f680abc175e912435bd980384dc
mreficent <miquel.raich@eficent.com>
2019-07-19 13:54:36 +0200

    [MIG] hr_payroll

---
+++ addons/hr_payroll/migrations/12.0.1.0/post-migration.py
@@ -0,0 +1,10 @@
+# Copyright 2018 Eficent <http://www.eficent.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    openupgrade.load_data(
+        env.cr, 'hr_payroll', 'migrations/12.0.1.0/noupdate_changes.xml')
