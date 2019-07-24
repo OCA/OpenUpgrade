41c2b51cd32720726acea9c4b276ce2b22fd53d2
Jairo Llopis <yajo.sk8@gmail.com>
2019-07-24 10:38:25 +0100

    [FIX] event: Apply Bootstrap 4 migration to description

--- addons/event/migrations/12.0.1.0/post-migration.py
+++ addons/event/migrations/12.0.1.0/post-migration.py
@@ -1,12 +1,14 @@
 # Copyright 2018 Eficent <http://www.eficent.com>
 # Copyright 2019 Tecnativa - Pedro M. Baeza
+# Copyright 2019 Tecnativa - Jairo Llopis
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openupgradelib import openupgrade
+from openupgradelib import openupgrade, openupgrade_120
 
 
-@openupgrade.migrate(use_env=False)
-def migrate(cr, version):
+@openupgrade.migrate()
+def migrate(env, version):
+    cr = env.cr
     openupgrade.load_data(
         cr, 'event', 'migrations/12.0.1.0/noupdate_changes.xml')
     openupgrade.delete_record_translations(
@@ -16,3 +18,5 @@ def migrate(cr, version):
             'event_subscription',
         ],
     )
+    openupgrade_120.convert_field_bootstrap_3to4(
+        env, "event.event", "description")
