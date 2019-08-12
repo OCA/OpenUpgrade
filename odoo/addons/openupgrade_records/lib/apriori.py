e931dbf8b7ba997474396ff0771294a73e79473d
mreficent <miquel.raich@eficent.com>
2019-08-12 15:24:06 +0200

    [MIG] 'purchase_fiscal_position_update' merged into 'purchase'

--- odoo/addons/openupgrade_records/lib/apriori.py
+++ odoo/addons/openupgrade_records/lib/apriori.py
@@ -61,6 +61,8 @@ merged_modules = [
     ('website_sale_b2c', 'sale'),  # used groups are in sale
     # OCA/manufacture
     ('mrp_production_unreserve', 'mrp'),
+    # OCA/purchase-workflow
+    ('purchase_fiscal_position_update', 'purchase'),
     # OCA/sale-workflow
     ('sale_order_back2draft', 'sale'),
     ('product_customer_code_sale',
