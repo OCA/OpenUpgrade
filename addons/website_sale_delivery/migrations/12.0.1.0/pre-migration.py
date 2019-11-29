d3ba9eb826765313ed9f4a00285d23b68eca2330
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2019-11-29 19:00:09 +0100

    [FIX] website_sale_delivery: rename_fields require the model and table

--- addons/website_sale_delivery/migrations/12.0.1.0/pre-migration.py
+++ addons/website_sale_delivery/migrations/12.0.1.0/pre-migration.py
@@ -1,9 +1,10 @@
-# Copyright 2019 Sergio Teruel <http://www.tecantiva.com>
+# Copyright 2019 Sergio Teruel <http://www.tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from openupgradelib import openupgrade
 
 _field_renames = [
-    ('website_published', 'is_published'),
+    ('delivery.carrier', 'delivery_carrier', 'website_published',
+     'is_published'),
 ]
 
 
