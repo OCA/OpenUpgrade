ec318d1196ab1b55ab7fb9edb58784295ac08433
Sergio Teruel <sergio.teruel@tecnativa.com>
2020-01-23 21:40:15 +0100

    [MIG] website_blog: Migration script

--- addons/website_blog/migrations/12.0.1.0/post-migration.py
+++ addons/website_blog/migrations/12.0.1.0/post-migration.py
@@ -1,6 +1,31 @@
 # Copyright 2019 Eficent <http://www.eficent.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from openupgradelib import openupgrade, openupgrade_120
+from openupgradelib.openupgrade_tools import convert_html_fragment,\
+    convert_html_replacement_class_shortcut as _r
+
+
+def replace_old_snippet_reference(env):
+    """
+    This method change the external identifier from snippet_latest_posts to
+    website_blog for all views that render this snippet
+    """
+    views_with_snippet = env['ir.ui.view'].with_context(
+        active_test=False
+    ).search([
+        ('arch_db', 'ilike', 'snippet_latest_posts'),
+    ])
+    for view in views_with_snippet:
+        replacement = (
+            _r(selector="div",
+               attr_rm="snippet_latest_posts."
+                       "s_latest_posts_big_picture_template",
+               attr_add={
+                   "data-template":
+                       "website_blog.s_latest_posts_big_picture_template"}),
+        )
+        new_content = convert_html_fragment(view.arch_db, replacement)
+        view.arch = new_content
 
 
 @openupgrade.migrate()
@@ -10,3 +35,4 @@ def migrate(env, version):
     openupgrade_120.convert_field_bootstrap_3to4(
         env, 'blog.post', 'content',
     )
+    replace_old_snippet_reference(env)
