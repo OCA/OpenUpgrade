4e8878b996822f52aa55cdb491d990961389e4c0
mreficent <miquel.raich@eficent.com>
2020-04-01 16:54:25 +0200

    [MIG] calendar

---
+++ addons/calendar/migrations/13.0.1.0/pre-migration.py
@@ -0,0 +1,13 @@
+# Copyright 2020 ForgeFlow <http://www.forgeflow.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openupgradelib import openupgrade
+
+_field_renames = [
+    ('calendar.alarm ', 'calendar_alarm ', 'type', 'alarm_type'),
+]
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    openupgrade.rename_fields(env, _field_renames)
