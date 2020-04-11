4e5c97c69b99135baec00dd02eb4be97ce31d772
Payam Yasaie <payam.yasaie@outlook.com>
2020-04-11 13:48:43 +0430

    [MIG] hr_timesheet

---
+++ addons/hr_timesheet/migrations/13.0.1.0/pre-migration.py
@@ -0,0 +1,15 @@
+# Copyright 2020 Payam Yasaie <https://www.tashilgostar.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    openupgrade.set_xml_ids_noupdate_value(
+        env,
+        "hr_timesheet",
+        [
+            "group_timesheet_manager",
+        ],
+        False,
+    )
