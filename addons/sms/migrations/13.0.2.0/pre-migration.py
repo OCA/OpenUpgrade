3b5e3bc9c1e5ab4a8100ceee85d6499f65dffa78
mreficent <miquel.raich@eficent.com>
2020-04-24 17:04:36 +0200

    [MIG] sms

---
+++ addons/sms/migrations/13.0.2.0/pre-migration.py
@@ -0,0 +1,17 @@
+# Copyright 2020 ForgeFlow <http://www.forgeflow.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from openupgradelib import openupgrade
+
+_model_renames = [
+    ('sms.send_sms', 'sms.composer'),
+]
+
+_table_renames = [
+    ('sms_send_sms', 'sms_composer'),
+]
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    openupgrade.rename_models(env.cr, _model_renames)
+    openupgrade.rename_tables(env.cr, _table_renames)
