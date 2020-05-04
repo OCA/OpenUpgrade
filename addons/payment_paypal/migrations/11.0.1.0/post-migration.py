cad86043df3cdd29591b2e6a3f91a439d84d0ef9
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-05-04 19:21:46 +0200

    [FIX] payment_paypal: Replace old Paypal view template
    If not, you will get:
    
    psycopg2.IntegrityError: null value in column "view_template_id" violates not-null constraint

--- addons/payment_paypal/migrations/11.0.1.0/post-migration.py
+++ addons/payment_paypal/migrations/11.0.1.0/post-migration.py
@@ -1,10 +1,25 @@
 # Copyright 2018 Tecnativa - Vicent Cubells
+# Copyright 2020 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 from openupgradelib import openupgrade
 
+
+def _replace_paypal_view_template_id(env):
+    """As this view is removed being noupdate=1 (and we don't want to keep it
+    as invalid), we need to switch the view for possible records of this
+    provider.
+    """
+    env['payment.acquirer'].search([
+        ('provider', '=', 'paypal'),
+        ('view_template_id', '=', env.ref(
+            'payment_paypal.paypal_acquirer_button').id),
+    ]).write({
+        'view_template_id': env.ref('payment_paypal.paypal_form').id,
+    })
 
 @openupgrade.migrate()
 def migrate(env, version):
     openupgrade.load_data(
         env.cr, 'payment_paypal', 'migrations/11.0.1.0/noupdate_changes.xml',
     )
+    _replace_paypal_view_template_id(env)
