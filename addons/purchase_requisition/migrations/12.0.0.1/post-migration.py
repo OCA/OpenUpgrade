6f521b8f682737940810dafb352ce4efe4b84e98
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-05-22 11:35:21 +0200

    [FIX] purchase_requisition: Switch tender sequence codes
    The code that is put on previous TE sequence is different in this version, so if you
    have copied it for different companies, you will get errors when confirming
    requisitions on them. We switch the code for all of them in batch.

--- addons/purchase_requisition/migrations/12.0.0.1/post-migration.py
+++ addons/purchase_requisition/migrations/12.0.0.1/post-migration.py
@@ -26,10 +26,22 @@ def map_purchase_requisition_state_ongoing(cr):
     )
 
 
+def _change_requisition_sequence_code(env):
+    """The code that is put on previous TE sequence is different in this
+    version, so if you have copied it for different companies, you will get
+    errors when confirming requisitions on them. We switch the code for all
+    of them in batch.
+    """
+    env['ir.sequence'].search([
+        ("code", "=", "purchase.order.requisition")
+    ]).write({"code": "purchase.requisition.purchase.tender"})
+
+
 @openupgrade.migrate()
 def migrate(env, version):
     fill_purchase_requisition_currency_id(env.cr)
     map_purchase_requisition_state_ongoing(env.cr)
+    _change_requisition_sequence_code(env)
     openupgrade.load_data(
         env.cr, 'purchase_requisition',
         'migrations/12.0.0.1/noupdate_changes.xml')
