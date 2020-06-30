dd2f0510da76f5b21270109dc05034dd9a070a4b
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-06-30 16:11:47 +0200

    [REV] website: Revert preservation of website specific views
    This patch was needed for v12 for the BS3 > BS4 conversion of website specific views in
    previous version, but now that there's no need of such conversion, standard code is OK for
    automatically recycling of specific views if there are upstream changes.

--- addons/website/models/ir_ui_view.py
+++ addons/website/models/ir_ui_view.py
@@ -165,14 +165,7 @@ class View(models.Model):
                     view.with_context(website_id=website.id).write({'name': view.name})
 
         specific_views = self.env['ir.ui.view']
-
-        # OpenUpgrade: force a False condition to never enter this code block.
-        # This is needed because the update process could correctly unlink
-        # website-agnostic views, but we don't want the website-specific
-        # equivalents to be removed during the migration.
-        if False and self and self.pool._init:
-        # OpenUpgrade: end of modification
-
+        if self and self.pool._init:
             for view in self.filtered(lambda view: not view.website_id):
                 specific_views += view._get_specific_views()
 
