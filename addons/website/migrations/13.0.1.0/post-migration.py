bd3533988d23c8a54a10b710ef70162e3fe0ebbd
Jairo Llopis <jairo.llopis@tecnativa.com>
2020-10-15 12:06:22 +0100

    [FIX] website: migrate logo
    In v12, websites used the company logo. In v13, websites have their own logo.
    
    Without this patch, websites get migrated without logo.
    
    @Tecnativa TT23136

--- addons/website/migrations/13.0.1.0/post-migration.py
+++ addons/website/migrations/13.0.1.0/post-migration.py
@@ -4,6 +4,17 @@
 from openupgradelib import openupgrade
 
 
+def _fill_website_logo(env):
+    """V13 introduces website.logo, where v12 used res.company.logo."""
+    default_logo = env["website"]._default_logo()
+    websites_with_default_logo = env["website"].search([
+        ('logo', '=', default_logo),
+    ])
+    for website in websites_with_default_logo:
+        website.logo = website.company_id.logo
+
+
 @openupgrade.migrate()
 def migrate(env, version):
+    _fill_website_logo(env)
     openupgrade.load_data(env.cr, 'website', 'migrations/13.0.1.0/noupdate_changes.xml')
