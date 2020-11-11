af0960f4b79d90560cf494b9c83a376eb15908ee
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-11-11 19:37:15 +0100

    [MIG+IMP] project: Finish migration scripts
    * Remove unneeded SQL, as company_id is required on project
    * Update module field definition

--- addons/project/migrations/13.0.1.1/pre-migration.py
+++ addons/project/migrations/13.0.1.1/pre-migration.py
@@ -1,4 +1,5 @@
 # Copyright 2020 Payam Yasaie <https://www.tashilgostar.com>
+# Copyright 2020 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from openupgradelib import openupgrade
 
@@ -7,6 +8,25 @@ _xmlid_renames = [
     ('hr_timesheet.access_account_analytic_account_user', 'project.access_account_analytic_account_user'),
     ('hr_timesheet.access_account_analytic_line_project', 'project.access_account_analytic_line_project'),
 ]
+
+
+def update_module_moved_fields(env):
+    if not openupgrade.is_module_installed(env.cr, "hr_timesheet"):
+        return
+    openupgrade.update_module_moved_fields(
+        env.cr,
+        "account.analytic.account",
+        ["project_count", "project_ids"],
+        "hr_timesheet",
+        "project",
+    )
+    openupgrade.update_module_moved_fields(
+        env.cr,
+        "project.project",
+        ["analytic_account_id"],
+        "hr_timesheet",
+        "project",
+    )
 
 
 def fill_project_task_company_id(cr):
@@ -18,19 +38,12 @@ def fill_project_task_company_id(cr):
         WHERE pp.id = pt.project_id AND pt.company_id IS NULL
         """
     )
-    openupgrade.logged_query(
-        cr, """
-        UPDATE project_task pt
-        SET company_id = ru.company_id
-        FROM res_users ru
-        WHERE pt.project_id IS NULL AND pt.company_id IS NULL AND pt.create_uid = ru.id
-        """
-    )
 
 
 @openupgrade.migrate()
 def migrate(env, version):
     openupgrade.rename_xmlids(env.cr, _xmlid_renames)
+    update_module_moved_fields(env)
     fill_project_task_company_id(env.cr)
     openupgrade.set_xml_ids_noupdate_value(
         env,
