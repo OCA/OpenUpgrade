200f50ac4a4860bd2a0d9675436d379ea720bab5
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-11-13 20:26:58 +0100

    [FIX+IMP] sale: Finish migration scripts
    * Transfer comments from invoices to moves
    * Properly transfer order lines - invoice lines relation
    * Transfer UTM fields to moves
    * Set invoice_target to 0 when not use_invoice
    * Install sale_product_configurator when optional products are used
    * Update some field module definition
    * Pre-create company_id field in utm.campaign
    * Set confirmation and signature dates properly
    * Add group for sales auto lock if system parameter was present

--- addons/sale/migrations/13.0.1.1/pre-migration.py
+++ addons/sale/migrations/13.0.1.1/pre-migration.py
@@ -1,19 +1,26 @@
 # Copyright 2020 Payam Yasaie <https://www.tashilgostar.com>
 # Copyright 2020 ForgeFlow <http://www.forgeflow.com>
+# Copyright 2020 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from openupgradelib import openupgrade
+
+_table_renames = [
+    ("sale_order_line_invoice_rel", "ou_sale_order_line_invoice_rel"),
+]
 
 _field_renames = [
     ('product.attribute', 'product_attribute',
      'type', 'display_type'),
-    ('sale.order', 'sale_order',
-     'confirmation_date', 'signed_on'),
 ]
 
-column_renames = {
+_column_renames = {
     'product_attribute_custom_value': [
         ('attribute_value_id', None),
     ],
+}
+
+_column_copies = {
+    "sale_order": [("date_order", None, None)],
 }
 
 _xmlid_renames = [
@@ -23,8 +30,29 @@ _xmlid_renames = [
 ]
 
 
+def nullify_invoiced_target_when_not_used(env):
+    """If `use_invoices` was not checked in previous version, the invoice target
+    was not evaluated, no matter the value. But now in v13, there's no check,
+    and the target is evaluated if distinct from 0, so we need to nullify this
+    target for not checked teams.
+    """
+    openupgrade.logged_query(
+        env.cr,
+        "UPDATE crm_team SET invoiced_target = 0 WHERE NOT use_invoices",
+    )
+
+
 @openupgrade.migrate()
 def migrate(env, version):
+    openupgrade.rename_tables(env.cr, _table_renames)
     openupgrade.rename_fields(env, _field_renames)
-    openupgrade.rename_columns(env.cr, column_renames)
+    openupgrade.rename_columns(env.cr, _column_renames)
+    openupgrade.copy_columns(env.cr, _column_copies)
     openupgrade.rename_xmlids(env.cr, _xmlid_renames)
+    openupgrade.update_module_moved_fields(
+        env.cr, "sale.order", ["campaign_id", "medium_id", "source_id"], "sale_crm", "sale"
+    )
+    openupgrade.add_fields(
+        env, [("company_id", "utm.campaign", False, "many2one", False, "sale")],
+    )
+    nullify_invoiced_target_when_not_used(env)
