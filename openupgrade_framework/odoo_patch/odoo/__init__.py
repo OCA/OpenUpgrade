4a8134b5b9ee809e23c228b68d8fdb9a63113be3
Stefan Rijnhart <stefan@opener.am>
2020-11-30 00:38:49 +0100

    [DEL] openupgrade_records -> server-tools/14.0

--- openupgrade_framework/odoo_patch/odoo/__init__.py
+++ openupgrade_framework/odoo_patch/odoo/__init__.py
@@ -1,10 +1,5 @@
 from . import modules
 from . import service
-from . import tools
 
 # Nothing todo the function, the function check_security didn't changed
 from . import http
-
-# adapted to V14
-# TODO, OpenUpgrade maintainers : check if it's OK
-from . import models
