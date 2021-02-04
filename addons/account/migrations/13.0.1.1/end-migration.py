6b0924d0b75d023200fcbec251a185c9e76ba800
Jordi Ballester <jordi.ballester@forgeflow.com>
2021-02-04 06:14:20 +0100

    [OU-FIX] account: do not create new accounts if they are going to conflict with existing ones.

--- addons/account/migrations/13.0.1.1/end-migration.py
+++ addons/account/migrations/13.0.1.1/end-migration.py
@@ -40,11 +40,22 @@ def add_default_account_data(env):
                 if code_main > 0 and code_main <= chart.code_digits:
                     code_acc = str(code_acc) + (
                         str("0" * (chart.code_digits - code_main)))
-                tax_template_ref = {}
-                vals = chart._get_account_vals(
-                    company, account_template, code_acc, tax_template_ref)
-                account = chart._create_records_with_xmlid(
-                    "account.account", [(account_template, vals)], company)
+                account = env['account.account'].search([
+                    ('code', '=', code_acc),
+                    ('company_id', '=', company.id),
+                ], limit=1)
+                # If there's already an account with the same code and company don't create it
+                # as it is not allowed by "account_account_code_company_uniq" constraint.
+                if not account:
+                    tax_template_ref = {}
+                    vals = chart._get_account_vals(
+                        company, account_template, code_acc, tax_template_ref)
+                    account = chart._create_records_with_xmlid(
+                        "account.account", [(account_template, vals)], company)
+                else:
+                    # we add xmlid for such existing account
+                    env["ir.model.data"].sudo()._update_xmlids(
+                        [dict(xml_id=xml_id, noupdate=True, record=account)])
             company[company_field] = account.id
 
 
