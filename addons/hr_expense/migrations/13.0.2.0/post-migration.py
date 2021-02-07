ac635eb7a60abb56185f38d01fa80841abd51c06
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2021-02-07 20:20:20 +0100

    [OU-FIX] hr_expense: Finish migration scripts
    - The company of the user is not a reliable source for filling missing
      company in the expenses, as the user may switch the current company,
      and a user may create expenses for other employees. Use employee
      instead.
    - Don't rename ir.rule XML-IDs, but remove existing one and let Odoo to
      create the new ones, as the content is not exactly the same, so this
      better than rename + update the changes.
    - Try to remove product. Although it's present in sale_expense, it's as
      demo data, so for regular DBs, if not used, that product shouldn't be
      present.

--- addons/hr_expense/migrations/13.0.2.0/post-migration.py
+++ addons/hr_expense/migrations/13.0.2.0/post-migration.py
@@ -1,4 +1,5 @@
 # Copyright 2020 ForgeFlow <http://www.forgeflow.com>
+# Copyright 2021 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from openupgradelib import openupgrade
 
@@ -7,9 +8,9 @@ def fill_hr_expense_company_id(env):
     openupgrade.logged_query(
         env.cr, """
         UPDATE hr_expense he
-        SET company_id = ru.company_id
-        FROM res_users ru
-        WHERE ru.id = he.create_uid AND
+        SET company_id = emp.company_id
+        FROM hr_employee emp
+        WHERE emp.id = he.employee_id AND
         he.company_id is NULL""",
     )
 
@@ -18,9 +19,9 @@ def fill_hr_expense_sheet_company_id(env):
     openupgrade.logged_query(
         env.cr, """
         UPDATE hr_expense_sheet hes
-        SET company_id = ru.company_id
-        FROM res_users ru
-        WHERE ru.id = hes.create_uid AND
+        SET company_id = emp.company_id
+        FROM hr_employee emp
+        WHERE emp.id = hes.employee_id AND
         hes.company_id is NULL""",
     )
 
@@ -29,5 +30,12 @@ def fill_hr_expense_sheet_company_id(env):
 def migrate(env, version):
     fill_hr_expense_company_id(env)
     fill_hr_expense_sheet_company_id(env)
+    openupgrade.delete_records_safely_by_xml_id(
+        env, [
+            "hr_expense.ir_rule_hr_expense_user",
+            "hr_expense.ir_rule_hr_expense_sheet_user",
+            "hr_expense.product_product_fixed_cost",
+        ],
+    )
     openupgrade.load_data(
         env.cr, 'hr_expense', 'migrations/13.0.2.0/noupdate_changes.xml')
