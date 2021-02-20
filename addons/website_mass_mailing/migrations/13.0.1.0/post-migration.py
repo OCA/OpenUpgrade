c6d2c8c257a6d15adaec764089a38d767f0f1de9
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2021-02-20 10:49:45 +0100

    [OU-FIX] website_mass_mailing: Move to SQL + handle translations

--- addons/website_mass_mailing/migrations/13.0.1.0/post-migration.py
+++ addons/website_mass_mailing/migrations/13.0.1.0/post-migration.py
@@ -1,24 +1,30 @@
 # Copyright 2020 Payam Yasaie <https://www.tashilgostar.com>
+# Copyright 2021 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from openupgradelib import openupgrade
 
 
 def fill_mailing_list_website_popup_ids(env):
-    popup = env['website.mass_mailing.popup']
-
-    sql = """
-    SELECT id, {popup_content} 
-    FROM mailing_list 
-    WHERE {popup_content} IS NOT NULL
-    """.format(popup_content=openupgrade.get_legacy_name('popup_content'))
-    env.cr.execute(sql)
-    rows = env.cr.fetchall()
-
-    for row in rows:
-        popup.create({
-            'mailing_list_id': row[0],
-            'popup_content': row[1]
-        })
+    openupgrade.logged_query(
+        env.cr, """
+        INSERT INTO website_mass_mailing_popup
+        (create_date, write_date, create_uid, write_uid, mailing_list_id,
+         popup_content)
+        SELECT create_date, write_date, create_uid, write_uid, id, popup_content
+        FROM mailing_list
+        WHERE popup_content IS NOT NULL
+        RETURNING id, mailing_list_id"""
+    )
+    # Handle translations
+    for row in env.cr.fetchall():
+        env.cr.execute(
+            """UPDATE ir_translation
+            SET name='website.mass_mailing.popup,popup_content',
+                res_id = %s
+            WHERE name='mail.mass_mailing.list,popup_content'
+                AND res_id = %s""",
+            (row[0], row[1]),
+        )
 
 
 @openupgrade.migrate()
