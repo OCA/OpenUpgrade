e41d591fe049b6404b2ba52732723100edd6e9e5
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2021-02-21 20:22:56 +0100

    [OU-IMP] website_slides: Finish migration scripts

--- addons/website_slides/migrations/13.0.2.0/pre-migration.py
+++ addons/website_slides/migrations/13.0.2.0/pre-migration.py
@@ -2,9 +2,7 @@
 from openupgradelib import openupgrade
 
 _field_renames = [
-    # slide.slide
-    ('slide.slide', 'slide_slide', 'index_content', 'html_content'),
-    # slide.channel
+    ('slide.channel', 'slide_channel', 'description', 'description_html'),
     ('slide.channel', 'slide_channel', 'total', 'total_slides'),
     ('slide.channel', 'slide_channel', 'nbr_videos', 'nbr_video'),
     ('slide.channel', 'slide_channel', 'nbr_documents', 'nbr_document'),
@@ -14,10 +12,37 @@ _field_renames = [
     ('slide.channel', 'slide_channel', 'access_error_msg', 'enroll_msg'),
 ]
 
+_column_renames = {
+    "slide_slide": [
+        ("category_id", None),
+    ],
+    "slide_channel": [
+        ("visibility", None),
+        ("promote_strategy", None),
+    ],
+    # m2m table before renaming it
+    "rel_channel_groups": [
+        ("channel_id", "slide_channel_id"),
+        ("group_id", "res_groups_id"),
+    ]
+}
+
+_column_copies = {
+    "slide_slide": [
+        ("likes", None, None),
+        ("dislikes", None, None),
+        ("slide_views", None, None),
+    ],
+}
+
 
 @openupgrade.migrate()
 def migrate(env, version):
+    openupgrade.rename_columns(env.cr, _column_renames)
+    openupgrade.copy_columns(env.cr, _column_copies)
     openupgrade.rename_fields(env, _field_renames)
+    openupgrade.logged_query(env.cr, "ALTER TABLE slide_slide ADD COLUMN category_id int4")
+    openupgrade.rename_tables(env.cr, [("rel_channel_groups", "res_groups_slide_channel_rel")])
     openupgrade.set_xml_ids_noupdate_value(
         env,
         "website_slides",
@@ -27,3 +52,4 @@ def migrate(env, version):
         ],
         False,
     )
+    openupgrade.add_fields(env, [("user_id", "slide.channel", "slide_channel", "many2one", False, "website_slides")])
