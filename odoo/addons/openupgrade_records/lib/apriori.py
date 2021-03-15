9a597adfb7de3c75be0ac2ed0323d157ffcb7314
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2021-03-15 08:53:23 +0100

    [OU-ADD] website_form_builder: Merged on website_form

--- odoo/addons/openupgrade_records/lib/apriori.py
+++ odoo/addons/openupgrade_records/lib/apriori.py
@@ -70,6 +70,7 @@ merged_modules = {
     'web_widget_many2many_tags_multi_selection': 'web',
     # OCA/website
     'website_canonical_url': 'website',
+    'website_form_builder': 'website_form',
     'website_logo': 'website',
     'website_snippet_anchor': 'website',
 }
