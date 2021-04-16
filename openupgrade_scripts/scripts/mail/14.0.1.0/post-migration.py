60cbc87d57671b48bb024d573c98f10518f91232
RaulOForgeFlow <raul.ortiz@forgeflow.com>
2021-04-16 12:18:43 +0200

    [MIG] mail: Migration to 14.0

--- openupgrade_scripts/scripts/mail/14.0.1.0/post-migration.py
+++ openupgrade_scripts/scripts/mail/14.0.1.0/post-migration.py
@@ -1,23 +1,32 @@
+# Copyright 2021 ForgeFlow S.L.  <https://www.forgeflow.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from openupgradelib import openupgrade
+
 
 def fill_mail_tracking_value_field(env):
     openupgrade.logged_query(
-        env.cr, """
+        env.cr,
+        """
         UPDATE mail_tracking_value mtv
         SET field = imf.id
         FROM ir_model_fields imf
-        WHERE imf.name = mtv.{}
-        AND imf.model = 'mail.tracking.value'
-        """.format(openupgrade.get_legacy_name("field"))
+        JOIN mail_message mm ON mtv.mail_message_id = mm.id
+        WHERE imf.name = mtv.{} AND imf.model = mm.model
+        """.format(
+            openupgrade.get_legacy_name("field")
+        ),
     )
+
 
 @openupgrade.migrate()
 def migrate(env, version):
     fill_mail_tracking_value_field(env)
     openupgrade.load_data(
-        env.cr, "mail", "openupgrade_scripts/scripts/mail/14.0.1.0/noupdate_changes.xml")
+        env.cr, "mail", "openupgrade_scripts/scripts/mail/14.0.1.0/noupdate_changes.xml"
+    )
     openupgrade.delete_records_safely_by_xml_id(
-        env, [
+        env,
+        [
             "mail.mail_followers_read_write_own",
-        ]
+        ],
     )
