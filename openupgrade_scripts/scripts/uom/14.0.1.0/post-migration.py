5f1748133df2d23c9c96257a2a5a9e5b63c8b790
RaulOForgeFlow <raul.ortiz@forgeflow.com>
2021-04-19 11:59:26 +0200

    [14.0][MIG] uom: migration to v14

---
+++ openupgrade_scripts/scripts/uom/14.0.1.0/post-migration.py
@@ -0,0 +1,8 @@
+# Copyright 2021 ForgeFlow S.L.  <https://www.forgeflow.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    openupgrade.load_data(env.cr, "uom", "14.0.1.0/noupdate_changes.xml")
