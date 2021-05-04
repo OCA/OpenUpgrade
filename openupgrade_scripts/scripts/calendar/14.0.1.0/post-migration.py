1bedaa201d016c37571e97e112130d0f29223421
mreficent <miquel.raich@eficent.com>
2021-05-04 18:32:19 +0200

    [OU-IMP] calendar: fill calendar_recurrence table

--- openupgrade_scripts/scripts/calendar/14.0.1.0/post-migration.py
+++ openupgrade_scripts/scripts/calendar/14.0.1.0/post-migration.py
@@ -24,8 +24,44 @@ def map_calendar_event_byday(env):
     )
 
 
+def fill_calendar_recurrence_table(env):
+    openupgrade.logged_query(
+        env.cr,
+        """
+        WITH recur AS (
+            INSERT INTO calendar_recurrence (base_event_id,
+                event_tz,rrule,rrule_type,end_type,interval,count,
+                mo,tu,we,th,fr,sa,su,month_by,day,weekday,byday,until,
+                create_uid,create_date,write_uid,write_date)
+            SELECT id,event_tz,rrule,rrule_type,end_type,interval,count,
+                mo,tu,we,th,fr,sa,su,month_by,day,week_list,byday,final_date,
+                create_uid,create_date,write_uid,write_date)
+            FROM calendar_event
+            WHERE recurrency AND recurrence_id IS NULL
+                AND (recurrent_id IS NULL OR recurrent_id = '')
+            RETURNING id,base_event_id
+        )
+        UPDATE calendar_event ce
+        SET recurrence_id = recur.id
+        FROM recur
+        WHERE recur.base_event_id = ce.id
+        """,
+    )
+    openupgrade.logged_query(
+        env.cr,
+        """
+        UPDATE calendar_event ce
+        SET recurrence_id = ce2.recurrence_id
+        FROM calendar_event ce2
+        WHERE ce.recurrency AND ce.recurrence_id IS NULL
+            AND ce.recurrent_id = ce2.id || ''
+        """,
+    )
+
+
 @openupgrade.migrate()
 def migrate(env, version):
     update_follow_recurrence_field(env)
     map_calendar_event_byday(env)
+    fill_calendar_recurrence_table(env)
     openupgrade.load_data(env.cr, "calendar", "14.0.1.0/noupdate_changes.xml")
