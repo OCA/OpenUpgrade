3f6c8cd98119c31b62937d84ef15f4678b5d03b6
Holger Brunn <mail@hunki-enterprises.com>
2021-05-07 19:02:55 +0200

    [IMP] recompute invoice_status for po's with products invoiced by
    delivered quantities

--- addons/purchase/migrations/10.0.1.2/post-migration.py
+++ addons/purchase/migrations/10.0.1.2/post-migration.py
@@ -5,8 +5,16 @@
 from openupgradelib import openupgrade
 
 
-@openupgrade.migrate(use_env=False)
-def migrate(cr, version):
+@openupgrade.migrate(use_env=True)
+def migrate(env, version):
     openupgrade.load_data(
-        cr, 'purchase', 'migrations/10.0.1.2/noupdate_changes.xml',
+        env.cr, 'purchase', 'migrations/10.0.1.2/noupdate_changes.xml',
     )
+    # v10 added correcly handling the invoice state of products invoiced
+    # by delivered quantity
+    records = env['purchase.order'].search([
+        ('order_line.product_id.invoice_policy', '=', 'delivery'),
+        ('invoice_status', '=', 'to invoice'),
+    ])
+    records._recompute_todo(env['purchase.order']._fields['invoice_status'])
+    records.recompute()
