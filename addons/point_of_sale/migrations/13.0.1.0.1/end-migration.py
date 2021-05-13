8d5fb92c76271edb864412ac05648564055f06e3
mreficent <miquel.raich@eficent.com>
2021-05-13 16:19:17 +0200

    [IMP] point_of_sale: let odoo create the pos methods

--- addons/point_of_sale/migrations/13.0.1.0.1/end-migration.py
+++ addons/point_of_sale/migrations/13.0.1.0.1/end-migration.py
@@ -5,31 +5,7 @@ from openupgradelib import openupgrade
 
 
 def create_pos_payment_methods(env):
-    env.cr.execute("""
-        SELECT DISTINCT journal_id
-        FROM pos_config_journal_rel
-    """)
-    journal_ids = [aj[0] for aj in env.cr.fetchall()]
-    if journal_ids:
-        vals_list = []
-        for journal in env['account.journal'].browse(journal_ids):
-            vals = {
-                'name': 'Cash',
-                'cash_journal_id': journal.id,
-                'is_cash_count': True,
-                'company_id': journal.company_id.id,
-                'receivable_account_id': journal.company_id.account_default_pos_receivable_account_id.id,
-            }
-            vals_list += [vals]
-        env['pos.payment.method'].create(vals_list)
-    openupgrade.logged_query(
-        env.cr, """
-        INSERT INTO pos_config_pos_payment_method_rel
-            (pos_config_id, pos_payment_method_id)
-        SELECT pos_config_id, ppm.id
-        FROM pos_config_journal_rel pcjr
-        JOIN account_journal aj ON pcjr.journal_id = aj.id
-        JOIN pos_payment_method ppm ON ppm.cash_journal_id = aj.id""")
+    env["pos.config"].post_install_pos_localisation()
 
 
 def create_pos_payments(env):
