195272bfbb648b8a770eaf2db92dfcb33826803b
mreficent <miquel.raich@eficent.com>
2021-07-06 16:08:47 +0200

    [MIG] product_expiry (continuation)

--- openupgrade_scripts/scripts/product_expiry/14.0.1.0/pre-migration.py
+++ openupgrade_scripts/scripts/product_expiry/14.0.1.0/pre-migration.py
@@ -1,9 +1,10 @@
 # Copyright (C) 2021 Open Source Integrators
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 from openupgradelib import openupgrade
+
 _field_renames = [
-    ('product.template', 'product_template', 'life_time', 'expiration_time'),
-    ('stock.production.lot', 'stock_production_lot', 'life_date', 'expiration_date'),
+    ("product.template", "product_template", "life_time", "expiration_time"),
+    ("stock.production.lot", "stock_production_lot", "life_date", "expiration_date"),
 ]
 
 
