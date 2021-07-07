54a85ba29a64fc697d1224a901da2dc1bc19ce49
Stefan Rijnhart <stefan@opener.am>
2021-07-07 22:00:50 +0200

    [FIX] Don't break on missing model in selection _process_ondelete

--- openupgrade_framework/odoo_patch/odoo/addons/base/models/ir_model.py
+++ openupgrade_framework/odoo_patch/odoo/addons/base/models/ir_model.py
@@ -10,6 +10,7 @@ from odoo.addons.base.models.ir_model import (
     IrModelData,
     IrModelFields,
     IrModelRelation,
+    IrModelSelection,
 )
 
 
@@ -84,3 +85,19 @@ def _module_data_uninstall(self):
 
 
 IrModelRelation._module_data_uninstall = _module_data_uninstall
+
+
+def _process_ondelete(self):
+    """Don't break on missing models when deleting their selection fields"""
+    to_process = self.browse([])
+    for selection in self:
+        try:
+            self.env[selection.field_id.model]  # pylint: disable=pointless-statement
+            to_process += selection
+        except KeyError:
+            continue
+    return IrModelSelection._process_ondelete._original_method(to_process)
+
+
+_process_ondelete._original_method = IrModelSelection._process_ondelete
+IrModelSelection._process_ondelete = _process_ondelete
