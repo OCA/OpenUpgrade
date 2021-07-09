6a239c5ed87e578842e8ee4b5d9b5602b3bf516a
Chankya Soni <csoni@opensourceintegrators.com>
2021-07-09 19:01:13 +0530

    [14.0][MIG] hr_timesheet

---
+++ openupgrade_scripts/scripts/hr_timesheet/14.0.1.0/pre-migration.py
@@ -0,0 +1,17 @@
+# Copyright (C) 2021 Open Source Integrators
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    openupgrade.set_xml_ids_noupdate_value(
+        env,
+        "hr_timesheet",
+        [
+            "timesheet_line_rule_user",
+            "group_timesheet_manager",
+            "group_hr_timesheet_approver",
+        ],
+        True,
+    )
