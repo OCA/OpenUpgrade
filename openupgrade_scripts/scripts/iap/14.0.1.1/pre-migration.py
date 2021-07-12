bac33a341f7fdc9931b2e8faacca7bdff146ec06
mreficent <miquel.raich@eficent.com>
2021-07-12 11:21:56 +0200

    [MIG] iap

---
+++ openupgrade_scripts/scripts/iap/14.0.1.1/pre-migration.py
@@ -0,0 +1,15 @@
+# Copyright 2021 ForgeFlow S.L.  <https://www.forgeflow.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    openupgrade.set_xml_ids_noupdate_value(
+        env,
+        "iap",
+        [
+            "user_iap_account",
+        ],
+        True,
+    )
