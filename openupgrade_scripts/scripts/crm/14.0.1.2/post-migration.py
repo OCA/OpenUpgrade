6b99285279bb18e0ef390409fa4a5b1de69aa21c
mreficent <miquel.raich@eficent.com>
2021-07-14 14:55:08 +0200

    [MIG] crm

---
+++ openupgrade_scripts/scripts/crm/14.0.1.2/post-migration.py
@@ -0,0 +1,11 @@
+# Copyright 2021 ForgeFlow S.L.  <https://www.forgeflow.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    openupgrade.load_data(env.cr, "crm", "14.0.1.2/noupdate_changes.xml")
+    openupgrade.delete_records_safely_by_xml_id(
+        env, [("crm.crm_pls_rebuild_threshold_param")]
+    )
---
+++ openupgrade_scripts/scripts/crm/14.0.1.2/pre-migration.py
@@ -0,0 +1,15 @@
+# Copyright 2021 ForgeFlow S.L.  <https://www.forgeflow.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    openupgrade.rename_fields(
+        env,
+        [
+            ("crm.lead", "crm_lead", "expected_revenue", "prorated_revenue"),
+            ("crm.lead", "crm_lead", "planned_revenue", "expected_revenue"),
+        ],
+    )
+    openupgrade.rename_tables(env.cr, [("crm_lead_tag_rel", "crm_tag_rel")])
