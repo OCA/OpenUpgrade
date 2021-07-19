be49b05a12da487db58b01db30399b036f0401af
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2021-07-19 20:14:55 +0200

    [FIX+IMP] crm + sales_team: Finish migration scripts

--- openupgrade_scripts/scripts/crm/14.0.1.2/pre-migration.py
+++ openupgrade_scripts/scripts/crm/14.0.1.2/pre-migration.py
@@ -1,10 +1,17 @@
 # Copyright 2021 ForgeFlow S.L.  <https://www.forgeflow.com>
+# Copyright 2021 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from openupgradelib import openupgrade
 
 
 @openupgrade.migrate()
 def migrate(env, version):
+    openupgrade.logged_query(
+        env.cr, "ALTER TABLE crm_lead ADD recurring_revenue_monthly NUMERIC"
+    )
+    openupgrade.logged_query(
+        env.cr, "ALTER TABLE crm_lead ADD recurring_revenue_monthly_prorated NUMERIC"
+    )
     openupgrade.rename_fields(
         env,
         [
--- openupgrade_scripts/scripts/sales_team/14.0.1.0/pre-migration.py
+++ openupgrade_scripts/scripts/sales_team/14.0.1.0/pre-migration.py
@@ -1,4 +1,5 @@
 # Copyright 2021 ForgeFlow S.L.  <https://www.forgeflow.com>
+# Copyright 2021 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from openupgradelib import openupgrade
 
@@ -20,4 +21,15 @@ def migrate(env, version):
         )
     openupgrade.set_xml_ids_noupdate_value(
         env, "sales_team", ["crm_rule_all_salesteam", "sale_team_comp_rule"], True
+    )
+    openupgrade.rename_xmlids(
+        env.cr,
+        [
+            ("crm.crm_lead_tag_action", "sales_team.sales_team_crm_tag_action"),
+            ("crm.crm_lead_tag_form", "sales_team.sales_team_crm_tag_view_form"),
+            ("crm.crm_lead_tag_tree", "sales_team.sales_team_crm_tag_view_tree"),
+            ("crm.access_crm_lead_tag", "sales_team.access_crm_tag"),
+            ("crm.access_crm_lead_tag_manager", "sales_team.access_crm_tag_manager"),
+            ("crm.access_crm_lead_tag_salesman", "sales_team.access_crm_tag_salesman"),
+        ],
     )
