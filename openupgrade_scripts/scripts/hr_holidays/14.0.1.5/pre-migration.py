d2341ca135c424e5b338f57bb4870dff703954ee
mreficent <miquel.raich@eficent.com>
2021-07-22 20:09:33 +0200

    [MIG] hr_holidays (continue)

--- openupgrade_scripts/scripts/hr_holidays/14.0.1.5/pre-migration.py
+++ openupgrade_scripts/scripts/hr_holidays/14.0.1.5/pre-migration.py
@@ -1,23 +1,21 @@
 # Copyright (C) 2021 Open Source Integrators
+# Copyright 2021 ForgeFlow S.L.  <https://www.forgeflow.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 from openupgradelib import openupgrade
+
+_column_copies = {
+    "hr_leave_type": [("validation_type", "allocation_validation_type", "varchar")]
+}
 
 _field_renames = [
     ("hr.leave.type", "hr_leave_type", "validation_type", "leave_validation_type"),
 ]
 
-xmlid_renames = [
-    (
-        "hr_holidays_calendar.hr_leave_report_calendar_rule_multi_company",
-        "hr_holidays.hr_leave_report_calendar_rule_multi_company",
-    ),
-]
-
 
 @openupgrade.migrate()
 def migrate(env, version):
+    openupgrade.copy_columns(env.cr, _column_copies)
     openupgrade.rename_fields(env, _field_renames)
-    openupgrade.rename_xmlids(env.cr, xmlid_renames)
     openupgrade.set_xml_ids_noupdate_value(
         env,
         "hr_holidays",
@@ -26,8 +24,10 @@ def migrate(env, version):
             "hr_leave_allocation_rule_employee",
             "hr_leave_allocation_rule_employee_update",
             "hr_leave_allocation_rule_manager",
+            "hr_leave_allocation_rule_multicompany",
             "hr_leave_allocation_rule_officer_read",
             "hr_leave_allocation_rule_officer_update",
+            "hr_leave_report_calendar_rule_multi_company",
             "hr_leave_rule_employee",
             "hr_leave_rule_employee_unlink",
             "hr_leave_rule_employee_update",
