231a533cb5039ad28c382fe4739d94df92673c52
Sylvain LE GAL <sylvain.legal@grap.coop>
2021-08-04 15:35:01 +0200

    [REF] Add maintainers

--- openupgrade_framework/__manifest__.py
+++ openupgrade_framework/__manifest__.py
@@ -5,6 +5,7 @@
     "summary": """Module to integrate in the server_wide_modules
     option to make upgrades between two major revisions.""",
     "author": "Odoo Community Association (OCA)," " Therp BV, Opener B.V., GRAP",
+    "maintainers": ["legalsylvain", "StefanRijnhart"],
     "website": "https://github.com/OCA/openupgrade",
     "category": "Migration",
     "version": "14.0.1.0.0",
