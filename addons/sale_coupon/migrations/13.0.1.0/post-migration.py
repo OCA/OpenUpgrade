7573df50e592270c83a1f9f777118038a419487c
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2021-08-31 13:39:44 +0200

    [OU-ADD] sale_coupon: Migration scripts
    When coming from enterprise v12 module.
    
    TT27871

---
+++ addons/sale_coupon/migrations/13.0.1.0/post-migration.py
@@ -0,0 +1,33 @@
+# Copyright 2021 Tecnativa - Pedro M. Baeza
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from openupgradelib import openupgrade
+
+
+def _adjust_reward_specific_products(env):
+    """Now the value is `specific_products` instead of `specific_product`, and
+    the many2one is now a many2many.
+    """
+    openupgrade.map_values(
+        env.cr,
+        openupgrade.get_legacy_name("discount_apply_on"),
+        "discount_apply_on",
+        [("specific_product", "specific_product")],
+        table="sale_coupon_reward",
+    )
+    openupgrade.m2o_to_x2m(
+        env.cr,
+        "sale.coupon.reward",
+        "sale_coupon_reward",
+        "discount_specific_product_ids",
+        "discount_specific_product_id",
+    )
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    """Scripts coming from enterprise v12."""
+    openupgrade.load_data(
+        env.cr, 'sale_coupon', 'migrations/13.0.1.0/noupdate_changes.xml',
+    )
+    openupgrade.delete_record_translations(env.cr, "sale_coupon", ["mail_template_sale_coupon"])
+    _adjust_reward_specific_products(env)
