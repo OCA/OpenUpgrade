65b5e149a6ddf8823bda88eb0e44694d8ebe22c8
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2021-09-29 18:57:04 +0200

    [OU-FIX+IMP] stock_dropshipping: Finish migration scripts
    - Don't change current preferences on picking type code/sequence prefix.
    - Keep curent record for first company and duplicate it for the rest.
    - Save mapping of records for later references.
    - Use smart foreign key reference replacing for extra created records.
    
    TT28115

--- addons/stock_dropshipping/migrations/13.0.1.0/post-migration.py
+++ addons/stock_dropshipping/migrations/13.0.1.0/post-migration.py
@@ -1,135 +1,137 @@
 # Copyright 2020 ForgeFlow <http://www.forgeflow.com>
+# Copyright 2021 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from openupgradelib import openupgrade
-
-
-def update_stock_picking_type_sequence_code_dropship(env):
-    openupgrade.logged_query(
-        env.cr, """
-        UPDATE stock_picking_type
-        SET sequence_code = 'DS'
-        WHERE code = 'incoming' AND name LIKE '%Dropship%'
-        """
-    )
+from openupgradelib import openupgrade_merge_records
 
 
 def run_dropshipping_functions(env):
+    companies = env["res.company"].with_context(active_test=False).search([], order="id")
+    mapping = {company.id: {} for company in companies}
+    # Duplicate/create/adjust sequences
     sequence = env.ref('stock_dropshipping.seq_picking_type_dropship', False)
-    picking_type = env.ref('stock_dropshipping.picking_type_dropship', False)
-    stock_rule = env.ref('stock_dropshipping.stock_rule_drop_shipping', False)
     if sequence and sequence.exists():
-        if not picking_type or not picking_type.exists():
-            picking_type = env['stock.picking.type'].search(
-                [('sequence_id', '=', sequence.id)], limit=1)
-        if not picking_type:
-            sequence.company_id = False
-        else:
-            company = picking_type.company_id
-            sequence.update({
-                'name': 'Dropship (%s)' % company.name,
-                'code': 'stock.dropshipping',
-                'company_id': company.id,
-                'prefix': 'DS/',
-            })
-    if picking_type and picking_type.exists():
-        picking_type.update({
-            'sequence_code': 'DS',
-        })
-    if stock_rule and stock_rule.exists():
-        supplier_location = env.ref('stock.stock_location_suppliers', False)
-        customer_location = env.ref('stock.stock_location_customers', False)
-        if supplier_location and customer_location:
-            stock_rule.update({
-                'name': '%s → %s' % (supplier_location.name, customer_location.name),
-            })
-
-    # force execute this functions (they are noupdate=1 in xml data)
+        for i, company in enumerate(companies):
+            vals = {
+                "code": "stock.dropshipping",
+                "name": "Dropship (%s)" % company.name,
+                "company_id": company.id,
+            }
+            if i == 0:
+                sequence.write(vals)
+                new_sequence = sequence
+            else:
+                new_sequence = sequence.copy(vals)
+            mapping[company.id]["sequence"] = new_sequence
     env['res.company'].create_missing_dropship_sequence()
+    for company in companies:
+        # Look for missing mapped sequences
+        if not mapping[company.id].get("sequence"):
+            sequence = env['ir.sequence'].search([
+                ('code', '=', 'stock.dropshipping'),
+                ('company_id', '=', company.id),
+            ])
+            mapping[company.id]["sequence"] = sequence
+    # Duplicate/create/adjust picking types
+    picking_type = env.ref('stock_dropshipping.picking_type_dropship', False)
+    if picking_type and picking_type.exists():
+        for i, company in enumerate(companies):
+            vals = {
+                "sequence_id": mapping[company.id]["sequence"].id,
+                "company_id": company.id,
+            }
+            if picking_type.default_location_dest_id.usage != "customer":
+                vals["default_location_dest_id"] = env.ref('stock.stock_location_customers').id
+            if picking_type.default_location_src_id.usage != "supplier":
+                vals["default_location_src_id"] = env.ref('stock.stock_location_suppliers').id
+            if i == 0:
+                picking_type.write(vals)
+                new_picking_type = picking_type
+            else:
+                new_picking_type = picking_type.copy(vals)
+            mapping[company.id]["picking_type"] = new_picking_type
+    for company in companies:
+        # Look for missing mapped picking types
+        if not mapping[company.id].get("picking_type"):
+            dropship_picking_type = env['stock.picking.type'].search([
+                ('company_id', '=', company.id),
+                ('default_location_src_id.usage', '=', 'supplier'),
+                ('default_location_dest_id.usage', '=', 'customer'),
+            ], limit=1, order='sequence')
+            mapping[company.id]["picking_type"] = dropship_picking_type
     env['res.company'].create_missing_dropship_picking_type()
+    # Duplicate/create/adjust stock rules
+    stock_rule = env.ref('stock_dropshipping.stock_rule_drop_shipping', False)
+    dropship_route = env.ref('stock_dropshipping.route_drop_shipping')
+    if stock_rule and stock_rule.exists():
+        for i, company in enumerate(companies):
+            supplier_location = stock_rule.location_src_id
+            customer_location = stock_rule.location_id
+            if supplier_location.usage != "supplier":
+                supplier_location = env.ref('stock.stock_location_suppliers').id
+            if customer_location.usage != "customer":
+                customer_location = env.ref('stock.stock_location_customers').id
+            dropship_picking_type = mapping[company.id]["picking_type"]
+            vals = ({
+                "route_id": dropship_route.id,
+                "picking_type_id": dropship_picking_type.id,
+                "company_id": company.id,
+                "location_src_id": supplier_location.id,
+                "location_id": customer_location.id,
+                "name": "%s → %s" % (supplier_location.name, customer_location.name),
+            })
+            if i == 0:
+                stock_rule.write(vals)
+                new_stock_rule = stock_rule
+            else:
+                new_stock_rule = stock_rule.copy(vals)
+            mapping[company.id]["stock_rule"] = new_stock_rule
     env['res.company'].create_missing_dropship_rule()
-
+    for company in companies:
+        # Look for missing mapped stock rules
+        if not mapping[company.id].get("stock_rule"):
+            stock_rule = env["stock.rule"].search([
+                ("route_id", "=", dropship_route.id),
+                ("company_id", "=", company.id),
+            ])
+            mapping[company.id]["stock_rule"] = stock_rule
+    # Remove old XML-IDs
     env["ir.model.data"].search([
         ("module", "=", "stock_dropshipping"),
         ("name", "in", ["seq_picking_type_dropship",
                         "picking_type_dropship", "stock_rule_drop_shipping"]),
     ]).unlink()
-
     # now, we redirect referenced records to new records if needed company-wise
-
-    # xml_id = stock_dropshipping.seq_picking_type_dropship
-    model = "ir.sequence"
-    conditions = {
-        'seq_picking_type_dropship': "WHERE xml_tab2.code = 'stock.dropshipping'",
-    }
-    affected_models = {
-        'stock.picking.type': ['sequence_id'],
-    }
-    redirect_affected_models(env, model, affected_models, conditions)
-
-    # xml_id = stock_dropshipping.picking_type_dropship
-    model = "stock.picking.type"
-    conditions = {
-        'picking_type_dropship': "WHERE xml_tab2.sequence_code = 'DS'",
-    }
-    affected_models = {
-        'stock.picking.type': ['return_picking_type_id'],
-        'stock.picking': ['picking_type_id'],
-        'stock.rule': ['picking_type_id'],
-        'stock.warehouse': ['pick_type_id', 'pack_type_id',
-                            'out_type_id', 'in_type_id', 'int_type_id'],
-        'purchase.order': ['picking_type_id'],
-    }
-    redirect_affected_models(env, model, affected_models, conditions)
-
-    # xml_id = stock_dropshipping.stock_rule_drop_shipping
-    model = "stock.rule"
-    conditions = {
-        'stock_rule_drop_shipping': "JOIN stock_picking_type spt ON xml_tab2.picking_type_id = spt.id "
-                                    "WHERE spt.sequence_code = 'DS'",
-    }
-    affected_models = {
-        'stock.move': ['rule_id'],
-        'stock.warehouse': ['mto_pull_id'],
-    }
-    redirect_affected_models(env, model, affected_models, conditions)
-
-
-def redirect_affected_models(env, xmlid_model, affected_models, conditions):
-    xmlid_table = env[xmlid_model]._table
-    for model, fields in affected_models.items():
-        table = env[model]._table
-        for field in fields:
-            for xmlid_name, condition in conditions.items():
-                openupgrade.logged_query(
-                    env.cr, """
-        UPDATE {table} tab
-        SET {field} = (
-            SELECT xml_tab2.id
-            FROM {xmlid_table} xml_tab2
-            LEFT JOIN ir_model_data imd2 ON (
-                imd2.module = 'stock_dropshipping' AND
-                imd2.model = '{xmlid_model}' AND imd2.res_id = xml_tab2.id)
-            LEFT JOIN res_users ru2 ON ru2.id = xml_tab2.create_uid
-            {condition}
-                AND imd2.name IS NULL AND
-                COALESCE(xml_tab2.company_id, ru2.company_id) =
-                    COALESCE(tab.company_id, ru.company_id)
-            LIMIT 1
-            )
-        FROM {xmlid_table} xml_tab
-        JOIN ir_model_data imd ON (imd.module = 'stock_dropshipping' AND
-            imd.model = '{xmlid_model}' AND imd.res_id = xml_tab.id)
-        LEFT JOIN res_users ru ON xml_tab.create_uid = ru.id
-        WHERE tab.{field} = xml_tab.id AND
-            imd.name = '{xmlid_name}' AND xml_tab.company_id != tab.company_id
-                    """.format(
-                        table=table, field=field,
-                        xmlid_table=xmlid_table, xmlid_model=xmlid_model,
-                        xmlid_name=xmlid_name, condition=condition,
-                    ))
+    for company in companies[1:]:
+        openupgrade_merge_records._change_foreign_key_refs(
+            env,
+            "ir.sequence",
+            sequence.ids,
+            mapping[company.id]["sequence"].id,
+            [],
+            "ir_sequence",
+            extra_where=" AND company_id = %s" % company.id,
+        )
+        openupgrade_merge_records._change_foreign_key_refs(
+            env,
+            "stock.picking.type",
+            picking_type.ids,
+            mapping[company.id]["picking_type"].id,
+            [],
+            "stock_picking_type",
+            extra_where=" AND company_id = %s" % company.id,
+        )
+        openupgrade_merge_records._change_foreign_key_refs(
+            env,
+            "stock.rule",
+            stock_rule.ids,
+            mapping[company.id]["stock_rule"].id,
+            [],
+            "stock_rule",
+            extra_where=" AND company_id = %s" % company.id,
+        )
 
 
 @openupgrade.migrate()
 def migrate(env, version):
-    update_stock_picking_type_sequence_code_dropship(env)
     run_dropshipping_functions(env)
