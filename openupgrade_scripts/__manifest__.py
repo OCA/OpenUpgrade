12a9275557306cbc92d0fe3f24143bb27afbc962
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2021-10-22 08:39:37 +0200

    Update __manifest__.py

--- openupgrade_scripts/__manifest__.py
+++ openupgrade_scripts/__manifest__.py
@@ -4,7 +4,7 @@
     "name": "Openupgrade Scripts",
     "summary": """Module that contains all the migrations analysis
         and scripts for migrate Odoo SA modules.""",
-    "author": "Odoo Community Association (OCA)"
+    "author": "Odoo Community Association (OCA)",
     "maintainers": ["MiquelRForgeFlow", "StefanRijnhart", "pedrobaeza"],
     "website": "https://github.com/OCA/openupgrade",
     "category": "Migration",
