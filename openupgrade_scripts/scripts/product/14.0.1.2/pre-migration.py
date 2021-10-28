262cc23af75e0e10169a7b4b99da979595af1277
Miquel Raïch <miquel.raich@forgeflow.com>
2021-10-28 12:12:33 +0200

    [FIX] product: assure m2m table has correct values

--- openupgrade_scripts/scripts/product/14.0.1.2/pre-migration.py
+++ openupgrade_scripts/scripts/product/14.0.1.2/pre-migration.py
@@ -26,7 +26,26 @@ def copy_date_end_date_start_columns(env):
     )
 
 
+def assure_m2m_table_correct_values(env):
+    # if you come migrating from older versions, you may have problems with this table
+    openupgrade.logged_query(
+        env.cr,
+        """
+        DELETE FROM product_attribute_value_product_template_attribute_line_rel rel
+        USING product_attribute_value_product_template_attribute_line_rel rel0
+        LEFT JOIN product_attribute_value pav
+            ON rel0.product_attribute_value_id = pav.id
+        LEFT JOIN product_template_attribute_line ptal
+            ON rel0.product_template_attribute_line_id = ptal.id
+        WHERE (rel.product_attribute_value_id = rel0.product_attribute_value_id
+            AND rel.product_template_attribute_line_id =
+                rel0.product_template_attribute_line_id)
+            AND (pav.id IS NULL OR ptal.id IS NULL)""",
+    )
+
+
 @openupgrade.migrate()
 def migrate(env, version):
     fill_empty_discount_policy(env)
     copy_date_end_date_start_columns(env)
+    assure_m2m_table_correct_values(env)
