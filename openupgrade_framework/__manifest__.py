789b40fff29c6701ec508b8ef2e70140079a9a17
Sylvain LE GAL <sylvain.legal@grap.coop>
2021-11-15 11:52:32 +0100

    [MIG][15.0] openupgrade_framework

--- openupgrade_framework/__manifest__.py
+++ openupgrade_framework/__manifest__.py
@@ -8,7 +8,7 @@
     "maintainers": ["legalsylvain", "StefanRijnhart"],
     "website": "https://github.com/OCA/openupgrade",
     "category": "Migration",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "license": "AGPL-3",
     "depends": ["base"],
     "images": ["static/description/banner.jpg"],
