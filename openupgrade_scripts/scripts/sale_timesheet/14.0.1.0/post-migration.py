5f81e5c239025e78913f0002772fa24162aaae83
Miquel Raïch <miquel.raich@forgeflow.com>
2021-12-02 11:40:12 +0100

    [MIG] sale_timesheet (continuation)

--- openupgrade_scripts/scripts/sale_timesheet/14.0.1.0/post-migration.py
+++ openupgrade_scripts/scripts/sale_timesheet/14.0.1.0/post-migration.py
@@ -6,29 +6,20 @@ from openupgradelib import openupgrade
 def fill_bill_type(env):
     openupgrade.logged_query(
         env.cr,
-        """ UPDATE
-                        project_project
-                    SET
-                        bill_type = 'customer_project'
-                    WHERE
-                        sale_order_id is not null
-                """,
+        """
+        UPDATE project_project
+        SET bill_type = 'customer_project'
+        WHERE sale_order_id IS NOT NULL""",
     )
 
 
 def fill_allow_billable(env):
-    timesheet_product_id = env.ref("sale_timesheet.time_product", False)
     openupgrade.logged_query(
         env.cr,
-        """ UPDATE
-                        project_project
-                    SET
-                        allow_billable = 't',
-                        timesheet_product_id = %s
-                    WHERE
-                        pricing_type is not null
-                """,
-        (timesheet_product_id.id,),
+        """
+        UPDATE project_project
+        SET allow_billable = TRUE
+        WHERE pricing_type IS NOT NULL""",
     )
 
 
@@ -36,5 +27,4 @@ def fill_allow_billable(env):
 def migrate(env, version):
     fill_bill_type(env)
     fill_allow_billable(env)
-    # Load noupdate changes
     openupgrade.load_data(env.cr, "sale_timesheet", "14.0.1.0/noupdate_changes.xml")
