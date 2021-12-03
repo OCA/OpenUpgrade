8c8987da15745620b9d287f5d9b359f627377ef7
Miquel Raïch <miquel.raich@forgeflow.com>
2021-12-03 10:30:23 +0100

    [OU-ADD] repair: Migration scripts (continuation)

--- openupgrade_scripts/scripts/repair/14.0.1.0/post-migration.py
+++ openupgrade_scripts/scripts/repair/14.0.1.0/post-migration.py
@@ -1,4 +1,5 @@
 # Copyright (C) 2021 Open Source Integrators
+# Copyright 2021 ForgeFlow S.L.  <https://www.forgeflow.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 from openupgradelib import openupgrade
 
@@ -7,3 +8,6 @@ from openupgradelib import openupgrade
 def migrate(env, version):
     # Load noupdate changes
     openupgrade.load_data(env.cr, "repair", "14.0.1.0/noupdate_changes.xml")
+    openupgrade.delete_record_translations(
+        env.cr, "repair", ["mail_template_repair_quotation"]
+    )
