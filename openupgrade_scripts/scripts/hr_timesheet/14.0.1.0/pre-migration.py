3436b72b5f733d9f0b54b6387a5283a60b81bde7
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2021-12-11 21:07:17 +0100

    [IMP] hr_timesheet: Improve analysis + fill overtime by SQL + remove unneded noupdate changes
    TT29983

--- openupgrade_scripts/scripts/hr_timesheet/14.0.1.0/pre-migration.py
+++ openupgrade_scripts/scripts/hr_timesheet/14.0.1.0/pre-migration.py
@@ -1,6 +1,19 @@
-# Copyright (C) 2021 Open Source Integrators
+# Copyright 2021 Open Source Integrators
+# Copyright 2021 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 from openupgradelib import openupgrade
+
+
+def _fill_task_overtime(env):
+    openupgrade.logged_query(env.cr, "ALTER TABLE project_task ADD overtime NUMERIC")
+    openupgrade.logged_query(
+        env.cr,
+        """UPDATE project_task SET overtime =
+        CASE WHEN planned_hours > 0 THEN GREATEST(
+            effective_hours + subtask_effective_hours - planned_hours, 0.0
+        )
+        ELSE 0 END""",
+    )
 
 
 @openupgrade.migrate()
@@ -15,3 +28,4 @@ def migrate(env, version):
         ],
         True,
     )
+    _fill_task_overtime(env)
