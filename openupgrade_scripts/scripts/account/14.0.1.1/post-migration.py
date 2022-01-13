272b2c46aa019abce32c074dbc11dbb7fd191dd4
remi-filament <30716308+remi-filament@users.noreply.github.com>
2022-01-13 14:08:36 +0100

    [ADD] calculation for different currencies

--- openupgrade_scripts/scripts/account/14.0.1.1/post-migration.py
+++ openupgrade_scripts/scripts/account/14.0.1.1/post-migration.py
@@ -69,6 +69,7 @@ def fill_payment_id_and_statement_line_id_fields(env):
 
 
 def fill_partial_reconcile_debit_and_credit_amounts(env):
+    # compute debit and credit amount when currencies are the same
     openupgrade.logged_query(
         env.cr,
         """
@@ -78,7 +79,30 @@ def fill_partial_reconcile_debit_and_credit_amounts(env):
             AND credit_currency_id = debit_currency_id
        """,
     )
-    # TODO: compute debit and credit amount when currencies are different
+    # compute debit and credit amount when currencies are different
+    partial_reconcile_lines = (
+        env["account.partial.reconcile"]
+        .search(
+            [
+                ("debit_amount_currency", "=", False),
+                ("credit_amount_currency", "=", False),
+            ]
+        )
+        .filtered(lambda line: line.credit_currency_id != line.debit_currency_id)
+    )
+    for line in partial_reconcile_lines:
+        line.debit_amount_currency = line.credit_currency_id._convert(
+            line.amount,
+            line.debit_currency_id,
+            line.company_id,
+            line.credit_move_id.date,
+        )
+        line.credit_amount_currency = line.debit_currency_id._convert(
+            line.amount,
+            line.credit_currency_id,
+            line.company_id,
+            line.debit_move_id.date,
+        )
 
 
 def create_account_reconcile_model_lines(env):
