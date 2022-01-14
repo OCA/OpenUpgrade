4804f092385e570ab74b8f2f35de941483b29bf3
Roy Le <roy.le@viindoo.com>
2022-01-14 15:09:38 +0700

    [IMP] install unmet dependencies

--- openupgrade_scripts/scripts/base/14.0.1.3/post-migrate.py
+++ openupgrade_scripts/scripts/base/14.0.1.3/post-migrate.py
@@ -4,7 +4,28 @@
 from openupgradelib import openupgrade
 
 
+def install_unmet_dependencies(env):
+    """When changing the dependency of module which is not installed on the target version,
+    this method to install the missing modules to avoid error unmet_dependencies
+    """
+    installed_modules = env['ir.module.module'].search([('state','=','installed')])
+    installed_modules.update_list()
+    modules_to_install = installed_modules.upstream_dependencies(
+        exclude_states=('uninstallable', 'installed', 'to upgrade', 'to remove', 'to install'),
+        )
+
+    openupgrade.logged_query(
+        env.cr,
+        """
+        UPDATE ir_module_module
+        SET state='to install'
+        WHERE id in (%s)"""
+        % (", ".join([str(m_id) for m_id in modules_to_install.ids]),)
+    )
+
+
 @openupgrade.migrate()
 def migrate(env, version):
+    install_unmet_dependencies(env)
     # Load noupdate changes
     openupgrade.load_data(env.cr, "base", "14.0.1.3/noupdate_changes.xml")
