ab8b3bcd8a25c6dcb60681c01cd63d93a8c10343
ddejong-therp <88718955+ddejong-therp@users.noreply.github.com>
2022-02-25 16:58:08 +0100

    [OU-FIX] hr_expense: Migrate tax tags (#3152)
    Added a function that migrates the tax tags (account.account.tag) of move lines (account.move.line) that belong to an expense (hr.expense).
    
    The account module does not migrate tags for these lines.

--- addons/hr_expense/migrations/13.0.2.0/post-migration.py
+++ addons/hr_expense/migrations/13.0.2.0/post-migration.py
@@ -4,6 +4,25 @@
 from openupgradelib import openupgrade
 
 
+def assign_account_tags_to_move_lines_from_expenses(env):
+    """ Migrate all account.account.tag's of move lines that are a result of
+        expenses.
+    """
+    openupgrade.logged_query(
+        env.cr, """
+        INSERT INTO account_account_tag_account_move_line_rel (
+            account_move_line_id, account_account_tag_id)
+        SELECT aml.id, aatatrlr.account_account_tag_id
+        FROM account_move_line aml
+        JOIN expense_tax et ON et.expense_id = aml.expense_id
+        JOIN account_tax_repartition_line atrl ON
+            atrl.invoice_tax_id = et.tax_id OR atrl.refund_tax_id = et.tax_id
+        JOIN account_account_tag_account_tax_repartition_line_rel aatatrlr ON
+            aatatrlr.account_tax_repartition_line_id = atrl.id
+        ON CONFLICT DO NOTHING"""
+    )
+
+
 def fill_hr_expense_company_id(env):
     openupgrade.logged_query(
         env.cr, """
@@ -28,6 +47,7 @@ def fill_hr_expense_sheet_company_id(env):
 
 @openupgrade.migrate()
 def migrate(env, version):
+    assign_account_tags_to_move_lines_from_expenses(env)
     fill_hr_expense_company_id(env)
     fill_hr_expense_sheet_company_id(env)
     openupgrade.delete_records_safely_by_xml_id(
