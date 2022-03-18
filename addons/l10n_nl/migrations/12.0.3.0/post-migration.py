2473ed88da0d84c8afc7c35f125e789a19936f69
Danny de Jong <ddejong@therp.nl>
2022-03-18 15:18:21 +0100

    [REM] l10n_nl: Stop deletion of tag 3b, the tag is reintroduced in 13.0, and leaving it installed means that it can be properly migrated in 13.0.

--- addons/l10n_nl/migrations/12.0.3.0/post-migration.py
+++ addons/l10n_nl/migrations/12.0.3.0/post-migration.py
@@ -5,8 +5,4 @@ from openupgradelib import openupgrade
 
 @openupgrade.migrate()
 def migrate(env, version):
-    openupgrade.delete_records_safely_by_xml_id(
-        env, [
-            'l10n_nl.tag_nl_13',
-        ],
-    )
+    openupgrade.set_xml_ids_noupdate_value(env, 'l10n_nl', ['tag_nl_13'], True)
