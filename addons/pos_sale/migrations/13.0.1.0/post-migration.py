e095f0433cd0e66a9ca4da40378f09acde8f1b00
david <david.vidal@tecnativa.com>
2022-05-26 12:40:07 +0200

    [OU-FIX] pos_sale: map old pos.order to crm_team_id
    From now on, every pos.order will have stored its crm.team relation, so if it's
    changed later, those sales don't go to the new team. Anyway we have to map it in
    the old orders so we don't loose that info in the sales report.
    
    TT36891

--- addons/pos_sale/migrations/13.0.1.0/post-migration.py
+++ addons/pos_sale/migrations/13.0.1.0/post-migration.py
@@ -3,6 +3,25 @@
 from openupgradelib import openupgrade
 
 
+def _map_crm_team_id(env):
+    """From now on, every pos.order will have stored its crm.team relation, so if it's
+    changed later, those sales don't go to the new team. Anyway we have to map it in
+    the old orders so we don't loose that info in the sales report."""
+    openupgrade.logged_query(
+        env.cr,
+        """
+        UPDATE pos_order po
+        SET crm_team_id = pc.crm_team_id
+        FROM pos_config pc, pos_session ps
+        WHERE
+            ps.id = po.session_id
+            AND pc.id = ps.config_id
+            AND po.crm_team_id IS NULL
+            AND pc.crm_team_id IS NOT NULL
+        """,
+    )
+
+
 @openupgrade.migrate()
 def migrate(env, version):
     openupgrade.load_data(
@@ -12,3 +31,4 @@ def migrate(env, version):
             "point_of_sale.pos_config_main",
         ]
     )
+    _map_crm_team_id(env)
