3687072f79aca9917028bde7be05488991fd38d2
sang250399 <sangfpt1999alhp@gmail.com>
2022-06-20 09:50:22 +0700

    [MIG] payment: migration script

---
+++ openupgrade_scripts/scripts/payment/15.0.2.0/pre-migration.py
@@ -0,0 +1,52 @@
+from openupgradelib import openupgrade
+
+_renamed_fields = [
+    (
+        "payment.token",
+        "payment_token",
+        "payment_ids",
+        "transaction_ids",
+    ),
+    (
+        "payment.transaction",
+        "payment_transaction",
+        "is_processed",
+        "is_post_processed",
+    ),
+    (
+        "payment.transaction",
+        "payment_transaction",
+        "payment_token_id",
+        "token_id",
+    ),
+]
+
+
+def fast_fill_payment_token_name(env):
+    openupgrade.logged_query(
+        env.cr,
+        """
+        UPDATE payment_token
+        SET name = 'XXXXXXXXXX????'
+        WHERE name IS NULL""",
+    )
+
+
+def fast_fill_payment_transaction_partner_id(env):
+    openupgrade.logged_query(
+        env.cr,
+        """
+        UPDATE payment_transaction pt
+        SET partner_id = ap.partner_id
+        FROM account_payment ap
+        WHERE ap.partner_id IS NOT NULL
+            AND pt.partner_id IS NULL
+            AND ap.payment_transaction_id = pt.id""",
+    )
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    openupgrade.rename_fields(env, _renamed_fields)
+    fast_fill_payment_token_name(env)
+    fast_fill_payment_transaction_partner_id(env)
