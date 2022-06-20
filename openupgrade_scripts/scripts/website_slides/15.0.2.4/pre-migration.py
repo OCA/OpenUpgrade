3c34dd174aa28ef198564e9d251adc7b554b0be3
sang250399 <sangfpt1999alhp@gmail.com>
2022-06-20 11:24:02 +0700

    [MIG] website_slides: migration script

---
+++ openupgrade_scripts/scripts/website_slides/15.0.2.4/pre-migration.py
@@ -0,0 +1,14 @@
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    openupgrade.convert_field_to_html(
+        env.cr, "slide_channel", "description", "description"
+    )
+    openupgrade.convert_field_to_html(
+        env.cr, "slide_channel", "description_short", "description"
+    )
+    openupgrade.convert_field_to_html(
+        env.cr, "slide_slide", "description", "description"
+    )
