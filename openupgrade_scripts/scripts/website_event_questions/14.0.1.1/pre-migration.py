fb168078b3fb9c491886dae4b93bc2b2a43fa747
CarlosRoca13 <carlos.roca@tecnativa.com>
2022-07-11 07:55:04 +0200

    [OU-ADD] website_event_registration_free_text: Merged to website_event_registration

--- openupgrade_scripts/scripts/website_event_questions/14.0.1.1/pre-migration.py
+++ openupgrade_scripts/scripts/website_event_questions/14.0.1.1/pre-migration.py
@@ -1,6 +1,24 @@
 from openupgradelib import openupgrade
 
 
+def migrate_website_event_questions_free_text(env):
+    openupgrade.logged_query(
+        env.cr,
+        """UPDATE event_question
+        SET question_type = "text_box"
+        WHERE free_text = True
+        """,
+    )
+    openupgrade.logged_query(
+        env.cr,
+        """INSERT INTO event_registration_answer
+            (question_id, registration_id, value_text_box)
+        SELECT eaf.question_id, eaf.registration_id, eaf.answer
+        FROM event_answer_free eaf
+        """,
+    )
+
+
 @openupgrade.migrate()
 def migrate(env, version):
     openupgrade.rename_models(env.cr, [("event.answer", "event.question.answer")])
@@ -27,3 +45,5 @@ def migrate(env, version):
             ),
         ],
     )
+    if openupgrade.column_exists(env.cr, "event_question", "free_text"):
+        migrate_website_event_questions_free_text(env)
