7556020029dba1c0fafa8c6c068fa796c8eb12bb
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2022-07-24 16:22:27 +0200

    [FIX] openupgrade_framework: Proper module dependency resolution

--- openupgrade_framework/odoo_patch/odoo/modules/graph.py
+++ openupgrade_framework/odoo_patch/odoo/modules/graph.py
@@ -21,5 +21,40 @@ def update_from_db(self, cr):
             package.dbdemo = False
 
 
+def add_modules(self, cr, module_list, force=None):
+    """Add extra dependencies directly to the graph for immediate installation and
+    proper dependency resolution.
+    """
+    modules_in = list(module_list)
+    dependencies = module_list
+    while dependencies:
+        cr.execute(
+            """
+            SELECT DISTINCT dep.name
+            FROM
+                ir_module_module,
+                ir_module_module_dependency dep
+            WHERE
+                module_id = ir_module_module.id
+                AND ir_module_module.name in %s
+                AND dep.name not in %s
+            """,
+            (tuple(dependencies), tuple(module_list)),
+        )
+        dependencies = [x[0] for x in cr.fetchall()]
+        module_list += dependencies
+    # Set proper state for new dependencies so that any init scripts are run
+    cr.execute(
+        """
+        UPDATE ir_module_module SET state = 'to install'
+        WHERE name IN %s AND name NOT IN %s AND state = 'uninstalled'
+        """,
+        (tuple(module_list), tuple(modules_in)),
+    )
+    return Graph.add_modules._original_method(self, cr, module_list, force=force)
+
+
 update_from_db._original_method = Graph.update_from_db
 Graph.update_from_db = update_from_db
+add_modules._original_method = Graph.add_modules
+Graph.add_modules = add_modules
