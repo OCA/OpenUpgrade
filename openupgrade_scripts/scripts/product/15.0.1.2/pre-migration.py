bff712603b02be335e7641613625e63576e06a20
Miquel Raïch <miquel.raich@forgeflow.com>
2022-07-28 16:02:03 +0200

    [FIX] product: finish migration

--- openupgrade_scripts/scripts/product/15.0.1.2/pre-migration.py
+++ openupgrade_scripts/scripts/product/15.0.1.2/pre-migration.py
@@ -2,6 +2,7 @@ from openupgradelib import openupgrade
 
 
 def compute_product_template_attribute_line_value_count(env):
+    # fast compute value_count
     if not openupgrade.column_exists(
         env.cr, "product_template_attribute_line", "value_count"
     ):
@@ -31,20 +32,79 @@ def convert_text_to_html(env):
 
 
 def compute_detailed_type_and_type(env):
-    if not openupgrade.column_exists(env.cr, "product_template", "detailed_type"):
-        openupgrade.logged_query(
+    if openupgrade.column_exists(env.cr, "product_template", "detailed_type"):
+        openupgrade.rename_columns(
             env.cr,
-            """
-            ALTER TABLE product_template
-            ADD COLUMN detailed_type varchar
-            """,
+            {
+                "product_template": [
+                    ("detailed_type", None),
+                ],
+            },
         )
+    openupgrade.logged_query(
+        env.cr,
+        """
+        ALTER TABLE product_template
+        ADD COLUMN detailed_type varchar
+        """,
+    )
     openupgrade.logged_query(
         env.cr,
         """
         UPDATE product_template
         SET detailed_type = type
-        WHERE type = 'consu' OR type = 'service'
+        """,
+    )
+
+
+def fill_product_attribute_value_color(env):
+    # fast fill color
+    if openupgrade.column_exists(env.cr, "product_attribute_value", "color"):
+        openupgrade.rename_columns(
+            env.cr,
+            {
+                "product_attribute_value": [
+                    ("color", None),
+                ],
+            },
+        )
+    openupgrade.logged_query(
+        env.cr,
+        """
+        ALTER TABLE product_attribute_value
+        ADD COLUMN color integer
+        """,
+    )
+    openupgrade.logged_query(
+        env.cr,
+        """
+        UPDATE product_attribute_value
+        SET color = FLOOR(random() * 11 + 1)::int
+        WHERE color IS NULL
+        """,
+    )
+    if openupgrade.column_exists(env.cr, "product_template_attribute_value", "color"):
+        openupgrade.rename_columns(
+            env.cr,
+            {
+                "product_template_attribute_value": [
+                    ("color", None),
+                ],
+            },
+        )
+    openupgrade.logged_query(
+        env.cr,
+        """
+        ALTER TABLE product_template_attribute_value
+        ADD COLUMN color integer
+        """,
+    )
+    openupgrade.logged_query(
+        env.cr,
+        """
+        UPDATE product_template_attribute_value
+        SET color = FLOOR(random() * 11 + 1)::int
+        WHERE color IS NULL
         """,
     )
 
@@ -54,3 +114,4 @@ def migrate(env, version):
     compute_detailed_type_and_type(env)
     compute_product_template_attribute_line_value_count(env)
     convert_text_to_html(env)
+    fill_product_attribute_value_color(env)
