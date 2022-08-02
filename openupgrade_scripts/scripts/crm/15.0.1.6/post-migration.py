337f68015c023f03b85e7e19400edb78b7154409
Miquel Raïch <miquel.raich@forgeflow.com>
2022-08-02 10:25:33 +0200

    [FIX] crm: avoid deleting mail alias record

--- openupgrade_scripts/scripts/crm/15.0.1.6/post-migration.py
+++ openupgrade_scripts/scripts/crm/15.0.1.6/post-migration.py
@@ -5,7 +5,6 @@ def try_delete_noupdate_records(env):
     openupgrade.delete_records_safely_by_xml_id(
         env,
         [
-            "crm.mail_alias_lead_info",
             "crm.email_template_opportunity_mail",
         ],
     )
@@ -15,3 +14,8 @@ def try_delete_noupdate_records(env):
 def migrate(env, version):
     openupgrade.load_data(env.cr, "crm", "15.0.1.6/noupdate_changes.xml")
     try_delete_noupdate_records(env)
+    openupgrade.logged_query(
+        env.cr,
+        """DELETE FROM ir_model_data
+        WHERE module = 'crm' AND name = 'mail_alias_lead_info'""",
+    )
