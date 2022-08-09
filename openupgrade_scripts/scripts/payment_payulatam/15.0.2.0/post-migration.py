e81479ea835fe248380422fc6c7e5029ac60e9f0
Miquel Raïch <miquel.raich@forgeflow.com>
2022-08-09 14:25:05 +0200

    [MIG] payment_payulatam

---
+++ openupgrade_scripts/scripts/payment_payulatam/15.0.2.0/post-migration.py
@@ -0,0 +1,6 @@
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    openupgrade.load_data(env.cr, "payment_payulatam", "15.0.2.0/noupdate_changes.xml")
