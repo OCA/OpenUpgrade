236d7a46aa1187eafd5ff3d4824f25f3f892ff48
Stefan Rijnhart <stefan@opener.am>
2022-10-05 15:52:54 +0200

    [MIG] 16.0: remove 15.0 scripts

--- openupgrade_scripts/scripts/maintenance/15.0.1.0/post-migration.py
+++
@@ -1,6 +0,0 @@
-from openupgradelib import openupgrade
-
-
-@openupgrade.migrate()
-def migrate(env, version):
-    openupgrade.load_data(env.cr, "maintenance", "15.0.1.0/noupdate_changes.xml")
--- openupgrade_scripts/scripts/rating/15.0.1.0/pre-migration.py
+++
@@ -1,9 +0,0 @@
-from openupgradelib import openupgrade
-
-
-@openupgrade.migrate()
-def migrate(env, version):
-    openupgrade.copy_columns(
-        env.cr,
-        {"rating_rating": [("rating_text", None, None)]},
-    )
