f03ab8d3eb6f9d0cffd8a0cf39998441be10dff4
Stefan Rijnhart <stefan@opener.am>
2022-10-05 23:15:32 +0200

    [MIG] Reset apriori

--- openupgrade_scripts/apriori.py
+++ openupgrade_scripts/apriori.py
@@ -5,13 +5,6 @@ to help the matching process
 # Renamed modules is a mapping from old module name to new module name
 renamed_modules = {
     # odoo
-    "crm_iap_lead": "crm_iap_mine",
-    "crm_iap_lead_enrich": "crm_iap_enrich",
-    "mail_client_extension": "mail_plugin",
-    "payment_ingenico": "payment_ogone",
-    "website_mail_channel": "website_mail_group",
-    # OCA/project
-    "project_stage_state": "project_task_stage_state",
     # OCA/...
 }
 
@@ -19,41 +12,14 @@ renamed_modules = {
 # preexisting module names
 merged_modules = {
     # odoo
-    "account_edi_extended": "account_edi",
-    "l10n_be_invoice_bba": "l10n_be",
-    "l10n_eu_service": "l10n_eu_oss",  # due to OCA/account-fiscal-rule
-    "l10n_ch_qr_iban": "l10n_ch",
-    "l10n_se_ocr": "l10n_se",
-    "payment_fix_register_token": "payment",
-    "procurement_jit": "sale_stock",
-    "sale_timesheet_edit": "sale_timesheet",
-    "website_event_track_exhibitor": "website_event_exhibitor",
-    "website_form": "website",
-    "website_sale_management": "website_sale",
-    # odoo/enterprise
-    "stock_barcode_mobile": "stock_barcode",
-    # OCA/account-financial-tools
-    "stock_account_prepare_anglo_saxon_out_lines_hook": "stock_account",
-    # OCA/stock-logistics-workflow
-    "stock_deferred_assign": "stock",
-    "stock_move_assign_picking_hook": "stock",
-    # OCA/project
-    "project_mail_chatter": "project",
-    "project_task_dependency": "project",
-    # OCA/web
-    "web_decimal_numpad_dot": "web",
+    # OCA/...
 }
 
 # only used here for upgrade_analysis
 renamed_models = {
     # odoo
-    "calendar.contacts": "calendar.filters",
-    "mail.moderation": "mail.group.moderation",
     # OCA/...
 }
 
 # only used here for upgrade_analysis
-merged_models = {
-    "stock.inventory": "stock.quant",
-    "stock.inventory.line": "stock.quant",
-}
+merged_models = {}
