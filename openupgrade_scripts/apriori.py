bc5ec7554348df0191fa452b7a18cc9ebf0f4b9e
mle <marie.lejeune@acsone.eu>
2022-10-19 12:49:52 +0200

    [ADD] apriori.py: change knowledge module name

---
+++ openupgrade_scripts/apriori.py
@@ -0,0 +1,9 @@
+""" Encode any known changes to the database here
+to help the matching process
+"""
+
+# Renamed modules is a mapping from old module name to new module name
+renamed_modules = {
+    # OCA/knowledge
+    "knowledge": "document_knowledge",
+}
