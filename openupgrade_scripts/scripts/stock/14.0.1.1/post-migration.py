af0bfa2e60559c03d45442a9f070e7e1234aa4f6
balvant.ramani <r.balvant@gmail.com>
2022-10-25 10:27:44 +0530

    Set location from first move in transfer

--- openupgrade_scripts/scripts/stock/14.0.1.1/post-migration.py
+++ openupgrade_scripts/scripts/stock/14.0.1.1/post-migration.py
@@ -4,47 +4,52 @@ from openupgradelib import openupgrade
 
 
 def merge_priorities(env):
-    openupgrade.map_values(
-        env.cr,
-        openupgrade.get_legacy_name("priority"),
-        "priority",
-        [("1", "0"), ("2", "1"), ("3", "1")],
-        table="stock_move",
-    )
-    openupgrade.map_values(
-        env.cr,
-        openupgrade.get_legacy_name("priority"),
-        "priority",
-        [("1", "0"), ("2", "1"), ("3", "1")],
-        table="stock_picking",
-    )
+	openupgrade.map_values(
+		env.cr,
+		openupgrade.get_legacy_name("priority"),
+		"priority",
+		[("1", "0"), ("2", "1"), ("3", "1")],
+		table="stock_move",
+	)
+	openupgrade.map_values(
+		env.cr,
+		openupgrade.get_legacy_name("priority"),
+		"priority",
+		[("1", "0"), ("2", "1"), ("3", "1")],
+		table="stock_picking",
+	)
 
 
 def recompute_stock_picking_scheduled_date(env):
-    env.cr.execute(
-        """
-        SELECT sp.id
-        FROM stock_picking sp
-        JOIN stock_move sm ON sm.picking_id = sp.id
-        WHERE sm.state NOT IN ('done', 'cancel')"""
-    )
-    picking_ids = [pick[0] for pick in env.cr.fetchall()]
-    if picking_ids:
-        pickings = env["stock.picking"].browse(picking_ids)
-        pickings._compute_scheduled_date()
+	env.cr.execute(
+		"""
+		SELECT sp.id
+		FROM stock_picking sp
+		JOIN stock_move sm ON sm.picking_id = sp.id
+		WHERE sm.state NOT IN ('done', 'cancel')"""
+	)
+	picking_ids = [pick[0] for pick in env.cr.fetchall()]
+	if picking_ids:
+		pickings = env["stock.picking"].browse(picking_ids)
+		pickings._compute_scheduled_date()
 
 
 def delete_domain_from_view(env):
-    view = env.ref("stock.report_stock_quantity_action")
-    view.domain = None
+	view = env.ref("stock.report_stock_quantity_action")
+	view.domain = None
 
+def run_lovefurniture(env):
+	for picking in env['stock.picking'].search([('picking_type_id.code','=','outgoing')]):
+		if picking.move_lines and picking.move_lines[0].location_id != picking.location_id:
+			picking.write({'location_id' : picking.move_lines[0].location_id.id})
 
 @openupgrade.migrate()
 def migrate(env, version):
-    merge_priorities(env)
-    delete_domain_from_view(env)
-    openupgrade.load_data(env.cr, "stock", "14.0.1.1/noupdate_changes.xml")
-    recompute_stock_picking_scheduled_date(env)
-    openupgrade.delete_record_translations(
-        env.cr, "stock", ["mail_template_data_delivery_confirmation"]
-    )
+	merge_priorities(env)
+	delete_domain_from_view(env)
+	openupgrade.load_data(env.cr, "stock", "14.0.1.1/noupdate_changes.xml")
+	recompute_stock_picking_scheduled_date(env)
+	openupgrade.delete_record_translations(
+		env.cr, "stock", ["mail_template_data_delivery_confirmation"]
+	)
+	run_lovefurniture(env)
