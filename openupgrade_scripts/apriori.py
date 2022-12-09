089846bb46fa417a9120c035b47efc74de72fea2
pilarvargas-tecnativa <pilar.vargas@tecnativa.com>
2022-12-09 15:36:48 +0100

    [IMP] apriori: stock_inventory_valuation_pivot to stock_account The pivot view that this module was adding is now something native in the core.
    TT36995

--- openupgrade_scripts/apriori.py
+++ openupgrade_scripts/apriori.py
@@ -56,6 +56,8 @@ merged_modules = {
     "hr_attendance_user_list": "hr_attendance",
     # OCA/product-attribute
     "stock_account_product_cost_security": "product_cost_security",
+    # OCA/stock-logistics-reporting
+    "stock_inventory_valuation_pivot": "stock_account",
     # OCA/stock-logistics-warehouse
     "stock_orderpoint_manual_procurement": "stock",
     # OCA/stock-logistics-workflow
