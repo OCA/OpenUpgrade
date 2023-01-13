93d808b71403586dd358cf6f89e2db48bb9020e3
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2023-01-13 19:54:41 +0100

    [OU-FIX] hr_expense: Finish migration scripts

--- openupgrade_scripts/scripts/hr_expense/15.0.2.0/pre-migration.py
+++ openupgrade_scripts/scripts/hr_expense/15.0.2.0/pre-migration.py
@@ -1,38 +1,6 @@
 from openupgradelib import openupgrade
 
 
-def _compute_payment_state(env):
-    openupgrade.logged_query(
-        env.cr,
-        """
-        ALTER TABLE hr_expense_sheet
-        ADD COLUMN IF NOT EXISTS payment_state varchar
-        """,
-    )
-    openupgrade.logged_query(
-        env.cr,
-        """
-        UPDATE hr_expense_sheet
-        SET payment_state = 'not_paid'
-        WHERE account_move_id IS NULL
-        """,
-    )
-    openupgrade.logged_query(
-        env.cr,
-        """
-        UPDATE hr_expense_sheet expense_sheet
-        SET payment_state = CASE
-            WHEN am.payment_state IS NOT NULL THEN am.payment_state
-            ELSE 'not_paid'
-            END
-        FROM account_move am
-        WHERE
-            expense_sheet.account_move_id IS NOT NULL
-            AND am.id = expense_sheet.account_move_id
-        """,
-    )
-
-
 def _compute_currency_id_has_not_value(env):
     openupgrade.logged_query(
         env.cr,
@@ -48,4 +16,7 @@ def _compute_currency_id_has_not_value(env):
 @openupgrade.migrate()
 def migrate(env, version):
     _compute_currency_id_has_not_value(env)
-    _compute_payment_state(env)
+    openupgrade.logged_query(
+        env.cr,
+        "ALTER TABLE hr_expense_sheet ADD COLUMN IF NOT EXISTS payment_state varchar",
+    )
