0f5349b745b9f11db9d9d279f27d8558bfb6e0d1
pilarvargas-tecnativa <pilar.vargas@tecnativa.com>
2023-02-01 15:35:14 +0100

    [IMP] website_hr_recruitment: Convert an html string from odoo version 13 to new definitions in version 14
    TT41464

--- openupgrade_scripts/scripts/website_hr_recruitment/14.0.1.0/post-migration.py
+++ openupgrade_scripts/scripts/website_hr_recruitment/14.0.1.0/post-migration.py
@@ -1,4 +1,4 @@
-from openupgradelib import openupgrade
+from openupgradelib import openupgrade, openupgrade_140
 
 
 @openupgrade.migrate()
@@ -6,4 +6,9 @@ def migrate(env, version):
     openupgrade.delete_records_safely_by_xml_id(
         env,
         ["website_hr_recruitment.menu_jobs"],
+    )
+    openupgrade_140.convert_field_html_string_13to14(
+        env,
+        "hr.job",
+        "website_description",
     )
