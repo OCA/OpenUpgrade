4d31e1f4bcc5fe522e8b6dd15eb4fd28181017be
pilarvargas-tecnativa <pilar.vargas@tecnativa.com>
2023-02-01 17:55:10 +0100

    [14.0][IMP] website_blog: html string from version 13 to 14
    TT41471

--- openupgrade_scripts/scripts/website_blog/14.0.1.0/post-migration.py
+++ openupgrade_scripts/scripts/website_blog/14.0.1.0/post-migration.py
@@ -1,6 +1,11 @@
-from openupgradelib import openupgrade
+from openupgradelib import openupgrade, openupgrade_140
 
 
 @openupgrade.migrate()
 def migrate(env, version):
     openupgrade.load_data(env.cr, "website_blog", "14.0.1.0/noupdate_changes.xml")
+    openupgrade_140.convert_field_html_string_13to14(
+        env,
+        "blog.post",
+        "content",
+    )
