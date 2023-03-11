8b31964ad05032ef611cffec1049a0d5286b5e8f
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2023-03-11 12:09:56 +0100

    [OU-IMP] delivery: Scripts adjustments

--- openupgrade_scripts/scripts/delivery/15.0.1.0/post-migration.py
+++ openupgrade_scripts/scripts/delivery/15.0.1.0/post-migration.py
@@ -4,9 +4,12 @@ from openupgradelib import openupgrade
 def _move_data_from_product_packaging_to_stock_package_type(env):
     openupgrade.logged_query(
         env.cr,
+        """ALTER TABLE stock_package_type
+        ADD COLUMN IF NOT EXISTS old_product_packaging_id INTEGER""",
+    )
+    openupgrade.logged_query(
+        env.cr,
         """
-        ALTER TABLE stock_package_type
-            ADD COLUMN IF NOT EXISTS old_product_packaging_id INTEGER;
         INSERT INTO stock_package_type(old_product_packaging_id,
             name, sequence, height, width,
             shipper_package_code, package_carrier_type,
@@ -18,19 +21,16 @@ def _move_data_from_product_packaging_to_stock_package_type(env):
             packaging_length, max_weight, barcode, company_id,
             create_uid, create_date, write_uid, write_date
         FROM product_packaging
-        WHERE product_id IS NULL;
+        WHERE product_id IS NULL
         """,
     )
-
-
-def _fill_stock_quant_package_package_type_id(env):
     openupgrade.logged_query(
         env.cr,
         """
         UPDATE stock_quant_package sqp
         SET package_type_id = spt.id
         FROM stock_package_type spt
-        WHERE spt.old_product_packaging_id = sqp.packaging_id;
+        WHERE spt.old_product_packaging_id = sqp.packaging_id
         """,
     )
 
@@ -38,4 +38,3 @@ def _fill_stock_quant_package_package_type_id(env):
 @openupgrade.migrate()
 def migrate(env, version):
     _move_data_from_product_packaging_to_stock_package_type(env)
-    _fill_stock_quant_package_package_type_id(env)
