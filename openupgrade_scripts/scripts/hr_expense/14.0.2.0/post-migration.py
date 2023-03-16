4b521ca89b1e33a7ea520385b21bc3b11d264802
Miquel Raïch <miquel.raich@forgeflow.com>
2023-03-16 10:17:44 +0100

    [FIX] hr_expense: do write only when is necessary

--- openupgrade_scripts/scripts/hr_expense/14.0.2.0/post-migration.py
+++ openupgrade_scripts/scripts/hr_expense/14.0.2.0/post-migration.py
@@ -6,9 +6,9 @@ from openupgradelib import openupgrade
 @openupgrade.migrate()
 def migrate(env, version):
     product = env.ref("product_product_fixed_cost", raise_if_not_found=False)
-    if product:
+    if product and product.default_code == "EXP_GEN":
         try:
             product.default_code = False
         except Exception:
-            # this means default_code is requited
-            product.default_code += "/1"
+            # this means default_code is required
+            product.default_code = "EXP_GEN/1"
