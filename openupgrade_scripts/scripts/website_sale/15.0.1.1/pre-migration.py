6146ba93cb3e47be9b44a4b01945e929ff298c8d
Carlos Roca <carlos.roca@tecnativa.com>
2023-03-21 13:44:23 +0100

    [OU-ADD] website_sale_attribute_filter_price: Price filter added to website_sale

---
+++ openupgrade_scripts/scripts/website_sale/15.0.1.1/pre-migration.py
@@ -0,0 +1,22 @@
+from openupgradelib import openupgrade
+
+
+def enable_price_filter_view(env):
+    for website in env["website"].search([]):
+        view = env["ir.ui.view"].search(
+            [
+                ("key", "=", "website_sale_attribute_filter_price.pricefilter"),
+                ("website_id", "=", website.id),
+            ]
+        )
+        if view:
+            # It's important to set the context to create the new view related to the
+            # current website. See write method of ir.ui.view in website module
+            website.with_context(website_id=website.id).viewref(
+                "website_sale.filter_products_price"
+            ).active = True
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    enable_price_filter_view(env)
