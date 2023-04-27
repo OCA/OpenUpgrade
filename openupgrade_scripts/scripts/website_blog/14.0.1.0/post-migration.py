9774fb908de960ed8111ad2cad227339b992a669
pilarvargas-tecnativa <pilar.vargas@tecnativa.com>
2023-04-27 10:40:54 +0200

    [OU-ADD] website_blog: update cover properties field
    In v13, blog posts without an image in the header were indicated with
    the parameters "backgroung-image" : "none" and "background-color" : "value"
    stored in the blog_post.cover_properties field.
    When migrating to v14 this style setting is indicated by
    "backgroung-image" : "none" and "backgroung_color_class" : "value"
    and "background-color" disappears.
    For this reason the old publications do not adopt the corresponding
    style on the website if they use colour and not an image in their header.
    This is solved by rewriting the data contained in the field
    blog_post.cover_properties.
    
    TT39016

--- openupgrade_scripts/scripts/website_blog/14.0.1.0/post-migration.py
+++ openupgrade_scripts/scripts/website_blog/14.0.1.0/post-migration.py
@@ -1,6 +1,18 @@
 from openupgradelib import openupgrade
 
+from odoo.tools.json import scriptsafe as json_safe
+
+
+def update_cover_properties_field(env):
+    blog_post = env["blog.post"].search([])
+    for post in blog_post:
+        cover_properties = json_safe.loads(post.cover_properties)
+        cover_properties["background_color_class"] = "o_cc3"
+        cover_properties.pop("background-color", None)
+        post.write({"cover_properties": json_safe.dumps(cover_properties)})
+
 
 @openupgrade.migrate()
 def migrate(env, version):
     openupgrade.load_data(env.cr, "website_blog", "14.0.1.0/noupdate_changes.xml")
+    update_cover_properties_field(env)
