0fff55ac35e09fbbe185fdde0077ec0431c200bd
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2023-06-30 13:43:15 +0200

    [OU-FIX] website: BS processing fine-tuning
    - Exclude directly mail.message table.

--- openupgrade_scripts/scripts/website/14.0.1.0/end-migration.py
+++ openupgrade_scripts/scripts/website/14.0.1.0/end-migration.py
@@ -3,11 +3,14 @@ from openupgradelib import openupgrade, openupgrade_140
 
 
 def convert_field_html_string(env):
+    exclusions = ["mail.message"]
     fields = env["ir.model.fields"].search(
         [("ttype", "=", "html"), ("store", "=", True)]
     )
     for field in fields:
         model = field.model_id.model
+        if model in exclusions:
+            continue
         if env.get(model, False) is not False and env[model]._auto:
             if openupgrade.table_exists(env.cr, env[model]._table):
                 if openupgrade.column_exists(env.cr, env[model]._table, field.name):
