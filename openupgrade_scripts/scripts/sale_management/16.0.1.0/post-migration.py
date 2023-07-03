464fa896312b3bded1d612320ec19874e5b54917
hugues de keyzer <odoo@hugues.info>
2023-07-03 14:31:20 +0200

    [OU-ADD] sale_management

---
+++ openupgrade_scripts/scripts/sale_management/16.0.1.0/post-migration.py
@@ -0,0 +1,9 @@
+# Copyright 2023 Coop IT Easy SC
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    openupgrade.load_data(env.cr, "sale_management", "16.0.1.0/noupdate_changes.xml")
