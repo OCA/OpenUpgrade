677af5937821dc5c8a71fc272059854f462b9381
Hoang Tien Dung <hoangtiendung070797@gmail.com>
2023-07-04 08:41:51 +0700

    [OU-ADD] sale_timesheet: migrate 15.0 to 16.0

---
+++ openupgrade_scripts/scripts/sale_timesheet/16.0.1.0/pre-migration.py
@@ -0,0 +1,34 @@
+from openupgradelib import openupgrade
+
+
+def _update_account_analytic_line_timesheet_invoice_type(env):
+    openupgrade.logged_query(
+        env.cr,
+        """
+        WITH timesheet_info as (
+            SELECT aal.id as id,
+                   product_tmpl.service_type as service_type
+            FROM account_analytic_line aal
+            JOIN sale_order_line sol
+                ON sol.id = aal.so_line
+            JOIN product_product product
+                ON product.id = sol.product_id
+            JOIN product_template product_tmpl
+                ON product_tmpl.id = product.product_tmpl_id
+            WHERE aal.project_id > 0
+                AND product_tmpl.type = 'service'
+                AND product_tmpl.invoice_policy = 'delivery'
+                AND product_tmpl.service_type IN ('milestones', 'manual')
+                AND aal.timesheet_invoice_type = 'billable_fixed'
+        )
+        UPDATE account_analytic_line aal
+            SET timesheet_invoice_type = CONCAT('billable_', info.service_type)
+        FROM timesheet_info info
+        WHERE aal.id = info.id
+        """,
+    )
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    _update_account_analytic_line_timesheet_invoice_type(env)
