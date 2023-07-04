31f05fd6dafbe5eb97e54da80024534c7aad8ee5
mle <marie.lejeune@acsone.eu>
2023-07-04 09:27:38 +0200

    [OU-FIX] portal: delete translations after loading data changes

--- openupgrade_scripts/scripts/portal/16.0.1.0/post-migration.py
+++ openupgrade_scripts/scripts/portal/16.0.1.0/post-migration.py
@@ -3,7 +3,12 @@
 
 from openupgradelib import openupgrade
 
+_translations_to_delete = [
+    "mail_template_data_portal_welcome",
+]
+
 
 @openupgrade.migrate()
 def migrate(env, version):
     openupgrade.load_data(env.cr, "portal", "16.0.1.0/noupdate_changes.xml")
+    openupgrade.delete_record_translations(env.cr, "portal", _translations_to_delete)
