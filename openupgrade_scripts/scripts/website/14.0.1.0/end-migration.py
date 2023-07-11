f0f1e1564e925e146b938776c3164571ab9d7ed5
Holger Brunn <mail@hunki-enterprises.com>
2023-07-11 22:38:49 +0200

    [FIX] website: don't crash on fields in the db but not the orm

--- openupgrade_scripts/scripts/website/14.0.1.0/end-migration.py
+++ openupgrade_scripts/scripts/website/14.0.1.0/end-migration.py
@@ -13,7 +13,9 @@ def convert_field_html_string(env):
             continue
         if env.get(model, False) is not False and env[model]._auto:
             if openupgrade.table_exists(env.cr, env[model]._table):
-                if openupgrade.column_exists(env.cr, env[model]._table, field.name):
+                if field.name in env[model]._fields and openupgrade.column_exists(
+                    env.cr, env[model]._table, field.name
+                ):
                     openupgrade_140.convert_field_html_string_13to14(
                         env,
                         model,
