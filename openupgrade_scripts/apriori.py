78fed4901755a9551f249f88f0c75bfdb4124503
stenex <stefano.nana@nextev.it>
2023-10-06 17:05:40 +0200

    [MIG] assets_management: Rename to l10n_it_asset_management

--- openupgrade_scripts/apriori.py
+++ openupgrade_scripts/apriori.py
@@ -26,6 +26,8 @@ renamed_modules = {
     "website_sale_coupon_page": "website_sale_loyalty_page",
     # OCA/server-ux
     "mass_editing": "server_action_mass_edit",
+    # OCA/l10n-italy
+    "assets_management": "l10n_it_asset_management",
     # OCA/...
 }
 
