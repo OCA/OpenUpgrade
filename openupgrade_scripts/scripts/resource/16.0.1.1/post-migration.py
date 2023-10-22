a0c7d3e4ba1fa2e6eb6e0af8545dee921e3784e2
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2023-10-22 23:57:19 +0200

    [OU-ADD] resource: Migration scripts
    After latest updated analysis.

---
+++ openupgrade_scripts/scripts/resource/16.0.1.1/post-migration.py
@@ -0,0 +1,9 @@
+# Copyright 2023 Tecnativa - Pedro M. Baeza
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    openupgrade.load_data(env.cr, "resource", "16.0.1.1/noupdate_changes.xml")
