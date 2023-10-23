fb8a31b319793b27a85daf11c56a0010300fc285
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2023-10-23 19:15:27 +0200

    [OU-FIX] analytic: Refine migration scripts
    - Database structure changes should be always applied before business
      logic changes.
    - No need to implement literally the compute root_plan_id, as we are
      creating it for the first time. We just assign it on the accounts.
    - Restructured dummy plan creation, for having the ID available for
      several queries and not having to make a fuzzy select.
    - For a smooth migration, the default_applicability should be optional,
      so that users can still continue applying their accounts into
      documents.
    - Rename default plan to 'Legacy' for better understanding.

--- openupgrade_scripts/scripts/analytic/16.0.1.1/pre-migration.py
+++ openupgrade_scripts/scripts/analytic/16.0.1.1/pre-migration.py
@@ -18,64 +18,24 @@ _models_renames = [("account.analytic.group", "account.analytic.plan")]
 _tables_renames = [("account_analytic_group", "account_analytic_plan")]
 
 
-def analytic_account_set_group_id_if_null(env):
-    """
-    We would like to fill for group_id column before start renaming it into plan_id
-    """
-    if not openupgrade.column_exists(
-        env.cr, "account_analytic_group", "default_applicability"
-    ):
-        openupgrade.add_fields(
-            env,
-            [
-                (
-                    "default_applicability",
-                    "account.analytic.group",
-                    "account_analytic_group",
-                    "selection",
-                    "character varying",
-                    "analytic",
-                    False,
-                ),
-            ],
-        )
+def analytic_account_set_plan_id_if_null(env):
     openupgrade.logged_query(
         env.cr,
         """
-        WITH inserted_group AS (
-            INSERT INTO account_analytic_group
-            (name, default_applicability, complete_name)
-            SELECT 'Dummy Analytic Plan', 'unavailable', 'Dummy Analytic Plan'
-            RETURNING id
-        )
-        UPDATE account_analytic_account aaa
-        SET group_id = (
-            SELECT id
-            FROM inserted_group
-            LIMIT 1
-        )
-        WHERE aaa.group_id IS NULL;
+        ALTER TABLE account_analytic_plan
+        ADD COLUMN IF NOT EXISTS default_applicability VARCHAR
         """,
     )
-    # Manually update parent_path for dummy plan that we have created
-    # or else we will get error when accessing it
     openupgrade.logged_query(
         env.cr,
         """
-        UPDATE account_analytic_group
-        SET parent_path = id || '/'
-        WHERE name = 'Dummy Analytic Plan' AND
-        default_applicability = 'unavailable' AND
-        parent_path IS NULL
+        INSERT INTO account_analytic_plan
+        (name, default_applicability, complete_name)
+        SELECT 'Legacy', 'optional', 'Legacy'
+        RETURNING id
         """,
     )
-
-
-def create_root_plan_and_prefill_value_for_analytic_plan(env):
-    """
-    Pre fill value for column root_plan_id using the logic
-    in its compute method to avoid computed by ORM
-    """
+    plan_id = env.cr.fetchone()[0]
     openupgrade.logged_query(
         env.cr,
         """
@@ -86,22 +46,28 @@ def create_root_plan_and_prefill_value_for_analytic_plan(env):
     openupgrade.logged_query(
         env.cr,
         """
-        UPDATE account_analytic_account AS aaa
-        SET root_plan_id = (
-            SELECT split_part(parent.parent_path, '/', 1)::integer
-                FROM account_analytic_plan AS parent
-                WHERE parent.id = aaa.plan_id
-                AND parent.parent_path IS NOT NULL
-            LIMIT 1
-        )
+        UPDATE account_analytic_account
+        SET plan_id = %(plan_id)s, root_plan_id = %(plan_id)s
+        WHERE plan_id IS NULL
         """,
+        {"plan_id": plan_id},
+    )
+    # Manually update parent_path for legacy plan that we have created
+    # or else we will get error when accessing it
+    openupgrade.logged_query(
+        env.cr,
+        """
+        UPDATE account_analytic_plan
+        SET parent_path = id || '/'
+        WHERE id = %s
+        """,
+        (plan_id,),
     )
 
 
 @openupgrade.migrate()
 def migrate(env, version):
-    analytic_account_set_group_id_if_null(env)
     openupgrade.rename_fields(env, _fields_renames)
     openupgrade.rename_models(env.cr, _models_renames)
     openupgrade.rename_tables(env.cr, _tables_renames)
-    create_root_plan_and_prefill_value_for_analytic_plan(env)
+    analytic_account_set_plan_id_if_null(env)
