9f9e9668c65f57f0feab6ae96e3b2c94be51bf77
David <david.vidal@tecnativa.com>
2023-10-30 17:04:04 +0100

    [FIX] payment_transfer: custom acquirers redirect form
    We're assigning with the noupdate changes the new redirect form to the
    transfer acquirer but for those transfer acquirers made by the users we
    have to do it manually or they won't work properly.
    
    TT45663

--- openupgrade_scripts/scripts/payment_transfer/15.0.2.0/post-migration.py
+++ openupgrade_scripts/scripts/payment_transfer/15.0.2.0/post-migration.py
@@ -4,4 +4,8 @@ from openupgradelib import openupgrade
 @openupgrade.migrate()
 def migrate(env, version):
     openupgrade.load_data(env.cr, "payment_transfer", "15.0.2.0/noupdate_changes.xml")
+    # Delete the old form record and manually link the custom acquirers made by
+    # the users.
     openupgrade.delete_records_safely_by_xml_id(env, ["payment_transfer.transfer_form"])
+    custom_transfer = env["payment.acquirer"].search([("provider", "=", "transfer")])
+    custom_transfer.redirect_form_view_id = env.ref("payment_transfer.redirect_form")
