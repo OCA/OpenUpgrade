3efc2eadabb3eb7f3d211e5eb3725d4f9014fac7
Stefan Rijnhart <stefan@opener.amsterdam>
2024-02-02 20:26:00 +0100

    [MIG] openupgrade_scripts: scaffolding the 17.0 module

---
+++ openupgrade_scripts/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright Odoo Community Association (OCA)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Openupgrade Scripts",
+    "summary": """Module that contains all the migrations analysis
+        and scripts for migrate Odoo SA modules.""",
+    "author": "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/OpenUpgrade",
+    "category": "Migration",
+    "version": "17.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["base"],
+    "images": ["static/description/banner.jpg"],
+    "external_dependencies": {
+        "python": [
+            "openupgradelib @ git+https://github.com/OCA/openupgradelib.git@master"
+        ]
+    },
+    "installable": True,
+}
