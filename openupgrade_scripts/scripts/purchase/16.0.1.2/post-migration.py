a6e86fa4b4fc1ebcfd886929756c63e9059b2ee2
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2024-02-28 19:36:08 +0100

    [OU-IMP] purchase: Delete noupdate changes delete_translations
    So that the system also renews that content.

--- openupgrade_scripts/scripts/purchase/16.0.1.2/post-migration.py
+++ openupgrade_scripts/scripts/purchase/16.0.1.2/post-migration.py
@@ -1,4 +1,5 @@
 # Copyright 2024 Le Filament
+# Copyright 2024 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from openupgradelib import openupgrade
 
@@ -6,6 +7,15 @@ from openupgradelib import openupgrade
 @openupgrade.migrate()
 def migrate(env, version):
     openupgrade.load_data(env.cr, "purchase", "16.0.1.2/noupdate_changes.xml")
+    openupgrade.delete_record_translations(
+        env.cr,
+        "purchase",
+        [
+            "email_template_edi_purchase",
+            "email_template_edi_purchase_done",
+            "email_template_edi_purchase_reminder",
+        ],
+    )
     openupgrade.delete_records_safely_by_xml_id(
         env,
         ["purchase.mail_notification_confirm"],
