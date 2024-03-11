48d9602c4ec6869dce7276244ff48fd2bb6061b7
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2024-03-11 17:24:23 +0100

    [OU-FIX+IMP] website_slides: Complete migration scripts
    - Better explanations on work file
    - Rework slide_type > slide_category conversion
    - Shorten lines
    - Fill source_type properly
    - Delete record translations
    - Handle nbr_article field
    
    TT45249

--- openupgrade_scripts/scripts/website_slides/16.0.2.6/pre-migration.py
+++ openupgrade_scripts/scripts/website_slides/16.0.2.6/pre-migration.py
@@ -1,10 +1,12 @@
+# Copyright 2023 Viindoo - Nguyễn Việt Lâm
+# Copyright 2024 Tecnativa - Pedro M. Baeza
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from openupgradelib import openupgrade
 
 renamed_fields = [
     ("slide.slide", "slide_slide", "datas", "binary_content"),
     ("slide.channel", "slide_channel", "share_template_id", "share_slide_template_id"),
 ]
-
 xml_ids_to_rename = [
     (
         "website_slides.rule_slide_slide_resource_manager",
@@ -12,40 +14,26 @@ xml_ids_to_rename = [
     ),
 ]
 
-# slide_slide
+
+def _create_and_fill_data_from_slide_type_to_slide_category(env):
+    openupgrade.copy_columns(
+        env.cr, {"slide_slide": [("slide_type", "slide_category", None)]}
+    )
+    openupgrade.map_values(
+        env.cr,
+        "slide_type",
+        "slide_category",
+        [("webpage", "article"), ("presentation", "document")],
+        table="slide_slide",
+    )
+    openupgrade.rename_columns(env.cr, {"slide_slide": [("slide_type", None)]})
 
 
-def create_and_fill_data_from_slide_type_to_slide_category(env):
+def _create_and_fill_data_for_source_type(env):
     openupgrade.logged_query(
         env.cr,
-        """
-        ALTER TABLE slide_slide
-        ADD COLUMN IF NOT EXISTS slide_category VARCHAR
-        """,
+        "ALTER TABLE slide_slide ADD COLUMN IF NOT EXISTS source_type VARCHAR",
     )
-
-    openupgrade.logged_query(
-        env.cr,
-        """
-        UPDATE slide_slide
-        SET slide_category = CASE
-            WHEN slide_type = 'webpage' THEN 'article'
-            WHEN slide_type = 'presentation' THEN 'document'
-            ELSE slide_type
-        END
-        """,
-    )
-
-
-def create_and_fill_data_for_source_type(env):
-    openupgrade.logged_query(
-        env.cr,
-        """
-        ALTER TABLE slide_slide
-        ADD COLUMN IF NOT EXISTS source_type VARCHAR
-        """,
-    )
-
     openupgrade.logged_query(
         env.cr,
         """
@@ -58,10 +46,7 @@ def create_and_fill_data_for_source_type(env):
     )
 
 
-# slide_slide_resource
-
-
-def create_column_and_migrate_data_from_slide_link_to_slide_resource(env):
+def _create_column_and_migrate_data_from_slide_link_to_slide_resource(env):
     openupgrade.logged_query(
         env.cr,
         """
@@ -70,31 +55,36 @@ def create_column_and_migrate_data_from_slide_link_to_slide_resource(env):
         ADD COLUMN IF NOT EXISTS resource_type VARCHAR
         """,
     )
-
+    openupgrade.logged_query(
+        env.cr,
+        "UPDATE slide_slide_resource SET resource_type = 'file'",
+    )
     openupgrade.logged_query(
         env.cr,
         """
-        UPDATE slide_slide_resource
-        SET resource_type = 'file'
+        INSERT INTO slide_slide_resource
+        (name, link, slide_id, resource_type)
+        SELECT name, link, slide_id, 'url'
+        FROM slide_slide_link
         """,
     )
 
+
+def _create_nbr_article(env):
+    """Pre-create and fill these fields for avoiding KeyError crashes as the compute
+    method uses read_group.
+    """
     openupgrade.logged_query(
         env.cr,
-        """
-        INSERT INTO slide_slide_resource (
-            name,
-            link,
-            slide_id,
-            resource_type
-        )
-        SELECT
-            name,
-            link,
-            slide_id,
-            'url'
-        FROM slide_slide_link
-        """,
+        "ALTER TABLE slide_channel ADD COLUMN IF NOT EXISTS nbr_article INT4",
+    )
+    openupgrade.logged_query(
+        env.cr,
+        "ALTER TABLE slide_slide ADD COLUMN IF NOT EXISTS nbr_article INT4 DEFAULT 0",
+    )
+    openupgrade.logged_query(
+        env.cr,
+        "ALTER TABLE slide_slide ALTER COLUMN nbr_article DROP DEFAULT",
     )
 
 
@@ -102,6 +92,7 @@ def create_column_and_migrate_data_from_slide_link_to_slide_resource(env):
 def migrate(env, version):
     openupgrade.rename_fields(env, renamed_fields)
     openupgrade.rename_xmlids(env.cr, xml_ids_to_rename)
-    create_and_fill_data_from_slide_type_to_slide_category(env)
-    create_column_and_migrate_data_from_slide_link_to_slide_resource(env)
-    create_and_fill_data_for_source_type(env)
+    _create_and_fill_data_from_slide_type_to_slide_category(env)
+    _create_column_and_migrate_data_from_slide_link_to_slide_resource(env)
+    _create_and_fill_data_for_source_type(env)
+    _create_nbr_article(env)
