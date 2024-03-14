a94f427b8462456d9fe4c648c9663bcbfecebe02
Rémy Taymans <remytms@tsmail.eu>
2024-03-14 14:54:45 +0100

    [OU-ADD] apriori: rename pos_journal_image

--- odoo/addons/openupgrade_records/lib/apriori.py
+++ odoo/addons/openupgrade_records/lib/apriori.py
@@ -25,6 +25,8 @@ renamed_modules = {
     'quality_control_stock': 'quality_control_stock_oca',
     # OCA/margin-analysis
     'product_pricelist_margin': 'product_pricelist_simulation',
+    # OCA/pos
+    "pos_journal_image": 'pos_payment_method_image',
     # OCA/product-attribute
     'product_pricelist_print_website_sale': 'product_pricelist_direct_print_website_sale',
     'sale_product_classification': 'product_abc_classification_sale',
