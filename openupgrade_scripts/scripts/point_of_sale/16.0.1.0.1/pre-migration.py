5a2eb05c10e02ec8a5d6a4c6e1692a75866554e3
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2024-04-26 20:15:46 +0200

    [OU-IMP] point_of_sale: Complete migration scripts
    TT45176

---
+++ openupgrade_scripts/scripts/point_of_sale/16.0.1.0.1/pre-migration.py
@@ -0,0 +1,36 @@
+# SPDX-FileCopyrightText: 2024 Tecnativa - Pedro M. Baeza
+# SPDX-License-Identifier: AGPL-3.0-or-later
+import uuid
+
+from openupgradelib import openupgrade
+
+
+def _assign_pos_order_token(env):
+    openupgrade.add_fields(
+        env,
+        [("access_token", "pos.order", "pos_order", "char", False, "point_of_sale")],
+    )
+    env.cr.execute("SELECT id FROM survey_survey")
+    for row in env.cr.fetchall():
+        env.cr.execute(
+            "UPDATE survey_survey SET access_token = %s WHERE id = %s",
+            (str(uuid.uuid4()), row[0]),
+        )
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    openupgrade.add_fields(
+        env,
+        [
+            (
+                "cash_register_balance_start",
+                "pos.session",
+                "pos_session",
+                "monetary",
+                False,
+                "point_of_sale",
+            )
+        ],
+    )
+    _assign_pos_order_token(env)
