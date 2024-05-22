9ed931fcb0e02696da02f0fd289e5d018070ac12
Nguyễn Đại Dương <daiduongnguyen2709@gmail.com>
2024-05-22 11:48:49 +0700

    [OU-ADD] mail: migration to 17.0

---
+++ openupgrade_scripts/scripts/mail/17.0.1.15/pre-migration.py
@@ -0,0 +1,127 @@
+# Copyright 2024 Viindoo Technology Joint Stock Company (Viindoo)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from openupgradelib import openupgrade
+
+_models_renames = [
+    ("mail.channel", "discuss.channel"),
+    ("mail.channel.member", "discuss.channel.member"),
+    ("mail.channel.rtc.session", "discuss.channel.rtc.session"),
+]
+_tables_renames = [
+    ("mail_channel", "discuss_channel"),
+    ("mail_channel_member", "discuss_channel_member"),
+    ("mail_channel_rtc_session", "discuss_channel_rtc_session"),
+]
+_fields_renames = [
+    (
+        "discuss.channel",
+        "discuss_channel",
+        "field",
+        "field_id",
+    ),
+]
+
+
+def _discuss_channel_fill_allow_public_upload(env):
+    openupgrade.logged_query(
+        env.cr,
+        """
+        ALTER TABLE discuss_channel
+        ADD COLUMN IF NOT EXISTS allow_public_upload BOOLEAN;
+        """,
+    )
+    openupgrade.logged_query(
+        env.cr,
+        """
+        UPDATE discuss_channel
+        SET allow_public_upload = True
+        WHERE channel_type = 'livechat'
+        """,
+    )
+
+
+def _mail_alias_fill_multiple_values(env):
+    """
+    We will fill value for alias_full_name in post because alias_domain has not been
+    present yet
+    """
+    openupgrade.logged_query(
+        env.cr,
+        """
+        ALTER TABLE mail_alias
+        ADD COLUMN IF NOT EXISTS alias_full_name VARCHAR,
+        ADD COLUMN IF NOT EXISTS alias_incoming_local BOOLEAN,
+        ADD COLUMN IF NOT EXISTS alias_status VARCHAR;
+        """,
+    )
+    openupgrade.logged_query(
+        env.cr,
+        """
+        UPDATE mail_alias
+        SET alias_incoming_local = True
+        """,
+    )
+    openupgrade.logged_query(
+        env.cr,
+        """
+        UPDATE mail_alias
+        SET alias_status = 'not_tested'
+        """,
+    )
+
+
+def _mail_tracking_value_update_monetary_tracking_values(env):
+    openupgrade.logged_query(
+        env.cr,
+        """
+        UPDATE mail_tracking_value
+            SET old_value_float = old_value_monetary,
+                new_value_float = new_value_monetary
+        WHERE old_value_monetary IS NOT NULL
+            OR new_value_monetary IS NOT NULL;
+        """,
+    )
+
+
+def _company_update_email_colors(env):
+    openupgrade.logged_query(
+        env.cr,
+        """
+        ALTER TABLE res_company
+        ADD COLUMN IF NOT EXISTS email_primary_color VARCHAR,
+        ADD COLUMN IF NOT EXISTS email_secondary_color VARCHAR;
+        """,
+    )
+    openupgrade.logged_query(
+        env.cr,
+        """
+        UPDATE res_company
+        SET email_primary_color = CASE
+            WHEN primary_color IS NOT NULL then primary_color
+            ELSE '#000000'
+        END,
+            email_secondary_color = CASE
+            WHEN secondary_color IS NOT NULL then secondary_color
+            ELSE '#875A7B'
+        END
+        """,
+    )
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    openupgrade.rename_models(env.cr, _models_renames)
+    openupgrade.rename_tables(env.cr, _tables_renames)
+    openupgrade.rename_fields(env, _fields_renames)
+    _discuss_channel_fill_allow_public_upload(env)
+    _mail_alias_fill_multiple_values(env)
+    _mail_tracking_value_update_monetary_tracking_values(env)
+    _company_update_email_colors(env)
+    # create column to avoid model mail.alias is loaded before model res.company
+    openupgrade.logged_query(
+        env.cr,
+        """
+        ALTER TABLE res_company
+        ADD COLUMN IF NOT EXISTS alias_domain_id INTEGER;
+        """,
+    )
