1833a930b9e626ddc084d68404b339f3f3e3c57b
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2024-05-25 19:23:22 +0200

    [OU-ADD] hr_holidays_attendance: Migration scripts
    TT45869

---
+++ openupgrade_scripts/scripts/hr_holidays_attendance/16.0.1.0/post-migration.py
@@ -0,0 +1,11 @@
+# Copyright 2024 Tecnativa - Pedro M. Baeza
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    openupgrade.load_data(
+        env.cr, "hr_holidays_attendance", "16.0.1.0/noupdate_changes.xml"
+    )
