74a59945e8fc90f01e2bc8a8f5d2135a2a6fe3be
David <david.vidal@tecnativa.com>
2024-06-13 15:56:24 +0200

    [IMP] stock: pre-compute quantity_done
    The field stock.move.quantity_done has passed to be stored.
    
    In large databases (millions of records) it takes a good amount of time
    to compute it by the ORM, so it's worth it to pre-compute its values as
    much as possible.
    
    TT46020

--- openupgrade_scripts/scripts/stock/16.0.1.1/post-migration.py
+++ openupgrade_scripts/scripts/stock/16.0.1.1/post-migration.py
@@ -42,8 +42,25 @@ def _handle_stock_picking_backorder_strategy(env):
         )
 
 
+def _complete_stock_move_quantity_done_with_orm(env):
+    """In pre-migration we left out the moves with lines with different units of
+    measure to be treated by the ORM"""
+    env.cr.execute(
+        """
+        SELECT move_id
+        FROM stock_move_line
+        GROUP BY move_id
+        HAVING COUNT(DISTINCT product_uom_id) > 1
+            AND SUM(qty_done) <> 0
+    """
+    )
+    move_ids = [id for id, *_ in env.cr.fetchall() if id]
+    env["stock.move"].browse(move_ids)._quantity_done_compute()
+
+
 @openupgrade.migrate()
 def migrate(env, version):
     _handle_multi_location_visibility(env)
     _handle_stock_picking_backorder_strategy(env)
     openupgrade.load_data(env.cr, "stock", "16.0.1.1/noupdate_changes.xml")
+    _complete_stock_move_quantity_done_with_orm(env)
