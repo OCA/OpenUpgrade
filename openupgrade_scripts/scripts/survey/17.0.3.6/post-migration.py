c67609aee114d9820ab9196699358e022213f586
Alexis <acp@microcom.ca>
2024-09-11 10:58:11 -0400

    [MIG] survey: Migration to 17.0

---
+++ openupgrade_scripts/scripts/survey/17.0.3.6/post-migration.py
@@ -0,0 +1,10 @@
+# Copyright 2024 Viindoo Technology Joint Stock Company (Viindoo)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from openupgradelib import openupgrade
+
+
+
+
+
+# @openupgrade.migrate()
+# def migrate(env, version):
