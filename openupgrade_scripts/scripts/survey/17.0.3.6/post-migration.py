d81849dc978abc9be57707d0e0218f8296285da2
Alexis <acp@microcom.ca>
2024-09-11 10:58:26 -0400

    [MIG] survey: Migration to 17.0

--- openupgrade_scripts/scripts/survey/17.0.3.6/post-migration.py
+++ openupgrade_scripts/scripts/survey/17.0.3.6/post-migration.py
@@ -1,10 +1,17 @@
 # Copyright 2024 Viindoo Technology Joint Stock Company (Viindoo)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
 from openupgradelib import openupgrade
 
+_deleted_xml_records = [
+    "survey.certification_report_view_classic",
+    "survey.certification_report_view_modern",
+]
 
 
-
-
-# @openupgrade.migrate()
-# def migrate(env, version):
+@openupgrade.migrate()
+def migrate(env, version):
+    openupgrade.delete_records_safely_by_xml_id(
+        env,
+        _deleted_xml_records,
+    )
