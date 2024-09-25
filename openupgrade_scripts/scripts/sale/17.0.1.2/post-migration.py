06193a5f9676911fe251101bdc44eb88c864d90a
Alexis <acp@microcom.ca>
2024-09-25 11:01:16 -0400

    [MIG] sale: Migration to 17.0

--- openupgrade_scripts/scripts/sale/17.0.1.2/post-migration.py
+++ openupgrade_scripts/scripts/sale/17.0.1.2/post-migration.py
@@ -4,6 +4,23 @@
 from openupgradelib import openupgrade
 
 
+def _sale_sale_order_(env):
+    openupgrade.logged_query(
+        env.cr,
+        """
+        UPDATE sale_order so
+        SET locked = True
+        WHERE so.temp_state = 'done'
+        """,
+    )
+    openupgrade.logged_query(
+        env.cr,
+        """
+        "ALTER TABLE sale_order DROP COLUMN temp_state",
+        """,
+    )
+
+
 @openupgrade.migrate()
 def migrate(env, version):
     openupgrade.load_data(env, "sale", "17.0.1.2/noupdate_changes.xml")
--- openupgrade_scripts/scripts/sale/17.0.1.2/pre-migration.py
+++ openupgrade_scripts/scripts/sale/17.0.1.2/pre-migration.py
@@ -13,6 +13,33 @@ def _remove_table_constraints(env):
     )
 
 
+def _sale_sale_order(env):
+    openupgrade.logged_query(
+        env.cr,
+        """
+        ALTER TABLE sale_order
+            ADD COLUMN IF NOT EXISTS locked BOOLEAN
+            ADD COLUMN IF NOT EXISTS temp_state VARCHAR
+        """,
+    )
+    openupgrade.logged_query(
+        env.cr,
+        """
+        UPDATE sale_order so
+        SET temp_state = so.state
+        """,
+    )
+    openupgrade.logged_query(
+        env.cr,
+        """
+        UPDATE sale_order so
+        SET so.state = 'sale'
+        WHERE so.state = 'done'
+        """,
+    )
+
+
 @openupgrade.migrate()
 def migrate(env, version):
     _remove_table_constraints(env)
+    _sale_sale_order(env)
