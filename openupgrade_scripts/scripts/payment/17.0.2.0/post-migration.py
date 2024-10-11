39973e6163900a2cf3a21f2ba812d23f18b7c83e
Rémi - Le Filament <30716308+remi-filament@users.noreply.github.com>
2024-10-11 14:38:51 +0200

    [OU-ADD] payment: continue migration script Remove payment.payment_method_acss_debit as does not exist anymore Move payment.icon to payment.method Reload payment methods and links to providers

--- openupgrade_scripts/scripts/payment/17.0.2.0/post-migration.py
+++ openupgrade_scripts/scripts/payment/17.0.2.0/post-migration.py
@@ -1,4 +1,5 @@
 # Copyright 2024 Viindoo Technology Joint Stock Company (Viindoo)
+# Copyright 2024 Le Filament (https://le-filament.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from openupgradelib import openupgrade
@@ -9,16 +10,18 @@ def _fill_payment_method(env):
     PaymentTransaction = env["payment.transaction"].with_context(active_test=False)
     PaymentMethod = env["payment.method"].with_context(active_test=False)
 
+    unknown_payment_method = env.ref("payment.payment_method_unknown")
+
     for payment_token in PaymentToken.search([("payment_method_id", "=", False)]):
         payment_token.payment_method_id = (
             PaymentMethod._get_from_code(payment_token.provider_id.code)
-            or payment_token.provider_id.payment_method_ids[:1]
+            or unknown_payment_method
         ).id
 
     for transaction in PaymentTransaction.search([("payment_method_id", "=", False)]):
         transaction.payment_method_id = (
             PaymentMethod._get_from_code(transaction.provider_id.code)
-            or transaction.provider_id.payment_method_ids[:1]
+            or unknown_payment_method
         ).id
 
 
