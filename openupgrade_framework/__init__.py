5ff2be4f6b443bf6d12220b87c006bb54f11c403
Holger Brunn <mail@hunki-enterprises.com>
2024-11-18 18:05:31 +0100

    [OU-ADD] enable tests, add decorator to make them convenient to use

--- openupgrade_framework/__init__.py
+++ openupgrade_framework/__init__.py
@@ -14,3 +14,20 @@ if not config.get("upgrade_path"):
             "location of openupgrade_scripts"
         )
         config["upgrade_path"] = os.path.join(path, "scripts")
+
+
+def openupgrade_test(cls):
+    """
+    Set attributes on a test class necessary for the test framework
+    Use as decorator on test classes in openupgrade_scripts/scripts/*/tests/test_*.py
+    """
+    tags = getattr(cls, "test_tags", None) or set()
+    if "openupgrade" not in tags:
+        tags.add("openupgrade")
+    if not any(t.endswith("_install") for t in tags):
+        tags.add("at_install")
+    cls.test_tags = tags
+    cls.test_module = cls.__module__.split(".")[2]
+    cls.test_class = cls.__name__
+    cls.test_sequence = 0
+    return cls
