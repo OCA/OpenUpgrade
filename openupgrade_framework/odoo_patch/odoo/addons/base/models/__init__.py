b60c30b49cb65423a305b18e6d0587e5c0cba274
Holger Brunn <mail@hunki-enterprises.com>
2025-06-30 15:40:09 +0200

    [IMP] openupgrade_framework: install newly available auto_install addons

--- openupgrade_framework/odoo_patch/odoo/addons/base/models/__init__.py
+++ openupgrade_framework/odoo_patch/odoo/addons/base/models/__init__.py
@@ -1,2 +1,3 @@
 from . import ir_model
+from . import ir_module
 from . import ir_ui_view
---
+++ openupgrade_framework/odoo_patch/odoo/addons/base/models/ir_module.py
@@ -0,0 +1,33 @@
+# Copyright Odoo Community Association (OCA)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+
+from odoo.addons.base.models.ir_module import Module
+
+
+def update_list(self):
+    """
+    Mark auto_install modules as to install if all their dependencies are some kind of
+    installed.
+    Ignore localization modules that are set to auto_install
+    """
+    Module.update_list._original_method(self)
+    new_auto_install_modules = self.browse([])
+    for module in self.env["ir.module.module"].search(
+        [
+            ("auto_install", "=", True),
+            ("state", "=", "uninstalled"),
+            ("name", "not like", ("l10n_%")),
+        ]
+    ):
+        if all(
+            state in ("to upgrade", "to install", "installed")
+            for state in module.dependencies_id.mapped("state")
+        ):
+            new_auto_install_modules |= module
+    if new_auto_install_modules:
+        new_auto_install_modules.button_install()
+
+
+update_list._original_method = Module.update_list
+Module.update_list = update_list
