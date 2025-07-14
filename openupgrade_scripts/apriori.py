e2e08ef16208c9940c47f8beb4c638717b37502d
Víctor Martínez <victor.martinez@tecnativa.com>
2025-07-14 10:52:39 +0200

    [OU-ADD] sale_loyalty_initial_date_validity: Merged into sale_loyalty
    TT54485

--- openupgrade_scripts/apriori.py
+++ openupgrade_scripts/apriori.py
@@ -65,6 +65,7 @@ merged_modules = {
     "purchase_discount": "purchase",
     # OCA/sale-promotion
     "loyalty_initial_date_validity": "loyalty",
+    "sale_loyalty_initial_date_validity": "sale_loyalty",
     # OCA/social
     "mail_activity_plan": "mail",
     "mass_mailing_custom_unsubscribe_event": "mass_mailing",
