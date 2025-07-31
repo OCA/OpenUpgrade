bd1d3c30bf21dbf6573e51cecd56c5d7ce0f7c64
Miquel Raïch <miquel.raich@forgeflow.com>
2025-07-31 16:28:28 +0200

    [OU-FIX] hr_holidays: finish migration

--- openupgrade_scripts/scripts/hr_holidays/18.0.1.6/post-migration.py
+++ openupgrade_scripts/scripts/hr_holidays/18.0.1.6/post-migration.py
@@ -1,30 +1,12 @@
-# Copyright 2025 Hunki Enterprises BV
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-
 from openupgradelib import openupgrade
 
 
 @openupgrade.migrate()
 def migrate(env, version):
     openupgrade.load_data(env, "hr_holidays", "18.0.1.6/noupdate_changes.xml")
-    # Restore values from the temp field to the actual float field
-    openupgrade.logged_query(
-        env.cr,
-        """
-        UPDATE hr_leave SET request_hour_to = request_hour_to_float
-        WHERE request_hour_to_float IS NOT NULL;
-        UPDATE hr_leave SET request_hour_from = request_hour_from_float
-        WHERE request_hour_from_float IS NOT NULL;
-
-        """,
+    openupgrade.delete_record_translations(
+        env.cr, "hr_holidays", ["mt_leave_allocation"], ["name"]
     )
-
-    # Clean up temp column
-    openupgrade.logged_query(
-        env.cr,
-        """
-            ALTER TABLE hr_leave DROP COLUMN request_hour_to_float;
-            ALTER TABLE hr_leave DROP COLUMN request_hour_from_float;
-
-            """,
+    openupgrade.delete_records_safely_by_xml_id(
+        env, ["hr_holidays.mt_leave_home_working"]
     )
--- openupgrade_scripts/scripts/hr_holidays/18.0.1.6/pre-migration.py
+++ openupgrade_scripts/scripts/hr_holidays/18.0.1.6/pre-migration.py
@@ -1,73 +1,97 @@
 from openupgradelib import openupgrade
 
+_column_renames = {
+    "hr_leave": [("request_hour_from", None), ("request_hour_to", None)],
+    "hr_leave_allocation": [("private_name", "name")],
+}
+
+_column_adds = [
+    ("hr.leave", "request_hour_from", "float"),
+    ("hr.leave", "request_hour_to", "float"),
+]
+
+
+def refill_hr_leave_request_hours(env):
+    old_request_hour_from = openupgrade.get_legacy_name("request_hour_from")
+    openupgrade.logged_query(
+        env.cr,
+        f"""
+        UPDATE hr_leave SET request_hour_from = {old_request_hour_from}::float
+        WHERE {old_request_hour_from} IS NOT NULL;
+        """,
+    )
+    old_request_hour_to = openupgrade.get_legacy_name("request_hour_to")
+    openupgrade.logged_query(
+        env.cr,
+        f"""
+        UPDATE hr_leave SET request_hour_to = {old_request_hour_to}::float
+        WHERE {old_request_hour_to} IS NOT NULL;
+        """,
+    )
+    openupgrade.logged_query(
+        env.cr,  # to avoid AttributeError: 'Float' object has no attribute 'ondelete'
+        """
+        DELETE FROM ir_model_fields_selection imfs
+        USING ir_model_fields imf
+        WHERE imfs.field_id = imf.id AND imf.model = 'hr.leave'
+        AND imf.name in ('request_hour_from', 'request_hour_to')
+        """,
+    )
+
+
+def update_states(env):
+    openupgrade.logged_query(
+        env.cr,
+        """
+        UPDATE hr_leave
+        SET state = 'confirm'
+        WHERE state = 'draft'
+        """,
+    )
+    openupgrade.logged_query(
+        env.cr,
+        """
+        UPDATE hr_leave
+        SET state = 'cancel'
+        WHERE active IS DISTINCT FROM TRUE
+        """,
+    )
+    openupgrade.logged_query(
+        env.cr,
+        """
+        UPDATE hr_leave_allocation
+        SET state = 'cancel'
+        WHERE active IS DISTINCT FROM TRUE
+        """,
+    )
+
+
+def update_allocation_validation_type(env):
+    openupgrade.logged_query(
+        env.cr,
+        """
+        UPDATE hr_leave_type
+        SET allocation_validation_type = 'hr'
+        WHERE allocation_validation_type = 'officer'
+        """,
+    )
+    openupgrade.logged_query(
+        env.cr,
+        """
+        UPDATE hr_leave_type
+        SET allocation_validation_type = 'no_validation'
+        WHERE allocation_validation_type = 'no'
+        """,
+    )
+
 
 @openupgrade.migrate()
 def migrate(env, version):
-    # Add a temp float column to store the value safely
-    openupgrade.logged_query(
-        env.cr,
-        """
-        ALTER TABLE hr_leave ADD COLUMN request_hour_to_float float;
-        ALTER TABLE hr_leave ADD COLUMN request_hour_from_float float;
-        """,
-    )
-    openupgrade.logged_query(
-        env.cr,
-        """
-        UPDATE hr_leave SET request_hour_to_float = request_hour_to::float
-        WHERE request_hour_to IS NOT NULL;
-        UPDATE hr_leave SET request_hour_from_float = request_hour_from::float
-        WHERE request_hour_from IS NOT NULL;
-
-        """,
-    )
-
-    # Delete selection values BEFORE registry tries to unlink
-    openupgrade.logged_query(
-        env.cr,
-        """
-        DELETE FROM ir_model_fields_selection
-        WHERE field_id IN (
-            SELECT f.id
-            FROM ir_model_fields f
-            JOIN ir_model m ON f.model_id = m.id
-            WHERE f.name = 'request_hour_to' AND m.model = 'hr.leave'
-        );
-        DELETE FROM ir_model_fields_selection
-        WHERE field_id IN (
-            SELECT f.id
-            FROM ir_model_fields f
-            JOIN ir_model m ON f.model_id = m.id
-            WHERE f.name = 'request_hour_from' AND m.model = 'hr.leave'
-        );
-        """,
-    )
-
-    # Remove the Many2many relation table if it exists
-    openupgrade.logged_query(
-        env.cr,
-        """
-        DROP TABLE IF EXISTS hr_employee_hr_leave_allocation_rel CASCADE
-        """,
-    )
-
-    # Optionally drop the column from the table (not usually required for M2M)
-    openupgrade.logged_query(
-        env.cr,
-        """
-        ALTER TABLE hr_leave_allocation
-        DROP COLUMN IF EXISTS employee_ids
-        """,
-    )
-
-    openupgrade.logged_query(
-        env.cr,
-        """
-        DELETE FROM ir_rule
-        WHERE id = (
-            SELECT res_id FROM ir_model_data
-            WHERE module = 'hr_holidays'
-            AND name = 'hr_leave_allocation_rule_multicompany'
-        )
-        """,
+    openupgrade.rename_columns(env.cr, _column_renames)
+    openupgrade.add_columns(env, _column_adds)
+    refill_hr_leave_request_hours(env)
+    update_states(env)
+    update_allocation_validation_type(env)
+    openupgrade.remove_tables_fks(
+        env.cr, ["hr_employee_hr_leave_rel", "hr_employee_hr_leave_allocation_rel"]
     )
