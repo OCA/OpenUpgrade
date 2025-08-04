5e6e8a9f5e1c7236d5b0fa0f3310930ff8484cf8
Miquel Raïch <miquel.raich@forgeflow.com>
2025-08-04 20:03:59 +0200

    [OU-FIX] hr_recruitment: finish migration

--- openupgrade_scripts/scripts/hr_recruitment/18.0.1.1/post-migration.py
+++ openupgrade_scripts/scripts/hr_recruitment/18.0.1.1/post-migration.py
@@ -1,38 +1,53 @@
-from openupgradelib import openupgrade
+from openupgradelib import openupgrade, openupgrade_merge_records
 
 from odoo import Command
 
 
 def _fill_hr_candidate(env):
-    # Update hr_candidate table with hr_applicant data
-    # once the table is created with all fields
+    # create helper
+    column_adds = [
+        ("hr.candidate", "old_applicant_id", "many2one"),  # helper
+    ]
+    openupgrade.add_columns(env, column_adds)
+    # Create candidate records from applicant
     openupgrade.logged_query(
         env.cr,
         """
-        UPDATE hr_candidate AS hc
-        SET
-            active = ha.active,
-            create_uid = ha.create_uid,
-            create_date = ha.create_date,
-            write_uid = ha.write_uid,
-            write_date = ha.write_date,
-            company_id = ha.company_id,
-            partner_id = ha.partner_id,
-            partner_name = ha.partner_name,
-            email_from = ha.email_from,
-            email_normalized = ha.email_normalized,
-            partner_phone = ha.partner_phone,
-            partner_phone_sanitized = ha.partner_phone_sanitized,
-            phone_sanitized = ha.phone_sanitized,
-            linkedin_profile = ha.linkedin_profile,
-            type_id = ha.type_id,
-            availability = ha.availability,
-            color = ha.color,
-            priority = ha.priority,
-            user_id = ha.user_id,
-            employee_id = ha.emp_id
-        FROM hr_applicant AS ha
-        WHERE ha.candidate_id = hc.id;
+        INSERT INTO hr_candidate (
+            partner_id, partner_name, email_from, email_cc, email_normalized,
+            partner_phone, partner_phone_sanitized, phone_sanitized,
+            employee_id, linkedin_profile, type_id, priority,
+            availability, color, message_bounce, active,
+            user_id, company_id, old_applicant_id,
+            create_date, write_date, create_uid, write_uid
+        )
+        SELECT partner_id, partner_name, email_from, email_cc, email_normalized,
+               COALESCE(partner_mobile, partner_phone),
+               COALESCE(partner_mobile_sanitized, partner_phone_sanitized),
+               phone_sanitized, emp_id, linkedin_profile, type_id, priority,
+               availability, color, message_bounce, TRUE,
+               user_id, company_id, id,
+               create_date, write_date, create_uid, write_uid
+        FROM hr_applicant
+        WHERE candidate_id IS NULL;
+        """,
+    )
+    # Update hr_applicant.candidate_id using helper old_applicant_id
+    openupgrade.logged_query(
+        env.cr,
+        """
+        UPDATE hr_applicant ha
+        SET candidate_id = hc.id
+        FROM hr_candidate hc
+        WHERE ha.id = hc.old_applicant_id
+        """,
+    )
+    # remove helper
+    openupgrade.logged_query(
+        env.cr,
+        """
+        ALTER TABLE hr_candidate
+        DROP COLUMN old_applicant_id
         """,
     )
     # fill many2many field categ_ids
@@ -45,13 +60,80 @@ def _fill_hr_candidate(env):
         SELECT ha.candidate_id, m2m.hr_applicant_category_id
         FROM hr_applicant_hr_applicant_category_rel as m2m
             JOIN hr_applicant AS ha ON ha.id = m2m.hr_applicant_id
-        WHERE ha.candidate_id IS NOT NULL
         ON CONFLICT DO NOTHING
+        """,
+    )
+    # Fill calendar_event.candidate_id
+    openupgrade.logged_query(
+        env.cr,
+        """
+        UPDATE calendar_event ce
+        SET candidate_id = ha.candidate_id
+        FROM hr_applicant ha
+        WHERE ce.applicant_id = ha.id
+        """,
+    )
+    # unify candidates
+    env.cr.execute(
+        """
+        SELECT array_agg(id order by create_date desc), partner_id, company_id
+        FROM hr_candidate
+        WHERE partner_id IS NOT NULL
+        GROUP BY partner_id, company_id
+        HAVING COUNT(id) > 1
+        """,
+    )
+    all_candidates = [x[0] for x in env.cr.fetchall()]
+    for candidates in all_candidates:
+        candidate_ids = env["hr.candidate"].browse(candidates)
+        openupgrade_merge_records.merge_records(
+            env,
+            "hr.candidate",
+            candidate_ids[1:].ids,
+            candidate_ids[0].id,
+            {
+                "partner_id": "first_not_null",
+                "partner_name": "first_not_null",
+                "email_from": "first_not_null",
+                "email_normalized": "first_not_null",
+                "email_cc": "first_not_null",
+                "partner_phone": "first_not_null",
+                "partner_phone_sanitized": "first_not_null",
+                "phone_sanitized": "first_not_null",
+                "employee_id": "first_not_null",
+                "linkedin_profile": "first_not_null",
+                "type_id": "first_not_null",
+                "priority": "first_not_null",
+                "color": "first_not_null",
+                "user_id": "first_not_null",
+                "company_id": "first_not_null",
+                "categ_ids": "merge",
+                "availability": "max",
+                "write_date": "max",
+                "create_date": "min",
+                "openupgrade_other_fields": "preserve",
+            },
+        )
+    candidates = env["hr.candidate"].search([])
+    for candidate in candidates:
+        if len(candidate.applicant_ids) > 1:
+            candidate._compute_priority()  # recompute priority
+        if all(not applicant.active for applicant in candidate.applicant_ids):
+            # archive inactive candidates if all applicants are inactive
+            candidate.active = False
+    # update attachments
+    openupgrade.logged_query(
+        env.cr,
+        """
+        UPDATE ir_attachment ia
+        SET res_model = 'hr.candidate', res_id = ha.candidate_id
+        FROM hr_applicant ha
+        WHERE ia.res_model = 'hr.applicant' AND ia.res_id = ha.id
         """,
     )
 
 
-def _normalice_res_groups_implied(env):
+def _normalize_res_groups_implied(env):
     """
     Remove the implied_ids from group_hr_recruitment_manager
     according to the changes in Odoo 18.0.
@@ -69,15 +151,22 @@ def _normalice_res_groups_implied(env):
 
 @openupgrade.migrate()
 def migrate(env, version):
-    openupgrade.load_data(env, "hr_recruitment", "18.0.1.1/noupdate_changes.xml")
+    openupgrade.load_data(env, "hr_recruitment", "18.0.1.1/noupdate_changes_work.xml")
+    openupgrade.delete_record_translations(
+        env.cr, "hr_recruitment", ["mt_job_new"], ["description"]
+    )
+    openupgrade.delete_record_translations(
+        env.cr,
+        "hr_recruitment",
+        ["refuse_reason_1", "refuse_reason_2", "refuse_reason_5", "refuse_reason_8"],
+        ["name"],
+    )
     _fill_hr_candidate(env)
-    _normalice_res_groups_implied(env)
+    _normalize_res_groups_implied(env)
     openupgrade.delete_records_safely_by_xml_id(
         env,
         [
             "hr_recruitment.categ_meet_interview",
-            "hr_recruitment.refuse_reason_3",
-            "hr_recruitment.refuse_reason_4",
             "hr_recruitment.hr_recruitment_blacklisted_emails",
         ],
     )
--- openupgrade_scripts/scripts/hr_recruitment/18.0.1.1/pre-migration.py
+++ openupgrade_scripts/scripts/hr_recruitment/18.0.1.1/pre-migration.py
@@ -1,109 +1,46 @@
 from openupgradelib import openupgrade
 
-column_creates = [
-    ("calendar.event", "candidate_id", "many2one"),
-    # Pre-create to prevent recompute; this will be filled in post-migration.
-    ("hr_candidate", "email_from", "char"),
-    ("hr_candidate", "email_normalized", "char"),
-    ("hr_candidate", "partner_phone", "char"),
-    ("hr_candidate", "partner_phone_sanitized", "char"),
-    ("hr_candidate", "phone_sanitized", "char"),
-    ("hr_candidate", "priority", "selection"),
-]
-
-_columns_copy = {
-    # Save a copy of the columns
-    # that were previously stored and are now related to hr.candidate.
+columns_copy = {
     "hr_applicant": [
         ("name", None, None),
-        ("description", None, None),
-        ("partner_id", None, None),
-        ("email_from", None, None),
-        ("email_normalized", None, None),
-        ("availability", None, None),
-        ("partner_name", None, None),
-        ("partner_phone", None, None),
-        ("partner_phone_sanitized", None, None),
-        ("partner_mobile", None, None),
-        ("partner_mobile_sanitized", None, None),
-        ("phone_sanitized", None, None),
-        ("type_id", None, None),
-        ("color", None, None),
-        ("emp_id", None, None),
-        ("linkedin_profile", None, None),
     ],
 }
 
+column_creates = [
+    ("hr.applicant", "refuse_date", "datetime"),
+]
 
-def _create_and_fill_hr_candidate(env):
-    """
-    Pre-create the minimal hr_candidate table
-    and populate it with data from hr_applicant.
-    Create a temporary column hr_applicant_id to link the two tables.
-    This is done to avoid errors with NOT NULL constraints,
-    since candidate_id is a required field in hr.applicant.
-    """
-    openupgrade.logged_query(
-        env.cr,
-        """
-        CREATE TABLE IF NOT EXISTS hr_candidate (
-            id SERIAL PRIMARY KEY,
-            tmp_hr_applicant_id INTEGER
-        )
-        """,
-    )
-    openupgrade.logged_query(
-        env.cr,
-        """
-        ALTER TABLE hr_applicant
-        ADD COLUMN IF NOT EXISTS candidate_id INTEGER REFERENCES hr_candidate(id)
-        """,
-    )
-    openupgrade.logged_query(
-        env.cr,
-        """
-        INSERT INTO hr_candidate (tmp_hr_applicant_id)
-        SELECT id FROM hr_applicant
-        WHERE candidate_id IS NULL
-        """,
-    )
-    openupgrade.logged_query(
-        env.cr,
-        """
-        UPDATE hr_applicant AS ha
-        SET candidate_id = hc.id
-        FROM hr_candidate AS hc
-        WHERE ha.id = hc.tmp_hr_applicant_id
-            AND ha.candidate_id IS NULL
-        """,
-    )
-    # remove the temporary column used to link the two tables
-    openupgrade.logged_query(
-        env.cr,
-        """
-        ALTER TABLE hr_candidate DROP COLUMN tmp_hr_applicant_id
-        """,
-    )
+field_renames = [
+    ("hr.applicant", "hr_applicant", "description", "applicant_notes"),
+]
 
-
-def fill_calendar_event_candidate_id(env):
-    """
-    Set candidate_id for calendar events from hr.applicant.
-    """
-    env.cr.execute(
-        """
-        UPDATE calendar_event ce
-        SET
-            candidate_id = ha.candidate_id
-        FROM hr_applicant ha
-        WHERE ha.id = ce.applicant_id AND ce.candidate_id IS NULL
-        """
-    )
+xmlid_renames = [
+    (
+        "hr_recruitment.applicant_hired_template",
+        "hr_recruitment.candidate_hired_template",
+    ),
+]
 
 
 @openupgrade.migrate()
 def migrate(env, version):
-    _create_and_fill_hr_candidate(env)
-    openupgrade.copy_columns(env.cr, _columns_copy)
+    openupgrade.copy_columns(env.cr, columns_copy)
     openupgrade.add_columns(env, column_creates)
-    fill_calendar_event_candidate_id(env)
+    openupgrade.rename_fields(env, field_renames)
+    openupgrade.rename_xmlids(env.cr, xmlid_renames)
+    openupgrade.rename_xmlids(
+        env.cr,
+        [
+            ("hr_recruitment.refuse_reason_3", "hr_recruitment.refuse_reason_2"),
+            ("hr_recruitment.refuse_reason_4", "hr_recruitment.refuse_reason_2"),
+        ],
+        allow_merge=True,
+    )
+    openupgrade.logged_query(
+        env.cr,
+        """
+        UPDATE hr_applicant
+        SET refuse_date = write_date
+        WHERE refuse_reason_id IS NOT NULL
+        """,
+    )
