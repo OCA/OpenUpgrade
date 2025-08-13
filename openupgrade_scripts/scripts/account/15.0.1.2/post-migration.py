b9530817e10b10a480e5e87273b4628c4220c290
Miquel Raïch <miquel.raich@forgeflow.com>
2025-08-13 17:55:51 +0200

    [IMP] account: handle better website_legal_page

--- openupgrade_scripts/scripts/account/15.0.1.2/post-migration.py
+++ openupgrade_scripts/scripts/account/15.0.1.2/post-migration.py
@@ -45,6 +45,19 @@ def _handle_website_legal_page(env):
     the information added in the page created by 'website_legal_page' has to be
     transferred to the invoice_terms_html field of the company.
     """
+    openupgrade.update_module_names(
+        env.cr, [("website_legal_page", "account")], merge_modules=True
+    )
+    env[
+        "ir.module.module"
+    ].update_list()  # solves odoo.exceptions.CacheMiss: 'ir.module.module...
+    openupgrade.logged_query(
+        env.cr,  # let's install again
+        """
+        UPDATE ir_module_module
+        SET state='to upgrade'
+        WHERE name = 'website_legal_page' AND state='uninstalled'""",
+    )
     for company in env["res.company"].with_context(active_test=False).search([]):
         openupgrade.logged_query(
             env.cr,
