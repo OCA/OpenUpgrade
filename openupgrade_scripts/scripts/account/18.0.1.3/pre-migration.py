3ff84f03a310d1d54c30cc1e40064c7741618082
Miquel Raïch <miquel.raich@forgeflow.com>
2025-08-20 12:14:51 +0200

    [FIX] docsource

--- openupgrade_scripts/scripts/account/18.0.1.3/pre-migration.py
+++ openupgrade_scripts/scripts/account/18.0.1.3/pre-migration.py
@@ -134,7 +134,7 @@ def adapt_account_move_sending_data(env):
             '{author_user_id}', sending_data::jsonb->'sp_user_id')
         WHERE sending_data IS NOT NULL AND sending_data::jsonb ? 'sp_user_id'""",
     )
-    # send_mail: True -> 'sending_methods': {"email"}:
+    # send_mail: True -> 'sending_methods': ["email"]:
     openupgrade.logged_query(
         env.cr,
         """
