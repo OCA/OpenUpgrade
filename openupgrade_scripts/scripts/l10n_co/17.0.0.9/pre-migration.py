7f19134cf5ac7816dba8f0a901a44badb298f8d6
David Bañón <david.banon@tecnativa.com>
2026-02-23 17:00:40 +0100

    [OU-ADD] l10n_co: Migration scripts
    TT57833

---
+++ openupgrade_scripts/scripts/l10n_co/17.0.0.9/pre-migration.py
@@ -0,0 +1,20 @@
+from openupgradelib import openupgrade
+
+
+def _disable_obsolete_taxes(env):
+    """IN 17.0, some taxes are gone and need to be disabled"""
+    for xmlid in ["l10n_co_tax_3"]:
+        imds = env["ir.model.data"].search(
+            [
+                ("module", "=", "account"),
+                ("model", "=", "account.tax"),
+                ("name", "=like", f"%_{xmlid}"),
+            ]
+        )
+        taxes = env["account.tax"].browse(imds.mapped("record_id"))
+        taxes.active = False
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    _disable_obsolete_taxes(env)
