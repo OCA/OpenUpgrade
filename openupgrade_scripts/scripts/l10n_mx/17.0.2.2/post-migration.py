e5fc004a4ae6277ecd955a91633385dbd506184e
David Bañón <david.banon@tecnativa.com>
2026-02-23 19:42:10 +0100

    [OU-ADD] l10n_mx: Migration scripts
    TT57840

---
+++ openupgrade_scripts/scripts/l10n_mx/17.0.2.2/post-migration.py
@@ -0,0 +1,44 @@
+# Copyright 2026 Tecnativa - David Bañón Gil
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from openupgradelib import openupgrade
+
+
+def _compute_new_tax_type(env):
+    openupgrade.logged_query(
+        env.cr,
+        """
+    UPDATE account_tax t
+    SET l10n_mx_tax_type =
+        CASE
+            WHEN c.code = 'MX' THEN 'iva'
+            ELSE NULL
+        END
+    FROM res_country c
+    WHERE t.country_id = c.id;
+    """,
+    )
+
+
+def _delete_analytic_tags(env):
+    l10n_mx_xmlids = [
+        "tag_diot_0",
+        "tag_diot_16",
+        "tag_diot_16_imp",
+        "tag_diot_16_non_cre",
+        "tag_diot_8",
+        "tag_diot_8_non_cre",
+        "tag_diot_exento",
+        "tag_diot_ret",
+        "tag_ieps",
+        "tag_isr",
+        "tag_iva",
+    ]
+    openupgrade.delete_records_safely_by_xml_id(
+        env, [f"l10n_mx.{x}" for x in l10n_mx_xmlids]
+    )
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    _delete_analytic_tags(env)
+    _compute_new_tax_type(env)
