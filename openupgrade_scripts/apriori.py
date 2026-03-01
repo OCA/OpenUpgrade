3d4e428eeb162d244abec6a70309dc585d26eb2e
Holger Brunn <mail@hunki-enterprises.com>
2026-03-01 14:53:10 +0100

    [IMP] merge mass_operation_abstract into base

--- openupgrade_scripts/apriori.py
+++ openupgrade_scripts/apriori.py
@@ -100,6 +100,8 @@ merged_modules = {
     "stock_account_product_cost_security": "product_cost_security",
     # OCA/server-tools
     "base_jsonify": "jsonifier",
+    # OCA/server-ux
+    "mass_operation_abstract": "base",
     # OCA/stock-logistics-reporting
     "stock_inventory_valuation_pivot": "stock_account",
     # OCA/stock-logistics-warehouse
