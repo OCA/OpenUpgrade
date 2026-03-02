bd335036f85e19049390102a5953155c248e1be1
Holger Brunn <mail@hunki-enterprises.com>
2026-03-02 10:56:12 +0100

    [MIG] barcodes_gs1_nomenclature

---
+++ openupgrade_scripts/scripts/barcodes_gs1_nomenclature/19.0.1.0/post-migration.py
@@ -0,0 +1,11 @@
+# Copyright 2026 Hunki Enterprises BV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    openupgrade.load_data(
+        env, "barcodes_gs1_nomenclature", "19.0.1.0/noupdate_changes.xml"
+    )
