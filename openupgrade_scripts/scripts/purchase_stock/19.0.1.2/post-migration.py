2554a0154ed1e549d6fa54f3fdb4617751b9797b
Holger Brunn <mail@hunki-enterprises.com>
2026-03-16 15:26:07 +0100

    [MIG] purchase_stock

---
+++ openupgrade_scripts/scripts/purchase_stock/19.0.1.2/post-migration.py
@@ -0,0 +1,16 @@
+# Copyright 2026 Hunki Enterprises BV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    openupgrade.m2o_to_x2m(
+        env.cr,
+        env["purchase.order"],
+        "purchase_order",
+        "reference_ids",
+        "group_id",
+    )
+    openupgrade.load_data(env, "purchase_stock", "19.0.1.2/noupdate_changes.xml")
