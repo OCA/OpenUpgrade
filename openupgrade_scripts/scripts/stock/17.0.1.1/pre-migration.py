c93597d154c1679127d2d77788e3c9ace87014bb
Ane <aneavanzosc@gmail.com>
2026-04-23 12:19:37 +0200

    [17.0][FIX] stock: prefill picked for done moves to avoid massive recompute

--- openupgrade_scripts/scripts/stock/17.0.1.1/pre-migration.py
+++ openupgrade_scripts/scripts/stock/17.0.1.1/pre-migration.py
@@ -35,9 +35,56 @@ def fix_move_line_quantity(env):
         """,
     )
 
+def prefill_picked(env):
+
+    """
+    Pre-fill picked for done stock moves and their move lines
+    to reduce ORM recomputation during migration.
+    """
+
+    openupgrade.logged_query(
+        env.cr,
+        """
+        ALTER TABLE stock_move
+        ADD COLUMN IF NOT EXISTS picked boolean
+        """,
+    )
+
+    openupgrade.logged_query(
+        env.cr,
+        """
+        ALTER TABLE stock_move_line
+        ADD COLUMN IF NOT EXISTS picked boolean
+        """,
+    )
+
+    openupgrade.logged_query(
+        env.cr,
+        """
+        UPDATE stock_move_line sml
+        SET picked = TRUE
+        FROM stock_move sm
+        WHERE sml.move_id = sm.id
+          AND sm.state = 'done'
+          AND (sml.picked IS DISTINCT FROM TRUE)
+        """,
+    )
+
+    openupgrade.logged_query(
+        env.cr,
+        """
+        UPDATE stock_move
+        SET picked = TRUE
+        WHERE state = 'done'
+          AND (picked IS DISTINCT FROM TRUE)
+        """,
+    )
+
+
 
 @openupgrade.migrate()
 def migrate(env, version):
     openupgrade.rename_fields(env, _field_renames)
     openupgrade.copy_columns(env.cr, _column_copies)
     fix_move_line_quantity(env)
+    prefill_picked(env)
