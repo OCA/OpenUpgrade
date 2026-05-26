a12849e94295ecb2216cc8b8886ed0ca43348487
Holger Brunn <mail@hunki-enterprises.com>
2026-05-26 10:53:27 +0200

    [FIX] loyalty: incomplete migration

--- openupgrade_scripts/scripts/loyalty/19.0.1.0/post-migration.py
+++ openupgrade_scripts/scripts/loyalty/19.0.1.0/post-migration.py
@@ -3,11 +3,4 @@ from openupgradelib import openupgrade
 
 @openupgrade.migrate()
 def migrate(env, version):
-    """Reset "lang" field for these records, as they were set in previous
-    version in XML, but not in this one, and Odoo doesn't reset non present
-    fields.
-    """
-    for xmlid in ("mail_template_gift_card", "mail_template_loyalty_card"):
-        template = env.ref(f"loyalty.{xmlid}", False)
-        if template:
-            template.lang = False
+    openupgrade.load_data(env, "loyalty", "19.0.1.0/noupdate_changes.xml")
