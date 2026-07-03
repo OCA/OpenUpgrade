d3e81c7748d6f18fa90904b6021a74731a659547
Carlos Lopez <carlos.lopez@tecnativa.com>
2026-07-03 12:44:14 -0500

    [OU-ADD] base_automation: Migration scripts

---
+++ openupgrade_scripts/scripts/base_automation/19.0.1.0/post-migration.py
@@ -0,0 +1,8 @@
+# Copyright 2026 Tecnativa - Carlos Lopez
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    openupgrade.load_data(env, "base_automation", "19.0.1.0/noupdate_changes.xml")
