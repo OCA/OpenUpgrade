e82190ed810ddcf51292705635ba4e5eb75e9622
Miquel Raïch <miquel.raich@forgeflow.com>
2026-07-30 16:27:01 +0200

    [MIG] website_slides_survey

---
+++ openupgrade_scripts/scripts/website_slides_survey/19.0.1.0/post-migration.py
@@ -0,0 +1,16 @@
+# Copyright 2026 ForgeFlow S.L. (https://www.forgeflow.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    openupgrade.load_data(env, "website_slides_survey", "19.0.1.0/noupdate_changes.xml")
+    openupgrade.delete_record_translations(
+        env.cr,
+        "website_slides_survey",
+        [
+            "mail_template_user_input_certification_failed",
+        ],
+    )
