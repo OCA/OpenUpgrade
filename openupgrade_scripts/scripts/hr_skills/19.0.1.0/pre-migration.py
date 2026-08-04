3739d73f4c065c57ccd8aa0c7b5630a917918b7e
Bhavesh Heliconia <oca@heliconia.in>
2026-08-04 12:30:10 +0530

    [FIX] hr_skills: rename resume_type_training XML ID from hr_skills_slides

--- openupgrade_scripts/scripts/hr_skills/19.0.1.0/pre-migration.py
+++ openupgrade_scripts/scripts/hr_skills/19.0.1.0/pre-migration.py
@@ -6,8 +6,12 @@ from openupgradelib import openupgrade
 _added_fields = [
     ("valid_from", "hr.employee.skill", "hr_employee_skill", "date", None, "hr_skills"),
 ]
+_renamed_xmlids = [
+    ("hr_skills_slides.resume_type_training", "hr_skills.resume_type_training"),
+]
 
 
 @openupgrade.migrate()
 def migrate(env, version):
     openupgrade.add_fields(env, _added_fields)
+    openupgrade.rename_xmlids(env.cr, _renamed_xmlids)
