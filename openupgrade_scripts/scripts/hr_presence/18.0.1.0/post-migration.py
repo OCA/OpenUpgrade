8af6c4e57d91e9d8bde4e858b9a0d35b2e99ddff
odooerpdevelopers <odooerpdevelopers@gmail.com>
2026-08-08 15:25:46 +0200

    [FIX] hr_presence: Add noupdate_changes loading and work analysis file

--- openupgrade_scripts/scripts/hr_presence/18.0.1.0/post-migration.py
+++ openupgrade_scripts/scripts/hr_presence/18.0.1.0/post-migration.py
@@ -13,3 +13,10 @@ def migrate(env, version):
         [("to_define", "out_of_working_hour")],
         table="hr_employee",
     )
+    openupgrade.load_data(env, "hr_presence", "18.0.1.0/noupdate_changes.xml")
+    openupgrade.delete_record_translations(
+        env.cr, "hr_presence", ["mail_template_presence"], ["body_html"]
+    )
+    openupgrade.delete_record_translations(
+        env.cr, "hr_presence", ["sms_template_data_hr_presence"], ["body"]
+    )
