e70690c1693c064cc8276a231022462b997dc52a
Miquel Raïch <miquel.raich@forgeflow.com>
2026-08-19 15:00:05 +0200

    [MIG] mail_group

---
+++ openupgrade_scripts/scripts/mail_group/19.0.1.1/post-migration.py
@@ -0,0 +1,9 @@
+# Copyright 2026 ForgeFlow S.L. (https://www.forgeflow.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    openupgrade.load_data(env, "mail_group", "19.0.1.1/noupdate_changes.xml")
