b1762bad34cdade4e56c3bb2a1f42e2a6daf3c0d
Holger Brunn <mail@hunki-enterprises.com>
2026-09-08 10:41:20 +0200

    [FIX] sale_management: create sections for optional products
    [FIX] sale_management: copy translated strings correctly
    
    fixes #5970

--- openupgrade_scripts/scripts/sale_management/tests/test_sale_management_migration.py
+++ openupgrade_scripts/scripts/sale_management/tests/test_sale_management_migration.py
@@ -1,4 +1,4 @@
-from odoo.tests import TransactionCase
+from odoo.tests import TransactionCase, tagged
 
 from odoo.addons.openupgrade_framework import openupgrade_test
 
@@ -13,9 +13,48 @@ class TestSaleManagementMigration(TransactionCase):
             [("sale_order_template_id", "=", template.id)]
         )
         self.assertItemsEqual(
-            order.order_line.mapped("name"), ("some option", "another option")
+            order.order_line.mapped("name"),
+            (
+                "non-optional line",
+                "non-optional line2",
+                "/",
+                "some option",
+                "another option",
+            ),
         )
-        self.assertItemsEqual(order.order_line.mapped("is_optional"), (True, True))
         self.assertItemsEqual(
-            template.sale_order_template_line_ids.mapped("is_optional"), (True, True)
+            order.order_line.mapped("is_optional"), (False, False, True, False, False)
+        )
+        self.assertItemsEqual(
+            map(lambda x: x._is_line_optional(), order.order_line),
+            (False, False, False, True, True),
+        )
+        self.assertItemsEqual(
+            template.sale_order_template_line_ids.mapped("name"),
+            ("/", "some option", "another option"),
+        )
+        self.assertItemsEqual(
+            template.sale_order_template_line_ids.mapped("is_optional"),
+            (True, False, False),
+        )
+
+
+@openupgrade_test
+@tagged("-at_install", "post_install")
+class TestSaleManagementMigrationPost(TransactionCase):
+    def test_sale_order_template_migration(self):
+        template = self.env["sale.order.template"].search(
+            [("name", "=", "Sale order template")],
+        )
+        self.assertItemsEqual(
+            template.sale_order_template_line_ids.with_context(lang="fr_FR").mapped(
+                "name"
+            ),
+            ("Section produits optionnels", "some option", "another option"),
+        )
+        self.assertItemsEqual(
+            template.sale_order_template_line_ids.with_context(lang="en_US").mapped(
+                "name"
+            ),
+            ("Optional Products Section", "some option", "another option"),
         )
